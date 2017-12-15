844dedb49d5cbefcf81d9d34eb8f24dc170c7324
Daniel Mahu <dmahu@google.com>
2017-12-15 22:02:10 +0000

    Work in progress on the ReadRowsParser class.

---
+++ bigtable/client/readrowsparser.cc
@@ -0,0 +1,121 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bigtable/client/readrowsparser.h"
+
+#include <stdexcept>
+
+namespace bigtable {
+inline namespace BIGTABLE_CLIENT_NS {
+using google::bigtable::v2::ReadRowsResponse_CellChunk;
+
+void ReadRowsParser::HandleChunk(ReadRowsResponse_CellChunk& chunk) {
+  if (not chunk.row_key().empty()) {
+    if (last_seen_row_key_.compare(chunk.row_key()) >= 0) {
+      throw std::runtime_error("Row keys are expected in increasing order");
+    }
+    cell_.row_ = chunk.row_key();
+  }
+
+  if (chunk.has_family_name()) {
+    if (not chunk.has_qualifier()) {
+      throw std::runtime_error("New column family must specify qualifier");
+    }
+    cell_.family_ = chunk.family_name().value();
+  }
+
+  if (chunk.has_qualifier()) {
+    cell_.column_ = chunk.qualifier().value();
+  }
+
+  if (cell_first_chunk_) {
+    cell_.timestamp_ = chunk.timestamp_micros();
+  }
+  cell_first_chunk_ = false;
+
+  for (const auto& l : chunk.labels()) {
+    cell_.labels_.push_back(l);
+  }
+
+  if (chunk.value_size() > 0) {
+    cell_.value_.reserve(chunk.value_size());
+  }
+  cell_.value_.append(chunk.value());
+
+  // Last chunk in the cell has zero for value size
+  if (chunk.value_size() == 0) {
+    if (cells_.empty()) {
+      if (cell_.row_.empty()) {
+        throw std::runtime_error("Missing row key at last chunk in cell");
+      }
+      row_key_ = cell_.row_;
+    } else {
+      if (row_key_.compare(cell_.row_) != 0) {
+        throw std::runtime_error("Different row key in cell chunk");
+      }
+    }
+    cells_.emplace_back(cell_.MoveToCell());
+    cell_first_chunk_ = true;
+  }
+
+  if (chunk.reset_row()) {
+    cells_.clear();
+    cell_ = {};
+    if (not cell_first_chunk_) {
+      throw std::runtime_error("Reset row with an unfinished cell");
+    }
+  } else if (chunk.commit_row()) {
+    if (not cell_first_chunk_) {
+      throw std::runtime_error("Commit row with an unfinished cell");
+    }
+    if (cells_.empty()) {
+      throw std::runtime_error("Commit row missing the row key");
+    }
+    row_ready_ = true;
+    last_seen_row_key_ = row_key_;
+    cell_.row_ = "";
+  }
+}
+
+void ReadRowsParser::HandleLastRowKey(std::string last_row_key) {
+  last_row_key_ = std::move(last_row_key);
+}
+
+void ReadRowsParser::HandleEOT() {
+  if (not cell_first_chunk_) {
+    throw std::runtime_error("EOT with unfinished cell");
+  }
+
+  if (cells_.begin() != cells_.end() and not row_ready_) {
+    throw std::runtime_error("EOT with unfinished row");
+  }
+}
+
+bool ReadRowsParser::HasNext() { return row_ready_; }
+
+Row ReadRowsParser::Next() {
+  if (not row_ready_) {
+    throw std::runtime_error("Next with row not ready");
+  }
+  row_ready_ = false;
+
+  Row row(std::move(row_key_), std::move(cells_));
+  row_key_ = "";
+  cells_.clear();
+
+  return row;
+}
+
+}  // namespace BIGTABLE_CLIENT_NS
+}  // namespace bigtable
---
+++ bigtable/client/readrowsparser.h
@@ -0,0 +1,128 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef BIGTABLE_CLIENT_READROWSPARSER_H_
+#define BIGTABLE_CLIENT_READROWSPARSER_H_
+
+#include "bigtable/client/version.h"
+
+#include "bigtable/client/cell.h"
+#include "bigtable/client/row.h"
+
+#include "google/bigtable/v2/bigtable.grpc.pb.h"
+
+#include <absl/strings/string_view.h>
+#include <vector>
+
+namespace bigtable {
+inline namespace BIGTABLE_CLIENT_NS {
+/**
+ * The internal module responsible for transforming ReadRowsResponse
+ * protobufs into Row objects.
+ *
+ * Users are expected to do something like:
+ *   while (!stream.EOT()) {
+ *     chunk = stream.NextChunk();
+ *     parser.HandleChunk(chunk);
+ *     if (parser.HasNext()) {
+ *       row = parser.Next();  // you now own `row`
+ *     }
+ *   }
+ *   parser.HandleEOT();
+ */
+class ReadRowsParser {
+ public:
+  /**
+   * Pass an input chunk proto to the parser. May throw errors, in
+   * which case valid data read before the error is still accessible.
+   */
+  void HandleChunk(google::bigtable::v2::ReadRowsResponse_CellChunk& chunk);
+
+  /**
+   * Set the last scanned row key reported in the response.
+   */
+  void HandleLastRowKey(std::string last_row_key);
+
+  /**
+   * Signal that the input stream reached the end. May throw errors if
+   * more data was expected, in which case valid data read before the
+   * error is still accessible.
+   */
+  void HandleEOT();
+
+  /**
+   * True if the data parsed so far yielded a Row. Call Next() to take
+   * the row. Throws an error if there is wrong or incomplete data in
+   * the chunks parsed.
+   */
+  bool HasNext();
+
+  /**
+   *  Extract and take ownership of the data in a row. Use HasNext()
+   *  first to find out if there are rows available.
+   *  Throws runtime_error if HasNext() is false.
+   */
+  Row Next();
+
+ private:
+  /**
+   *  Helper class to handle string ownership correctly. The value is
+   *  moved when converting to a result cell, but the key, family and
+   *  column are copied.
+   */
+  class ParseCell {
+   public:
+    /// Moves partial results into a Cell class.
+    Cell MoveToCell() {
+      Cell cell(row_, family_, column_, timestamp_, std::move(value_),
+                std::move(labels_));
+      value_ = "";
+      labels_.clear();
+      return cell;
+    }
+
+    std::string row_;
+    std::string family_;
+    std::string column_;
+    int64_t timestamp_;
+    std::string value_;
+    std::vector<std::string> labels_;
+  };
+
+  // Row key for the current row.
+  std::string row_key_;
+
+  // Parsed cells of a yet unfinished row.
+  std::vector<Cell> cells_;
+
+  // Is the next incoming chunk the first in a cell?
+  bool cell_first_chunk_;
+
+  // Stores partial fields.
+  ParseCell cell_;
+
+  // Received from the response.
+  std::string last_row_key_;
+
+  // Set when a row is ready.
+  std::string last_seen_row_key_;
+
+  // True iff cells_ make up a complete row.
+  bool row_ready_;
+};
+
+}  // namespace BIGTABLE_CLIENT_NS
+}  // namespace bigtable
+
+#endif  // BIGTABLE_CLIENT_READROWSPARSER_H_
