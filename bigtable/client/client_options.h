92277aa4d9beb32d993adca946c43e8c37fbd4c7
Pratham <pandeypratham8@gmail.com>
2017-12-21 02:32:37 +0530

    Added setters for Channel Arguments. (#96)
    * Added setters for Channel Arguments.
    
    * Added Unit tests for setters
    
    * Type casted int to size_t for comparision with num_args of args list.
    
    * Added arg type test for testing correct setter is called.
    
    * Minor changes.
    
    * Changed C-Style comments to C++ style and remove SHA-1 from URL.
    
    * Minor changes.
    
    * Changed EXPECT_EQ to ASSERT_EQ for preventing seg_fault and minor comment changes.
    
    * Removed inline keyword from member functions.
    
    * Fixed send message unit.

--- bigtable/client/client_options.h
+++ bigtable/client/client_options.h
@@ -68,6 +68,126 @@ class ClientOptions {
     return *this;
   }
 
+  /**
+   * Set compression algorithm for channel.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetCompressionAlgorithm()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetCompressionAlgorithm(grpc_compression_algorithm algorithm) {
+    channel_arguments_.SetCompressionAlgorithm(algorithm);
+  }
+
+  /**
+   * Set the grpclb fallback timeout (in ms) for the channel.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetGrpclbFallbackTimeout()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetGrpclbFallbackTimeout(int fallback_timeout) {
+    channel_arguments_.SetGrpclbFallbackTimeout(fallback_timeout);
+  }
+
+  /**
+   * Set Socket Mutator for channel.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetSocketMutator()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetSocketMutator(grpc_socket_mutator* mutator) {
+    channel_arguments_.SetSocketMutator(mutator);
+  }
+
+  /**
+   * Set the string to prepend to the user agent.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetUserAgentPrefix()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetUserAgentPrefix(const grpc::string& user_agent_prefix) {
+    channel_arguments_.SetUserAgentPrefix(user_agent_prefix);
+  }
+
+  /**
+   * Set the buffer pool to be attached to the constructed channel.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetResourceQuota()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetResourceQuota(const grpc::ResourceQuota& resource_quota) {
+    channel_arguments_.SetResourceQuota(resource_quota);
+  }
+
+  /**
+   * Set the max receive message size in bytes. -1 means unlimited.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetMaxReceiveMessageSize()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetMaxReceiveMessageSize(int size) {
+    channel_arguments_.SetMaxReceiveMessageSize(size);
+  }
+
+  /**
+   * Set the max send message size in bytes. -1 means unlimited.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetMaxSendMessageSize()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetMaxSendMessageSize(int size) {
+    channel_arguments_.SetMaxSendMessageSize(size);
+  }
+
+  /**
+   * Set LB policy name.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetLoadBalancingPolicyName()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetLoadBalancingPolicyName(const grpc::string& lb_policy_name) {
+    channel_arguments_.SetLoadBalancingPolicyName(lb_policy_name);
+  }
+
+  /**
+   * Set service config in JSON form.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetServiceConfigJSON()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetServiceConfigJSON(const grpc::string& service_config_json) {
+    channel_arguments_.SetServiceConfigJSON(service_config_json);
+  }
+
+  /**
+   * Set target name override for SSL host name checking.
+   *
+   * Please see the docs for grpc::ChannelArguments::SetSslTargetNameOverride()
+   * on https://grpc.io/grpc/cpp/classgrpc_1_1_channel_arguments.html
+   * for more details.
+   *
+   */
+  void SetSslTargetNameOverride(const grpc::string& name) {
+    channel_arguments_.SetSslTargetNameOverride(name);
+  }
+
  private:
   std::string data_endpoint_;
   std::string admin_endpoint_;
--- bigtable/client/client_options_test.cc
+++ bigtable/client/client_options_test.cc
@@ -78,3 +78,113 @@ TEST(ClientOptionsTest, EditCredentials) {
   EXPECT_EQ(typeid(grpc::InsecureChannelCredentials()),
             typeid(client_options_object.credentials()));
 }
+
+TEST(ClientOptionsTest, SetGrpclbFallbackTimeout) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetGrpclbFallbackTimeout(5);
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetGrpclbFallbackTimeout() inserts new argument to args_ hence comparing
+  // 2nd element of test_args.
+  EXPECT_EQ(GRPC_ARG_GRPCLB_FALLBACK_TIMEOUT_MS,
+            grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetCompressionAlgorithm) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetCompressionAlgorithm(GRPC_COMPRESS_NONE);
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetCompressionAlgorithm() inserts new argument to args_ hence comparing 2nd
+  // element of test_args.
+  EXPECT_EQ(GRPC_COMPRESSION_CHANNEL_DEFAULT_ALGORITHM,
+            grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetMaxReceiveMessageSize) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetMaxReceiveMessageSize(5);
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetMaxReceiveMessageSize() inserts new argument to args_ hence comparing
+  // 2nd element of test_args.
+  EXPECT_EQ(GRPC_ARG_MAX_RECEIVE_MESSAGE_LENGTH,
+            grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetMaxSendMessageSize) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetMaxSendMessageSize(5);
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetMaxSendMessageSize() inserts new argument to args_ hence comparing 2nd
+  // element of test_args.
+  EXPECT_EQ(GRPC_ARG_MAX_SEND_MESSAGE_LENGTH,
+            grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetLoadBalancingPolicyName) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetLoadBalancingPolicyName("test-policy-name");
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetLoadBalancingPolicyName() inserts new argument to args_ hence comparing
+  // 2nd element of test_args.
+  EXPECT_EQ(GRPC_ARG_LB_POLICY_NAME, grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetServiceConfigJSON) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetServiceConfigJSON("test-config");
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetServiceConfigJSON() inserts new argument to args_ hence comparing 2nd
+  // element of test_args.
+  EXPECT_EQ(GRPC_ARG_SERVICE_CONFIG, grpc::string(test_args.args[1].key));
+}
+
+TEST(ClientOptionsTest, SetUserAgentPrefix) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetUserAgentPrefix("test_prefix");
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(1UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetUserAgentPrefix() appends the new prefix to existing prefix hence
+  // comparing 1st element of test_args.
+  EXPECT_EQ(GRPC_ARG_PRIMARY_USER_AGENT_STRING,
+            grpc::string(test_args.args[0].key));
+}
+
+TEST(ClientOptionsTest, SetSslTargetNameOverride) {
+  bigtable::ClientOptions client_options_object = bigtable::ClientOptions();
+  client_options_object.SetSslTargetNameOverride("test-name");
+  grpc::ChannelArguments c_args = client_options_object.channel_arguments();
+  grpc_channel_args test_args = c_args.c_channel_args();
+  ASSERT_EQ(2UL, test_args.num_args);
+  // Use the low-level C API because grpc::ChannelArguments lacks high-level
+  // accessors.
+  // SetSslTargetNameOverride() inserts new argument to args_ hence comparing
+  // 2nd element of test_args.
+  EXPECT_EQ(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG,
+            grpc::string(test_args.args[1].key));
+}
