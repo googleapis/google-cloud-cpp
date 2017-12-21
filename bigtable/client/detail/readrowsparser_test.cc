96c3b58d31dee73c79ce3c3e7ba80b3bf1fa67f0
Daniel Mahu <dmahu@google.com>
2017-12-21 09:13:27 +0000

    Move to detail/

--- bigtable/client/detail/readrowsparser_test.cc
+++ bigtable/client/detail/readrowsparser_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "bigtable/client/readrowsparser.h"
+#include "bigtable/client/detail/readrowsparser.h"
 
 #include <google/protobuf/text_format.h>
 #include <gtest/gtest.h>
