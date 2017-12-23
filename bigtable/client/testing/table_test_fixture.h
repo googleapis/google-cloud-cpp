ac04ec1e96cd5891412a14eb9e650bd805e2c6c1
Carlos O'Ryan <coryan@users.noreply.github.com>
2017-12-23 11:19:03 -0500

    Return std::shared_ptr<> from DataClient::Stub().
    This is part of the fixes for #101.

--- bigtable/client/testing/table_test_fixture.h
+++ bigtable/client/testing/table_test_fixture.h
@@ -28,16 +28,7 @@ class TableTestFixture : public ::testing::Test {
 
   void SetUp() override;
 
-  std::shared_ptr<MockDataClient> SetupMockClient() {
-    auto client = std::make_shared<MockDataClient>();
-    EXPECT_CALL(*client, project_id())
-        .WillRepeatedly(::testing::ReturnRef(project_id_));
-    EXPECT_CALL(*client, instance_id())
-        .WillRepeatedly(::testing::ReturnRef(instance_id_));
-    EXPECT_CALL(*client, Stub())
-        .WillRepeatedly(::testing::ReturnRef(*bigtable_stub_));
-    return client;
-  }
+  std::shared_ptr<MockDataClient> SetupMockClient();
 
   std::string const kProjectId = "foo-project";
   std::string const kInstanceId = "bar-instance";
