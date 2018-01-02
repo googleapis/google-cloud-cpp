da6643d5dc25affaf37ee22ce1ad97758c0bf884
Pratham Pandey <xarcher@google.com>
2018-01-02 15:56:33 +0530

    Updated test directory path.

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -30,8 +30,7 @@ if ($LASTEXITCODE) {
 
 mkdir build
 cd build
-echo $dir
-cmake $dir\google-cloud-cpp\bigtable\client -DCMAKE_TOOLCHAIN_FILE="$dir\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static .. -DGOOGLE_CLOUD_CPP_GRPC_PROVIDER=package -DGTEST_USE_OWN_TR1_TUPLE=1
+cmake $dir\google-cloud-cpp\bigtable -DCMAKE_TOOLCHAIN_FILE="$dir\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static .. -DGOOGLE_CLOUD_CPP_GRPC_PROVIDER=package -DGTEST_USE_OWN_TR1_TUPLE=1
 
 if ($LASTEXITCODE) {
   throw "cmake failed with exit code $LASTEXITCODE"
