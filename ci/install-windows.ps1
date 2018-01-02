c8a842997419c35074e7ee0b944b3e48ffa26f27
Pratham Pandey <xarcher@google.com>
2018-01-02 17:45:08 +0530

    Minor changes to make it run.

--- ci/install-windows.ps1
+++ ci/install-windows.ps1
@@ -37,12 +37,6 @@ if ($LASTEXITCODE) {
   throw "git setup failed with exit code $LASTEXITCODE"
 }
 
-# ... install cmake to ensure appveyor is using latest version
-# choco install -y cmake cmake.portable
-# if ($LASTEXITCODE) {
-#   throw "choco install cmake failed with exit code $LASTEXITCODE"
-# }
-
 # ... build the tool each time, it is fast to do so ...
 powershell -exec bypass scripts\bootstrap.ps1
 if ($LASTEXITCODE) {
