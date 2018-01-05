63eebc69cbbc886dff09b8708ed23270604f6343
Daniel Mahu <dmahu@google.com>
2018-01-05 22:59:31 +0000

    Move row_reader out of internal/

--- bigtable/client/row_reader_test.cc
+++ bigtable/client/row_reader_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "bigtable/client/internal/row_reader.h"
+#include "bigtable/client/row_reader.h"
 
 #include "bigtable/client/table.h"
 #include "bigtable/client/testing/table_test_fixture.h"
