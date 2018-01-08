a9320e75f9b8ea47247e0d93fc2d1aeb3b59ddc0
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-01-08 23:10:16 +0000

    Automatically generated documentation

--- search/classes_b.js
+++ search/classes_b.js
@@ -3,6 +3,7 @@ var searchData=
   ['readrowsparser',['ReadRowsParser',['../classbigtable_1_1v0_1_1ReadRowsParser.html',1,'bigtable::v0']]],
   ['row',['Row',['../classbigtable_1_1v0_1_1Row.html',1,'bigtable::v0']]],
   ['rowrange',['RowRange',['../classbigtable_1_1v0_1_1RowRange.html',1,'bigtable::v0']]],
+  ['rowset',['RowSet',['../classbigtable_1_1v0_1_1RowSet.html',1,'bigtable::v0']]],
   ['rpcbackoffpolicy',['RPCBackoffPolicy',['../classbigtable_1_1v0_1_1RPCBackoffPolicy.html',1,'bigtable::v0']]],
   ['rpcretrypolicy',['RPCRetryPolicy',['../classbigtable_1_1v0_1_1RPCRetryPolicy.html',1,'bigtable::v0']]]
 ];
--- search/files_8.js
+++ search/files_8.js
@@ -8,6 +8,8 @@ var searchData=
   ['row_5frange_2ecc',['row_range.cc',['../row__range_8cc.html',1,'']]],
   ['row_5frange_2eh',['row_range.h',['../row__range_8h.html',1,'']]],
   ['row_5frange_5ftest_2ecc',['row_range_test.cc',['../row__range__test_8cc.html',1,'']]],
+  ['row_5fset_2eh',['row_set.h',['../row__set_8h.html',1,'']]],
+  ['row_5fset_5ftest_2ecc',['row_set_test.cc',['../row__set__test_8cc.html',1,'']]],
   ['row_5ftest_2ecc',['row_test.cc',['../row__test_8cc.html',1,'']]],
   ['rpc_5fbackoff_5fpolicy_2ecc',['rpc_backoff_policy.cc',['../rpc__backoff__policy_8cc.html',1,'']]],
   ['rpc_5fbackoff_5fpolicy_2eh',['rpc_backoff_policy.h',['../rpc__backoff__policy_8h.html',1,'']]],
