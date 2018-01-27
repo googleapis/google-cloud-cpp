6745af1722c881f8c52f5814d999517b2736f090
Carlos O'Ryan <coryan@google.com>
2018-01-27 09:54:54 -0500

    Helper classes to initialize PRNG.
    The benchmarks in #199, #196, and #189 will require good
    pseudo-random number generators (PRNGs).  Unfortunately C++11
    PRNGs are notoriously hard to initialize, and Abseil does
    not offer a solution either.  We also introduce some
    helper functions to create random strings in the benchmarks.

---
+++ bigtable/benchmarks/random.cc
@@ -0,0 +1,42 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bigtable/benchmarks/random.h"
+#include "bigtable/benchmarks/constants.h"
+
+namespace bigtable {
+namespace benchmarks {
+std::string Sample(DefaultPRNG& gen, int n, std::string const& population) {
+  std::uniform_int_distribution<std::size_t> rd(0, population.size() - 1);
+
+  std::string result(std::size_t(n), '0');
+  std::generate(result.begin(), result.end(),
+                [&rd, &gen, &population]() { return population[rd(gen)]; });
+  return result;
+}
+
+bigtable::Mutation MakeRandomMutation(DefaultPRNG& gen, int f) {
+  std::string field = "field" + std::to_string(f);
+  return bigtable::SetCell(kColumnFamily, std::move(field), 0,
+                           MakeRandomValue(gen));
+}
+
+std::string MakeRandomValue(DefaultPRNG& generator) {
+  static std::string const letters(
+      "ABCDEFGHIJLKMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz0123456789-/_");
+  return Sample(generator, kFieldSize, letters);
+}
+
+}  // namespace benchmarks
+}  // namespace bigtable
---
+++ bigtable/benchmarks/random_test.cc
@@ -0,0 +1,51 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bigtable/benchmarks/random.h"
+#include <gmock/gmock.h>
+#include "bigtable/benchmarks/constants.h"
+
+using namespace bigtable::benchmarks;
+
+TEST(BenchmarksRandom, Basic) {
+  // This is not a statistical test for PRNG, basically we want to make
+  // sure that MakeDefaultPRNG uses different seeds, or at least creates
+  // different series:
+  auto gen_string = []() {
+    auto g = MakeDefaultPRNG();
+    return Sample(g, 32, "0123456789abcdefghijklm");
+  };
+  std::string s0 = gen_string();
+  std::string s1 = gen_string();
+  EXPECT_NE(s0, s1);
+}
+
+TEST(BenchmarksRandom, RandomValue) {
+  auto g = MakeDefaultPRNG();
+  std::string val = MakeRandomValue(g);
+  EXPECT_EQ(static_cast<std::size_t>(kFieldSize), val.size());
+  std::string val2 = MakeRandomValue(g);
+  EXPECT_NE(val, val2);
+}
+
+TEST(BenchmarksRandom, RandomMutation) {
+  auto g = MakeDefaultPRNG();
+  auto m = MakeRandomMutation(g, 0).op;
+
+  ASSERT_TRUE(m.has_set_cell());
+  EXPECT_EQ(kColumnFamily, m.set_cell().family_name());
+  EXPECT_EQ("field0", m.set_cell().column_qualifier());
+  EXPECT_EQ(0, m.set_cell().timestamp_micros());
+  EXPECT_EQ(static_cast<std::size_t>(kFieldSize), m.set_cell().value().size());
+}
