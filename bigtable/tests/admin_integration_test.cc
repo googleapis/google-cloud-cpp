1bf38b59ecba1b24115e6dfbb53bf73c67fa92ca
Hemant <hemant@qlogic.io>
2018-02-19 23:26:10 +0530

    Changes as per Carlos comments

--- bigtable/tests/admin_integration_test.cc
+++ bigtable/tests/admin_integration_test.cc
@@ -12,35 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gmock/gmock.h>
-
 #include <absl/memory/memory.h>
 #include <absl/strings/str_join.h>
+#include <gmock/gmock.h>
 #include <google/protobuf/text_format.h>
 #include <google/protobuf/util/message_differencer.h>
 
-#include <bigtable/admin/admin_client.h>
-#include <bigtable/admin/table_admin.h>
+#include "bigtable/admin/admin_client.h"
+#include "bigtable/admin/table_admin.h"
 #include "bigtable/client/testing/table_integration_test.h"
 
 #include <string>
 #include <vector>
 
-using namespace std;
 namespace admin_proto = ::google::bigtable::admin::v2;
 
-namespace bigtable {
-namespace testing {
+namespace {
 
-class AdminIntegrationTest : public TableIntegrationTest {
+class AdminIntegrationTest : public bigtable::testing::TableIntegrationTest {
  protected:
   std::unique_ptr<bigtable::TableAdmin> table_admin_;
 
-  void SetUp() { CreateTableAdmin(); }
-
-  void TearDown() {}
-
-  void CreateTableAdmin() {
+  void SetUp() {
     std::shared_ptr<bigtable::AdminClient> admin_client =
         bigtable::CreateDefaultAdminClient(
             bigtable::testing::TableTestEnvironment::project_id(),
@@ -49,137 +42,162 @@ class AdminIntegrationTest : public TableIntegrationTest {
         admin_client, bigtable::testing::TableTestEnvironment::instance_id());
   }
 
-  bool TestForTableListCheck(vector<string> expectedTableList) {
-    vector<string> actualTableList;
-    vector<string> diffTableList;
+  void TearDown() {}
 
-    auto tableList = table_admin_->ListTables(admin_proto::Table::NAME_ONLY);
-    for (auto const& tableName : tableList)
-      actualTableList.push_back(tableName.name());
+  bool TestForTableListCheck(std::vector<std::string> expected_table_list) {
+    std::vector<std::string> actual_table_list;
+    std::vector<std::string> diff_table_list;
 
-    // Sort actualTableList
-    sort(actualTableList.begin(), actualTableList.end());
-    // Sort expectedTableList
-    sort(expectedTableList.begin(), expectedTableList.end());
+    auto table_list = table_admin_->ListTables(admin_proto::Table::NAME_ONLY);
+    for (auto const& table_name : table_list) {
+      actual_table_list.push_back(table_name.name());
+    }
 
-    // Get the difference of expectedTableList and actualTableList
-    set_difference(expectedTableList.begin(), expectedTableList.end(),
-                   actualTableList.begin(), actualTableList.end(),
-                   back_inserter(diffTableList));
+    // Sort actual_table_list
+    sort(actual_table_list.begin(), actual_table_list.end());
+    // Sort expected_table_list
+    sort(expected_table_list.begin(), expected_table_list.end());
 
-    return diffTableList.empty();
+    // Get the difference of expected_table_list and actual_table_list
+    set_difference(expected_table_list.begin(), expected_table_list.end(),
+                   actual_table_list.begin(), actual_table_list.end(),
+                   back_inserter(diff_table_list));
+
+    if (not diff_table_list.empty()) {
+      std::ostringstream ostring_stream;
+      ostring_stream << "Mismatched Tables: "
+                     << absl::StrJoin(diff_table_list, "\n")
+                     << "\nactual: " << absl::StrJoin(actual_table_list, "\n")
+                     << "\nexpected: "
+                     << absl::StrJoin(expected_table_list, "\n") << "\n";
+      std::cout << ostring_stream.str();
+    }
+
+    return diff_table_list.empty();
   }
 
-  bool checkTableSchema(admin_proto::Table const& actualTable,
-                        string const& expectedText, string const& message) {
-    admin_proto::Table expectedTable;
+  bool CheckTableSchema(admin_proto::Table const& actual_table,
+                        std::string const& expected_text,
+                        std::string const& message) {
+    admin_proto::Table expected_table;
 
-    // ASSERT_FALSE(google::protobuf::TextFormat::ParseFromString(expectedText,
-    // &expectedTable));
-    if (not google::protobuf::TextFormat::ParseFromString(expectedText,
-                                                          &expectedTable))
+    if (not google::protobuf::TextFormat::ParseFromString(expected_text,
+                                                          &expected_table)) {
+      std::ostringstream ostring_stream;
+      ostring_stream << message << ": could not parse protobuf string <\n"
+                     << expected_text << ">\n";
+      std::cout << ostring_stream.str();
+
       return false;
+    }
 
-    string delta;
-    google::protobuf::util::MessageDifferencer msgDifferencer;
-    msgDifferencer.ReportDifferencesToString(&delta);
-    // ASSERT_FALSE(msgDifferencer.Compare(expectedTable, actualTable));
-    if (not msgDifferencer.Compare(expectedTable, actualTable)) return false;
+    std::string delta;
+    google::protobuf::util::MessageDifferencer message_differencer;
+    message_differencer.ReportDifferencesToString(&delta);
+    bool is_msg_diff_notpresent =
+        message_differencer.Compare(expected_table, actual_table);
+    if (not is_msg_diff_notpresent) {
+      std::ostringstream ostring_stream;
+      ostring_stream << message << ": mismatch expected vs actual:\n" << delta;
+      std::cout << ostring_stream.str();
+    }
 
-    return true;
+    return is_msg_diff_notpresent;
   }
 };
-}
-}
 
-namespace bigtable {
-namespace testing {
+}  // anonymus namespace
 
 /**
  *  Test case for checking if Instance is empty or not.
  *  If instance is empty then go ahead.
- *  If instance is not empty the throw assertion
- * */
+ *  If instance is not empty the throw runtime error
+ */
 TEST_F(AdminIntegrationTest, CheckInstanceIsEmpty) {
   namespace admin_proto = ::google::bigtable::admin::v2;
+  bool table_list_empty =
+      table_admin_->ListTables(admin_proto::Table::NAME_ONLY).empty();
 
-  ASSERT_TRUE(table_admin_->ListTables(admin_proto::Table::NAME_ONLY).empty());
+  if (not table_list_empty) {
+    throw std::runtime_error("Expected empty instance in integration test");
+  }
+
+  std::cout << "Initial ListTables() successful" << std::endl;
+  ASSERT_TRUE(table_list_empty);
 }
 
 /***
  * Test case for checking create table
  * If created tableID and passed tableID is same then test is successful.
- *
- * */
+ */
 TEST_F(AdminIntegrationTest, CheckCreateTable) {
-  string table_id = "table0";
+  std::string const table_id = "table0";
 
   // Create Table
   auto table = table_admin_->CreateTable(table_id, bigtable::TableConfig());
-
   // Check table is created properly
   auto table_result = table_admin_->GetTable(table_id);
 
-  ASSERT_EQ(table.name(), table_result.name());
-
   // Delete this table so that next run should not throw error
   table_admin_->DeleteTable(table_id);
+
+  ASSERT_EQ(table.name(), table_result.name())
+      << "Mismatched names for GetTable(" << table_id << "): " << table.name()
+      << " != " << table_result.name();
 }
 
 /**
  * Check if list of table names are matching with the
  * expected tablename list
- *
- * */
-
+ */
 TEST_F(AdminIntegrationTest, CheckTableListWithSingleTable) {
-  const string table_id = "table0";
+  std::string const table_id = "table0";
 
   // Create table first here.
   table_admin_->CreateTable(table_id, bigtable::TableConfig());
 
-  vector<string> expectedTableList = {
+  std::vector<std::string> expected_table_list = {
       absl::StrCat(table_admin_->instance_name(), "/tables/", table_id)};
 
-  bool tableListIsEmpty = TestForTableListCheck(expectedTableList);
+  bool list_is_empty = TestForTableListCheck(expected_table_list);
 
   // Delete the created table here, so it should not interfere with other test
   // cases
   table_admin_->DeleteTable(table_id);
 
-  ASSERT_TRUE(tableListIsEmpty);
+  ASSERT_TRUE(list_is_empty);
 }
 
-TEST_F(AdminIntegrationTest, CheckTableListWithMultipleTable) {
-  const string table_prefix = "table";
+TEST_F(AdminIntegrationTest, CheckTableListWithMultipleTables) {
+  std::string const table_prefix = "table";
   int table_count = 5;
-  vector<string> expectedTableList;
+  std::vector<std::string> expected_table_list;
 
-  // Create multiple tableID in loop
-  for (int index = 0; index < table_count; index++) {
-    string table_id = table_prefix + to_string(index);
+  // Create multiple table_id in loop`
+  for (int index = 0; index < table_count; ++index) {
+    std::string table_id = table_prefix + std::to_string(index);
     // Create table First
     table_admin_->CreateTable(table_id, bigtable::TableConfig());
 
-    expectedTableList.push_back(
+    expected_table_list.push_back(
         absl::StrCat(table_admin_->instance_name(), "/tables/", table_id));
   }
 
-  bool tableListIsEmpty = TestForTableListCheck(expectedTableList);
+  bool list_is_empty = TestForTableListCheck(expected_table_list);
 
   // Delete the created table here, so it should not interfere with other test
   // cases
-  for (int index = 0; index < table_count; index++) {
-    string table_id = table_prefix + to_string(index);
+  for (int index = 0; index < table_count; ++index) {
+    std::string table_id = table_prefix + std::to_string(index);
     table_admin_->DeleteTable(table_id);
   }
 
-  ASSERT_TRUE(tableListIsEmpty);
+  ASSERT_TRUE(list_is_empty);
 }
 
 TEST_F(AdminIntegrationTest, CheckModifyTable) {
   using GC = bigtable::GcRule;
-  string table_id = "table2";
+  std::string const table_id = "table2";
 
   bigtable::TableConfig tab_config(
       {{"fam", GC::MaxNumVersions(5)},
@@ -188,7 +206,7 @@ TEST_F(AdminIntegrationTest, CheckModifyTable) {
 
   auto table = table_admin_->CreateTable(table_id, tab_config);
 
-  string expected_text_create = "name: '" + table.name() + "'\n";
+  std::string expected_text_create = "name: '" + table.name() + "'\n";
   // The rest is very deterministic, we control it by the previous operations:
   expected_text_create += R"""(
                           column_families {
@@ -203,14 +221,12 @@ TEST_F(AdminIntegrationTest, CheckModifyTable) {
 
   auto table_detailed =
       table_admin_->GetTable(table_id, admin_proto::Table::FULL);
-  bool valid_schema = checkTableSchema(table_detailed, expected_text_create,
+  bool valid_schema = CheckTableSchema(table_detailed, expected_text_create,
                                        "CheckModifyTable/Create");
 
   ASSERT_TRUE(valid_schema);
 
-  ///
-  string expected_text;
-  expected_text += R"""(
+  std::string expected_text = R"""(
                           column_families {
                                              key: 'fam'
                                              value { gc_rule { max_num_versions: 2 } } 
@@ -224,21 +240,18 @@ TEST_F(AdminIntegrationTest, CheckModifyTable) {
                                           }
                         )""";
 
-  vector<bigtable::ColumnFamilyModification,
-         allocator<bigtable::ColumnFamilyModification>>
-      column_modification_list = {
-          bigtable::ColumnFamilyModification::Create(
-              "newfam", GC::Intersection(GC::MaxAge(std::chrono::hours(7 * 24)),
-                                         GC::MaxNumVersions(1))),
-          bigtable::ColumnFamilyModification::Update("fam",
-                                                     GC::MaxNumVersions(2)),
-          bigtable::ColumnFamilyModification::Drop("foo")};
+  std::vector<bigtable::ColumnFamilyModification> column_modification_list = {
+      bigtable::ColumnFamilyModification::Create(
+          "newfam", GC::Intersection(GC::MaxAge(std::chrono::hours(7 * 24)),
+                                     GC::MaxNumVersions(1))),
+      bigtable::ColumnFamilyModification::Update("fam", GC::MaxNumVersions(2)),
+      bigtable::ColumnFamilyModification::Drop("foo")};
 
   auto table_modified =
       table_admin_->ModifyColumnFamilies(table_id, column_modification_list);
 
   table_modified.set_name("");
-  valid_schema = checkTableSchema(table_modified, expected_text,
+  valid_schema = CheckTableSchema(table_modified, expected_text,
                                   "CheckModifyTable/Modify");
 
   // Delete table so that is should not interfere with the test again on same
@@ -248,30 +261,28 @@ TEST_F(AdminIntegrationTest, CheckModifyTable) {
   ASSERT_TRUE(valid_schema);
 }
 // Test Cases Finished
-}
-}
 
 int main(int argc, char* argv[]) try {
   ::testing::InitGoogleTest(&argc, argv);
 
   // Check for arguments validity
   if (argc != 3) {
-    string const cmd = argv[0];
-    auto last_slash = string(cmd).find_last_of("/");
+    std::string const cmd = argv[0];
+    auto last_slash = std::string(cmd).find_last_of("/");
     // Show Usage if invalid no of arguments
-    cerr << "Usage: " << cmd.substr(last_slash + 1)
-         << "<project_id> <instance_id>" << endl;
+    std::cerr << "Usage: " << cmd.substr(last_slash + 1)
+              << "<project_id> <instance_id>" << std::endl;
     return 1;
   }
 
-  string project_id = argv[0];
-  string instance_id = argv[1];
+  std::string const project_id = argv[0];
+  std::string const instance_id = argv[1];
 
   (void)::testing::AddGlobalTestEnvironment(
       new bigtable::testing::TableTestEnvironment(project_id, instance_id));
 
   return RUN_ALL_TESTS();
-} catch (exception const& ex) {
+} catch (std::exception const& ex) {
   std::cerr << "Standard exception raised: " << ex.what() << std::endl;
   return 1;
 } catch (...) {
