1f43c340c7e6ab4b059898ac04b5e2363b381b8a
Carlos O'Ryan <coryan@users.noreply.github.com>
2018-02-25 11:10:24 -0500

    Implement InstanceAdmin::ListTables()
    This is part of the fixes for #271.

---
+++ bigtable/admin/instance_admin.cc
@@ -0,0 +1,49 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bigtable/admin/instance_admin.h"
+
+namespace btproto = ::google::bigtable::admin::v2;
+
+namespace bigtable {
+inline namespace BIGTABLE_CLIENT_NS {
+std::vector<btproto::Instance> InstanceAdmin::ListInstances() {
+  // Copy the policies in effect for the operation.
+  auto rpc_policy = rpc_retry_policy_->clone();
+  auto backoff_policy = rpc_backoff_policy_->clone();
+
+  std::string error = "InstanceAdmin::ListInstances(" + project() + ")";
+
+  // Build the RPC request, try to minimize copying.
+  std::vector<btproto::Instance> result;
+  std::string page_token;
+  do {
+    btproto::ListInstancesRequest request;
+    request.set_page_token(std::move(page_token));
+    request.set_parent(project_name_);
+
+    auto response = RpcUtils::CallWithRetry(
+        *client_, *rpc_policy, *backoff_policy, &StubType::ListInstances,
+        request, error.c_str());
+
+    for (auto& x : *response.mutable_instances()) {
+      result.emplace_back(std::move(x));
+    }
+    page_token = std::move(*response.mutable_next_page_token());
+  } while (not page_token.empty());
+  return result;
+}
+
+}  // namespace BIGTABLE_CLIENT_NS
+}  // namespace bigtable
---
+++ bigtable/admin/instance_admin.h
@@ -0,0 +1,79 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_BIGTABLE_ADMIN_INSTANCE_ADMIN_H_
+#define GOOGLE_CLOUD_CPP_BIGTABLE_ADMIN_INSTANCE_ADMIN_H_
+
+#include <memory>
+#include "bigtable/admin/instance_admin_client.h"
+#include "bigtable/client/internal/unary_rpc_utils.h"
+
+namespace bigtable {
+inline namespace BIGTABLE_CLIENT_NS {
+/**
+ * Implements a minimal API to administer Cloud Bigtable instances.
+ */
+class InstanceAdmin {
+ public:
+  /**
+   * @param client the interface to create grpc stubs, report errors, etc.
+   */
+  InstanceAdmin(std::shared_ptr<InstanceAdminClient> client)
+      : client_(std::move(client)),
+        project_name_("projects/" + project()),
+        rpc_retry_policy_(DefaultRPCRetryPolicy()),
+        rpc_backoff_policy_(DefaultRPCBackoffPolicy()) {}
+
+  /**
+   * Create a new InstanceAdmin using explicit policies to handle RPC errors.
+   *
+   * @tparam RPCRetryPolicy control which operations to retry and for how long.
+   * @tparam RPCBackoffPolicy control how does the client backs off after an RPC
+   *     error.
+   * @param client the interface to create grpc stubs, report errors, etc.
+   * @param retry_policy the policy to handle RPC errors.
+   * @param backoff_policy the policy to control backoff after an error.
+   */
+  template <typename RPCRetryPolicy, typename RPCBackoffPolicy>
+  InstanceAdmin(std::shared_ptr<InstanceAdminClient> client,
+                RPCRetryPolicy retry_policy, RPCBackoffPolicy backoff_policy)
+      : client_(std::move(client)),
+        project_name_("projects/" + project()),
+        rpc_retry_policy_(retry_policy.clone()),
+        rpc_backoff_policy_(backoff_policy.clone()) {}
+
+  std::string const& project() const { return client_->project(); }
+
+  /**
+   * Return the list of instances in the project.
+   * @return
+   */
+  std::vector<::google::bigtable::admin::v2::Instance> ListInstances();
+
+ private:
+  /// Shortcuts to avoid typing long names over and over.
+  using RpcUtils = bigtable::internal::UnaryRpcUtils<InstanceAdminClient>;
+  using StubType = RpcUtils::StubType;
+
+ private:
+  std::shared_ptr<InstanceAdminClient> client_;
+  std::string project_name_;
+  std::unique_ptr<RPCRetryPolicy> rpc_retry_policy_;
+  std::unique_ptr<RPCBackoffPolicy> rpc_backoff_policy_;
+};
+
+}  // namespace BIGTABLE_CLIENT_NS
+}  // namespace bigtable
+
+#endif  // GOOGLE_CLOUD_CPP_BIGTABLE_ADMIN_INSTANCE_ADMIN_H_
