e4c5f7014f0c4a7ffcc8e7cd31d82f3cd80d3ae3
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-03-02 15:12:38 +0000

    Automatically generated documentation

--- search/defines_0.js
+++ search/defines_0.js
@@ -6,8 +6,5 @@ var searchData=
   ['bigtable_5fclient_5fdefault_5fmaximum_5fretry_5fperiod',['BIGTABLE_CLIENT_DEFAULT_MAXIMUM_RETRY_PERIOD',['../rpc__retry__policy_8cc.html#a5a2ab1039168f2f90f14d2753e655926',1,'rpc_retry_policy.cc']]],
   ['bigtable_5fclient_5fns',['BIGTABLE_CLIENT_NS',['../version_8h.html#aebb4d9f586ac64a412e3302f8a4b1c37',1,'version.h']]],
   ['bigtable_5fclient_5fvconcat',['BIGTABLE_CLIENT_VCONCAT',['../version_8h.html#a337f0c4db29f33f05b85d371e826a5df',1,'version.h']]],
-  ['bigtable_5fclient_5fversion_5fmajor',['BIGTABLE_CLIENT_VERSION_MAJOR',['../version_8h.html#a2727e6827d809df63f4deacfd817455f',1,'version.h']]],
-  ['bigtable_5fclient_5fversion_5fminor',['BIGTABLE_CLIENT_VERSION_MINOR',['../version_8h.html#adc2a6c16f5b60d39118fb2e05862e80b',1,'version.h']]],
-  ['bigtable_5fclient_5fversion_5fpatch',['BIGTABLE_CLIENT_VERSION_PATCH',['../version_8h.html#aa738fe59a3d5826668a69d38395ea136',1,'version.h']]],
   ['bigtable_5fclient_5fveval',['BIGTABLE_CLIENT_VEVAL',['../version_8h.html#aea0294a9fcdf2bfaae45e52ddcb9f135',1,'version.h']]]
 ];
