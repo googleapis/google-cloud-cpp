dafa0a8768eb90d49324fa2cf62b71115c58eae0
Carlos O'Ryan <coryan@google.com>
2018-03-07 12:38:17 -0500

    Initial version of ReadModifyWriteRule wrapper.
    This is not a complete implementation, but fixes #300 and #301.
    That unblocks a few more bugs.

---
+++ bigtable/client/read_modify_write_rule.h
@@ -0,0 +1,70 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_BIGTABLE_CLIENT_READ_MODIFY_WRITE_RULE_H_
+#define GOOGLE_CLOUD_CPP_BIGTABLE_CLIENT_READ_MODIFY_WRITE_RULE_H_
+
+#include <google/bigtable/v2/data.pb.h>
+#include <chrono>
+#include "bigtable/client/version.h"
+
+namespace bigtable {
+inline namespace BIGTABLE_CLIENT_NS {
+/**
+ * Define the interfaces to create ReadWriteModifyRule operations.
+ *
+ * Cloud Bigtable has operations to perform atomic updates to a row, such as
+ * incrementing an integer value or appending to an string value.  The changes
+ * are represented by a ReadModifyWriteRule operations.  One or much such
+ * operations can be sent in a single request.  This class helps users create
+ * the operations through a more idiomatic C++ interface.
+ */
+class ReadModifyWriteRule {
+ public:
+  ReadModifyWriteRule(ReadModifyWriteRule&& rhs) noexcept = default;
+  ReadModifyWriteRule& operator=(ReadModifyWriteRule&& rhs) noexcept = default;
+  ReadModifyWriteRule(ReadModifyWriteRule const& rhs) = default;
+  ReadModifyWriteRule& operator=(ReadModifyWriteRule const& rhs) = default;
+
+  /// Create an operation that appends a string value.
+  static ReadModifyWriteRule AppendValue(std::string value) {
+    ReadModifyWriteRule tmp;
+    tmp.rule_.set_append_value(std::move(value));
+    return tmp;
+  }
+
+  /// Return the filter expression as a protobuf.
+  google::bigtable::v2::ReadModifyWriteRule as_proto() const { return rule_; }
+
+  /// Move out the underlying protobuf value.
+  google::bigtable::v2::ReadModifyWriteRule as_proto_move() {
+    return std::move(rule_);
+  }
+
+ private:
+  /**
+   * Create an empty operation.
+   *
+   * Empty operations are not usable, so this constructor is private.
+   */
+  ReadModifyWriteRule() {}
+
+ private:
+  google::bigtable::v2::ReadModifyWriteRule rule_;
+};
+
+}  // namespace BIGTABLE_CLIENT_NS
+}  // namespace bigtable
+
+#endif  // GOOGLE_CLOUD_CPP_BIGTABLE_CLIENT_READ_MODIFY_WRITE_RULE_H_
