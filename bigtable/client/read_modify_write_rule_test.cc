2fcc923432e3a6d29aaa73ea74961ff02a0637c5
Carlos O'Ryan <coryan@users.noreply.github.com>
2018-03-07 21:42:07 -0500

    Initial version of ReadModifyWriteRule wrapper. (#307)
    This is not a complete implementation, but fixes #300 and #301.
    That unblocks a few more bugs.

---
+++ bigtable/client/read_modify_write_rule_test.cc
@@ -0,0 +1,25 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "bigtable/client/read_modify_write_rule.h"
+#include <gmock/gmock.h>
+
+namespace btproto = ::google::bigtable::v2;
+
+TEST(ReadModifyWriteRuleTest, AppendValue) {
+  auto const proto =
+      bigtable::ReadModifyWriteRule::AppendValue("foo").as_proto();
+  EXPECT_EQ(btproto::ReadModifyWriteRule::kAppendValue, proto.rule_case());
+  EXPECT_EQ("foo", proto.append_value());
+}
