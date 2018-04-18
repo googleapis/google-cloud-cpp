18afd71e951b1778d51019f53a3a22cef987b1fb
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-04-18 12:27:22 +0000

    Automatically generated documentation

--- latest/bigtable/search/all_9.js
+++ latest/bigtable/search/all_9.js
@@ -6,5 +6,6 @@ var searchData=
   ['limitederrorcountretrypolicy',['LimitedErrorCountRetryPolicy',['../classbigtable_1_1v0_1_1LimitedErrorCountRetryPolicy.html',1,'bigtable::v0::LimitedErrorCountRetryPolicy'],['../classbigtable_1_1v0_1_1LimitedErrorCountRetryPolicy.html#aed1aa614fa5d35c3d5f32d292060b400',1,'bigtable::v0::LimitedErrorCountRetryPolicy::LimitedErrorCountRetryPolicy()']]],
   ['limitedtimeretrypolicy',['LimitedTimeRetryPolicy',['../classbigtable_1_1v0_1_1LimitedTimeRetryPolicy.html',1,'bigtable::v0::LimitedTimeRetryPolicy'],['../classbigtable_1_1v0_1_1LimitedTimeRetryPolicy.html#a08843863b4cde38be17a3d8642bb4c44',1,'bigtable::v0::LimitedTimeRetryPolicy::LimitedTimeRetryPolicy()']]],
   ['listinstances',['ListInstances',['../classbigtable_1_1v0_1_1InstanceAdmin.html#a22372449d93bc0f17b8781748cd4e056',1,'bigtable::v0::InstanceAdmin::ListInstances()'],['../classbigtable_1_1v0_1_1InstanceAdminClient.html#ad63089e4f6e4b9e869b2eb8dff3808fd',1,'bigtable::v0::InstanceAdminClient::ListInstances()']]],
+  ['listsnapshots',['ListSnapshots',['../classbigtable_1_1v0_1_1TableAdmin.html#a4357f6b49096fc83049bb36733649e77',1,'bigtable::v0::TableAdmin']]],
   ['listtables',['ListTables',['../classbigtable_1_1v0_1_1TableAdmin.html#ad5a1b18d58345517ba1e7fd5ab8c6708',1,'bigtable::v0::TableAdmin']]]
 ];
