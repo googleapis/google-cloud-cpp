529bdb0eff6609ab6a39417a9678ff5e69d91252
Carlos O'Ryan <coryan@google.com>
2018-05-02 13:16:21 -0400

    Rename includes to manipulate diagnostics.
    Thanks to @mbrukman for making me think about it, also fix
    Bazel build, we were missing a few header files in the CMake file.

--- bigtable/client/table_admin_test.cc
+++ bigtable/client/table_admin_test.cc
@@ -691,9 +691,6 @@ TEST_F(TableAdminTest, CheckConsistencyFailure) {
 #endif  // GOOGLE_CLOUD_CPP_HAVE_EXCEPTIONS
 }
 
-#include "bigtable/client/internal/diagnostic_push.h"
-#include "bigtable/client/internal/wdeprecated_as_warnings.h"
-
 /**
  * @test Verify that `bigtable::TableAdmin::GetSnapshot` works in the easy case.
  */
@@ -913,5 +910,3 @@ TEST_F(TableAdminTest, ListSnapshots_UnrecoverableFailures) {
                             "exceptions are disabled");
 #endif  // GOOGLE_CLOUD_CPP_HAVE_EXCEPTIONS
 }
-
-#include "bigtable/client/internal/diagnostic_pop.h"
