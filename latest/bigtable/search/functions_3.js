ce6d80b7af082d7259e699ab3761477fd6a78284
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-05-03 12:10:22 +0000

    Automatically generated documentation

--- latest/bigtable/search/functions_3.js
+++ latest/bigtable/search/functions_3.js
@@ -10,6 +10,7 @@ var searchData=
   ['deletefromcolumnstartingfrom',['DeleteFromColumnStartingFrom',['../namespacebigtable_1_1v0.html#a635950a39b8d9350df35a907590977b7',1,'bigtable::v0']]],
   ['deletefromfamily',['DeleteFromFamily',['../namespacebigtable_1_1v0.html#a85e807d3730c0f25481c960147f2e409',1,'bigtable::v0']]],
   ['deletefromrow',['DeleteFromRow',['../namespacebigtable_1_1v0.html#afd8f1d9efa4b470295cba73b2c52172d',1,'bigtable::v0']]],
+  ['deleteinstance',['DeleteInstance',['../classbigtable_1_1v0_1_1InstanceAdmin.html#a6826e63f8726dc579c005143a49b2212',1,'bigtable::v0::InstanceAdmin::DeleteInstance()'],['../classbigtable_1_1v0_1_1InstanceAdminClient.html#a3956a1d4cf66b95b5722fb0da442a3c9',1,'bigtable::v0::InstanceAdminClient::DeleteInstance()']]],
   ['deletesnapshot',['DeleteSnapshot',['../classbigtable_1_1v0_1_1AdminClient.html#ad923c352624255c9e47a7285a5e75538',1,'bigtable::v0::AdminClient::DeleteSnapshot()'],['../classbigtable_1_1v0_1_1TableAdmin.html#a5a351081fbe08adc5b380e169d660083',1,'bigtable::v0::TableAdmin::DeleteSnapshot()']]],
   ['deletetable',['DeleteTable',['../classbigtable_1_1v0_1_1AdminClient.html#a5d5e6fdd90f8c749ab71aa2bf8a45633',1,'bigtable::v0::AdminClient::DeleteTable()'],['../classbigtable_1_1v0_1_1TableAdmin.html#af4c02c5fc34839026117a1b35283705d',1,'bigtable::v0::TableAdmin::DeleteTable()']]],
   ['drop',['Drop',['../classbigtable_1_1v0_1_1ColumnFamilyModification.html#af65c7620951f2139432807052200ebe9',1,'bigtable::v0::ColumnFamilyModification']]],
