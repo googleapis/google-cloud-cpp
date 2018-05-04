46e3bc07ffe6eb0ded4d9189cf1a4595e2b572bb
Carlos O'Ryan <coryan@google.com>
2018-05-04 10:25:23 -0400

    Integration test for InstanceAdmin::CreateTable.
    Part of the fixes for #418.

--- bigtable/tests/instance_admin_integration_test.cc
+++ bigtable/tests/instance_admin_integration_test.cc
@@ -12,9 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "bigtable/client/grpc_error.h"
 #include "bigtable/client/instance_admin.h"
 #include "bigtable/client/internal/make_unique.h"
+#include "bigtable/client/testing/random.h"
+#include <google/protobuf/text_format.h>
 #include <gmock/gmock.h>
+
+namespace btadmin = google::bigtable::admin::v2;
 
 namespace {
 class InstanceTestEnvironment : public ::testing::Environment {
@@ -42,23 +47,71 @@ class InstanceAdminIntegrationTest : public ::testing::Test {
 
  protected:
   std::unique_ptr<bigtable::InstanceAdmin> instance_admin_;
+  bigtable::testing::DefaultPRNG generator_ =
+      bigtable::testing::MakeDefaultPRNG();
 };
 
 bool UsingCloudBigtableEmulator() {
   return std::getenv("BIGTABLE_EMULATOR_HOST") != nullptr;
 }
 
-bool IsInstancePresent(
-    std::vector<::google::bigtable::admin::v2::Instance> instances,
-    std::string const& instance_name) {
-  for (auto const& i : instances) {
-    if (instance_name == i.name()) {
-      return true;
-    }
-  }
-  return false;
+bool IsInstancePresent(std::vector<btadmin::Instance> const& instances,
+                       std::string const& instance_name) {
+  return instances.end() !=
+      std::find_if(instances.begin(), instances.end(),
+                   [&instance_name](btadmin::Instance const &i) {
+                     return i.name() == instance_name;
+                   });
+}
+
+bigtable::InstanceConfig IntegrationTestConfig(std::string const& id) {
+  bigtable::InstanceId instance_id(id);
+  bigtable::DisplayName display_name("Integration Tests " + id);
+  auto cluster_config =
+      bigtable::ClusterConfig("us-central1-f", 0, bigtable::ClusterConfig::HDD);
+  bigtable::InstanceConfig config(instance_id, display_name,
+                                  {{id + "-c1", cluster_config}});
+  config.set_type(bigtable::InstanceConfig::DEVELOPMENT);
+  return config;
 }
 }  // namespace
+
+/// @test Verify that InstanceAdmin::CreateInstance works as expected.
+#if GOOGLE_CLOUD_CPP_HAVE_EXCEPTIONS
+TEST_F(InstanceAdminIntegrationTest, CreateInstanceTest) {
+  std::string instance_id =
+      "it-" + bigtable::testing::Sample(generator_, 8,
+                                        "abcdefghijklmnopqrstuvwxyz0123456789");
+  auto config = IntegrationTestConfig(instance_id);
+
+  std::cout << "instance_id = " << instance_id << std::endl;
+  std::string config_text;
+  google::protobuf::TextFormat::PrintToString(config.as_proto(), &config_text);
+  std::cout << "config = " << config_text << std::endl;
+
+  if (UsingCloudBigtableEmulator()) {
+    // We cannot test the functionality against the emulator, but we can at
+    // least test that the gRPC calls are made and the right error is returned.
+    auto future = instance_admin_->CreateInstance(config);
+    try {
+      future.get();
+    } catch (bigtable::GRpcError const& ex) {
+      EXPECT_EQ(grpc::StatusCode::UNIMPLEMENTED, ex.error_code());
+    }
+    return;
+  }
+
+  auto instances_before = instance_admin_->ListInstances();
+  auto instance = instance_admin_->CreateInstance(config).get();
+  EXPECT_NE(std::string::npos, instance.name().find(instance_id));
+  EXPECT_NE(std::string::npos,
+            instance.name().find(InstanceTestEnvironment::project_id()));
+  EXPECT_NE(std::string::npos, instance.display_name().find(instance_id));
+  auto instances_after = instance_admin_->ListInstances();
+  EXPECT_FALSE(IsInstancePresent(instances_before, instance.name()));
+  EXPECT_TRUE(IsInstancePresent(instances_after, instance.name()));
+}
+#endif  // GOOGLE_CLOUD_CPP_HAVE_EXCEPTIONS
 
 /// @test Verify that InstanceAdmin::ListInstances works as expected.
 TEST_F(InstanceAdminIntegrationTest, ListInstancesTest) {
