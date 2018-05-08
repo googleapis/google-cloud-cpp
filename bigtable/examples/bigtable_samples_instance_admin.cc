9996b147b6d568ca7648bd42f1cb560b07aa4e74
roopak-qlogic <36889394+roopak-qlogic@users.noreply.github.com>
2018-05-08 08:57:01 +0530

    Add sample snippet for InstanceAdmin::GetInstance() (#493)
    This is part of the fixes for #419.

--- bigtable/examples/bigtable_samples_instance_admin.cc
+++ bigtable/examples/bigtable_samples_instance_admin.cc
@@ -14,6 +14,7 @@
 
 #include "bigtable/client/instance_admin.h"
 #include "bigtable/client/instance_admin_client.h"
+#include <google/protobuf/text_format.h>
 
 namespace btproto = ::google::bigtable::admin::v2;
 
@@ -91,7 +92,14 @@ void ListInstances(bigtable::InstanceAdmin instance_admin, int argc,
 //! [get instance]
 void GetInstance(bigtable::InstanceAdmin instance_admin, int argc,
                  char* argv[]) {
-  // TODO(#419) implement tests and examples for GetInstance
+  if (argc != 2) {
+    throw Usage{"get-instance: <project-id> <instance-id>"};
+  }
+  std::string instance_id = ConsumeArg(argc, argv);
+  auto instance = instance_admin.GetInstance(instance_id);
+  std::string instance_detail;
+  google::protobuf::TextFormat::PrintToString(instance, &instance_detail);
+  std::cout << "GetInstance details : " << instance_detail << std::endl;
 }
 //! [get instance]
 
