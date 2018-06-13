0c343e3dab4be3db9a386c93bc240dd61aaa0b7c
Carlos O'Ryan <coryan@google.com>
2018-06-12 23:56:58 -0400

    Use ExternalProject_Add() for gRPC.
    This is part of #518. It makes the management of the gRPC
    dependency homogenous between Bazel and CMake build, both
    will download the same version, as opposed to using a submodule
    in CMake and downloads in Bazel. It also should speed up the build
    by a few seconds, because the download of a single version is
    faster than downloading the full history.
    
    It also cleans up our namespace for targets in CMake, we no longer
    get random targets (such as gtest) introduced by the gRPC
    submodule.

--- cmake/IncludeGMock.cmake
+++ cmake/IncludeGMock.cmake
@@ -25,10 +25,12 @@ set_property(CACHE GOOGLE_CLOUD_CPP_GMOCK_PROVIDER
                       "vcpkg"
                       "pkg-config")
 
-if ("${GOOGLE_CLOUD_CPP_GMOCK_PROVIDER}" STREQUAL "module")
+if (TARGET gmock)
+    # If the gmock target is already defined then skip the rest.
+elseif ("${GOOGLE_CLOUD_CPP_GMOCK_PROVIDER}" STREQUAL "module")
 
-    # Compile the googlemock library. This library is rarely installed or pre-
-    # compiled because it should be configured with the same flags as the
+    # Compile the googlemock library.  This library is rarely installed or
+    # pre-compiled because it should be configured with the same flags as the
     # application.
     #
     # TODO(#310) - consider changing the name of this target as it can conflict
---
+++ cmake/external/grpc.cmake
@@ -0,0 +1,57 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(external/c-ares)
+include(external/protobuf)
+
+if (NOT TARGET gprc_project)
+    if("${CMAKE_GENERATOR}" STREQUAL "Unix Makefiles" OR "${CMAKE_GENERATOR}" STREQUAL "Ninja")
+        include(ProcessorCount)
+        ProcessorCount(NCPU)
+        set(PARALLEL "--" "-j" "${NCPU}")
+    else()
+        set(PARALLEL "")
+    endif()
+
+    include(ExternalProject)
+    ExternalProject_Add(grpc_project
+        DEPENDS c_ares_project protobuf_project
+        EXCLUDE_FROM_ALL ON
+        PREFIX "external/gprc"
+        INSTALL_DIR "external"
+        URL https://github.com/grpc/grpc/archive/v1.10.0.tar.gz
+        URL_HASH SHA256=39a73de6fa2a03bdb9c43c89a4283e09880833b3c1976ef3ce3edf45c8cacf72
+        CONFIGURE_COMMAND
+            ${CMAKE_COMMAND}
+            $ENV{CMAKE_FLAGS}
+            ${GOOGLE_CLOUD_CPP_EXTERNAL_PROJECT_CCACHE}
+            -DCMAKE_BUILD_TYPE=Release
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+            -DCMAKE_PREFIX_PATH=<INSTALL_DIR>
+            -DgRPC_BUILD_TESTS=OFF
+            -DgRPC_ZLIB_PROVIDER=package
+            -DgRPC_SSL_PROVIDER=package
+            -DgRPC_CARES_PROVIDER=package
+            -DgRPC_PROTOBUF_PROVIDER=package
+            -H<SOURCE_DIR>
+            -B<BINARY_DIR>/Release
+        BUILD_COMMAND
+            ${CMAKE_COMMAND} --build Release ${PARALLEL}
+        INSTALL_COMMAND
+            ${CMAKE_COMMAND} --build Release --target install
+        LOG_DOWNLOAD ON
+        LOG_CONFIGURE ON
+        LOG_BUILD ON
+        LOG_INSTALL ON)
+endif ()
