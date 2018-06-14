02df907be364a944c2ca1b7e28a2f55dee9a29f4
Carlos O'Ryan <coryan@google.com>
2018-06-14 14:32:44 -0400

    Switch Kokoro configuration to Bazel.
    This will free up a slot in the Travis build for more things we
    cannot do in Kokoro, and also means we can upload the test results
    to the ResultStore. That should help with #642, it also unblocks
    external projects (the installed OpenSSL is not good enough).

--- ci/kokoro/ubuntu/build.sh
+++ ci/kokoro/ubuntu/build.sh
@@ -16,36 +16,100 @@
 
 set -eu
 
-export GOOGLE_APPLICATION_CREDENTIALS="${KOKORO_GFILE_DIR}/service-account.json"
+echo "Reading CI secret configuration parameters."
 source "${KOKORO_GFILE_DIR}/test-configuration.sh"
-
-echo "Getting cbt tool"
-export GOPATH="${KOKORO_ROOT}/golang"
-go get -u cloud.google.com/go/bigtable/cmd/cbt
-
-echo "Getting python dependencies"
-sudo python2 -m pip install gunicorn httpbin
 
 echo "Running build and tests"
 cd "$(dirname $0)/../../.."
 readonly PROJECT_ROOT="${PWD}"
 
-git submodule update --init --recursive
-cmake -DCMAKE_BUILD_TYPE=Release -H. -Bbuild-output
-cmake --build build-output -- -j $(nproc)
 
-cd build-output
-ctest --output-on-failure
+cat >>kokoro-bazelrc <<_EOF_
+# Set flags for uploading to BES without Remote Build Execution.
+startup --host_jvm_args=-Dbazel.DigestFunction=SHA256
+build:results-local --remote_cache=remotebuildexecution.googleapis.com
+build:results-local --spawn_strategy=local
+build:results-local --remote_timeout=3600
+build:results-local --bes_backend="buildeventservice.googleapis.com"
+build:results-local --bes_best_effort=false
+build:results-local --bes_timeout=10m
+build:results-local --tls_enabled=true
+build:results-local --auth_enabled=true
+build:results-local --auth_scope=https://www.googleapis.com/auth/cloud-source-tools
+build:results-local --experimental_remote_spawn_cache
+_EOF_
 
-# The integration tests
-export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH="${PROJECT_ROOT}/third_party/grpc/etc/roots.pem"
+# First build and run the unit tests.
+INVOCATION_ID="$(python -c 'import uuid; print uuid.uuid4()')"
+echo "Configure and start Bazel: " ${INVOCATION_ID}
+echo "================================================================"
+echo "    https://source.cloud.google.com/results/invocations/${INVOCATION_ID}"
+echo "================================================================"
+echo ${INVOCATION_ID} >> "${KOKORO_ARTIFACTS_DIR}/bazel_invocation_ids"
 
+bazel --bazelrc=kokoro-bazelrc test \
+    --test_output=errors \
+    --verbose_failures=true \
+    --keep_going \
+    --project_id=${KOKORO_PROJECT_ID} \
+    --auth_credentials="${KOKORO_GFILE_DIR}/build-results-service-account.json" \
+    --remote_instance_name="projects/${KOKORO_PROJECT_ID}" \
+    --invocation_id="${INVOCATION_ID}" \
+    --config=results-local \
+    -- //google/cloud/...:all
+
+echo
+echo "================================================================"
+echo "================================================================"
+echo "Copying artifacts"
+cp "$(bazel info output_base)/java.log" "${KOKORO_ARTIFACTS_DIR}/" || echo "java log copy failed."
+echo "End of copying."
+
+echo
+echo "================================================================"
+echo "================================================================"
+# Then build everything else (integration tests, examples, etc). This needs to
+# go last (and in a separate invocation) so Bazel state is preserved to run the
+# integration tests.
+bazel build \
+    --test_output=errors \
+    --verbose_failures=true \
+    --keep_going \
+    -- //google/cloud/...:all
+
+# The integration tests need further configuration and tools.
+echo
+echo "================================================================"
+echo "================================================================"
+export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH="$(bazel info output_base)/external/com_github_grpc_grpc/etc/roots.pem"
+export GOOGLE_APPLICATION_CREDENTIALS="${KOKORO_GFILE_DIR}/service-account.json"
+
+echo "Download dependencies for integration tests."
+echo "    Getting cbt tool"
+export GOPATH="${KOKORO_ROOT}/golang"
+go get -u cloud.google.com/go/bigtable/cmd/cbt
+# echo "    Getting python modules"
+# sudo python2 -m pip install httpbin
+echo "End of download."
+
+echo ROOTS PATH = ${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}
+
+ls -l ${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH} || echo "no roots file"
+ls -l $(dirname ${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}) || echo "no roots file dir"
+
+echo
+echo "================================================================"
+echo "================================================================"
 echo "Running Google Cloud Bigtable Integration Tests"
-(cd google/cloud/bigtable/tests && \
-    "${PROJECT_ROOT}/google/cloud/bigtable/tests/run_integration_tests_production.sh")
+(cd $(bazel info bazel-bin)/google/cloud/bigtable/tests && \
+   "${PROJECT_ROOT}/google/cloud/bigtable/tests/run_integration_tests_production.sh")
+(cd $(bazel info bazel-bin)/google/cloud/bigtable/tests && \
+   "${PROJECT_ROOT}/google/cloud/bigtable/examples/run_examples_production.sh")
 
 # TODO(#640) - disabled until we can get the credentials to work with key files.
 # echo "Running Google Cloud Storage Integration Tests"
-# (cd google/cloud/storage/tests && "${PROJECT_ROOT}/google/cloud/storage/tests/run_integration_tests_production.sh")
+# (cd $(bazel info bazel-bin)/google/cloud/storage/tests && \
+#     "${PROJECT_ROOT}/google/cloud/storage/tests/run_integration_tests_production.sh")
 # echo "Running Google Cloud Storage Examples"
-# (cd google/cloud/storage/examples && "${PROJECT_ROOT}/google/cloud/storage/examples/run_examples_production.sh")
+# (cd $(bazel info bazel-bin)/google/cloud/storage/examples && \
+#     "${PROJECT_ROOT}/google/cloud/storage/examples/run_examples_production.sh")
