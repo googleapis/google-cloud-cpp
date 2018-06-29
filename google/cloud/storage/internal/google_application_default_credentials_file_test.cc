8b3df1d9e5c585208ff997778eeee7fc82f6bb68
Carlos O'Ryan <coryan@users.noreply.github.com>
2018-06-29 17:17:53 -0400

    Refactor EnvironmentVariableRestore to testing library. (#783)
    This class will be used in a future test related to #570, I think
    a pure refactor PR is easier to grok.
    
    Thanks to @mbrukman for the suggestion to rename `cloud_testing`
    to `testing_util`, the other name was kind of repetitive.

--- google/cloud/storage/internal/google_application_default_credentials_file_test.cc
+++ google/cloud/storage/internal/google_application_default_credentials_file_test.cc
@@ -14,27 +14,11 @@
 
 #include "google/cloud/storage/internal/google_application_default_credentials_file.h"
 #include "google/cloud/internal/setenv.h"
+#include "google/cloud/testing_util/environment_variable_restore.h"
 #include <gmock/gmock.h>
 
 using namespace google::cloud::storage::internal;
-
-class EnvironmentVariableRestore {
- public:
-  explicit EnvironmentVariableRestore(char const* variable_name)
-      : EnvironmentVariableRestore(std::string(variable_name)) {}
-
-  explicit EnvironmentVariableRestore(std::string variable_name)
-      : variable_name_(std::move(variable_name)) {}
-
-  void SetUp() { previous_ = std::getenv(variable_name_.c_str()); }
-  void TearDown() {
-    google::cloud::internal::SetEnv(variable_name_.c_str(), previous_);
-  }
-
- private:
-  std::string variable_name_;
-  char const* previous_;
-};
+using google::cloud::testing_util::EnvironmentVariableRestore;
 
 class DefaultServiceAccountFileTest : public ::testing::Test {
  public:
