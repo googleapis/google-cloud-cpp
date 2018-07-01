a1d49fb3ec9a7a3e70925c43e393eec2a3f57e65
Carlos O'Ryan <coryan@google.com>
2018-06-30 20:44:03 -0400

    Use PollingPolicy in InstanceAdmin operations.
    This fixes #631.

--- google/cloud/bigtable/internal/instance_admin.h
+++ google/cloud/bigtable/internal/instance_admin.h
@@ -17,10 +17,14 @@
 
 #include "google/cloud/bigtable/app_profile_config.h"
 #include "google/cloud/bigtable/instance_admin_client.h"
+#include "google/cloud/bigtable/internal/grpc_error_delegate.h"
 #include "google/cloud/bigtable/metadata_update_policy.h"
+#include "google/cloud/bigtable/polling_policy.h"
 #include "google/cloud/bigtable/rpc_backoff_policy.h"
 #include "google/cloud/bigtable/rpc_retry_policy.h"
 #include <memory>
+#include <sstream>
+#include <thread>
 
 namespace google {
 namespace cloud {
@@ -42,6 +46,7 @@ class InstanceAdmin {
         project_name_("projects/" + project_id()),
         rpc_retry_policy_(DefaultRPCRetryPolicy()),
         rpc_backoff_policy_(DefaultRPCBackoffPolicy()),
+        polling_policy_(DefaultPollingPolicy()),
         metadata_update_policy_(project_name(), MetadataParamTypes::PARENT) {}
 
   /**
@@ -61,6 +66,31 @@ class InstanceAdmin {
         project_name_("projects/" + project_id()),
         rpc_retry_policy_(retry_policy.clone()),
         rpc_backoff_policy_(backoff_policy.clone()),
+        polling_policy_(DefaultPollingPolicy()),
+        metadata_update_policy_(project_name(), MetadataParamTypes::PARENT) {}
+
+  /**
+   * Create a new InstanceAdmin using explicit policies to handle RPC errors.
+   *
+   * @tparam RPCRetryPolicy control which operations to retry and for how long.
+   * @tparam RPCBackoffPolicy control how does the client backs off after an RPC
+   *     error.
+   * @tparam Control polling for long running operations.
+   * @param client the interface to create grpc stubs, report errors, etc.
+   * @param retry_policy the policy to handle RPC errors.
+   * @param backoff_policy the policy to control backoff after an error.
+   * @param polling_policy the PollingPolicy instance.
+   */
+  template <typename RPCRetryPolicy, typename RPCBackoffPolicy,
+            typename PollingPolicy>
+  InstanceAdmin(std::shared_ptr<InstanceAdminClient> client,
+                RPCRetryPolicy retry_policy, RPCBackoffPolicy backoff_policy,
+                PollingPolicy polling_policy)
+      : client_(std::move(client)),
+        project_name_("projects/" + project_id()),
+        rpc_retry_policy_(retry_policy.clone()),
+        rpc_backoff_policy_(backoff_policy.clone()),
+        polling_policy_(polling_policy.clone()),
         metadata_update_policy_(project_name(), MetadataParamTypes::PARENT) {}
 
   /// The full name (`projects/<project_id>`) of the project.
@@ -116,12 +146,62 @@ class InstanceAdmin {
       std::string const& instance_id, grpc::Status& status);
   //@}
 
+  template <typename ResultType>
+  ResultType PollLongRunningOperation(google::longrunning::Operation& operation,
+                                      char const* error_message,
+                                      grpc::Status& status) {
+    auto polling_policy = polling_policy_->clone();
+    do {
+      if (operation.done()) {
+        if (operation.has_response()) {
+          auto const& any = operation.response();
+          if (not any.Is<ResultType>()) {
+            std::ostringstream os;
+            os << error_message << "(" << metadata_update_policy_.value()
+               << ") - "
+               << "invalid result type in operation=" << operation.name();
+            status = grpc::Status(grpc::StatusCode::UNKNOWN, os.str());
+            return ResultType{};
+          }
+          ResultType result;
+          any.UnpackTo(&result);
+          return result;
+        }
+        if (operation.has_error()) {
+          std::ostringstream os;
+          os << error_message << "(" << metadata_update_policy_.value()
+             << ") - "
+             << "error reported by operation=" << operation.name();
+          status = grpc::Status(
+              static_cast<grpc::StatusCode>(operation.error().code()),
+              operation.error().message(), os.str());
+          return ResultType{};
+        }
+      }
+      auto delay = polling_policy->WaitPeriod();
+      std::this_thread::sleep_for(delay);
+      google::longrunning::GetOperationRequest op;
+      op.set_name(operation.name());
+      grpc::ClientContext context;
+      status = client_->GetOperation(&context, op, &operation);
+      if (not status.ok() and not polling_policy->OnFailure(status)) {
+        return ResultType{};
+      }
+    } while (not polling_policy->Exhausted());
+    std::ostringstream os;
+    os << error_message << "(" << metadata_update_policy_.value() << ") - "
+       << "polling policy exhausted in operation=" << operation.name();
+    status = grpc::Status(grpc::StatusCode::UNKNOWN, os.str());
+    return ResultType{};
+  }
+
  private:
   friend class bigtable::BIGTABLE_CLIENT_NS::InstanceAdmin;
   std::shared_ptr<InstanceAdminClient> client_;
   std::string project_name_;
   std::shared_ptr<RPCRetryPolicy> rpc_retry_policy_;
   std::shared_ptr<RPCBackoffPolicy> rpc_backoff_policy_;
+  std::shared_ptr<PollingPolicy> polling_policy_;
   MetadataUpdatePolicy metadata_update_policy_;
 };
 
