4cf77e2f7b2baa1849d34083501945714233d31b
Carlos O'Ryan <coryan@google.com>
2018-07-10 15:50:02 -0400

    Add options to configure separator and row key.

--- google/cloud/bigtable/examples/bigtable_upload_csv.cc
+++ google/cloud/bigtable/examples/bigtable_upload_csv.cc
@@ -95,38 +95,90 @@ class GenericCircularBuffer {
  * TODO() - make the separator configurable.
  */
 std::vector<std::string> ParseLine(long lineno, std::string const& line,
-                                   char separator = ',');
+                                   char separator);
 
 namespace cbt = google::cloud::bigtable;
 using CircularBuffer = GenericCircularBuffer<cbt::BulkMutation>;
 
+struct Options {
+  char separator;
+  std::vector<int> keys;
+  std::string keys_separator;
+
+  std::string ConsumeArg(int& argc, char* argv[], char const* arg_name) {
+    std::string const separator_option = "--separator=";
+    std::string const key_option = "--key=";
+    std::string const keys_separator_option = "--key-separator=";
+
+    std::string const usage = R""(
+[options] <project> <instance> <table> <family>
+The options are:
+    --help: produce this help.
+    --separator=c: use the 'c' character instead of comma (',') to separate the
+        values in the CSV file.
+    --key=N: use field number N as part of the row key. The fields are numbered
+        starting at one. They are concatenated in the order provided.
+    --key-separator=sep: use 'sep' to separate the fields when forming the row
+        key.
+    project: the Google Cloud Platform project id for your table.
+    instance: the Cloud Bigtable instance hosting your table.
+    table: the table where you want to upload the CSV file.
+    family: the column family where you want to upload the CSV file.
+)"";
+    while (argc >= 2) {
+      std::string argument(argv[1]);
+      std::copy(argv + 2, argv + argc, argv + 1);
+      argc--;
+      if (argument == "--help") {
+      } else if (0 == argument.find(separator_option)) {
+        separator = argument.substr(separator_option.size())[0];
+      } else if (0 == argument.find(key_option)) {
+        keys.push_back(std::stoi(argument.substr(key_option.size())) - 1);
+      } else if (0 == argument.find(keys_separator_option)) {
+        keys_separator = argument.substr(keys_separator_option.size());
+      } else {
+        return argument;
+      }
+    }
+    std::string cmd = argv[0];
+    auto last_slash = std::string(cmd).find_last_of('/');
+    cmd = cmd.substr(last_slash);
+
+    std::ostringstream os;
+    os << "Missing argument " << arg_name << "\n";
+    os << "Usage: " << cmd << usage << std::endl;
+    throw std::runtime_error(os.str());
+  }
+};
+
 }  // anonymous namespace
 
 int main(int argc, char* argv[]) try {
-  // Make sure we have the right number of arguments.
-  if (argc != 6) {
-    std::string const cmd = argv[0];
-    auto last_slash = std::string(argv[0]).find_last_of('/');
-    std::cerr << "Usage: " << cmd.substr(last_slash + 1)
-              << " <project> <instance> <table> <family>" << std::endl;
-    return 1;
+  // Parse the command-line arguments.
+  Options options;
+  std::string const project_id = options.ConsumeArg(argc, argv, "project_id");
+  std::string const instance_id = options.ConsumeArg(argc, argv, "instance_id");
+  std::string const table_id = options.ConsumeArg(argc, argv, "table_id");
+  std::string const family = options.ConsumeArg(argc, argv, "family");
+  std::string const filename = options.ConsumeArg(argc, argv, "filename");
+
+  // If the user does not say, use the first column as the row key.
+  if (options.keys.empty()) {
+    options.keys.push_back(0);
   }
-  std::string const project_id = argv[1];
-  std::string const instance_id = argv[2];
-  std::string const table_id = argv[3];
-  std::string const family = argv[4];
-  std::string const filename = argv[5];
 
   // Create a connection to Cloud Bigtable and an object to manipulate the
   // specific table used in this demo.
-  cbt::Table table(cbt::CreateDefaultDataClient(project_id, instance_id,
-                                                cbt::ClientOptions()),
+  cbt::Table table(cbt::CreateDefaultDataClient(
+                       project_id, instance_id,
+                       cbt::ClientOptions().set_connection_pool_size(
+                           std::thread::hardware_concurrency())),
                    table_id);
 
-  // How often do we print a progress message.
-  int const report_progress_rate = 100000;
-  // The maximum size of a bulk apply.
-  int const bulk_apply_size = 10000;
+  // How often do we print a progress marker ('.') in the reader thread.
+  int const report_reader_progress_rate = 50000;
+  // How often do we print a progress marker ('+') in the worker threads.
+  int const report_worker_progress_rate = 10;
   // The size of the circular buffer.
   int const buffer_size = 1000;
   // The size of the thread pool pushing data to Cloud Bigtable
@@ -137,41 +189,70 @@ int main(int argc, char* argv[]) try {
   CircularBuffer buffer(buffer_size);
   // Then create a few threads, each one of which pulls mutations out of the
   // circular buffer and then applies the mutation to the table.
-  auto read_buffer = [&buffer, &table]() {
+  auto read_buffer = [&buffer](cbt::Table table, int report_progress_rate) {
+    long count = 0;
     cbt::BulkMutation mutation;
     while (buffer.Pop(mutation)) {
       table.BulkApply(std::move(mutation));
+      if (++count % report_progress_rate == 0) {
+        std::cout << '+' << std::flush;
+      }
     }
   };
+
+  std::cout << "Starting " << thread_pool_size << " workers ..." << std::flush;
   std::vector<std::future<void>> workers;
   for (int i = 0; i != thread_pool_size; ++i) {
-    workers.push_back(std::async(std::launch::async, read_buffer));
+    workers.push_back(std::async(std::launch::async, read_buffer, table,
+                                 report_worker_progress_rate));
   }
+  std::cout << " DONE" << std::endl;
 
   // The main thread just reads the file one line at a time.
   std::ifstream is(filename);
   std::string line;
   std::getline(is, line, '\n');
   int lineno = 0;
-  auto headers = ParseLine(++lineno, line);
+  auto headers = ParseLine(++lineno, line, options.separator);
 
-  std::cout << "Start reading input file " << std::flush;
+  std::cout << "Reading input file " << std::flush;
   auto start = std::chrono::steady_clock::now();
 
+  // We can only do about 100,000 Apply operations in each BulkApply (see the
+  // Cloud Bigtable bigtable.proto file for details), and each SingleRowMutation
+  // we create below has roughly headers.size() elements. So we want at most:
+  auto bulk_apply_size = static_cast<int>(10000 / headers.size());
   cbt::BulkMutation bulk;
   int count = 0;
   while (not is.eof()) {
     ++lineno;
     std::getline(is, line, '\n');
-    auto parsed = ParseLine(lineno, line);
+    if (line.empty()) {
+      break;
+    }
+    auto parsed = ParseLine(lineno, line, options.separator);
 
     using std::chrono::milliseconds;
     auto ts = std::chrono::duration_cast<milliseconds>(
         std::chrono::system_clock::now().time_since_epoch());
+
+    // Create the row key by concatenating the desired columns.
+    std::string row_key;
+    std::string row_key_separator;
+    for (auto&& index : options.keys) {
+      row_key += row_key_separator;
+      row_key_separator = options.keys_separator;
+      if (index == -1) {
+        // Magic index, use the line number
+        row_key += std::to_string(lineno);
+      } else {
+        row_key += parsed.at(index);
+      }
+    }
+
     // Create a mutation that inserts one column per field, the name of the
     // column is derived from the header.
-    // TODO(coryan) - use an option to join several fields for the key.
-    cbt::SingleRowMutation mutation(std::to_string(lineno));
+    cbt::SingleRowMutation mutation(row_key);
     auto field_count = std::min(headers.size(), parsed.size());
     for (std::size_t i = 0; i != field_count; ++i) {
       mutation.emplace_back(cbt::SetCell(family, headers[i], ts, parsed[i]));
@@ -185,18 +266,18 @@ int main(int argc, char* argv[]) try {
       count = 0;
     }
 
-    if (lineno % report_progress_rate == 0) {
+    if (lineno % report_reader_progress_rate == 0) {
       std::cout << '.' << std::flush;
     }
   }
   if (count != 0) {
     buffer.Push(std::move(bulk));
   }
-  // Let the workers know that they can exit if they find an
+  std::cout << " DONE" << std::endl;
+
+  std::cout << "Waiting for worker threads " << std::flush;
+  // Let the workers know that they can exit.
   buffer.Shutdown();
-
-  std::cout << "***" << std::flush;
-
   int worker_count = 0;
   for (auto& task : workers) {
     // If there was an exception in any thread continue, and report any
@@ -207,11 +288,13 @@ int main(int argc, char* argv[]) try {
       std::cerr << "Exception raised by worker " << worker_count << std::endl;
     }
     ++worker_count;
+    std::cout << '.' << std::flush;
   }
+  std::cout << " DONE" << std::endl;
 
   auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(
       std::chrono::steady_clock::now() - start);
-  std::cout << " DONE in " << elapsed.count() << "s" << std::endl;
+  std::cout << "Total running time " << elapsed.count() << "s" << std::endl;
 
   return 0;
 } catch (std::exception const& ex) {
