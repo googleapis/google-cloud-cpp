d068add969f80dc929e6c3ab20895703e090d944
Carlos O'Ryan <coryan@google.com>
2018-07-13 13:56:44 -0400

    Create scripts for Kokoro Windows build.
    Building on Kokoro gives us more bandwidth. The servers have four
    cores (or threads), while AppVeyor has only 2, and we get more
    parallel builds: Kokoro runs all presubmit builds in parallel.
    
    The disadvantage is that we do not get to cache build artifacts, which
    slows down some things.
    
    I am getting 35 minute builds with Kokoro, which is worse than the 25
    minutes for AppVeyor, but when more than one build is queued.

--- ci/kokoro/windows/build-dependencies.ps1
+++ ci/kokoro/windows/build-dependencies.ps1
@@ -1,4 +1,4 @@
-#!/usr/bin/env powershell
+# !/usr/bin/env powershell
 
 # Copyright 2017 Google Inc.
 #
@@ -14,27 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Stop on errors. This has a similar effect as `set -e` on Unix shells.
+# Stop on errors. This is similar to `set -e` on Unix shells.
 $ErrorActionPreference = "Stop"
 
-# First check if the compilation is setup to use modules.
-if (-not (Test-Path env:PROVIDER)) {
-    throw "Aborting build because the PROVIDER environment variable is not set."
+# First check the required environment variables.
+if (-not (Test-Path env:CONFIG)) {
+    throw "Aborting build because the CONFIG environment variable is not set."
 }
-
-# If this is a build using the submodules just install nasm.  The rest of this script configures vcpkg, which
-# is not used in this case.
-if ($env:PROVIDER -eq "module") {
-    choco install -y nasm
-    return
-}
-
-# BEGIN WORKAROUND
-# As of 2018-08-30 AppVeyor had a broken image, reportedly this is a workaround.
-#     https://github.com/Microsoft/vcpkg/issues/4189#issuecomment-417462822
-Remove-Item C:\OpenSSL-v11-Win32 -Recurse -Force -ErrorAction Ignore
-Remove-Item C:\OpenSSL-v11-Win64 -Recurse -Force -ErrorAction Ignore
-# END WORKAROUND
+$CONFIG = $env:CONFIG
 
 # Update or clone the 'vcpkg' package manager, this is a bit overly complicated,
 # but it works well on your workstation where you may want to run this script
@@ -55,30 +42,33 @@ if (Test-Path vcpkg\.git) {
     cd vcpkg
 }
 if ($LastExitCode) {
-    throw "git setup failed with exit code $LastExitCode"
+    throw "vcpkg git setup failed with exit code $LastExitCode"
 }
 
-# Build the tool each time, it is fast to do so.
 powershell -exec bypass scripts\bootstrap.ps1
 if ($LastExitCode) {
     throw "vcpkg bootstrap failed with exit code $LastExitCode"
 }
 
+# Only compile the release version of the packages we need, for Debug builds
+# this trick does not work, so we need to compile all versions (yuck).
+if ($CONFIG -eq "Release") {
+    Add-Content triplets\x64-windows-static.cmake "set(VCPKG_BUILD_TYPE release)"
+}
+
 # Integrate installed packages into the build environment.
-& .\vcpkg.exe integrate install
+.\vcpkg.exe integrate install
 if ($LastExitCode) {
     throw "vcpkg integrate failed with exit code $LastExitCode"
 }
 
-# AppVeyor limits builds to 60 minutes. Building all the dependencies takes
-# longer than that. Cache the dependencies to work around the build time
-# restrictions. Explicitly install each dependency because if we run out of
-# time in the AppVeyor build the cache is at least partially refreshed, a
-# rebuild will start with some dependencies already cached, and likely
-# complete before the AppVeyor build time limit.
-$packages = @("zlib:x64-windows-static", "openssl:x64-windows-static",
-              "protobuf:x64-windows-static", "c-ares:x64-windows-static",
-              "grpc:x64-windows-static", "curl:x64-windows-static",
+.\vcpkg.exe remove --outdated
+if ($LastExitCode) {
+    throw "vcpkg remove outdated failed with exit code $LastExitCode"
+}
+
+# Build the dependencies that we cannot use as external packages.
+$packages = @("curl:x64-windows-static", "grpc:x64-windows-static",
               "gtest:x64-windows-static")
 foreach ($pkg in $packages) {
     .\vcpkg.exe install $pkg
---
+++ ci/kokoro/windows/build-project.ps1
@@ -0,0 +1,77 @@
+# !/usr/bin/env powershell
+
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Stop on errors. This is similar to `set -e` on Unix shells.
+$ErrorActionPreference = "Stop"
+
+# First check the required environment variables.
+if (-not (Test-Path env:PROVIDER)) {
+    throw "Aborting build because the PROVIDER environment variable is not set."
+}
+if (-not (Test-Path env:GENERATOR)) {
+    throw "Aborting build because the GENERATOR environment variable is not set."
+}
+if (-not (Test-Path env:CONFIG)) {
+    throw "Aborting build because the CONFIG environment variable is not set."
+}
+
+$CONFIG = $env:CONFIG
+$GENERATOR = $env:GENERATOR
+$PROVIDER = $env:PROVIDER
+
+git submodule update --init
+if ($LastExitCode) {
+    throw "git submodule failed with exit code $LastExitCode"
+}
+
+# By default assume "module", use the configuration parameters and build in the `build-output` directory.
+$cmake_flags=@("-G$GENERATOR", "-DCMAKE_BUILD_TYPE=$CONFIG", "-H.", "-Bbuild-output")
+
+# This script expects vcpkg to be installed in ..\vcpkg, discover the full
+# path to that directory:
+$dir = Split-Path (Get-Item -Path ".\" -Verbose).FullName
+
+# Run the vcpkg integration.
+$integrate = "$dir\vcpkg\vcpkg.exe integrate install"
+Invoke-Expression $integrate
+if ($LastExitCode) {
+    throw "vcpkg integrate failed with exit code $LastExitCode"
+}
+
+# Setup the environment for vcpkg:
+$cmake_flags += "-DGOOGLE_CLOUD_CPP_GRPC_PROVIDER=$PROVIDER"
+$cmake_flags += "-DGOOGLE_CLOUD_CPP_GMOCK_PROVIDER=$PROVIDER"
+$cmake_flags += "-DCMAKE_TOOLCHAIN_FILE=`"$dir\vcpkg\scripts\buildsystems\vcpkg.cmake`""
+$cmake_flags += "-DVCPKG_TARGET_TRIPLET=x64-windows-static"
+
+# Configure CMake and create the build directory.
+cmake $cmake_flags
+if ($LastExitCode) {
+    throw "cmake config failed with exit code $LastExitCode"
+}
+
+Get-Date -Format o
+cmake --build build-output --config $CONFIG -- /m
+if ($LastExitCode) {
+    throw "cmake for 'all' target failed with exit code $LastExitCode"
+}
+
+Get-Date -Format o
+cd build-output
+ctest --output-on-failure -C $CONFIG -j 4
+if ($LastExitCode) {
+    throw "ctest failed with exit code $LastExitCode"
+}
