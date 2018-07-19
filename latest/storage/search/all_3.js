7385d5b61e2ae145739c80b4e8854ad04f2eaab5
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-19 22:27:15 +0000

    Automatically generated documentation

--- latest/storage/search/all_3.js
+++ latest/storage/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['delete_5fmetadata',['delete_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1ObjectMetadata.html#a1b347ecc84286d025f1048d24b60daed',1,'google::cloud::storage::v::ObjectMetadata']]],
-  ['deleteobject',['DeleteObject',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#a756c436e78cb97d877e22b03a94555c7',1,'google::cloud::storage::v::Client']]]
+  ['deleteobject',['DeleteObject',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#a756c436e78cb97d877e22b03a94555c7',1,'google::cloud::storage::v::Client']]],
+  ['deleteobjectacl',['DeleteObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#ac2e23aa1e95c0bdf5d55fbf378618b3b',1,'google::cloud::storage::v::Client']]]
 ];
