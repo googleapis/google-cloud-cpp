325b90dac18195371bac0fb2bc576fdff8a0ec2f
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-23 20:56:32 +0000

    Automatically generated documentation

--- latest/storage/search/functions_5.js
+++ latest/storage/search/functions_5.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['generation',['generation',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1ObjectAccessControl.html#a85fb24e051a61c6609e2c1b98f2486f3',1,'google::cloud::storage::v::ObjectAccessControl::generation()'],['../classgoogle_1_1cloud_1_1storage_1_1v_1_1ObjectMetadata.html#a1df10ae8462bf30817c9adb289b8edb8',1,'google::cloud::storage::v::ObjectMetadata::generation()']]],
   ['getbucketmetadata',['GetBucketMetadata',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#a600b8a5bcb7807d3c3691a820dd25d63',1,'google::cloud::storage::v::Client']]],
+  ['getobjectacl',['GetObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#a838dddf31eea5c097e7d0b274c9184be',1,'google::cloud::storage::v::Client']]],
   ['getobjectmetadata',['GetObjectMetadata',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#a7253cda31bbaf2921bfe5c4a8611ddbf',1,'google::cloud::storage::v::Client']]],
   ['googledefaultcredentials',['GoogleDefaultCredentials',['../namespacegoogle_1_1cloud_1_1storage_1_1v.html#a8481d2d3c4ead81c0f70d722b652a4d8',1,'google::cloud::storage::v']]]
 ];
