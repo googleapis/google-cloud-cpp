67b0f5a66086cf989cf6db66dd609b7203d69d7c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-25 01:44:11 +0000

    Automatically generated documentation

--- latest/storage/search/all_11.js
+++ latest/storage/search/all_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['underflow',['underflow',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1ObjectReadStreamBuf.html#afff0dda852a834a6ca7b3d4747f68a23',1,'google::cloud::storage::v::ObjectReadStreamBuf']]],
+  ['updateobjectacl',['UpdateObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1Client.html#afd62811bce2eba1bbb5fff69b1d2367d',1,'google::cloud::storage::v::Client']]],
   ['uploadtype',['UploadType',['../structgoogle_1_1cloud_1_1storage_1_1v_1_1UploadType.html',1,'google::cloud::storage::v']]],
   ['upsert_5fmetadata',['upsert_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v_1_1ObjectMetadata.html#acb878b7e1637d67d893874272122d718',1,'google::cloud::storage::v::ObjectMetadata']]],
   ['userproject',['UserProject',['../structgoogle_1_1cloud_1_1storage_1_1v_1_1UserProject.html',1,'google::cloud::storage::v']]]
