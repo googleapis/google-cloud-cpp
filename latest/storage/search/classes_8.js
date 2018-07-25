e369cb2266f77b9e36c489bcc83b12bbd06f788b
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-25 22:03:55 +0000

    Automatically generated documentation

--- latest/storage/search/classes_8.js
+++ latest/storage/search/classes_8.js
@@ -3,5 +3,6 @@ var searchData=
   ['objectaccesscontrol',['ObjectAccessControl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectAccessControl.html',1,'google::cloud::storage::v0']]],
   ['objectmetadata',['ObjectMetadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html',1,'google::cloud::storage::v0']]],
   ['objectreadstream',['ObjectReadStream',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectReadStream.html',1,'google::cloud::storage::v0']]],
-  ['objectreadstreambuf',['ObjectReadStreamBuf',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectReadStreamBuf.html',1,'google::cloud::storage::v0']]]
+  ['objectreadstreambuf',['ObjectReadStreamBuf',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectReadStreamBuf.html',1,'google::cloud::storage::v0']]],
+  ['objectwritestream',['ObjectWriteStream',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectWriteStream.html',1,'google::cloud::storage::v0']]]
 ];
