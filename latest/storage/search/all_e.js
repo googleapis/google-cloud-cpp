0f9399e666c6ede82fe80d96cf0b1fe7c2e3b490
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-30 20:25:16 +0000

    Automatically generated documentation

--- latest/storage/search/all_e.js
+++ latest/storage/search/all_e.js
@@ -4,6 +4,5 @@ var searchData=
   ['requester_5fpays',['requester_pays',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketBilling.html#ac118ae6b3b540d0eaafa1a46b37c875c',1,'google::cloud::storage::v0::BucketBilling']]],
   ['response_5fheader',['response_header',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1CorsEntry.html#a2177e95854c311ffc4f34ae74be279b8',1,'google::cloud::storage::v0::CorsEntry']]],
   ['retry_5fpolicy_2eh',['retry_policy.h',['../retry__policy_8h.html',1,'']]],
-  ['retrypolicy',['RetryPolicy',['../namespacegoogle_1_1cloud_1_1storage_1_1v0.html#a89b2c258bf4be827c64887cd2fd59c92',1,'google::cloud::storage::v0']]],
-  ['role',['role',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketAccessControl.html#a7f84a580740e9ddc570772cedfaba8a3',1,'google::cloud::storage::v0::BucketAccessControl::role()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectAccessControl.html#a9fbce1a4c27b156cd526c64d6771f8a9',1,'google::cloud::storage::v0::ObjectAccessControl::role()']]]
+  ['retrypolicy',['RetryPolicy',['../namespacegoogle_1_1cloud_1_1storage_1_1v0.html#a89b2c258bf4be827c64887cd2fd59c92',1,'google::cloud::storage::v0']]]
 ];
--- latest/storage/search/functions_d.js
+++ latest/storage/search/functions_d.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['read',['Read',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#ad002d9cca6eedaf51ad6cd3b20770dcc',1,'google::cloud::storage::v0::Client']]],
-  ['role',['role',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketAccessControl.html#a7f84a580740e9ddc570772cedfaba8a3',1,'google::cloud::storage::v0::BucketAccessControl::role()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectAccessControl.html#a9fbce1a4c27b156cd526c64d6771f8a9',1,'google::cloud::storage::v0::ObjectAccessControl::role()']]]
+  ['read',['Read',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#ad002d9cca6eedaf51ad6cd3b20770dcc',1,'google::cloud::storage::v0::Client']]]
 ];
