5f01135eedde94e3d3121404f0c965d6f05b2b56
Carlos O'Ryan <coryan@google.com>
2018-07-30 12:32:52 -0400

    Implement the Quickstart example.

---
+++ google/cloud/bigtable/examples/bigtable_quickstart.cc
@@ -0,0 +1,59 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! [all code] [START bigtable_quickstart]
+
+#include "google/cloud/bigtable/table.h"
+
+int main(int argc, char* argv[]) try {
+  if (argc != 4) {
+    std::string const cmd = argv[0];
+    auto last_slash = std::string(cmd).find_last_of('/');
+    std::cerr << "Usage: " << cmd.substr(last_slash + 1)
+              << " <project_id> <instance_id> <table_id>" << std::endl;
+    return 1;
+  }
+
+  std::string const project_id = argv[1];
+  std::string const instance_id = argv[2];
+  std::string const table_id = argv[3];
+
+  google::cloud::bigtable::Table table(
+      google::cloud::bigtable::CreateDefaultDataClient(
+          project_id, instance_id, google::cloud::bigtable::ClientOptions()),
+      table_id);
+
+  std::string row_key = "r1";
+  std::string column_family = "cf1";
+
+  std::cout << "Getting a single row by row key:" << std::flush;
+  auto result = table.ReadRow(
+      row_key, google::cloud::bigtable::Filter::FamilyRegex(column_family));
+  if (not result.first) {
+    std::cout << "Cannot find row " << row_key << " in the table: " << table_id
+              << std::endl;
+    return 0;
+  }
+
+  auto const& cell = result.second.cells().front();
+  std::cout << cell.family_name() << ":" << cell.column_qualifier() << "    @ "
+            << cell.timestamp().count() << "us\n"
+            << '"' << cell.value() << '"' << std::endl;
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard C++ exception raised: " << ex.what() << std::endl;
+  return 1;
+}
+//! [all code] [END bigtable_quickstart]
