20eefa41b43a0bdd877859cade019170ab3f8907
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-07-31 22:04:22 +0000

    Automatically generated documentation

--- latest/storage/search/functions_6.js
+++ latest/storage/search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['has_5fcustomer_5fencryption',['has_customer_encryption',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#aa0724175f83a69b8b0df6cd121e85c7f',1,'google::cloud::storage::v0::ObjectMetadata']]],
   ['has_5flabel',['has_label',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#a8bd70ae38d1dcfbbe172d60d165f9cd7',1,'google::cloud::storage::v0::BucketMetadata']]],
   ['has_5fmetadata',['has_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a41fcb258ecc7e898420fcdaf5cdd3a80',1,'google::cloud::storage::v0::ObjectMetadata']]],
   ['has_5fvalue',['has_value',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1WellKnownParameter.html#af434a9af42408ea70103f8c17d93dcf4',1,'google::cloud::storage::v0::WellKnownParameter']]]
