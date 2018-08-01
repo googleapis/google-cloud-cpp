951a70283d6d84c08185c71d5071eeb91842bc85
Carlos O'Ryan <coryan@google.com>
2018-08-01 17:29:57 -0400

    Copy latest to 0.2.0.

---
+++ 0.2.0/bigtable/search/files_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['data_5fclient_2ecc',['data_client.cc',['../data__client_8cc.html',1,'']]],
+  ['data_5fclient_2eh',['data_client.h',['../data__client_8h.html',1,'']]]
+];
---
+++ 0.2.0/storage/search/files_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['retry_5fpolicy_2eh',['retry_policy.h',['../retry__policy_8h.html',1,'']]]
+];
