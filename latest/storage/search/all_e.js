c0fe6316a6bdf636a777105f02c10120420aa092
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-08-01 04:33:17 +0000

    Automatically generated documentation

--- latest/storage/search/all_e.js
+++ latest/storage/search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['raw_5fclient',['raw_client',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a84569cd8e135373f6e299c3c146317f6',1,'google::cloud::storage::v0::Client']]],
   ['readobject',['ReadObject',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a511f352533a2bda918c37f05b02f1469',1,'google::cloud::storage::v0::Client']]],
   ['regional',['Regional',['../namespacegoogle_1_1cloud_1_1storage_1_1v0_1_1storage__class.html#a9109a55c653f9da886e4756b1025c31f',1,'google::cloud::storage::v0::storage_class']]],
   ['requester_5fpays',['requester_pays',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketBilling.html#ac118ae6b3b540d0eaafa1a46b37c875c',1,'google::cloud::storage::v0::BucketBilling']]],
--- latest/storage/search/classes_7.js
+++ latest/storage/search/classes_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['noretry',['NoRetry',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1Client_1_1NoRetry.html',1,'google::cloud::storage::v0::Client']]]
+  ['nodecorations',['NoDecorations',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1Client_1_1NoDecorations.html',1,'google::cloud::storage::v0::Client']]]
 ];
