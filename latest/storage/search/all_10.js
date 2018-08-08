db9bac6d57341d2399f39cc2031e908c85a25ffd
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-08-08 01:47:48 +0000

    Automatically generated documentation

--- latest/storage/search/all_10.js
+++ latest/storage/search/all_10.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['time_5fdeleted',['time_deleted',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a6941888b59bab3ba8ba476bba4bfebdc',1,'google::cloud::storage::v0::ObjectMetadata']]],
   ['time_5fstorage_5fclass_5fupdated',['time_storage_class_updated',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#ab4191088c6f9a5704bc13d54c2fc6a8e',1,'google::cloud::storage::v0::ObjectMetadata']]],
+  ['tojsonstring',['ToJsonString',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#af30bbfd04003ebc94a4779f31782f341',1,'google::cloud::storage::v0::BucketMetadata']]],
   ['type',['type',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1LifecycleRuleAction.html#ac10447994761d1e0460a9d1c7e75a606',1,'google::cloud::storage::v0::LifecycleRuleAction']]]
 ];
