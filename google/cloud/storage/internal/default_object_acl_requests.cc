ef80bda6f323a2c968baf0651c584b4eae55eb99
Carlos O'Ryan <coryan@users.noreply.github.com>
2018-08-09 17:49:00 -0400

    DefaultObjectAccessControls request objects. (#985)
    * DefaultObjectAccessControls request objects.
    
    These objects represent each of the of the DefaultObjectAccessControls
    APIs.
    
    * Address review comments.

---
+++ google/cloud/storage/internal/default_object_acl_requests.cc
@@ -0,0 +1,139 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/default_object_acl_requests.h"
+#include "google/cloud/storage/internal/nljson.h"
+#include <iostream>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace internal {
+std::ostream& operator<<(std::ostream& os,
+                         ListDefaultObjectAclRequest const& r) {
+  os << "ListDefaultObjectAclRequest={bucket_name=" << r.bucket_name();
+  r.DumpOptions(os, ", ");
+  return os << "}";
+}
+
+ListDefaultObjectAclResponse ListDefaultObjectAclResponse::FromHttpResponse(
+    HttpResponse&& response) {
+  ListDefaultObjectAclResponse result;
+  auto json = nl::json::parse(response.payload);
+  for (auto const& kv : json["items"].items()) {
+    result.items.emplace_back(ObjectAccessControl::ParseFromJson(kv.value()));
+  }
+
+  return result;
+}
+
+std::ostream& operator<<(std::ostream& os,
+                         ListDefaultObjectAclResponse const& r) {
+  os << "ListDefaultObjectAclResponse={items={";
+  char const* sep = "";
+  for (auto const& acl : r.items) {
+    os << sep << acl;
+    sep = ", ";
+  }
+  return os << "}}";
+}
+
+std::ostream& operator<<(std::ostream& os,
+                         GetDefaultObjectAclRequest const& r) {
+  os << "GetDefaultObjectAclRequest={bucket_name=" << r.bucket_name()
+     << ", entity=" << r.entity();
+  r.DumpOptions(os, ", ");
+  return os << "}";
+}
+
+std::ostream& operator<<(std::ostream& os,
+                         DeleteDefaultObjectAclRequest const& r) {
+  os << "GetDefaultObjectAclRequest={bucket_name=" << r.bucket_name()
+     << ", entity=" << r.entity();
+  r.DumpOptions(os, ", ");
+  return os << "}";
+}
+
+std::ostream& operator<<(std::ostream& os,
+                         CreateDefaultObjectAclRequest const& r) {
+  os << "CreateDefaultObjectAclRequest={bucket_name=" << r.bucket_name()
+     << ", entity=" << r.entity() << ", role=" << r.role();
+  r.DumpOptions(os, ", ");
+  return os << "}";
+}
+
+PatchDefaultObjectAclRequest::PatchDefaultObjectAclRequest(
+    std::string bucket, std::string entity, ObjectAccessControl const& original,
+    ObjectAccessControl const& new_acl)
+    : GenericDefaultObjectAclRequest(std::move(bucket), std::move(entity)) {
+  PatchBuilder build_patch;
+  build_patch.AddStringField("bucket", original.bucket(), new_acl.bucket());
+  build_patch.AddStringField("domain", original.domain(), new_acl.domain());
+  build_patch.AddStringField("email", original.email(), new_acl.email());
+  build_patch.AddStringField("entity", original.entity(), new_acl.entity());
+  build_patch.AddStringField("entityId", original.entity_id(),
+                             new_acl.entity_id());
+  build_patch.AddStringField("etag", original.etag(), new_acl.etag());
+  build_patch.AddIntField("generation", original.generation(),
+                          new_acl.generation());
+  build_patch.AddStringField("id", original.id(), new_acl.id());
+  build_patch.AddStringField("kind", original.kind(), new_acl.kind());
+  build_patch.AddStringField("object", original.object(), new_acl.object());
+
+  if (original.project_team() != new_acl.project_team()) {
+    auto empty = [](ProjectTeam const& p) {
+      return p.project_number.empty() and p.team.empty();
+    };
+    if (empty(new_acl.project_team())) {
+      if (not empty(original.project_team())) {
+        build_patch.RemoveField("projectTeam");
+      }
+    } else {
+      PatchBuilder project_team_patch;
+      project_team_patch
+          .AddStringField("project_number",
+                          original.project_team().project_number,
+                          new_acl.project_team().project_number)
+          .AddStringField("team", original.project_team().team,
+                          new_acl.project_team().team);
+      build_patch.AddSubPatch("projectTeam", project_team_patch);
+    }
+  }
+
+  build_patch.AddStringField("role", original.role(), new_acl.role());
+  build_patch.AddStringField("selfLink", original.self_link(),
+                             new_acl.self_link());
+  payload_ = build_patch.ToString();
+}
+
+PatchDefaultObjectAclRequest::PatchDefaultObjectAclRequest(
+    std::string bucket, std::string entity,
+    ObjectAccessControlPatchBuilder const& patch)
+    : GenericDefaultObjectAclRequest(std::move(bucket), std::move(entity)),
+      payload_(patch.BuildPatch()) {}
+
+std::ostream& operator<<(std::ostream& os,
+                         PatchDefaultObjectAclRequest const& r) {
+  os << "DefaultObjectAclRequest={bucket_name=" << r.bucket_name()
+     << ", entity=" << r.entity();
+  r.DumpOptions(os, ", ");
+  return os << ", payload=" << r.payload() << "}";
+}
+
+}  // namespace internal
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/storage/internal/default_object_acl_requests.h
@@ -0,0 +1,176 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_DEFAULT_OBJECT_ACL_REQUESTS_H_
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_DEFAULT_OBJECT_ACL_REQUESTS_H_
+
+#include "google/cloud/storage/internal/generic_request.h"
+#include "google/cloud/storage/internal/http_response.h"
+#include "google/cloud/storage/object_access_control.h"
+#include "google/cloud/storage/well_known_headers.h"
+#include "google/cloud/storage/well_known_parameters.h"
+#include <iosfwd>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace internal {
+/// Represents a request to call the `DefaultObjectAccessControls: list` API.
+class ListDefaultObjectAclRequest
+    : public GenericRequest<ListDefaultObjectAclRequest, UserProject> {
+ public:
+  ListDefaultObjectAclRequest() : GenericRequest() {}
+  explicit ListDefaultObjectAclRequest(std::string bucket)
+      : bucket_name_(std::move(bucket)) {}
+
+  std::string const& bucket_name() const { return bucket_name_; }
+
+ private:
+  std::string bucket_name_;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         ListDefaultObjectAclRequest const& r);
+
+/// Represents a response to the `DefaultObjectAccessControls: list` API.
+struct ListDefaultObjectAclResponse {
+  static ListDefaultObjectAclResponse FromHttpResponse(HttpResponse&& response);
+
+  std::vector<ObjectAccessControl> items;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         ListDefaultObjectAclResponse const& r);
+
+/**
+ * Represents common attributes to multiple `DefaultObjectAccessControls`
+ * request types.
+ *
+ * The classes to represent requests for the `DefaultObjectAccessControls: get`,
+ * `create`, `delete`, `patch`, and `update` APIs have a lot of commonality.
+ * This template class refactors that code.
+ */
+template <typename Derived>
+class GenericDefaultObjectAclRequest
+    : public GenericRequest<Derived, IfMatchEtag, IfNoneMatchEtag,
+                            UserProject> {
+ public:
+  GenericDefaultObjectAclRequest() = default;
+  GenericDefaultObjectAclRequest(std::string bucket, std::string entity)
+      : bucket_name_(std::move(bucket)), entity_(std::move(entity)) {}
+
+  std::string const& bucket_name() const { return bucket_name_; }
+  std::string const& entity() const { return entity_; }
+
+ private:
+  std::string bucket_name_;
+  std::string entity_;
+};
+
+/**
+ * Represents a request to call the `DefaultObjectAccessControls: get` API.
+ */
+class GetDefaultObjectAclRequest
+    : public GenericDefaultObjectAclRequest<GetDefaultObjectAclRequest> {
+  using GenericDefaultObjectAclRequest::GenericDefaultObjectAclRequest;
+};
+
+std::ostream& operator<<(std::ostream& os, GetDefaultObjectAclRequest const& r);
+
+/**
+ * Represents a request to call the `DefaultObjectAccessControls: delete` API.
+ */
+class DeleteDefaultObjectAclRequest
+    : public GenericDefaultObjectAclRequest<DeleteDefaultObjectAclRequest> {
+  using GenericDefaultObjectAclRequest::GenericDefaultObjectAclRequest;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         DeleteDefaultObjectAclRequest const& r);
+
+/**
+ * Represents a request to call the `DefaultObjectAccessControls: insert` API.
+ */
+class CreateDefaultObjectAclRequest
+    : public GenericDefaultObjectAclRequest<CreateDefaultObjectAclRequest> {
+ public:
+  CreateDefaultObjectAclRequest() = default;
+
+  explicit CreateDefaultObjectAclRequest(std::string bucket, std::string entity,
+                                         std::string role)
+      : GenericDefaultObjectAclRequest<CreateDefaultObjectAclRequest>(
+            std::move(bucket), std::move(entity)),
+        role_(std::move(role)) {}
+
+  std::string const& role() const { return role_; }
+
+ private:
+  std::string role_;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         CreateDefaultObjectAclRequest const& r);
+
+/**
+ * Represents a request to call the `DefaultObjectAccessControls: update` API.
+ */
+class UpdateDefaultObjectAclRequest
+    : public GenericDefaultObjectAclRequest<UpdateDefaultObjectAclRequest> {
+ public:
+  UpdateDefaultObjectAclRequest() = default;
+
+  explicit UpdateDefaultObjectAclRequest(std::string bucket, std::string entity,
+                                         std::string role)
+      : GenericDefaultObjectAclRequest<UpdateDefaultObjectAclRequest>(
+            std::move(bucket), std::move(entity)),
+        role_(std::move(role)) {}
+
+  std::string const& role() const { return role_; }
+
+ private:
+  std::string role_;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         UpdateDefaultObjectAclRequest const& r);
+
+/**
+ * Represents a request to call the `DefaultObjectAccessControls: patch` API.
+ */
+class PatchDefaultObjectAclRequest
+    : public GenericDefaultObjectAclRequest<PatchDefaultObjectAclRequest> {
+ public:
+  PatchDefaultObjectAclRequest(std::string bucket, std::string entity,
+                               ObjectAccessControl const& original,
+                               ObjectAccessControl const& new_acl);
+  PatchDefaultObjectAclRequest(std::string bucket, std::string entity,
+                               ObjectAccessControlPatchBuilder const& patch);
+
+  std::string const& payload() const { return payload_; }
+
+ private:
+  std::string payload_;
+};
+
+std::ostream& operator<<(std::ostream& os,
+                         PatchDefaultObjectAclRequest const& r);
+
+}  // namespace internal
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_DEFAULT_OBJECT_ACL_REQUESTS_H_
