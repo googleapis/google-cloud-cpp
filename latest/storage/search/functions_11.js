4821754929d5f3eacc53c606f0c225baff161ce9
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-08-16 15:07:38 +0000

    Automatically generated documentation

--- latest/storage/search/functions_11.js
+++ latest/storage/search/functions_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['updatebucket',['UpdateBucket',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a7909a3ddbb72678ca9d9eb159234a16c',1,'google::cloud::storage::v0::Client']]],
+  ['updatebucketacl',['UpdateBucketAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#af181aaa4cf80882abee9fee0749837f5',1,'google::cloud::storage::v0::Client']]],
   ['updateobjectacl',['UpdateObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#ab620fb3cd2c62c990f8191449e1478b0',1,'google::cloud::storage::v0::Client']]],
   ['upsert_5fmetadata',['upsert_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a5d90005732160ea2fe0fd0477be1e554',1,'google::cloud::storage::v0::ObjectMetadata']]]
 ];
