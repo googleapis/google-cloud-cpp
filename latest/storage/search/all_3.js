34baeeccaca2efa6ec5b6be6de6c79a4c999adab
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-08-17 04:00:08 +0000

    Automatically generated documentation

--- latest/storage/search/all_3.js
+++ latest/storage/search/all_3.js
@@ -8,6 +8,7 @@ var searchData=
   ['delete_5frole',['delete_role',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketAccessControlPatchBuilder.html#a27ab1ea1f5fe98cd19f8728b958026e2',1,'google::cloud::storage::v0::BucketAccessControlPatchBuilder::delete_role()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectAccessControlPatchBuilder.html#a6431defd9dc92918ad8d673c95af18cf',1,'google::cloud::storage::v0::ObjectAccessControlPatchBuilder::delete_role()']]],
   ['deletebucket',['DeleteBucket',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a04ad0755e38a8b02538d719d0b2cd2ef',1,'google::cloud::storage::v0::Client']]],
   ['deletebucketacl',['DeleteBucketAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a0b22b96b696393ad27a913b1c374afc0',1,'google::cloud::storage::v0::Client']]],
+  ['deletedefaultobjectacl',['DeleteDefaultObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a7bc4c14d5528031b0bca69fe15b7b99b',1,'google::cloud::storage::v0::Client']]],
   ['deleteobject',['DeleteObject',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a0c9ed0e219ba99dce6024e326d7c9708',1,'google::cloud::storage::v0::Client']]],
   ['deleteobjectacl',['DeleteObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#adc39fbc4df076ec9f321a61bd0102371',1,'google::cloud::storage::v0::Client']]],
   ['disable_5fversioning',['disable_versioning',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#a34b65c5ba1bfad8408aa145f96b78ec0',1,'google::cloud::storage::v0::BucketMetadata']]],
