6e9b9edc5f58a5671b716c2f953c6688f67dfdce
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-08-22 23:31:09 +0000

    Automatically generated documentation

--- latest/storage/search/all_11.js
+++ latest/storage/search/all_11.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['updatebucket',['UpdateBucket',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a7909a3ddbb72678ca9d9eb159234a16c',1,'google::cloud::storage::v0::Client']]],
   ['updatebucketacl',['UpdateBucketAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#af181aaa4cf80882abee9fee0749837f5',1,'google::cloud::storage::v0::Client']]],
+  ['updatedefaultobjectacl',['UpdateDefaultObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a86b2abd5f3b2ded4e05309768891c709',1,'google::cloud::storage::v0::Client']]],
   ['updateobjectacl',['UpdateObjectAcl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#ab620fb3cd2c62c990f8191449e1478b0',1,'google::cloud::storage::v0::Client']]],
   ['uploadtype',['UploadType',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1UploadType.html',1,'google::cloud::storage::v0']]],
   ['upsert_5fmetadata',['upsert_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a5d90005732160ea2fe0fd0477be1e554',1,'google::cloud::storage::v0::ObjectMetadata']]],
