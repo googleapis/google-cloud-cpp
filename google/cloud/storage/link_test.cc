3ac27c00bd4217445fb4d23a66d62b6eba35387d
Carlos O'Ryan <coryan@google.com>
2018-08-28 00:07:30 -0400

    Verify OpenSSL versions match.
    This is part of the changes to support multi-threaded applications with
    Google Cloud Storage. OpenSSL 1.0.2 (which is still widely deployed)
    does not perform any internal locking. Instead, we must provide
    callbacks to lock and unlock the OpenSSL data structures. Because we use
    OpenSSL via libcurl, we must perform this configuration in the same
    version of OpenSSL that libcurl uses. Therefore, linking different
    versions of OpenSSL is a bad idea.

--- google/cloud/storage/link_test.cc
+++ google/cloud/storage/link_test.cc
@@ -13,14 +13,51 @@
 // limitations under the License.
 
 #include "google/cloud/storage/version.h"
-#include <gtest/gtest.h>
+#include <curl/curl.h>
+#include <gmock/gmock.h>
+#include <openssl/crypto.h>
+#include <openssl/opensslv.h>
 
-namespace storage = google::cloud::storage;
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace {
+using ::testing::StartsWith;
 
 /// @test A trivial test for the Google Cloud Storage C++ Client
 TEST(LinkTest, Simple) {
-  EXPECT_FALSE(storage::version_string().empty());
-  EXPECT_EQ(STORAGE_CLIENT_VERSION_MAJOR, storage::version_major());
-  EXPECT_EQ(STORAGE_CLIENT_VERSION_MINOR, storage::version_minor());
-  EXPECT_EQ(STORAGE_CLIENT_VERSION_PATCH, storage::version_patch());
+  EXPECT_FALSE(version_string().empty());
+  EXPECT_EQ(STORAGE_CLIENT_VERSION_MAJOR, version_major());
+  EXPECT_EQ(STORAGE_CLIENT_VERSION_MINOR, version_minor());
+  EXPECT_EQ(STORAGE_CLIENT_VERSION_PATCH, version_patch());
 }
+
+/// @test Verify that the OpenSSL and CURL libraries linked are compatible.
+TEST(LinkTest, CurlVsOpenSSL) {
+  auto v = curl_version_info(CURLVERSION_NOW);
+  std::string curl_ssl = v->ssl_version;
+  std::string expected_prefix = curl_ssl;
+  std::transform(expected_prefix.begin(), expected_prefix.end(),
+                 expected_prefix.begin(),
+                 [](char x) { return x == '/' ? ' ' : x; });
+#ifdef OPENSSL_VERSION
+  std::string openssl_v = OpenSSL_version(OPENSSL_VERSION);
+#else
+  std::string openssl_v = SSLeay_version(SSLEAY_VERSION);
+#endif  // OPENSSL_VERSION
+  EXPECT_THAT(openssl_v, StartsWith(expected_prefix))
+      << "Mismatched versions of OpenSSL linked in libcurl vs. the version"
+      << " linked by the Google Cloud Storage C++ library.\n"
+      << "libcurl is linked against " << curl_ssl
+      << "\nwhile the google cloud storage library links against " << openssl_v
+      << "\nMismatched versions are not supported.  The Google Cloud Storage"
+      << "\nC++ library needs to configure the OpenSSL library used by libcurl"
+      << "\nand this is not possible if you link different versions.";
+}
+
+}  // namespace
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
