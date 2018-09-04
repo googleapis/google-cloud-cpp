59688194a5e9ec57380bebb953ecc1994b3097b0
Carlos O'Ryan <coryan@google.com>
2018-09-04 11:18:47 -0400

    Implement request types for Notifications.
    The Notifications resource type needs request, just like Objects and
    Buckets do. This is part of the work for #1033, #1034, #1035, and #1036.

---
+++ google/cloud/storage/internal/notification_requests_test.cc
@@ -0,0 +1,116 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/notification_requests.h"
+#include "google/cloud/storage/notification_payload_format.h"
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace internal {
+namespace {
+using ::testing::HasSubstr;
+
+TEST(NotificationRequestTest, List) {
+  ListNotificationsRequest request("my-bucket");
+  request.set_multiple_options(UserProject("project-for-billing"));
+  EXPECT_EQ("my-bucket", request.bucket_name());
+
+  std::ostringstream os;
+  os << request;
+  auto str = os.str();
+  EXPECT_THAT(str, HasSubstr("userProject=project-for-billing"));
+  EXPECT_THAT(str, HasSubstr("my-bucket"));
+}
+
+TEST(NotificationRequestTest, ListResponse) {
+  std::string text = R"""({
+      "items": [{
+          "id": "test-notification-id-1",
+          "topic": "test-topic-1"
+      }, {
+          "id": "test-notification-id-2",
+          "topic": "test-topic-2"
+      }]})""";
+
+  auto actual =
+      ListNotificationsResponse::FromHttpResponse(HttpResponse{200, text, {}});
+  ASSERT_EQ(2UL, actual.items.size());
+  EXPECT_EQ("test-notification-id-1", actual.items.at(0).id());
+  EXPECT_EQ("test-topic-1", actual.items.at(0).topic());
+  EXPECT_EQ("test-notification-id-2", actual.items.at(1).id());
+  EXPECT_EQ("test-topic-2", actual.items.at(1).topic());
+
+  std::ostringstream os;
+  os << actual;
+  auto str = os.str();
+  EXPECT_THAT(str, HasSubstr("id=test-notification-id-1"));
+  EXPECT_THAT(str, HasSubstr("id=test-notification-id-2"));
+  EXPECT_THAT(str, HasSubstr("ListNotificationResponse={"));
+  EXPECT_THAT(str, HasSubstr("NotificationMetadata={"));
+}
+
+TEST(CreateNotificationRequestTest, Create) {
+  NotificationMetadata notification =
+      NotificationMetadata()
+          .set_topic("test-topic-1")
+          .set_payload_format(payload_format::JsonApiV1())
+          .set_object_name_prefix("test-object-prefix-");
+  CreateNotificationRequest request("my-bucket", notification);
+  request.set_multiple_options(UserProject("project-for-billing"));
+  EXPECT_EQ("my-bucket", request.bucket_name());
+  EXPECT_EQ(notification.JsonPayloadForInsert(), request.json_payload());
+
+  std::ostringstream os;
+  os << request;
+  auto str = os.str();
+  EXPECT_THAT(str, HasSubstr("userProject=project-for-billing"));
+  EXPECT_THAT(str, HasSubstr("my-bucket"));
+  EXPECT_THAT(str, HasSubstr(notification.JsonPayloadForInsert()));
+}
+
+TEST(NotificationRequestTest, Get) {
+  GetNotificationRequest request("my-bucket", "test-notification-id");
+  request.set_multiple_options(UserProject("my-project"));
+  EXPECT_EQ("my-bucket", request.bucket_name());
+  EXPECT_EQ("test-notification-id", request.notification_id());
+  std::ostringstream os;
+  os << request;
+  auto str = os.str();
+  EXPECT_THAT(str, HasSubstr("userProject=my-project"));
+  EXPECT_THAT(str, HasSubstr("my-bucket"));
+  EXPECT_THAT(str, HasSubstr("test-notification-id"));
+}
+
+TEST(NotificationRequestTest, Delete) {
+  DeleteNotificationRequest request("my-bucket", "test-notification-id");
+  request.set_multiple_options(UserProject("my-project"));
+  EXPECT_EQ("my-bucket", request.bucket_name());
+  EXPECT_EQ("test-notification-id", request.notification_id());
+  std::ostringstream os;
+  os << request;
+  auto str = os.str();
+  EXPECT_THAT(str, HasSubstr("userProject=my-project"));
+  EXPECT_THAT(str, HasSubstr("my-bucket"));
+  EXPECT_THAT(str, HasSubstr("test-notification-id"));
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
