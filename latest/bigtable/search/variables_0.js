8a02f5c13c0ae7cd24b0edffd64909a3fb724241
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-09-11 19:47:29 +0000

    Automatically generated documentation

--- latest/bigtable/search/variables_0.js
+++ latest/bigtable/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['context',['context',['../structgoogle_1_1cloud_1_1bigtable_1_1v0_1_1AsyncUnaryRpcResult.html#ab924f03cf0b75719561eba06f85658f7',1,'google::cloud::bigtable::v0::AsyncUnaryRpcResult']]],
   ['creating',['CREATING',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceUpdateConfig.html#a65fac7a533ba1f8d5bdeea58e8d7f730',1,'google::cloud::bigtable::v0::InstanceUpdateConfig']]]
 ];
--- latest/bigtable/search/variables_8.js
+++ latest/bigtable/search/variables_8.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['ssd',['SSD',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1ClusterConfig.html#a51a893ac1b4db32695039077f95a0279',1,'google::cloud::bigtable::v0::ClusterConfig']]],
   ['state_5fnot_5fknown',['STATE_NOT_KNOWN',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceUpdateConfig.html#af30e57a558f553ed99189aa2d0b0aeb0',1,'google::cloud::bigtable::v0::InstanceUpdateConfig']]],
+  ['status',['status',['../structgoogle_1_1cloud_1_1bigtable_1_1v0_1_1AsyncUnaryRpcResult.html#a2daf9e5c8b966ed03570d1432124ab50',1,'google::cloud::bigtable::v0::AsyncUnaryRpcResult']]],
   ['storage_5ftype_5funspecified',['STORAGE_TYPE_UNSPECIFIED',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1ClusterConfig.html#a38f0aa33c38e82aa9234a47ce5e8c9a3',1,'google::cloud::bigtable::v0::ClusterConfig']]]
 ];
