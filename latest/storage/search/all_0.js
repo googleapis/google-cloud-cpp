0b31b9f230289ca1a96adea5fd9d793ec057fd9f
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-09-28 04:03:50 +0000

    Automatically generated documentation

--- latest/storage/search/all_0.js
+++ latest/storage/search/all_0.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['acl',['acl',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#aed4959f9a931b84bb43df40728c80e88',1,'google::cloud::storage::v0::BucketMetadata::acl()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a45d2ede154740321b656889b2d741a79',1,'google::cloud::storage::v0::ObjectMetadata::acl()']]],
   ['action',['action',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1LifecycleRule.html#a8f3e9e8e2b42ac6613cb207818cfddcf',1,'google::cloud::storage::v0::LifecycleRule']]],
-  ['add_5fuser_5fagent_5fprefx',['add_user_agent_prefx',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ClientOptions.html#a37526865451ab740dc053ab8d107d0d0',1,'google::cloud::storage::v0::ClientOptions']]],
   ['age',['age',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1LifecycleRuleCondition.html#aa2d46c6e796b01c6d33298db09927ec2',1,'google::cloud::storage::v0::LifecycleRuleCondition']]],
   ['algorithm',['algorithm',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1EncryptionKeyData.html#a0b5c8e4f9176cade86754b04885dfa39',1,'google::cloud::storage::v0::EncryptionKeyData']]],
   ['append_5fevent_5ftype',['append_event_type',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1NotificationMetadata.html#aab44e2f3841fcbf42670de1261ab9fc4',1,'google::cloud::storage::v0::NotificationMetadata']]],
--- latest/storage/search/functions_13.js
+++ latest/storage/search/functions_13.js
@@ -8,6 +8,5 @@ var searchData=
   ['upload_5fbuffer_5fsize',['upload_buffer_size',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ClientOptions.html#a6c11783bcb3579f6c7453aaa9151893d',1,'google::cloud::storage::v0::ClientOptions']]],
   ['upsert_5fcustom_5fattributes',['upsert_custom_attributes',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1NotificationMetadata.html#a1ccd992526be854ce4ce0ceb15da6f41',1,'google::cloud::storage::v0::NotificationMetadata']]],
   ['upsert_5flabel',['upsert_label',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#a5099c7a6e15c1d2985fee67929af5257',1,'google::cloud::storage::v0::BucketMetadata']]],
-  ['upsert_5fmetadata',['upsert_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a5d90005732160ea2fe0fd0477be1e554',1,'google::cloud::storage::v0::ObjectMetadata']]],
-  ['user_5fagent_5fprefix',['user_agent_prefix',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ClientOptions.html#a3ccea8345336967ca6914a079ce493c4',1,'google::cloud::storage::v0::ClientOptions']]]
+  ['upsert_5fmetadata',['upsert_metadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a5d90005732160ea2fe0fd0477be1e554',1,'google::cloud::storage::v0::ObjectMetadata']]]
 ];
