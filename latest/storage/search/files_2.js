34f132f49149f8f82313a1051cfb1f3c8a172039
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-10-03 08:41:37 +0000

    Automatically generated documentation

--- latest/storage/search/files_2.js
+++ latest/storage/search/files_2.js
@@ -5,6 +5,5 @@ var searchData=
   ['client_5foptions_2ecc',['client_options.cc',['../client__options_8cc.html',1,'']]],
   ['client_5foptions_2eh',['client_options.h',['../client__options_8h.html',1,'']]],
   ['credential_5fconstants_2eh',['credential_constants.h',['../credential__constants_8h.html',1,'']]],
-  ['credentials_2ecc',['credentials.cc',['../credentials_8cc.html',1,'']]],
   ['credentials_2eh',['credentials.h',['../credentials_8h.html',1,'']]]
 ];
