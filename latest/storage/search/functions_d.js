98991f4b9b6aa711585fd4fed82748d8c92c6506
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-10-10 03:53:15 +0000

    Automatically generated documentation

--- latest/storage/search/functions_d.js
+++ latest/storage/search/functions_d.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['name',['name',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1MD5HashValue.html#aa0e5fbf8899f0d4670aca31a90bb9f86',1,'google::cloud::storage::v0::MD5HashValue']]],
+  ['name',['name',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1MD5HashValue.html#aa0e5fbf8899f0d4670aca31a90bb9f86',1,'google::cloud::storage::v0::MD5HashValue::name()'],['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1DisableMD5Hash.html#a57eaeb9406ee672b7cc04af7a41f2e23',1,'google::cloud::storage::v0::DisableMD5Hash::name()']]],
   ['nearline',['Nearline',['../namespacegoogle_1_1cloud_1_1storage_1_1v0_1_1storage__class.html#a7db3e6025fede1488265323447a3669c',1,'google::cloud::storage::v0::storage_class']]],
   ['noacl',['NoAcl',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1Projection.html#a27822a9fea81dd08275f00c5f4fb2a4c',1,'google::cloud::storage::v0::Projection']]],
   ['none',['None',['../namespacegoogle_1_1cloud_1_1storage_1_1v0_1_1payload__format.html#aa74d2bcc3c1b8593f95ab554727426f8',1,'google::cloud::storage::v0::payload_format']]],
