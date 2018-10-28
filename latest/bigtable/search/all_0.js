ac6502ecaae6711e3ae54cba73aea17be38bdd34
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-10-28 14:05:32 +0000

    Automatically generated documentation

--- latest/bigtable/search/all_0.js
+++ latest/bigtable/search/all_0.js
@@ -23,6 +23,7 @@ var searchData=
   ['asynccreatetable',['AsyncCreateTable',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1AdminClient.html#aabe70dc2b9855c2faf82a4235e4d910f',1,'google::cloud::bigtable::v0::AdminClient']]],
   ['asyncdeletecluster',['AsyncDeleteCluster',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceAdminClient.html#abadc27ffe28fb29f3a49982bc578c50f',1,'google::cloud::bigtable::v0::InstanceAdminClient']]],
   ['asyncdeleteinstance',['AsyncDeleteInstance',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceAdminClient.html#a68625b16fcf7b5f8a43c0c8bf77c3430',1,'google::cloud::bigtable::v0::InstanceAdminClient']]],
+  ['asyncdeletesnapshot',['AsyncDeleteSnapshot',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1AdminClient.html#a6260afd04046713a6ef99c3601a2bcf4',1,'google::cloud::bigtable::v0::AdminClient']]],
   ['asyncdroprowrange',['AsyncDropRowRange',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1AdminClient.html#abcd1591d57becb5880ac95ecfc360ad4',1,'google::cloud::bigtable::v0::AdminClient']]],
   ['asyncgetcluster',['AsyncGetCluster',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceAdminClient.html#ae9e878f90c0aacede206e156792f211f',1,'google::cloud::bigtable::v0::InstanceAdminClient']]],
   ['asyncgetinstance',['AsyncGetInstance',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1InstanceAdminClient.html#a292d8c8d42eb639cf8e039703f36536a',1,'google::cloud::bigtable::v0::InstanceAdminClient']]],
