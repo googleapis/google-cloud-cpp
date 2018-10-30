d6a7565533755430ba3d3a969acd9cb90b59bb34
Carlos O'Ryan <coryan@google.com>
2018-10-30 11:49:05 -0400

    Define interface for storage IdempotencyPolicy.
    This is part of the work for #714. This PR introduces no changes in
    behavior, it just adds a new abstract class to define the idempotency
    policy interface, and a trivial implementation that treats all requests
    as idempotent.
    
    This is a large PR, but most of the code is trivial, I hope this is
    Okay.

---
+++ google/cloud/storage/idempotency_policy.cc
@@ -0,0 +1,210 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/idempotency_policy.h"
+#include "google/cloud/internal/make_unique.h"
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+
+std::unique_ptr<IdempotencyPolicy> AlwaysRetryIdempotencyPolicy::clone() const {
+  return google::cloud::internal::make_unique<AlwaysRetryIdempotencyPolicy>(
+      *this);
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListBucketsRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CreateBucketRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetBucketMetadataRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteBucketRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::UpdateBucketRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::PatchBucketRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetBucketIamPolicyRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::SetBucketIamPolicyRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::TestBucketIamPermissionsRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::InsertObjectMediaRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CopyObjectRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetObjectMetadataRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ReadObjectRangeRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::InsertObjectStreamingRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListObjectsRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteObjectRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::UpdateObjectRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::PatchObjectRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ComposeObjectRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::RewriteObjectRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListBucketAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CreateBucketAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteBucketAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetBucketAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::UpdateBucketAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::PatchBucketAclRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CreateObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::UpdateObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::PatchObjectAclRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListDefaultObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CreateDefaultObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteDefaultObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetDefaultObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::UpdateDefaultObjectAclRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::PatchDefaultObjectAclRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetProjectServiceAccountRequest const& request) const {
+  return true;
+}
+
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::ListNotificationsRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::CreateNotificationRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::GetNotificationRequest const& request) const {
+  return true;
+}
+bool AlwaysRetryIdempotencyPolicy::IsIdempotent(
+    internal::DeleteNotificationRequest const& request) const {
+  return true;
+}
+
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
