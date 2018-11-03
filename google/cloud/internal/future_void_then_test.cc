70c236023b88a8e1aafc469781bf2b41a14009c9
Carlos O'Ryan <coryan@google.com>
2018-11-02 21:29:33 -0400

    Move future<void>::then_impl() to a separate file.
    The definition of `future<T>::then_impl()` may need to reference
    future<void>, and the definition of `future<void>::then_impl()` may
    need to reference `future<T>`. To break the cycles we need to move
    the definition of this template functions to a separate file, where
    `future<T>`, `future<void>` and (soon) `future<R&>` are all visible.
    This is part of the work for #1345.

--- google/cloud/internal/future_void_then_test.cc
+++ google/cloud/internal/future_void_then_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cloud/internal/future_void.h"
+#include "google/cloud/future.h"
 #include "google/cloud/testing_util/chrono_literals.h"
 #include <gmock/gmock.h>
 #include <functional>
