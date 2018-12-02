b7a01f2dd3c19c4095dfb3f7ee5be312e2e30e0d
Carlos O'Ryan <coryan@google.com>
2018-12-02 16:03:23 -0500

    Implement a request to sign urls.
    Though signing a URL is a local operation, passing options to the Client
    member function is convenient and consistent with the other requests.
    This PR introduces a request to capture all the parameters to a signed
    URL, including the variable number of extension headers.

---
+++ google/cloud/storage/internal/signed_url_requests.cc
@@ -0,0 +1,51 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/signed_url_requests.h"
+#include <sstream>
+
+namespace google {
+namespace cloud {
+namespace storage {
+inline namespace STORAGE_CLIENT_NS {
+namespace internal {
+
+std::string SignUrlRequest::CreateBlob() const {
+  std::ostringstream os;
+
+  auto seconds = std::chrono::duration_cast<std::chrono::seconds>(
+      expiration_time_.time_since_epoch());
+
+  os << verb() << "\n"
+     << md5_hash_value_ << "\n"
+     << content_type_ << "\n"
+     << seconds.count() << "\n";
+
+  for (auto const& kv : extension_headers_) {
+    os << kv.first << ":" << kv.second << "\n";
+  }
+  os << "/" << bucket_name() << "/" << object_name();
+
+  return std::move(os).str();
+}
+
+std::ostream& operator<<(std::ostream& os, SignUrlRequest const& r) {
+  return os << "SingUrlRequest={" << r.CreateBlob() << "}";
+}
+
+}  // namespace internal
+}  // namespace STORAGE_CLIENT_NS
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
