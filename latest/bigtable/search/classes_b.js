d61b7c539348c3d3c915b87c253456876166215c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-12-08 21:19:44 +0000

    Automatically generated documentation

--- latest/bigtable/search/classes_b.js
+++ latest/bigtable/search/classes_b.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['readmodifywriterule',['ReadModifyWriteRule',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1ReadModifyWriteRule.html',1,'google::cloud::bigtable::v0']]],
   ['row',['Row',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1Row.html',1,'google::cloud::bigtable::v0']]],
+  ['rowkeysample',['RowKeySample',['../structgoogle_1_1cloud_1_1bigtable_1_1v0_1_1RowKeySample.html',1,'google::cloud::bigtable::v0']]],
   ['rowrange',['RowRange',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1RowRange.html',1,'google::cloud::bigtable::v0']]],
   ['rowreader',['RowReader',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1RowReader.html',1,'google::cloud::bigtable::v0']]],
   ['rowset',['RowSet',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1RowSet.html',1,'google::cloud::bigtable::v0']]],
