d8c3a6cc9240b3e04f50965ee64a8f4d0e12cdce
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2018-12-17 03:11:20 +0000

    Automatically generated documentation

--- latest/storage/search/classes_10.js
+++ latest/storage/search/classes_10.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['refreshingcredentialswrapper',['RefreshingCredentialsWrapper',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1oauth2_1_1RefreshingCredentialsWrapper.html',1,'google::cloud::storage::v0::oauth2']]],
   ['rewriteprogress',['RewriteProgress',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1RewriteProgress.html',1,'google::cloud::storage::v0']]]
 ];
