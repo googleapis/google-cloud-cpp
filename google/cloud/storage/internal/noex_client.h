6cc115cf7fbc8992c9484ef3d354a2ba93e68b80
Carlos O'Ryan <coryan@google.com>
2018-12-18 10:42:10 -0500

    Always use the NoexPolicy in the noex::Client.
    Now that the skeleton noex::Client exists, and the RetryClient has a
    policy to disable exceptions go and use the policy.

--- google/cloud/storage/internal/noex_client.h
+++ google/cloud/storage/internal/noex_client.h
@@ -109,7 +109,8 @@ class Client {
     // TODO(#1694) - remove all the code duplicated in `storage::Client`.
     auto logging = std::make_shared<internal::LoggingClient>(std::move(client));
     auto retry = std::make_shared<internal::RetryClient>(
-        std::move(logging), std::forward<Policies>(policies)...);
+        std::move(logging), internal::RetryClient::NoexPolicy{},
+        std::forward<Policies>(policies)...);
     return retry;
   }
 
