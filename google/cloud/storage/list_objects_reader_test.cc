116cd578bfc3418f9ce00b2541be3caa14ffbdd4
Carlos O'Ryan <coryan@google.com>
2019-01-06 11:00:29 -0500

    Add ListObjectsReader test for failure case.

--- google/cloud/storage/list_objects_reader_test.cc
+++ google/cloud/storage/list_objects_reader_test.cc
@@ -26,10 +26,12 @@ namespace {
 namespace nl = internal::nl;
 using internal::ListObjectsRequest;
 using internal::ListObjectsResponse;
+using storage::testing::MockClient;
+using storage::testing::canonical_errors::PermanentError;
 using ::testing::_;
 using ::testing::ContainerEq;
+using ::testing::HasSubstr;
 using ::testing::Invoke;
-using testing::MockClient;
 using ::testing::Return;
 
 TEST(ListObjectsReaderTest, Basic) {
@@ -89,6 +91,72 @@ TEST(ListObjectsReaderTest, Empty) {
   ListObjectsReader reader(mock, "foo-bar-baz", Prefix("dir/"));
   auto count = std::distance(reader.begin(), reader.end());
   EXPECT_EQ(0U, count);
+}
+
+TEST(ListObjectsReaderTest, PermanentFailure) {
+  // Create a synthetic list of ObjectMetadata elements, each request will
+  // return 2 of them.
+  auto create_element = [](int index) {
+    std::string id = "object-" + std::to_string(index);
+    std::string name = id;
+    std::string link =
+        "https://www.googleapis.com/storage/v1/b/test-bucket/" + id + "/1";
+    internal::nl::json metadata{
+        {"bucket", "test-bucket"},
+        {"id", id},
+        {"name", name},
+        {"selfLink", link},
+        {"kind", "storage#object"},
+    };
+    return ObjectMetadata::ParseFromJson(metadata).value();
+  };
+  std::vector<ObjectMetadata> expected;
+
+  int const page_count = 2;
+  for (int i = 0; i != 2 * page_count; ++i) {
+    expected.emplace_back(create_element(i));
+  }
+
+  auto create_mock = [&](int i) {
+    ListObjectsResponse response;
+    response.next_page_token = "page-" + std::to_string(i);
+    response.items.emplace_back(create_element(2 * i));
+    response.items.emplace_back(create_element(2 * i + 1));
+    return [response](ListObjectsRequest const&) {
+      return StatusOr<ListObjectsResponse>(response);
+    };
+  };
+
+  auto mock = std::make_shared<MockClient>();
+  EXPECT_CALL(*mock, ListObjects(_))
+      .WillOnce(Invoke(create_mock(0)))
+      .WillOnce(Invoke(create_mock(1)))
+      .WillOnce(Invoke([](ListObjectsRequest const&) {
+        return StatusOr<ListObjectsResponse>(PermanentError());
+      }));
+
+  ListObjectsReader reader(mock, "test-bucket");
+  std::vector<ObjectMetadata> actual;
+  bool has_status_or_error = false;
+  for (auto&& object : reader) {
+    if (object.ok()) {
+      actual.emplace_back(*std::move(object));
+      continue;
+    }
+    // The iteration should fail only once, an error should reset the iterator
+    // to `end()`.
+    EXPECT_FALSE(has_status_or_error);
+    has_status_or_error = true;
+    // Verify the error is what we expect.
+    Status status = std::move(object).status();
+    EXPECT_EQ(PermanentError().status_code(), status.status_code());
+    EXPECT_EQ(PermanentError().error_message(), status.error_message());
+  }
+  // The iteration should have returned an error at least once.
+  EXPECT_TRUE(has_status_or_error);
+
+  // The iteration should have returned all the elements prior to the error.
+  EXPECT_THAT(actual, ContainerEq(expected));
 }
 
 }  // namespace
