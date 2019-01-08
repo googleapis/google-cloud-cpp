98bc3d83ca5db57a866ca6ed8e1e037c33c34f1e
Carlos O'Ryan <coryan@google.com>
2019-01-08 16:28:50 -0500

    Address review comments.

--- google/cloud/storage/examples/storage_notification_samples.cc
+++ google/cloud/storage/examples/storage_notification_samples.cc
@@ -79,18 +79,28 @@ void CreateNotification(google::cloud::storage::Client client, int& argc,
   //! [create notification] [START storage_create_pubsub_bucket_notification]
   namespace gcs = google::cloud::storage;
   [](gcs::Client client, std::string bucket_name, std::string topic_name) {
-    gcs::StatusOr<gcs::NotificationMetadata> notification = client.CreateNotification(
-        bucket_name, topic_name, gcs::payload_format::JsonApiV1(),
-        gcs::NotificationMetadata());
+    gcs::StatusOr<gcs::NotificationMetadata> notification =
+        client.CreateNotification(bucket_name, topic_name,
+                                  gcs::payload_format::JsonApiV1(),
+                                  gcs::NotificationMetadata());
     if (not notification.ok()) {
       std::cerr << "Error creating notification for " << bucket_name
-                << " on topic " << topic_name << ", status=" << notification.status()
-                << std::endl;
+                << " on topic " << topic_name
+                << ", status=" << notification.status() << std::endl;
       return;
     }
     std::cout << "Successfully created notification " << notification->id()
-              << " for bucket " << bucket_name
-              << "\nfull details=" << *notification << std::endl;
+              << " for bucket " << bucket_name << "\n";
+    if (notification->object_name_prefix().empty()) {
+      std::cout << "This notification will be sent for all objects in the"
+                << " bucket\n";
+    } else {
+      std::cout << "This notification will be set sent only for objects"
+                << " starting with the prefix "
+                << notification->object_name_prefix() << "\n";
+    }
+    std::cout << "Full details for the notification:\n"
+              << *notification << std::endl;
   }
   //! [create notification] [END storage_create_pubsub_bucket_notification]
   (std::move(client), bucket_name, topic_name);
@@ -115,7 +125,15 @@ void GetNotification(google::cloud::storage::Client client, int& argc,
       return;
     }
     std::cout << "Notification " << notification->id() << " for bucket "
-              << bucket_name << " details=" << *notification << std::endl;
+              << bucket_name << "\n";
+    if (notification->object_name_prefix().empty()) {
+      std::cout << "This notification is sent for all objects in the bucket\n";
+    } else {
+      std::cout << "This notification is sent only for objects starting with"
+                << " the prefix " << notification->object_name_prefix() << "\n";
+    }
+    std::cout << "Full details for the notification:\n"
+              << *notification << std::endl;
   }
   //! [get notification] [END storage_print_pubsub_bucket_notification]
   (std::move(client), bucket_name, notification_id);
