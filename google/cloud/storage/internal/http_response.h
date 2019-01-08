d32cf501ae0c2ba73315a89abc754109bb8e5987
Carlos O'Ryan <coryan@google.com>
2019-01-08 16:31:55 -0500

    Write function to map GCS HTTP codes to StatusCode.
    This is based on the documented status codes for GCS:
    
    https://cloud.google.com/storage/docs/json_api/v1/status-codes#http-status-and-error-codes

--- google/cloud/storage/internal/http_response.h
+++ google/cloud/storage/internal/http_response.h
@@ -15,6 +15,7 @@
 #ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_HTTP_RESPONSE_H_
 #define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_HTTP_RESPONSE_H_
 
+#include "google/cloud/status.h"
 #include "google/cloud/storage/version.h"
 #include <iosfwd>
 #include <map>
@@ -34,6 +35,32 @@ struct HttpResponse {
   std::string payload;
   std::multimap<std::string, std::string> headers;
 };
+
+/**
+ * Maps a HTTP response to a error `Status`.
+ *
+ * HTTP responses have a wide range of status codes (100 to 599), and we have
+ * a much more limited number of `StatusCode` values. This function performs
+ * the mapping between the two.
+ *
+ * The general principles in this mapping are:
+ * - A "code" outside the valid code for HTTP (from 100 to 599 both
+ *   inclusive) is always kUnknown.
+ * - Codes that are not specifically documented in
+ *     https://cloud.google.com/storage/docs/json_api/v1/status-codes
+ *   are mapped by these rules:
+ *     [100,300) -> kOk because they are all success status codes.
+ *     [300,400) -> kUnknown because libcurl should handle the redirects, so
+ *                  getting one is fairly strange.
+ *     [400,500) -> kInvalidArgument because these are generally "the client
+ *                  sent an invalid request" errors.
+ *     [500,600) -> kInternal because these are "server errors".
+ *
+ * @return A status with the code corresponding to @p http_response.status_code,
+ *     the error message in the status is initialized with
+ *     @p http_response.payload.
+ */
+Status AsStatus(HttpResponse const& http_response);
 
 std::ostream& operator<<(std::ostream& os, HttpResponse const& rhs);
 
