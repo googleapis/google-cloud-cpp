a0034838560de458c991fa660e7d2ab7a7147c08
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-01-09 01:17:14 +0000

    Automatically generated documentation

--- latest/storage/search/typedefs_5.js
+++ latest/storage/search/typedefs_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pointer',['pointer',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListBucketsIterator.html#a68562a9843f9a0c19a8c23bc5d6c79fc',1,'google::cloud::storage::v0::ListBucketsIterator::pointer()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListObjectsIterator.html#ab72a0927c82b6afe2161938d90ebcbfa',1,'google::cloud::storage::v0::ListObjectsIterator::pointer()']]]
+  ['pointer',['pointer',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListBucketsIterator.html#a68562a9843f9a0c19a8c23bc5d6c79fc',1,'google::cloud::storage::v0::ListBucketsIterator::pointer()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListObjectsIterator.html#a7fd5490bd41105af653c80c3c25691cb',1,'google::cloud::storage::v0::ListObjectsIterator::pointer()']]]
 ];
--- latest/storage/search/typedefs_7.js
+++ latest/storage/search/typedefs_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value_5ftype',['value_type',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListBucketsIterator.html#a982118097eeaa42b2f0beaeaddd63b65',1,'google::cloud::storage::v0::ListBucketsIterator::value_type()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListObjectsIterator.html#a0445aa4dde94c0e3009c84b9d3a61e4f',1,'google::cloud::storage::v0::ListObjectsIterator::value_type()']]]
+  ['value_5ftype',['value_type',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListBucketsIterator.html#a982118097eeaa42b2f0beaeaddd63b65',1,'google::cloud::storage::v0::ListBucketsIterator::value_type()'],['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ListObjectsIterator.html#a2a325393b7493f6ab4b1f23187836071',1,'google::cloud::storage::v0::ListObjectsIterator::value_type()']]]
 ];
