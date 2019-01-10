f94cd22fd1d3bad2226f62b1f0775d3ba803c5bb
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-01-10 15:17:25 -0500

    Write function to map GCS HTTP codes to StatusCode. (#1816)
    This is based on the documented status codes for GCS:
    
    https://cloud.google.com/storage/docs/json_api/v1/status-codes#http-status-and-error-codes

--- google/cloud/storage/internal/http_response_test.cc
+++ google/cloud/storage/internal/http_response_test.cc
@@ -34,6 +34,59 @@ TEST(HttpResponseTest, OStream) {
   EXPECT_THAT(actual, HasSubstr("header1: value1"));
   EXPECT_THAT(actual, HasSubstr("header2: value2"));
 }
+
+TEST(HttpResponseTest, AsStatus) {
+  EXPECT_EQ(StatusCode::kUnknown,
+            AsStatus(HttpResponse{-42, "weird"}).status_code());
+  EXPECT_EQ(StatusCode::kUnknown,
+            AsStatus(HttpResponse{99, "still weird"}).status_code());
+  EXPECT_EQ(StatusCode::kOk,
+            AsStatus(HttpResponse{100, "Continue"}).status_code());
+  EXPECT_EQ(StatusCode::kOk,
+            AsStatus(HttpResponse{200, "success"}).status_code());
+  EXPECT_EQ(StatusCode::kOk,
+            AsStatus(HttpResponse{299, "success"}).status_code());
+  EXPECT_EQ(StatusCode::kUnknown,
+            AsStatus(HttpResponse{300, "libcurl should handle this"}).status_code());
+  EXPECT_EQ(StatusCode::kOk,
+            AsStatus(HttpResponse{308, "pending"}).status_code());
+  EXPECT_EQ(StatusCode::kInvalidArgument,
+            AsStatus(HttpResponse{400, "invalid something"}).status_code());
+  EXPECT_EQ(StatusCode::kUnauthenticated,
+            AsStatus(HttpResponse{401, "unauthenticated"}).status_code());
+  EXPECT_EQ(StatusCode::kPermissionDenied,
+            AsStatus(HttpResponse{403, "forbidden"}).status_code());
+  EXPECT_EQ(StatusCode::kNotFound,
+            AsStatus(HttpResponse{404, "not found"}).status_code());
+  EXPECT_EQ(StatusCode::kPermissionDenied,
+            AsStatus(HttpResponse{405, "method not allowed"}).status_code());
+  EXPECT_EQ(StatusCode::kAborted,
+            AsStatus(HttpResponse{409, "conflict"}).status_code());
+  EXPECT_EQ(StatusCode::kNotFound,
+            AsStatus(HttpResponse{410, "gone"}).status_code());
+  EXPECT_EQ(StatusCode::kInvalidArgument,
+            AsStatus(HttpResponse{411, "length required"}).status_code());
+  EXPECT_EQ(StatusCode::kFailedPrecondition,
+            AsStatus(HttpResponse{412, "precondition failed"}).status_code());
+  EXPECT_EQ(StatusCode::kOutOfRange,
+            AsStatus(HttpResponse{413, "payload too large"}).status_code());
+  EXPECT_EQ(StatusCode::kOutOfRange,
+            AsStatus(HttpResponse{416, "request range"}).status_code());
+  EXPECT_EQ(StatusCode::kUnavailable,
+            AsStatus(HttpResponse{429, "too many requests"}).status_code());
+  EXPECT_EQ(StatusCode::kInvalidArgument,
+            AsStatus(HttpResponse{499, "some 4XX error"}).status_code());
+  EXPECT_EQ(StatusCode::kInternal,
+            AsStatus(HttpResponse{500, "internal server error"}).status_code());
+  EXPECT_EQ(StatusCode::kInternal,
+            AsStatus(HttpResponse{502, "bad gateway"}).status_code());
+  EXPECT_EQ(StatusCode::kUnavailable,
+            AsStatus(HttpResponse{503, "service unavailable"}).status_code());
+  EXPECT_EQ(StatusCode::kInternal,
+            AsStatus(HttpResponse{599, "some 5XX error"}).status_code());
+  EXPECT_EQ(StatusCode::kUnknown,
+            AsStatus(HttpResponse{600, "bad"}).status_code());
+}
 }  // namespace
 }  // namespace internal
 }  // namespace STORAGE_CLIENT_NS
