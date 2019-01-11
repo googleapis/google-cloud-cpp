ae81d5f7c700ebfd0650e536b84d56a41febca91
Netty Hernandez <45398907+NettyH@users.noreply.github.com>
2019-01-11 15:44:23 -0500

    Revert last commit until new version of Emulator is release

--- google/cloud/bigtable/tests/data_integration_test.cc
+++ google/cloud/bigtable/tests/data_integration_test.cc
@@ -589,6 +589,11 @@ TEST_F(DataIntegrationTest, TableSampleRowKeysTest) {
 }
 
 TEST_F(DataIntegrationTest, TableReadMultipleCellsBigValue) {
+  if (UsingCloudBigtableEmulator()) {
+    // TODO(#151) - remove workarounds for emulator bug(s).
+    return;
+  }
+
   std::string const table_id = RandomTableId();
   auto table = CreateTable(table_id, table_config);
 
