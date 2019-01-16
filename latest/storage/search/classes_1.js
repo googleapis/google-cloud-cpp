225a2985803ae53e4e84864a751ab8a60d53f1ce
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-01-16 17:24:19 +0000

    Automatically generated documentation

--- latest/storage/search/classes_1.js
+++ latest/storage/search/classes_1.js
@@ -9,7 +9,7 @@ var searchData=
   ['bucketlogging',['BucketLogging',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketLogging.html',1,'google::cloud::storage::v0']]],
   ['bucketmetadata',['BucketMetadata',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html',1,'google::cloud::storage::v0']]],
   ['bucketmetadatapatchbuilder',['BucketMetadataPatchBuilder',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadataPatchBuilder.html',1,'google::cloud::storage::v0']]],
-  ['bucketonlypolicy',['BucketOnlyPolicy',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketOnlyPolicy.html',1,'google::cloud::storage::v0']]],
+  ['bucketpolicyonly',['BucketPolicyOnly',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketPolicyOnly.html',1,'google::cloud::storage::v0']]],
   ['bucketretentionpolicy',['BucketRetentionPolicy',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketRetentionPolicy.html',1,'google::cloud::storage::v0']]],
   ['bucketversioning',['BucketVersioning',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketVersioning.html',1,'google::cloud::storage::v0']]],
   ['bucketwebsite',['BucketWebsite',['../structgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketWebsite.html',1,'google::cloud::storage::v0']]]
