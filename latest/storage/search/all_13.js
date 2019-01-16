03e75d03391f5bb842417d88dfd85f528be47e89
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-01-16 00:56:49 +0000

    Automatically generated documentation

--- latest/storage/search/all_13.js
+++ latest/storage/search/all_13.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['temporary_5fhold',['temporary_hold',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a2ef49cd236cd4bebc1b0bc58710e5bb3',1,'google::cloud::storage::v0::ObjectMetadata']]],
-  ['testbucketiampermissions',['TestBucketIamPermissions',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a8559215b6e6e9e8e1a0fb40c53a47142',1,'google::cloud::storage::v0::Client']]],
+  ['testbucketiampermissions',['TestBucketIamPermissions',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1Client.html#a261604a6537149c9ded66d46b5cd4768',1,'google::cloud::storage::v0::Client']]],
   ['time_5fdeleted',['time_deleted',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#a6941888b59bab3ba8ba476bba4bfebdc',1,'google::cloud::storage::v0::ObjectMetadata']]],
   ['time_5fstorage_5fclass_5fupdated',['time_storage_class_updated',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#ab4191088c6f9a5704bc13d54c2fc6a8e',1,'google::cloud::storage::v0::ObjectMetadata']]],
   ['tojsonstring',['ToJsonString',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketMetadata.html#af30bbfd04003ebc94a4779f31782f341',1,'google::cloud::storage::v0::BucketMetadata']]],
