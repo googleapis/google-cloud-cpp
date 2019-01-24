6070c8978accc50ffc1781662d2d8d09601a1138
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-01-24 17:44:07 +0000

    Automatically generated documentation

--- latest/storage/search/related_0.js
+++ latest/storage/search/related_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['bucketaccesscontrolparser',['BucketAccessControlParser',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1BucketAccessControl.html#a4c4d435dcf5bf90eb3b3b0be5b32d508',1,'google::cloud::storage::v0::BucketAccessControl']]],
+  ['notificationmetadataparser',['NotificationMetadataParser',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1NotificationMetadata.html#a2f10a1418d7d726bccdf8b1ed628075e',1,'google::cloud::storage::v0::NotificationMetadata']]],
   ['objectmetadataparser',['ObjectMetadataParser',['../classgoogle_1_1cloud_1_1storage_1_1v0_1_1ObjectMetadata.html#ab88ef8c136081907705d9063118d470d',1,'google::cloud::storage::v0::ObjectMetadata']]]
 ];
