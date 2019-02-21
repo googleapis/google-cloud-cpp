db2c58f229d8ccea1b79c390ff632a9042f88c29
rsimion <robert@qlogic.io>
2019-02-21 18:39:37 -0500

    Made a mistake in bigtable_instal_test.cc

--- ci/test-install/bigtable_install_test.cc
+++ ci/test-install/bigtable_install_test.cc
@@ -60,8 +60,8 @@ int main(int argc, char* argv[]) try {
   std::cout << "bulk mutation successful\n";
 
   auto row0 = table.ReadRow("row-key-0", bigtable::Filter::PassAllFilter());
-  if (!row0.ok()) {
-    throw std::runtime_error(row0.message());
+  if (!row0) {
+    throw std::runtime_error(row0.status().message());
   }
   if (!row0->first) {
     std::cout << "Cannot find row-key-0\n";
@@ -72,8 +72,8 @@ int main(int argc, char* argv[]) try {
     }
   }
   auto row1 = table.ReadRow("row-key-1", bigtable::Filter::PassAllFilter());
-  if (!row1.ok()) {
-    throw std::runtime_error(row1.message());
+  if (!row1) {
+    throw std::runtime_error(row1.status().message());
   }
   if (!row1->first) {
     std::cout << "Cannot find row-key-1\n";
