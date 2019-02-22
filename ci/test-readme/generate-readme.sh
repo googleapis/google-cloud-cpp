fdef8c57bac6563033a31807b1c396fcbf270276
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-02-21 19:00:37 -0500

    Show badges for the install build too. (#2067)
    I thought it would be nice to advertise that we actually test these
    install instructions.

--- ci/test-readme/generate-readme.sh
+++ ci/test-readme/generate-readme.sh
@@ -18,26 +18,45 @@ set -eu
 
 readonly BINDIR=$(dirname $0)
 
+badge() {
+  local -r distro="$1"
+  cat <<_EOF_
+
+[![Kokoro install ${distro} status][kokoro-install-${distro}-shield]][kokoro-install-${distro}-link]
+
+[kokoro-install-${distro}-shield]: https://storage.googleapis.com/cloud-cpp-kokoro-status/kokoro-install-${distro}.svg
+[kokoro-install-${distro}-link]: https://storage.googleapis.com/cloud-cpp-kokoro-status/kokoro-install-${distro}-link.html
+_EOF_
+}
+
 echo "### CentOS (7)"
+badge centos
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.centos"
 
 echo "### Debian (Stretch)"
+badge debian
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.debian"
 
 echo "### Fedora (29)"
+badge fedora
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.fedora"
 
 echo "### OpenSUSE (Tumbleweed)"
+badge opensuse
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.opensuse"
 
 echo "### OpenSUSE (Leap)"
+badge opensuse-leap
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.opensuse-leap"
 
 echo "### Ubuntu (18.04 - Bionic Beaver)"
+badge ubuntu
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.ubuntu"
 
 echo "### Ubuntu (16.04 - Xenial Xerus)"
+badge ubuntu-xenial
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.ubuntu-xenial"
 
 echo "### Ubuntu (14.04 - Trusty Tahr)"
+badge ubuntu-trusty
 "${BINDIR}/extract-readme.sh" "${BINDIR}/Dockerfile.ubuntu-trusty"
