9c7d2f2f268877c99e2ae3781742f9726cbb5da4
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-02-27 14:16:06 +0000

    Automatically generated documentation

--- latest/bigtable/search/files_1.js
+++ latest/bigtable/search/files_1.js
@@ -8,10 +8,10 @@ var searchData=
   ['bigtable_2dsamples_2edox',['bigtable-samples.dox',['../bigtable-samples_8dox.html',1,'']]],
   ['bigtable_5fhello_5fapp_5fprofile_2ecc',['bigtable_hello_app_profile.cc',['../bigtable__hello__app__profile_8cc.html',1,'']]],
   ['bigtable_5fhello_5fworld_2ecc',['bigtable_hello_world.cc',['../bigtable__hello__world_8cc.html',1,'']]],
+  ['bigtable_5finstance_5fadmin_5fsnippets_2ecc',['bigtable_instance_admin_snippets.cc',['../bigtable__instance__admin__snippets_8cc.html',1,'']]],
   ['bigtable_5fopencensus_2ecc',['bigtable_opencensus.cc',['../bigtable__opencensus_8cc.html',1,'']]],
   ['bigtable_5fquickstart_2ecc',['bigtable_quickstart.cc',['../bigtable__quickstart_8cc.html',1,'']]],
   ['bigtable_5fsamples_2ecc',['bigtable_samples.cc',['../bigtable__samples_8cc.html',1,'']]],
   ['bigtable_5fsamples_5finstance_5fadmin_2ecc',['bigtable_samples_instance_admin.cc',['../bigtable__samples__instance__admin_8cc.html',1,'']]],
-  ['bigtable_5fsamples_5finstance_5fadmin_5fext_2ecc',['bigtable_samples_instance_admin_ext.cc',['../bigtable__samples__instance__admin__ext_8cc.html',1,'']]],
   ['bigtable_5fstrong_5ftypes_2eh',['bigtable_strong_types.h',['../bigtable__strong__types_8h.html',1,'']]]
 ];
