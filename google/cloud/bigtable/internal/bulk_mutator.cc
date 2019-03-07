c602ba954fe8dfbb8f4a133abf638968d4926f36
rsimion <robert@qlogic.io>
2019-03-07 13:05:44 -0500

    Clang Tidy change

--- google/cloud/bigtable/internal/bulk_mutator.cc
+++ google/cloud/bigtable/internal/bulk_mutator.cc
@@ -170,10 +170,11 @@ std::vector<FailedMutation> BulkMutator::ExtractFinalFailures() {
   google::cloud::Status status(google::cloud::StatusCode::kUnknown, message);
 
   int idx = 0;
-  for (auto mutation : *pending_mutations_.mutable_entries()) {
+  for (auto const mutation : *pending_mutations_.mutable_entries()) {
     int original_index = pending_annotations_[idx++].original_index;
     result.emplace_back(status, original_index);
   }
+
   return result;
 }
 
