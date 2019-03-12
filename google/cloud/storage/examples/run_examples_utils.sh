bebfb0f0ed857976f452705147b5a04ba10a30c1
remyabel <47402505+remyabel@users.noreply.github.com>
2019-03-12 08:24:37 -0400

    Create storage examples for object versioning. (#2194)

--- google/cloud/storage/examples/run_examples_utils.sh
+++ google/cloud/storage/examples/run_examples_utils.sh
@@ -719,6 +719,45 @@ run_temporary_hold_examples() {
 }
 
 ################################################
+# Run the examples showing how to use object versioning.
+# Globals:
+#   COLOR_*: colorize output messages, defined in colors.sh
+#   EXIT_STATUS: control the final exit status for the program.
+# Arguments:
+#   bucket_name: the name of the bucket to run the examples against.
+# Returns:
+#   None
+################################################
+run_object_versioning_examples() {
+  local bucket_name=$1
+  shift
+
+  local object_name="object-$(date +%s)-${RANDOM}.txt"
+  local copied_object_name="object-$(date +%s)-${RANDOM}.txt"
+  run_example ./storage_bucket_samples enable-object-versioning \
+      "${bucket_name}"
+  run_example ./storage_bucket_samples get-object-versioning \
+      "${bucket_name}"
+  local archived_generation
+  archived_generation=$(./storage_object_samples insert-object \
+      "${bucket_name}" "${object_name}" "a-string-to-serve-as-object-media" | \
+      sed -n 's/.*, generation=\([0-9]*\).*/\1/p')
+  local live_generation
+  live_generation=$(./storage_object_samples insert-object \
+      "${bucket_name}" "${object_name}" "a-string-to-serve-as-object-media" | \
+      sed -n 's/.*, generation=\([0-9]*\).*/\1/p')
+  run_example ./storage_object_samples copy-versioned-object \
+      "${bucket_name}" "${object_name}" \
+      "${bucket_name}" "${copied_object_name}" "${archived_generation}"
+  run_example ./storage_object_samples delete-versioned-object \
+     "${bucket_name}" "${object_name}" "${archived_generation}"
+  run_example ./storage_object_samples delete-versioned-object \
+     "${bucket_name}" "${object_name}" "${live_generation}"
+  run_example ./storage_bucket_samples disable-object-versioning \
+     "${bucket_name}"
+}
+
+################################################
 # Run all Customer-managed Encryption Keys examples.
 # Globals:
 #   COLOR_*: colorize output messages, defined in colors.sh
@@ -1064,6 +1103,7 @@ run_all_storage_examples() {
   run_all_public_object_examples "${BUCKET_NAME}"
   run_event_based_hold_examples "${BUCKET_NAME}"
   run_temporary_hold_examples "${BUCKET_NAME}"
+  run_object_versioning_examples "${BUCKET_NAME}"
   run_all_signed_url_v2_examples "${BUCKET_NAME}"
   run_all_signed_url_v4_examples "${BUCKET_NAME}"
   run_all_object_acl_examples "${BUCKET_NAME}"
