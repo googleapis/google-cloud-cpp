5122fd885f18cc7c36167141ba92fc97edea45c7
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-04-12 05:16:16 -0700

    added some comments

--- google/cloud/spanner/spanner_tool.cc
+++ google/cloud/spanner/spanner_tool.cc
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <google/spanner/admin/database/v1/spanner_database_admin.grpc.pb.h>
 #include <google/spanner/v1/spanner.grpc.pb.h>
 #include <grpcpp/grpcpp.h>
@@ -36,6 +50,18 @@ void ListDatabases(std::string const& project, std::string const& instance) {
 
 }  // namespace
 
+// This is a command-line tool to let folks easily experiment with Spanner usng
+// C++. This works with bazel using a command like:
+//
+// $ bazel run google/cloud/spanner:spanner_tool -- jgm-cloud-cxx jgm-spanner-instance
+//
+// Currently, the above command will just invoke the "ListDatabases" RPC, which
+// makes it equivalent to the following command:
+//
+// $ gcloud spanner databases list --project jgm-cloud-cxx --instance jgm-spanner-instance 
+//
+// NOTE: The actual project and instance names will vary for other users; These
+// are just examples.
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cerr << "Usage: spanner_tool <project> <instance>\n";
