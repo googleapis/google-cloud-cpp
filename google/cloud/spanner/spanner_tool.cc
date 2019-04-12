0d33760ddc15c9b84f4d5aab79a58c1c8a64b37d
Greg Miller <9447643+devjgm@users.noreply.github.com>
2019-04-12 05:10:22 -0700

    added spanner_tool that works bazel build

--- google/cloud/spanner/spanner_tool.cc
+++ google/cloud/spanner/spanner_tool.cc
@@ -1,28 +1,46 @@
+#include <google/spanner/admin/database/v1/spanner_database_admin.grpc.pb.h>
+#include <google/spanner/v1/spanner.grpc.pb.h>
+#include <grpcpp/grpcpp.h>
 #include <iostream>
 #include <memory>
-#include <grpcpp/grpcpp.h>
-#include <google/spanner/v1/spanner.grpc.pb.h>
-#include <google/spanner/admin/database/v1/spanner_database_admin.grpc.pb.h>
+#include <string>
 
-int main() {
+namespace {
+
+void ListDatabases(std::string const& project, std::string const& instance) {
   namespace spanner = google::spanner::admin::database::v1;
-  auto const cred = grpc::GoogleDefaultCredentials();
-  auto const channel = grpc::CreateChannel("spanner.googleapis.com", cred);
+
+  std::shared_ptr<grpc::ChannelCredentials> cred =
+      grpc::GoogleDefaultCredentials();
+  std::shared_ptr<grpc::Channel> channel =
+      grpc::CreateChannel("spanner.googleapis.com", std::move(cred));
   std::unique_ptr<spanner::DatabaseAdmin::Stub> stub(
-      spanner::DatabaseAdmin::NewStub(channel));
+      spanner::DatabaseAdmin::NewStub(std::move(channel)));
 
   spanner::ListDatabasesResponse response;
   spanner::ListDatabasesRequest request;
-  request.set_parent("projects/jgm-cloud-cxx/instances/jgm-spanner-instance");
+  request.set_parent("projects/" + project + "/instances/" + instance);
 
   grpc::ClientContext context;
-  auto status = stub->ListDatabases(&context, request, &response);
+  grpc::Status status = stub->ListDatabases(&context, request, &response);
 
   if (!status.ok()) {
     std::cerr << "FAILED: " << status.error_code() << ": "
               << status.error_message() << "\n";
+    return;
   }
 
   std::cout << "Response:\n";
   std::cout << response.DebugString() << "\n";
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "Usage: spanner_tool <project> <instance>\n";
+    return 1;
+  }
+
+  ListDatabases(argv[1], argv[2]);
 }
