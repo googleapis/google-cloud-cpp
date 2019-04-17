55e40a59d55f9456ceeb94a1622115c9ffd2bfff
Carlos O'Ryan <coryan@google.com>
2019-04-17 16:05:43 -0400

    Address review comments.

--- google/cloud/bigtable/table.cc
+++ google/cloud/bigtable/table.cc
@@ -13,13 +13,11 @@
 // limitations under the License.
 
 #include "google/cloud/bigtable/table.h"
-
 #include "google/cloud/bigtable/internal/async_future_from_callback.h"
 #include "google/cloud/bigtable/internal/async_retry_unary_rpc.h"
 #include "google/cloud/bigtable/internal/bulk_mutator.h"
 #include "google/cloud/bigtable/internal/grpc_error_delegate.h"
 #include "google/cloud/bigtable/internal/unary_client_utils.h"
-
 #include <thread>
 #include <type_traits>
 
