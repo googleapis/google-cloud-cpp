ba716307cf6ada0db2bccdff0d69ad6a061c32f5
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-05-06 17:56:32 +0000

    Automatically generated documentation

--- latest/bigtable/menudata.js
+++ latest/bigtable/menudata.js
@@ -75,7 +75,8 @@ var menudata={children:[
 {text:"p",url:"functions_vars.html#index_p"},
 {text:"r",url:"functions_vars.html#index_r"},
 {text:"s",url:"functions_vars.html#index_s"},
-{text:"t",url:"functions_vars.html#index_t"}]},
+{text:"t",url:"functions_vars.html#index_t"},
+{text:"v",url:"functions_vars.html#index_v"}]},
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Related Functions",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
