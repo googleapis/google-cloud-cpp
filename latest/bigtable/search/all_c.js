c55bc617a83efe3d00e46145bad8358a33e44b0c
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-05-16 22:18:10 +0000

    Automatically generated documentation

--- latest/bigtable/search/all_c.js
+++ latest/bigtable/search/all_c.js
@@ -4,6 +4,5 @@ var searchData=
   ['name',['NAME',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1MetadataParamTypes.html#ad83f6ed80a89e9a79cc1451f2fa864b7',1,'google::cloud::bigtable::v0::MetadataParamTypes']]],
   ['name_5fonly',['NAME_ONLY',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1TableAdmin.html#afe94c9d1b0aa5e93edc28776239b20db',1,'google::cloud::bigtable::v0::TableAdmin']]],
   ['no_5frows_5flimit',['NO_ROWS_LIMIT',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1RowReader.html#af6c458e786f75dcd131865b6e195c33a',1,'google::cloud::bigtable::v0::RowReader']]],
-  ['nullstream',['NullStream',['../../common/structgoogle_1_1cloud_1_1v0_1_1NullStream.html',1,'google::cloud::v0']]],
-  ['table',['Table',['../classgoogle_1_1cloud_1_1bigtable_1_1v0_1_1DataClient.html#a4fdcfb33335be884db275224ed1c01fd',1,'google::cloud::bigtable::v0::DataClient']]]
+  ['nullstream',['NullStream',['../../common/structgoogle_1_1cloud_1_1v0_1_1NullStream.html',1,'google::cloud::v0']]]
 ];
