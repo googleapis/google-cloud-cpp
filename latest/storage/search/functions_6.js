aee4913ce1f816ece5416619fb28b88297375e61
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-05-28 02:54:51 +0000

    Automatically generated documentation

--- latest/storage/search/functions_6.js
+++ latest/storage/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['generation',['generation',['../classgoogle_1_1cloud_1_1storage_1_1v1_1_1ObjectAccessControl.html#a76d84f7e16abfc39a797d7b0ba775083',1,'google::cloud::storage::v1::ObjectAccessControl::generation()'],['../classgoogle_1_1cloud_1_1storage_1_1v1_1_1ObjectMetadata.html#a8aee9354f69bf95ad612e922e43e1e1a',1,'google::cloud::storage::v1::ObjectMetadata::generation()']]],
+  ['generation',['generation',['../classgoogle_1_1cloud_1_1storage_1_1v1_1_1ObjectMetadata.html#a8aee9354f69bf95ad612e922e43e1e1a',1,'google::cloud::storage::v1::ObjectMetadata::generation()'],['../classgoogle_1_1cloud_1_1storage_1_1v1_1_1ObjectAccessControl.html#a76d84f7e16abfc39a797d7b0ba775083',1,'google::cloud::storage::v1::ObjectAccessControl::generation()']]],
   ['get',['get',['../../common/classgoogle_1_1cloud_1_1v0_1_1future.html#a21286d6ee3ff3dee27371059275ba302',1,'google::cloud::v0::future::get()'],['../../common/classgoogle_1_1cloud_1_1v0_1_1future_3_01void_01_4.html#a32678eec3975641104339895f76cb6ce',1,'google::cloud::v0::future&lt; void &gt;::get()']]],
   ['get_5ffuture',['get_future',['../../common/classgoogle_1_1cloud_1_1v0_1_1promise.html#a3d7939e1b7b1ae3527f8fd7f8be688f2',1,'google::cloud::v0::promise::get_future()'],['../../common/classgoogle_1_1cloud_1_1v0_1_1promise_3_01void_01_4.html#a638694cda98e0707f55c9628c608e05b',1,'google::cloud::v0::promise&lt; void &gt;::get_future()']]],
   ['getbucketacl',['GetBucketAcl',['../classgoogle_1_1cloud_1_1storage_1_1v1_1_1Client.html#adc8f739b997e5582e5a59f1fbdc8ba22',1,'google::cloud::storage::v1::Client']]],
