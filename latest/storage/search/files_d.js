8f9dff5256d2e24bd73e86360991252098e3b118
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2019-05-28 21:18:06 +0000

    Automatically generated documentation

--- latest/storage/search/files_d.js
+++ latest/storage/search/files_d.js
@@ -14,6 +14,7 @@ var searchData=
   ['storage_5fbucket_5fsamples_2ecc',['storage_bucket_samples.cc',['../storage__bucket__samples_8cc.html',1,'']]],
   ['storage_5fclass_2eh',['storage_class.h',['../storage__class_8h.html',1,'']]],
   ['storage_5fdefault_5fobject_5facl_5fsamples_2ecc',['storage_default_object_acl_samples.cc',['../storage__default__object__acl__samples_8cc.html',1,'']]],
+  ['storage_5ffile_5ftransfer_5fbenchmark_2ecc',['storage_file_transfer_benchmark.cc',['../storage__file__transfer__benchmark_8cc.html',1,'']]],
   ['storage_5flatency_5fbenchmark_2ecc',['storage_latency_benchmark.cc',['../storage__latency__benchmark_8cc.html',1,'']]],
   ['storage_5fnotification_5fsamples_2ecc',['storage_notification_samples.cc',['../storage__notification__samples_8cc.html',1,'']]],
   ['storage_5fobject_5facl_5fsamples_2ecc',['storage_object_acl_samples.cc',['../storage__object__acl__samples_8cc.html',1,'']]],
