36e95a6a2c7f1a05ebed8edff61ffdf356fc04f1
Grant Timmerman <granttimmerman@gmail.com>
2019-06-13 13:33:53 -0700

    Remove storage location to match other code samples (#2760)

--- google/cloud/storage/examples/storage_quickstart.cc
+++ google/cloud/storage/examples/storage_quickstart.cc
@@ -39,11 +39,8 @@ int main(int argc, char* argv[]) {
   }
 
   google::cloud::StatusOr<gcs::BucketMetadata> bucket_metadata =
-      client->CreateBucketForProject(
-          bucket_name, project_id,
-          gcs::BucketMetadata()
-              .set_location("us-east1")
-              .set_storage_class(gcs::storage_class::Regional()));
+      client->CreateBucketForProject(bucket_name, project_id,
+                                     gcs::BucketMetadata());
 
   if (!bucket_metadata) {
     std::cerr << "Error creating bucket " << bucket_name
