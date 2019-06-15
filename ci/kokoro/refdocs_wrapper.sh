618925d7f6be9c374a3fec6f535e9a99b5efbe30
Takashi Matsuo <tmatsuo@google.com>
2019-06-15 13:53:26 -0700

    Add a kokoro job for generating and uploading refdocs to googleapis.dev (#2763)
    * Add configs and script for publishing refdocs
    
    * Add an option for using master for refdoc links
    
    * Fix style
    
    * Address code review
    
    * Fix style
    
    * Change how to trim
    
    * Refactor Doxygen configuration
    
    * Fix style
    
    * Introduce a trampoline wrapper script
    
    * Add number of CPU
    
    * File permission
    
    * Build the Docker image when BUILD_DOCKER_IMAGE is true
    
    * Remove pip install statement

---
+++ ci/kokoro/refdocs_wrapper.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+if [[ -z "${TRAMPOLINE_IMAGE:-}" ]]; then
+  TRAMPOLINE_IMAGE="gcr.io/cloud-devrel-kokoro-resources/cpp/refdocs"
+fi
+
+# work from the git root directory
+cd "$(dirname "$0")/../../"
+
+if [[ "${BUILD_DOCKER_IMAGE:-false}" == "true" ]]; then
+  # First build the docker image with dependency installed
+  docker build -t gcr.io/cloud-devrel-kokoro-resources/cpp/refdoc-base \
+    --build-arg NCPU="$(nproc)" \
+    -f ci/travis/Dockerfile.ubuntu-install ci
+
+  docker build -t "${TRAMPOLINE_IMAGE}" -f ci/kokoro/Dockerfile.refdocs ci
+
+  gcloud auth configure-docker
+  docker push "${TRAMPOLINE_IMAGE}"
+fi
+
+exec ci/kokoro/trampoline.sh
