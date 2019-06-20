9efeed1e5c282e13631caa26be1d6410594230cc
Carlos O'Ryan <coryan@google.com>
2019-06-19 21:41:04 -0400

    Fix overload resolution for std::int64_t.

--- google/cloud/bigtable/cell.h
+++ google/cloud/bigtable/cell.h
@@ -87,10 +87,10 @@ class Cell {
   /// Create a Cell and fill it with data.
   template <typename KeyType, typename ColumnType, typename ValueType,
             // This function does not participate in overload resolution if
-            // ValueType is an integral type. That is handled by the next
+            // ValueType is std::int64_t. That is handled by the next
             // overload, where the value is stored as a Big Endian number.
-            typename std::enable_if<!std::is_integral<ValueType>::value,
-                                    int>::type = 0>
+            typename std::enable_if<
+                !std::is_same<std::int64_t, ValueType>::value, int>::type = 0>
   Cell(KeyType row_key, std::string family_name, ColumnType column_qualifier,
        std::int64_t timestamp, ValueType value, std::vector<std::string> labels)
       : row_key_(std::forward<KeyType>(row_key)),
