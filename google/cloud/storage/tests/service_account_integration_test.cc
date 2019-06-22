455cf8a014b49bd123bed9482d9632dacbb5a341
Carlos O'Ryan <coryan@google.com>
2019-06-22 11:27:14 -0400

    Do not assume the Etag is unchanged between Create and Get.

--- google/cloud/storage/tests/service_account_integration_test.cc
+++ google/cloud/storage/tests/service_account_integration_test.cc
@@ -112,7 +112,11 @@ TEST(ServiceAccountIntegrationTest, HmacKeyCRUD) {
   ASSERT_STATUS_OK(get_details);
 
   EXPECT_EQ(access_id, get_details->access_id());
-  EXPECT_EQ(key->first, *get_details);
+  HmacKeyMetadata original = key->first;
+  // TODO(#3806) - remove this workaround: the etag may have changed since we
+  // the key was created.
+  original.set_etag(get_details->etag());
+  EXPECT_EQ(original, *get_details);
 
   StatusOr<HmacKeyMetadata> update_details =
       client.UpdateHmacKey(access_id, HmacKeyMetadata().set_state("INACTIVE"));
