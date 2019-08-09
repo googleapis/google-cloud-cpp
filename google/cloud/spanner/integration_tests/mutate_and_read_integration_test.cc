61d309d8055f47bdf5bf0d0815beb8cb6d407eeb
Bradley White <14679271+devbww@users.noreply.github.com>
2019-08-09 14:35:30 -0400

    Quash a clang-3.8 warning (suggest braces around initialization) (googleapis/google-cloud-cpp-spanner#332)

--- google/cloud/spanner/integration_tests/mutate_and_read_integration_test.cc
+++ google/cloud/spanner/integration_tests/mutate_and_read_integration_test.cc
@@ -93,12 +93,14 @@ TEST(CommitIntegrationTest, Insert) {
     SCOPED_TRACE("Parsing row[" + std::to_string(row_number++) + "]");
     auto parsed_row = ParseRow<std::int64_t, std::string,
                                std::string>(std::array<Value, 3>{
-        internal::FromProto(result_set->metadata().row_type().fields(0).type(),
-                            row.values(0)),
-        internal::FromProto(result_set->metadata().row_type().fields(1).type(),
-                            row.values(1)),
-        internal::FromProto(result_set->metadata().row_type().fields(2).type(),
-                            row.values(2))});
+        // The extra braces are working around an old clang bug that was fixed
+        // in 6.0 (https://bugs.llvm.org/show_bug.cgi?id=21629).
+        {internal::FromProto(result_set->metadata().row_type().fields(0).type(),
+                             row.values(0)),
+         internal::FromProto(result_set->metadata().row_type().fields(1).type(),
+                             row.values(1)),
+         internal::FromProto(result_set->metadata().row_type().fields(2).type(),
+                             row.values(2))}});
     EXPECT_STATUS_OK(parsed_row);
     returned_rows.emplace_back(*parsed_row);
   }
