a27563dd38c43e221baff6003127e95acf97a065
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-08-15 10:54:48 -0400

    feat: Add integration test for DeleteMutation. (googleapis/google-cloud-cpp-spanner#361)
    Also made the test a little faster by creating the database only once.

--- google/cloud/spanner/integration_tests/mutate_and_read_integration_test.cc
+++ google/cloud/spanner/integration_tests/mutate_and_read_integration_test.cc
@@ -14,12 +14,12 @@
 
 #include "google/cloud/spanner/client.h"
 #include "google/cloud/spanner/database_admin_client.h"
-#include "google/cloud/spanner/internal/spanner_stub.h"
 #include "google/cloud/spanner/mutations.h"
 #include "google/cloud/spanner/testing/random_database_name.h"
 #include "google/cloud/internal/getenv.h"
 #include "google/cloud/internal/random.h"
 #include "google/cloud/testing_util/assert_ok.h"
+#include "google/cloud/testing_util/init_google_mock.h"
 #include <gmock/gmock.h>
 
 namespace google {
@@ -30,36 +30,105 @@ namespace {
 
 using ::testing::UnorderedElementsAre;
 
-/// @test Verify the basic insert operations for transaction commits.
-TEST(CommitIntegrationTest, Insert) {
-  auto project_id =
-      google::cloud::internal::GetEnv("GOOGLE_CLOUD_PROJECT").value_or("");
-  auto instance_id =
-      google::cloud::internal::GetEnv("GOOGLE_CLOUD_CPP_SPANNER_INSTANCE")
-          .value_or("");
-  ASSERT_FALSE(project_id.empty());
-  ASSERT_FALSE(instance_id.empty());
+// Use a ::testing::Environment to create the database once.
+class IntegrationTestEnvironment : public ::testing::Environment {
+ public:
+  // NOLINTNEXTLINE
+  static std::string const& project_id() { return *project_id_; }
+  // NOLINTNEXTLINE
+  static std::string const& instance_id() { return *instance_id_; }
+  // NOLINTNEXTLINE
+  static std::string const& database_id() { return *database_id_; }
 
-  auto generator = google::cloud::internal::MakeDefaultPRNG();
-  std::string database_id = spanner_testing::RandomDatabaseName(generator);
+  static std::string RandomTableName() {
+    return google::cloud::internal::Sample(*generator_, 16,
+                                           "abcdefghijklmnopqrstuvwxyz");
+  }
 
-  DatabaseAdminClient admin_client;
-  auto database_future =
-      admin_client.CreateDatabase(project_id, instance_id, database_id, {R"""(
-                             CREATE TABLE Singers (
+ protected:
+  void SetUp() override {
+    project_id_ = new std::string(
+        google::cloud::internal::GetEnv("GOOGLE_CLOUD_PROJECT").value_or(""));
+    instance_id_ = new std::string(
+        google::cloud::internal::GetEnv("GOOGLE_CLOUD_CPP_SPANNER_INSTANCE")
+            .value_or(""));
+
+    generator_ = new google::cloud::internal::DefaultPRNG(
+        google::cloud::internal::MakeDefaultPRNG());
+    database_id_ =
+        new std::string(spanner_testing::RandomDatabaseName(*generator_));
+
+    std::cout << "Creating database and table" << std::flush;
+    DatabaseAdminClient admin_client;
+    auto database_future = admin_client.CreateDatabase(
+        project_id(), instance_id(), database_id(), {R"""(CREATE TABLE Singers (
                                 SingerId   INT64 NOT NULL,
                                 FirstName  STRING(1024),
                                 LastName   STRING(1024)
-                             ) PRIMARY KEY (SingerId)
-                            )"""});
-  auto database = database_future.get();
-  ASSERT_STATUS_OK(database);
+                             ) PRIMARY KEY (SingerId))"""});
 
-  auto database_name = MakeDatabaseName(project_id, instance_id, database_id);
-  auto conn = MakeConnection(database_name);
-  Client client(std::move(conn));
+    int i = 0;
+    int const timeout = 120;
+    while (++i < timeout) {
+      auto status = database_future.wait_for(std::chrono::seconds(1));
+      if (status == std::future_status::ready) break;
+      std::cout << '.' << std::flush;
+    }
+    if (i >= timeout) {
+      std::cout << "TIMEOUT\n";
+      FAIL();
+    }
+    auto database = database_future.get();
+    ASSERT_STATUS_OK(database);
+    std::cout << "DONE\n";
+  }
 
-  auto commit_result = client.Commit(
+  void TearDown() override {
+    DatabaseAdminClient admin_client;
+    auto drop_status =
+        admin_client.DropDatabase(project_id(), instance_id(), database_id());
+    EXPECT_STATUS_OK(drop_status);
+  }
+
+ private:
+  static std::string* project_id_;
+  static std::string* instance_id_;
+  static std::string* database_id_;
+  static google::cloud::internal::DefaultPRNG* generator_;
+};
+
+std::string* IntegrationTestEnvironment::project_id_;
+std::string* IntegrationTestEnvironment::instance_id_;
+std::string* IntegrationTestEnvironment::database_id_;
+google::cloud::internal::DefaultPRNG* IntegrationTestEnvironment::generator_;
+
+class MutateAndReadIntegrationTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    auto project_id = IntegrationTestEnvironment::project_id();
+    auto instance_id = IntegrationTestEnvironment::instance_id();
+    ASSERT_FALSE(project_id.empty());
+    ASSERT_FALSE(instance_id.empty());
+
+    auto database_id = IntegrationTestEnvironment::database_id();
+    client_ = google::cloud::internal::make_unique<Client>(
+        MakeConnection(MakeDatabaseName(project_id, instance_id, database_id)));
+
+    auto reader =
+        client_->ExecuteSql(MakeReadWriteTransaction(),
+                            SqlStatement("DELETE FROM Singers WHERE true;"));
+    // TODO(#360) - currently this fails with:
+    //   response metadata was missing row type information [INTERNAL]
+    // EXPECT_STATUS_OK(reader);
+  }
+
+ protected:
+  std::unique_ptr<Client> client_;
+};
+
+/// @test Verify the basic insert operations for transaction commits.
+TEST_F(MutateAndReadIntegrationTest, InsertAndCommit) {
+  auto commit_result = client_->Commit(
       MakeReadWriteTransaction(),
       {InsertMutationBuilder("Singers", {"SingerId", "FirstName", "LastName"})
            .EmplaceRow(1, "test-first-name-1", "test-last-name-1")
@@ -67,8 +136,8 @@ TEST(CommitIntegrationTest, Insert) {
            .Build()});
   EXPECT_STATUS_OK(commit_result);
 
-  auto reader = client.Read("Singers", KeySet::All(),
-                            {"SingerId", "FirstName", "LastName"});
+  auto reader = client_->Read("Singers", KeySet::All(),
+                              {"SingerId", "FirstName", "LastName"});
   // Cannot assert, we need to call DeleteDatabase() later.
   EXPECT_STATUS_OK(reader);
 
@@ -88,10 +157,45 @@ TEST(CommitIntegrationTest, Insert) {
               UnorderedElementsAre(
                   RowType(1, "test-first-name-1", "test-last-name-1"),
                   RowType(2, "test-first-name-2", "test-last-name-2")));
+}
 
-  auto drop_status =
-      admin_client.DropDatabase(project_id, instance_id, database_id);
-  EXPECT_STATUS_OK(drop_status);
+/// @test Verify the basic delete mutations work.
+TEST_F(MutateAndReadIntegrationTest, DeleteAndCommit) {
+  auto commit_result = client_->Commit(
+      MakeReadWriteTransaction(),
+      // TODO(#360) - we use InsertOrUpdate() because the DELETE fails.
+      {InsertOrUpdateMutationBuilder("Singers",
+                                     {"SingerId", "FirstName", "LastName"})
+           .EmplaceRow(1, "test-first-name-1", "test-last-name-1")
+           .EmplaceRow(2, "test-first-name-2", "test-last-name-2")
+           .Build()});
+  EXPECT_STATUS_OK(commit_result);
+
+  commit_result = client_->Commit(
+      MakeReadWriteTransaction(),
+      {MakeDeleteMutation("Singers", KeySetBuilder<Row<std::int64_t>>()
+                                         .Add(MakeRow(std::int64_t(1)))
+                                         .Build())});
+  EXPECT_STATUS_OK(commit_result);
+
+  auto reader = client_->Read("Singers", KeySet::All(),
+                              {"SingerId", "FirstName", "LastName"});
+  EXPECT_STATUS_OK(reader);
+
+  using RowType = Row<std::int64_t, std::string, std::string>;
+  std::vector<RowType> returned_rows;
+  if (reader) {
+    int row_number = 0;
+    for (auto& row : reader->Rows<std::int64_t, std::string, std::string>()) {
+      EXPECT_STATUS_OK(row);
+      if (!row) break;
+      SCOPED_TRACE("Parsing row[" + std::to_string(row_number++) + "]");
+      returned_rows.push_back(*std::move(row));
+    }
+  }
+
+  EXPECT_THAT(returned_rows, UnorderedElementsAre(RowType(
+                                 2, "test-first-name-2", "test-last-name-2")));
 }
 
 }  // namespace
@@ -99,3 +203,11 @@ TEST(CommitIntegrationTest, Insert) {
 }  // namespace spanner
 }  // namespace cloud
 }  // namespace google
+
+int main(int argc, char* argv[]) {
+  ::google::cloud::testing_util::InitGoogleMock(argc, argv);
+  (void)::testing::AddGlobalTestEnvironment(
+      new google::cloud::spanner::IntegrationTestEnvironment());
+
+  return RUN_ALL_TESTS();
+}
