bd2d140073fd3976394ef7b861b58f786c5a01f0
Marek Dopiera <marek@unoperate.com>
2019-10-03 22:59:36 +0200

    Fix badly named include guards.

--- google/cloud/storage/internal/parameter_pack.h
+++ google/cloud/storage/internal/parameter_pack.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_PARAMETER_PACK_H_
-#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_PARAMETER_PACK_H_
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_PARAMETER_PACK_H_
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_PARAMETER_PACK_H_
 
 #include "google/cloud/storage/version.h"
 
@@ -43,4 +43,4 @@ constexpr bool ContainsType() {
 }  // namespace cloud
 }  // namespace google
 
-#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_PARAMETER_PACK_H_
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_PARAMETER_PACK_H_
