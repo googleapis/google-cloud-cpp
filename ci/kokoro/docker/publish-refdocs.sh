22581fdc9a43d1a4f521ad1de82b08416ea8dfd9
Takashi Matsuo <tmatsuo@google.com>
2019-10-14 14:56:02 -0700

    cleanup: remove old refdocs config, update the release process (#3199)

--- ci/kokoro/docker/publish-refdocs.sh
+++ ci/kokoro/docker/publish-refdocs.sh
@@ -85,8 +85,6 @@ upload_docs() {
   return 0
 }
 
-upload_docs "google-cloud-common" "${BUILD_OUTPUT}/google/cloud/html" \
-  "${BRANCH}" "${CREDENTIALS_FILE}" "${STAGING_BUCKET}"
 upload_docs "google-cloud-bigtable" \
   "${BUILD_OUTPUT}/google/cloud/bigtable/html" "${BRANCH}" \
   "${CREDENTIALS_FILE}" "${STAGING_BUCKET}"
