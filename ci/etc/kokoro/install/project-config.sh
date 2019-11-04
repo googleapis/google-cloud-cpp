5f33e4af507c6f26fc9b16644c4e816225ba5ea6
Carlos O'Ryan <coryan@users.noreply.github.com>
2019-11-04 17:07:04 -0500

    ci: generate files in ci/kokoro/install (#3249)
    All the files except Dockerfile.ubuntu-trusty are automatically
    generated. This exception is caused by the complexity of building
    libcurl and gRPC with the same underlying OpenSSL library on this
    particular platform.

--- ci/etc/kokoro/install/project-config.sh
+++ ci/etc/kokoro/install/project-config.sh
@@ -26,6 +26,10 @@ declare -A ORIGINAL_COPYRIGHT_YEAR=(
   [ubuntu-bionic]=2018
 )
 
+declare -a FROZEN_FILES=(
+  "ci/kokoro/install/Dockerfile.ubuntu-trusty"
+)
+
 BUILD_AND_TEST_PROJECT_FRAGMENT=$(replace_fragments \
       "INSTALL_CRC32C_FROM_SOURCE" \
       "INSTALL_CPP_CMAKEFILES_FROM_SOURCE" \
@@ -63,7 +67,7 @@ BUILD_AND_TEST_PROJECT_FRAGMENT=$(replace_fragments \
 # all the Google Cloud C++ client libraries:
 
 # ```bash
-@INSTALL_GOOGLETEST_FROM_SOURCE@
+@INSTALL_GOOGLE_CLOUD_CPP_COMMON_FROM_SOURCE@
 # ```
 
 FROM devtools AS install
@@ -85,7 +89,7 @@ RUN cmake --build . --target install
 
 ## [END INSTALL.md]
 
-ENV PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
+ENV PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig
 
 # Verify that the installed files are actually usable
 WORKDIR /home/build/test-install-plain-make
