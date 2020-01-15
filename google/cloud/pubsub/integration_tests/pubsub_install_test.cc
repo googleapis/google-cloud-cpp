ac846402b551d077043e2d3c726cebd6b9367fd1
Carlos O'Ryan <coryan@google.com>
2020-01-15 16:17:55 -0500

    chore: create repository structure

---
+++ google/cloud/pubsub/integration_tests/pubsub_install_test.cc
@@ -0,0 +1,42 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/pubsub/version.h"
+#include <functional>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 1) {
+    std::string const cmd = argv[0];
+    auto last_slash = std::string(argv[0]).find_last_of('/');
+    std::cerr << "Usage: " << cmd.substr(last_slash + 1) << "\n";
+    return 1;
+  }
+
+  auto project_id =
+      google::cloud::internal::GetEnv("GOOGLE_CLOUD_PROJECT").value_or("");
+  if (project_id.empty()) {
+    throw std::runtime_error(
+        "The GOOGLE_CLOUD_PROJECT environment variable should be set to a "
+        "non-empty value");
+  }
+
+  // TODO(#...) - this is just a "does this link" test for now.
+  std::cout << google::cloud::pubsub::VersionString() << "\n";
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  drop_database();
+  return 1;
+}
