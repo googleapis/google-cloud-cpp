1a44f60dcf186ebe18be1b2a4e4e73f5d01af132
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-01-31 10:38:33 -0500

    chore: simplify include guard macros (googleapis/google-cloud-cpp-pubsub#19)
    Remove the trailing underscore, which makes it hard to use clang-tidy on
    header files.

--- google/cloud/pubsub/internal/build_info.h
+++ google/cloud/pubsub/internal/build_info.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H_
-#define GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H_
+#ifndef GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H
+#define GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H
 
 #include "google/cloud/pubsub/version.h"
 
@@ -43,4 +43,4 @@ std::string BuildMetadata();
 }  // namespace cloud
 }  // namespace google
 
-#endif  // GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H_
+#endif  // GOOGLE_CLOUD_CPP_PUBSUB_GOOGLE_CLOUD_PUBSUB_INTERNAL_BUILD_INFO_H
