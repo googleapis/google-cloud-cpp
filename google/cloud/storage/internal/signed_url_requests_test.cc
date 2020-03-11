487081e2ab19779965cc210e84cd888ebaaced0b
Marek Dopiera <marek@unoperate.com>
2020-03-11 19:24:03 +0100

    feat: allow domain named buckets in signed URLs v4
    This fixes #6874.

--- google/cloud/storage/internal/signed_url_requests_test.cc
+++ google/cloud/storage/internal/signed_url_requests_test.cc
@@ -530,6 +530,111 @@ TEST(V4SignedUrlRequests, VirtualHostnameWithBadHostname) {
   EXPECT_THAT(status.message(), HasSubstr("some-other-hostname"));
 }
 
+TEST(V4SignedUrlRequests, BucketBoundHostname) {
+  V4SignUrlRequest request("GET", "test-bucket", "test-object");
+  std::string const date = "2019-02-01T09:00:00Z";
+  auto const valid_for = std::chrono::seconds(10);
+  request.set_multiple_options(
+      SignedUrlTimestamp(google::cloud::internal::ParseRfc3339(date)),
+      SignedUrlDuration(valid_for), BucketBoundHostname("mydomain.tld"));
+  ASSERT_STATUS_OK(request.Validate());
+  request.AddMissingRequiredHeaders();
+  ASSERT_STATUS_OK(request.Validate());
+
+  std::string expected =
+      "GET\n"
+      "/test-object\n"
+      "X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=fake-client-id%"
+      "2F20190201%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date="
+      "20190201T090000Z&X-Goog-Expires=10&X-Goog-SignedHeaders=host\n"
+      "host:mydomain.tld\n"
+      "\n"
+      "host\n"
+      "UNSIGNED-PAYLOAD";
+  std::string actual = request.CanonicalRequest("fake-client-id");
+  EXPECT_EQ(expected, actual);
+}
+
+TEST(V4SignedUrlRequests, BucketBoundHostnameWithManualHostname) {
+  V4SignUrlRequest request("GET", "test-bucket", "test-object");
+  std::string const date = "2019-02-01T09:00:00Z";
+  auto const valid_for = std::chrono::seconds(10);
+  request.set_multiple_options(
+      SignedUrlTimestamp(google::cloud::internal::ParseRfc3339(date)),
+      SignedUrlDuration(valid_for), BucketBoundHostname("mydomain.tld"),
+      AddExtensionHeader("host", "mydomain.tld"));
+  ASSERT_STATUS_OK(request.Validate());
+  request.AddMissingRequiredHeaders();
+  ASSERT_STATUS_OK(request.Validate());
+
+  std::string expected =
+      "GET\n"
+      "/test-object\n"
+      "X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=fake-client-id%"
+      "2F20190201%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date="
+      "20190201T090000Z&X-Goog-Expires=10&X-Goog-SignedHeaders=host\n"
+      "host:mydomain.tld\n"
+      "\n"
+      "host\n"
+      "UNSIGNED-PAYLOAD";
+  std::string actual = request.CanonicalRequest("fake-client-id");
+  EXPECT_EQ(expected, actual);
+}
+
+TEST(V4SignedUrlRequests, BucketBoundHostnameWithBadHostname) {
+  V4SignUrlRequest request("GET", "test-bucket", "test-object");
+  std::string const date = "2019-02-01T09:00:00Z";
+  auto const valid_for = std::chrono::seconds(10);
+  request.set_multiple_options(
+      SignedUrlTimestamp(google::cloud::internal::ParseRfc3339(date)),
+      SignedUrlDuration(valid_for), BucketBoundHostname("mydomain.tld"),
+      AddExtensionHeader("host", "some-other-hostname"));
+  auto status = request.Validate();
+  ASSERT_FALSE(status.ok());
+  EXPECT_EQ(StatusCode::kInvalidArgument, status.code());
+  EXPECT_THAT(status.message(), HasSubstr("some-other-hostname"));
+}
+
+TEST(V4SignedUrlRequests, BucketBoundHostnameXORVirtualHostname) {
+  V4SignUrlRequest request("GET", "test-bucket", "test-object");
+  std::string const date = "2019-02-01T09:00:00Z";
+  auto const valid_for = std::chrono::seconds(10);
+  request.set_multiple_options(
+      SignedUrlTimestamp(google::cloud::internal::ParseRfc3339(date)),
+      SignedUrlDuration(valid_for), BucketBoundHostname("mydomain.tld"),
+      VirtualHostname(true));
+  auto status = request.Validate();
+  ASSERT_FALSE(status.ok());
+  EXPECT_EQ(StatusCode::kInvalidArgument, status.code());
+  EXPECT_THAT(status.message(), HasSubstr("simultaneously"));
+}
+
+TEST(V4SignedUrlRequests, BucketBoundHostnameReset) {
+  V4SignUrlRequest request("GET", "test-bucket", "test-object");
+  std::string const date = "2019-02-01T09:00:00Z";
+  auto const valid_for = std::chrono::seconds(10);
+  request.set_multiple_options(
+      SignedUrlTimestamp(google::cloud::internal::ParseRfc3339(date)),
+      SignedUrlDuration(valid_for), BucketBoundHostname("mydomain.tld"));
+  request.set_multiple_options(BucketBoundHostname{});
+  ASSERT_STATUS_OK(request.Validate());
+  request.AddMissingRequiredHeaders();
+  ASSERT_STATUS_OK(request.Validate());
+
+  std::string expected =
+      "GET\n"
+      "/test-bucket/test-object\n"
+      "X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=fake-client-id%"
+      "2F20190201%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date="
+      "20190201T090000Z&X-Goog-Expires=10&X-Goog-SignedHeaders=host\n"
+      "host:storage.googleapis.com\n"
+      "\n"
+      "host\n"
+      "UNSIGNED-PAYLOAD";
+  std::string actual = request.CanonicalRequest("fake-client-id");
+  EXPECT_EQ(expected, actual);
+}
+
 TEST(DefaultCtorsWork, Trivial) {
   EXPECT_FALSE(ExpirationTime().has_value());
   EXPECT_FALSE(AddExtensionHeaderOption().has_value());
@@ -540,6 +645,7 @@ TEST(DefaultCtorsWork, Trivial) {
   EXPECT_FALSE(SigningAccount().has_value());
   EXPECT_FALSE(SigningAccountDelegates().has_value());
   EXPECT_FALSE(VirtualHostname().has_value());
+  EXPECT_FALSE(BucketBoundHostname().has_value());
 }
 
 }  // namespace
