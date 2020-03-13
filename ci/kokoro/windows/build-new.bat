08a821516e8c9e915db20fa5ce6ade3d0e8c8c6d
Carlos O'Ryan <coryan@google.com>
2020-03-13 17:39:03 +0000

    Fix separator.

--- ci/kokoro/windows/build-new.bat
+++ ci/kokoro/windows/build-new.bat
@@ -16,7 +16,7 @@ REM Install Bazel using Chocolatey.
 choco install -y bazel --version 2.0.0
 
 REM Change PATH to use chocolatey's version of Bazel
-set PATH=C:\ProgramData\chocolatey\bin:%PATH%
+set PATH=C:\ProgramData\chocolatey\bin%PATH%
 bazel version
 
 REM Configure the environment to use MSVC 2019 and then switch to PowerShell.
