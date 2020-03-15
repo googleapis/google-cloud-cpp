f00253292b2c7f74279859ae6053d03df68ec8db
Carlos O'Ryan <coryan@google.com>
2020-03-15 07:54:09 -0400

    Address review comments.

--- ci/kokoro/docker/build-in-docker-bazel.sh
+++ ci/kokoro/docker/build-in-docker-bazel.sh
@@ -36,7 +36,7 @@ source "${PROJECT_ROOT}/ci/colors.sh"
 # ci/Dockerfile.* build scripts.
 
 echo
-echo "${COLOR_YELLOW}$(date -u): Starting docker build $(date) with ${NCPU} cores${COLOR_RESET}"
+echo "${COLOR_YELLOW}$(date -u): Starting docker build with ${NCPU} cores${COLOR_RESET}"
 echo
 
 echo "================================================================"
