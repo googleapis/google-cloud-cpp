72c6b189a7cf5ba5f6e37192df475bcce7a35178
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-03-15 14:33:12 -0400

    ci: add integration build to ci/kokoro/docker (#3477)
    This build with eventually replace the builds in ci/kokoro/ubuntu, and
    will bring this repository (mostly) inline with the other repositories.

--- ci/kokoro/docker/build-in-docker-bazel.sh
+++ ci/kokoro/docker/build-in-docker-bazel.sh
@@ -36,12 +36,12 @@ source "${PROJECT_ROOT}/ci/colors.sh"
 # ci/Dockerfile.* build scripts.
 
 echo
-echo "${COLOR_YELLOW}Starting docker build $(date) with ${NCPU} cores${COLOR_RESET}"
+echo "${COLOR_YELLOW}$(date -u): Starting docker build with ${NCPU} cores${COLOR_RESET}"
 echo
 
 echo "================================================================"
 readonly BAZEL_BIN="/usr/local/bin/bazel"
-echo "Using Bazel in ${BAZEL_BIN}"
+echo "$(date -u): Using Bazel in ${BAZEL_BIN}"
 "${BAZEL_BIN}" version
 echo "================================================================"
 
@@ -55,20 +55,20 @@ if [[ -n "${BAZEL_CONFIG}" ]]; then
 fi
 
 echo "================================================================"
-echo "Fetching dependencies $(date)"
+echo "$(date -u): Fetching dependencies"
 echo "================================================================"
 "${PROJECT_ROOT}/ci/retry-command.sh" \
     "${BAZEL_BIN}" fetch -- //google/cloud/...:all
 
 echo "================================================================"
-echo "Compiling and running unit tests $(date)"
+echo "$(date -u): Compiling and running unit tests"
 echo "================================================================"
 "${BAZEL_BIN}" test \
     "${bazel_args[@]}" \
     -- //google/cloud/...:all
 
 echo "================================================================"
-echo "Compiling all the code, including integration tests $(date)"
+echo "$(date -u): Compiling all the code, including integration tests"
 echo "================================================================"
 # Then build everything else (integration tests, examples, etc). So we can run
 # them next.
@@ -76,8 +76,126 @@ echo "================================================================"
     "${bazel_args[@]}" \
     -- //google/cloud/...:all
 
+readonly INTEGRATION_TESTS_CONFIG="/c/test-configuration.sh"
+readonly TEST_KEY_FILE_JSON="/c/service-account.json"
+readonly TEST_KEY_FILE_P12="/c/service-account.p12"
+readonly GOOGLE_APPLICATION_CREDENTIALS="/c/service-account.json"
+# yes: always try to run integration tests
+# auto: only try to run integration tests if the config file is executable.
+if [[ "${RUN_INTEGRATION_TESTS}" == "yes" || \
+      ( "${RUN_INTEGRATION_TESTS}" == "auto" && \
+        -r "${INTEGRATION_TESTS_CONFIG}" && \
+        -r "${TEST_KEY_FILE_JSON}" && \
+        -r "${TEST_KEY_FILE_P12}" && \
+        -r "${GOOGLE_APPLICATION_CREDENTIALS}" ) ]]; then
+  echo "================================================================"
+  echo "$(date -u): Running the integration tests"
+  echo "================================================================"
+  # shellcheck disable=SC1091
+  source "${INTEGRATION_TESTS_CONFIG}"
+
+  export INTEGRATION_TESTS_CONFIG
+  export TEST_KEY_FILE_JSON
+  export TEST_KEY_FILE_P12
+  export GOOGLE_APPLICATION_CREDENTIALS
+
+  BAZEL_BIN_DIR="$("${BAZEL_BIN}" info bazel-bin)"
+  readonly BAZEL_BIN_DIR
+
+  # Add gcloud to the PATH
+  PATH="/usr/local/google-cloud-sdk/bin:${PATH}"
+  export PATH
+
+  gcloud --quiet auth activate-service-account --key-file \
+      "${GOOGLE_APPLICATION_CREDENTIALS}"
+  # This is used in a Bigtable example showing how to use access tokens to
+  # create a grpc::Credentials object.
+  ACCESS_TOKEN="$(gcloud --quiet auth print-access-token)"
+  export ACCESS_TOKEN
+  # Deactivate all the accounts in `gcloud` to prevent accidents
+  gcloud --quiet auth revoke --all
+
+  if [[ "${ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS:-}" = "yes" ]]; then
+    echo
+    echo "================================================================"
+    echo "$(date -u): Running Google Cloud Bigtable Integration Tests"
+    echo "================================================================"
+    (cd "${BAZEL_BIN_DIR}/google/cloud/bigtable/tests" && \
+       "${PROJECT_ROOT}/google/cloud/bigtable/tests/run_admin_integration_tests_production.sh")
+    (cd "${BAZEL_BIN_DIR}/google/cloud/bigtable/examples" && \
+       "${PROJECT_ROOT}/google/cloud/bigtable/examples/run_admin_examples_production.sh")
+  fi
+
+  echo
+  echo "================================================================"
+  echo "$(date -u): Running Google Cloud Bigtable Integration Tests"
+  echo "================================================================"
+  (cd "${BAZEL_BIN_DIR}/google/cloud/bigtable/tests" && \
+      "${PROJECT_ROOT}/google/cloud/bigtable/tests/run_integration_tests_production.sh")
+  (cd "${BAZEL_BIN_DIR}/google/cloud/bigtable/examples" && \
+      "${PROJECT_ROOT}/google/cloud/bigtable/examples/run_examples_production.sh")
+  (cd "${BAZEL_BIN_DIR}/google/cloud/bigtable/examples" && \
+      "${PROJECT_ROOT}/google/cloud/bigtable/examples/run_grpc_credential_examples_production.sh")
+
+  echo
+  echo "================================================================"
+  echo "$(date -u): Create service account to run the storage HMAC tests."
+  echo "================================================================"
+  # Recall that each evaluation of ${RANDOM} produces a different value.
+  HMAC_SERVICE_ACCOUNT_NAME="hmac-sa-$(date +%s)-${RANDOM}"
+  HMAC_SERVICE_ACCOUNT="${HMAC_SERVICE_ACCOUNT_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+  export HMAC_SERVICE_ACCOUNT
+
+  gcloud --quiet auth activate-service-account --key-file \
+      "${GOOGLE_APPLICATION_CREDENTIALS}"
+  gcloud --quiet iam service-accounts create "--project=${PROJECT_ID}" \
+      "${HMAC_SERVICE_ACCOUNT_NAME}"
+  gcloud --quiet projects add-iam-policy-binding "${PROJECT_ID}" \
+      --member "serviceAccount:${HMAC_SERVICE_ACCOUNT}" \
+      --role roles/iam.serviceAccountTokenCreator
+  # Deactivate all the accounts in `gcloud` to prevent accidents
+  gcloud --quiet auth revoke --all
+
+  echo
+  echo "================================================================"
+  echo "$(date -u): Running Google Cloud Storage Integration Tests $(date)"
+  echo "================================================================"
+  set +e
+  (cd "${BAZEL_BIN_DIR}/google/cloud/storage/tests" && \
+      "${PROJECT_ROOT}/google/cloud/storage/tests/run_integration_tests_production.sh")
+  storage_integration_test_status=$?
+  echo "Running Google Cloud Storage Examples"
+  (cd "${BAZEL_BIN_DIR}/google/cloud/storage/examples" && \
+      "${PROJECT_ROOT}/google/cloud/storage/examples/run_examples_production.sh")
+  storage_examples_status=$?
+  set -e
+
+  echo
+  echo "================================================================"
+  echo "$(date -u): Delete service account to used in the storage HMAC tests."
+  echo "================================================================"
+  gcloud --quiet auth activate-service-account --key-file \
+      "${GOOGLE_APPLICATION_CREDENTIALS}"
+  gcloud --quiet projects remove-iam-policy-binding "${PROJECT_ID}" \
+      --member "serviceAccount:${HMAC_SERVICE_ACCOUNT}" \
+      --role roles/iam.serviceAccountTokenCreator
+  gcloud --quiet iam service-accounts delete --quiet "${HMAC_SERVICE_ACCOUNT}"
+  # Deactivate all the accounts in `gcloud` to prevent accidents
+  gcloud --quiet auth revoke --all
+
+  if [[ "${storage_integration_test_status}" != 0 ]]; then
+    echo "$(date -u): Error in storage integration tests."
+    exit 1
+  fi
+
+  if [[ "${storage_examples_status}" != 0 ]]; then
+    echo "$(date -u): Error in storage examples."
+    exit 1
+  fi
+fi
+
 echo "================================================================"
-echo "Build finished at $(date)"
+echo "$(date -u): Build finished successfully"
 echo "================================================================"
 
 exit 0
--- ci/kokoro/docker/build.sh
+++ ci/kokoro/docker/build.sh
@@ -71,6 +71,17 @@ elif [[ "${BUILD_NAME}" = "coverage" ]]; then
   export DISTRO=fedora-install
   export DISTRO_VERSION=31
   export BUILD_TYPE=Coverage
+elif [[ "${BUILD_NAME}" = "integration" ]]; then
+  export DISTRO=ubuntu
+  export DISTRO_VERSION=18.04
+  RUN_INTEGRATION_TESTS=auto
+  in_docker_script="ci/kokoro/docker/build-in-docker-bazel.sh"
+elif [[ "${BUILD_NAME}" = "integration-nightly" ]]; then
+  export DISTRO=ubuntu
+  export DISTRO_VERSION=18.04
+  RUN_INTEGRATION_TESTS=auto
+  ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS="yes"
+  in_docker_script="ci/kokoro/docker/build-in-docker-bazel.sh"
 elif [[ "${BUILD_NAME}" = "publish-refdocs" ]]; then
   export BUILD_TYPE=Debug
   export CC=clang
@@ -342,6 +353,11 @@ docker_flags=(
     # Additional flags to enable CMake features.
     "--env" "CMAKE_FLAGS=${CMAKE_FLAGS:-}"
 
+    # When running the integration tests this directory contains the
+    # configuration files needed to run said tests. Make it available inside
+    # the Docker container.
+    "--volume" "${KOKORO_GFILE_DIR:-/dev/shm}:/c"
+
     # The type of the build for Bazel.
     "--env" "BAZEL_CONFIG=${BAZEL_CONFIG:-}"
 
@@ -376,6 +392,10 @@ docker_flags=(
 
     # If set to 'no', skip the integration tests.
     "--env" "RUN_INTEGRATION_TESTS=${RUN_INTEGRATION_TESTS:-}"
+
+    # The Bigtable Admin integration tests can only run in nightly builds, the
+    # quota (as in calls per day) is too restrictive to run more often.
+    "--env" "ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS=${ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS:-no}"
 
     # If set, run the scripts to generate Doxygen docs. Note that the scripts
     # to upload said docs are not part of the build, they run afterwards on
