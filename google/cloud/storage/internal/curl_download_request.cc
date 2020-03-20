caa14a93feb738416a8cdc89430183886c29496e
Scott Hart <sdhart@google.com>
2020-03-20 16:59:31 -0400

    clang tidy fix

--- google/cloud/storage/internal/curl_download_request.cc
+++ google/cloud/storage/internal/curl_download_request.cc
@@ -149,7 +149,7 @@ StatusOr<ReadSourceResult> CurlDownloadRequest::Read(char* buf, std::size_t n) {
 
   handle_.FlushDebug(__func__);
   TRACE_STATE();
-  
+
 #if LIBCURL_VERSION_NUM >= 0x074500
   if (!curl_closed_ && paused_) {
 #else
