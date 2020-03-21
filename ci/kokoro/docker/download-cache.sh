c0989e2769d0c584185980107563eacd49811571
Carlos O'Ryan <coryan@google.com>
2020-03-21 11:02:44 -0400

    WIP - do not fix Bazel cache timestamps

--- ci/kokoro/docker/download-cache.sh
+++ ci/kokoro/docker/download-cache.sh
@@ -43,6 +43,8 @@ set -v
 gcloud --quiet auth activate-service-account --key-file "${KEYFILE}"
 gsutil -q cp "gs://${CACHE_FOLDER}/${CACHE_NAME}.tar.gz" "${HOME_DIR}"
 gcloud --quiet auth revoke --all || echo "Ignore revoke failure"
-tar --touch -zxf "${HOME_DIR}/${CACHE_NAME}.tar.gz"
+# Ignore timestamp warnings, Bazel has files with timestamps 10 years
+# into the future :shrug:
+tar -zxf "${HOME_DIR}/${CACHE_NAME}.tar.gz" 2>&1 | egrep -v 'tar:.*in the future'
 
 exit 0
