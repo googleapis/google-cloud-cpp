498cd15100bea33f86ec6d57db77c3f03dba789b
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-03-31 12:43:38 -0400

    ci: actually update the vcpkg cache (#3619)
    I disabled the vcpkg cache upload for continuous builds too, that
    was a mistake.

--- ci/kokoro/windows/build-cmake-dependencies.ps1
+++ ci/kokoro/windows/build-cmake-dependencies.ps1
@@ -118,8 +118,9 @@ Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) vcpkg list"
 # Do not update the vcpkg cache on PRs, it might dirty the cache for any
 # PRs running in parallel, and it is a waste of time in most cases.
 $IsPR = (Test-Path env:KOKORO_JOB_TYPE) -and `
-    ($env:KOKORO_JOB_TYPE = "GITHUB_PULL_REQUEST")
-if (-not $IsPR -and (Test-Path env:BUILD_CACHE)) {
+    ($env:KOKORO_JOB_TYPE -eq "GITHUB_PULL_REQUEST")
+$HasBuildCache = (Test-Path env:BUILD_CACHE)
+if ((-not $IsPR) -and $HasBuildCache) {
     Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
       "zip vcpkg cache for upload."
     7z a vcpkg-installed.zip installed\ -bsp0
@@ -137,4 +138,7 @@ if (-not $IsPR -and (Test-Path env:BUILD_CACHE)) {
                 "continue despite upload failure with code ${LastExitCode}".
         }
     }
+} else {
+    Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+      "vcpkg not updated IsPR == $IsPR, HasBuildCache == $HasBuildCache."
 }
