81fc1650a40169bfd4c8c66c701231bf44211733
Carlos O'Ryan <coryan@google.com>
2020-04-07 13:30:58 -0400

    refactor: run bigtable_instance_admin_snippets without scripts

--- google/cloud/bigtable/examples/bigtable_autorun_examples.bzl
+++ google/cloud/bigtable/examples/bigtable_autorun_examples.bzl
@@ -20,6 +20,7 @@ bigtable_autorun_examples = [
     "bigtable_grpc_credentials.cc",
     "bigtable_hello_app_profile.cc",
     "bigtable_hello_table_admin.cc",
+    "bigtable_instance_admin_snippets.cc",
     "data_snippets.cc",
     "table_admin_snippets.cc",
 ]
--- google/cloud/bigtable/examples/run_admin_examples_production.sh
+++ google/cloud/bigtable/examples/run_admin_examples_production.sh
@@ -45,9 +45,6 @@ run_hello_world_example "${PROJECT_ID}" "${INSTANCE_ID}"
 echo "${COLOR_YELLOW}run_all_table_admin_async_examples${COLOR_RESET}"
 run_all_table_admin_async_examples "${PROJECT_ID}" "${ZONE_A}" "${ZONE_B}"
 
-echo "${COLOR_YELLOW}run_all_instance_admin_examples${COLOR_RESET}"
-run_all_instance_admin_examples "${PROJECT_ID}" "${ZONE_A}" "${ZONE_B}"
-
 echo "${COLOR_YELLOW}run_all_instance_admin_async_examples${COLOR_RESET}"
 run_all_instance_admin_async_examples "${PROJECT_ID}" "${ZONE_A}" "${ZONE_B}"
 
