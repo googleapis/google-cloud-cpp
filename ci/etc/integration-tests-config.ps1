a59cc9fe50f3b5f68fe6707dd921b40521ff0705
Carlos O'Ryan <coryan@google.com>
2020-04-09 15:21:50 +0000

    ci: test quickstart instructions for Windows+Bazel

---
+++ ci/etc/integration-tests-config.ps1
@@ -0,0 +1,41 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Common configuration parameters.
+#
+$env:GOOGLE_CLOUD_PROJECT="cloud-cpp-testing-resources"
+
+# Cloud Bigtable configuration parameters
+$env:GOOGLE_CLOUD_CPP_BIGTABLE_TEST_INSTANCE_ID="test-instance"
+$env:GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_A="us-west2-b"
+$env:GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_B="us-west2-c"
+$env:GOOGLE_CLOUD_CPP_BIGTABLE_TEST_SERVICE_ACCOUNT="bigtable-test-iam-sa@${env:GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+
+# Cloud Storage configuration parameters
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME="cloud-cpp-testing-bucket"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_DESTINATION_BUCKET_NAME="cloud-cpp-testing-regional"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_REGION_ID="us-central1"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_LOCATION="${env:GOOGLE_CLOUD_CPP_STORAGE_TEST_REGION_ID}"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_SERVICE_ACCOUNT="storage-test-iam-sa@${env:GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_HMAC_SERVICE_ACCOUNT="hmac-keys-test-service-account@${env:GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_SERVICE_ACCOUNT="kokoro-agent@${env:GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_CMEK_KEY="projects/${env:GOOGLE_CLOUD_PROJECT}/locations/us/keyRings/gcs-testing-us-kr/cryptoKeys/integration-tests-key"
+$env:GOOGLE_CLOUD_CPP_STORAGE_TEST_TOPIC_NAME="projects/${env:GOOGLE_CLOUD_PROJECT}/topics/gcs-changes"
+
+# Cloud Spanner configuration parameters
+$env:GOOGLE_CLOUD_CPP_SPANNER_TEST_INSTANCE_ID="test-instance"
+$env:GOOGLE_CLOUD_CPP_SPANNER_TEST_SERVICE_ACCOUNT="spanner-iam-test-sa@${env:GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+
+# Cloud Pub/Sub only needs GOOGLE_CLOUD_PROJECT
---
+++ ci/kokoro/windows/build-quickstart-bazel.ps1
@@ -0,0 +1,206 @@
+# !/usr/bin/env powershell
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Stop on errors. This is similar to `set -e` on Unix shells.
+$ErrorActionPreference = "Stop"
+
+# Create output directory for Bazel. Bazel creates really long paths,
+# sometimes exceeding the Windows limits. Using a short name for the
+# root of the Bazel output directory works around this problem.
+$bazel_root="C:\b"
+if (-not (Test-Path $bazel_root)) {
+    Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) Create bazel user root (${bazel_root})"
+    New-Item -ItemType Directory -Path $bazel_root | Out-Null
+}
+
+$common_flags = @("--output_user_root=${bazel_root}")
+
+Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) Capture Bazel information for troubleshooting"
+bazel $common_flags version
+bazel $common_flags shutdown
+
+$GOOGLE_CLOUD_CPP_REPOSITORY="google-cloud-cpp"
+$CACHE_BUCKET=if(Test-Path env:GOOGLE_CLOUD_CPP_KOKORO_RESULTS) `
+    {$env:GOOGLE_CLOUD_CPP_KOKORO_RESULTS} else {"cloud-cpp-kokoro-results"}
+$BRANCH_NAME="master"
+$CACHE_FOLDER="${CACHE_BUCKET}/build-cache/${GOOGLE_CLOUD_CPP_REPOSITORY}/${BRANCH_NAME}"
+# Bazel creates many links (aka NTFS JUNCTIONS) and only .tgz files seem to
+# support those well.
+$CACHE_BASENAME="cache-windows-quickstart-bazel"
+
+$IsPR = (Test-Path env:KOKORO_JOB_TYPE) -and `
+    ($env:KOKORO_JOB_TYPE -eq "PRESUBMIT_GITHUB")
+$CacheConfigured = (Test-Path env:KOKORO_GFILE_DIR) -and `
+    (Test-Path "${env:KOKORO_GFILE_DIR}/build-results-service-account.json")
+$Has7z = Get-Command "7z" -ErrorAction SilentlyContinue
+
+# Shutdown the Bazel server to release any locks
+Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+    "Shutting down Bazel server"
+bazel $common_flags shutdown
+
+$download_dir = "T:\tmp"
+if (Test-Path env:TEMP) {
+    $download_dir="${env:TEMP}"
+} elseif (-not $download_dir) {
+    Make-Item -Type "Directory" ${download_dir}
+}
+if ($IsPR -and $CacheConfigured -and $Has7z) {
+    gcloud auth activate-service-account --key-file `
+        "${env:KOKORO_GFILE_DIR}/build-results-service-account.json"
+    Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+        "downloading Bazel cache."
+    gsutil -q cp "gs://${CACHE_FOLDER}/${CACHE_BASENAME}.tar" "${download_dir}"
+    if ($LastExitCode) {
+        # Ignore errors, caching failures should not break the build.
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "gsutil download failed with exit code ${LastExitCode}."
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "continue building without a cache"
+    } else {
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "extracting build cache."
+        $extract_flags=@(
+            # Preserve full path
+            "-spf",
+            # Preserve symbolic links
+            "-snl",
+            # Preserve hard links
+            "-snh",
+            # Overwrite all items
+            "-aoa",
+            # Suppress progress messages
+            "-bsp0",
+            # Suppress typicaly output messages
+            "-bso0"
+        )
+        7z x "${download_dir}/${CACHE_BASENAME}.tar" ${extract_flags}
+        if ($LastExitCode) {
+            # Ignore errors, caching failures should not break the build.
+            Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+                "extracting build cache failed with exit code ${LastExitCode}."
+            Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+                "Continue building without a cache"
+        }
+    }
+}
+
+$env:BAZEL_VC="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
+
+$project_root = (Get-Item -Path ".\" -Verbose).FullName
+# If at all possible, load the configuration for the integration tests and
+# set ${env:RUN_INTEGRATION_TESTS} to "true"
+if (-not (Test-Path env:KOKORO_GFILE_DIR)) {
+    ${env:RUN_INTEGRATION_TESTS}=""
+} else {
+    $integration_tests_config="${project_root}/ci/etc/integration-tests-config.ps1"
+    $test_key_file_json="${env:KOKORO_GFILE_DIR}/service-account.json"
+    $test_key_file_p12="${env:KOKORO_GFILE_DIR}/service-account.p12"
+    if ((Test-Path "${integration_tests_config}") -and
+        (Test-Path "${test_key_file_json}") -and 
+        (Test-Path "${test_key_file_p12}")) {
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) Loading integration tests config"
+        . "${integration_tests_config}"
+        ${env:GOOGLE_APPLICATION_CREDENTIALS}="${test_key_file_json}"
+        ${env:GOOGLE_CLOUD_CPP_AUTO_RUN_EXAMPLES}="yes"
+        (New-Object System.Net.WebClient).Downloadfile(
+            'https://raw.githubusercontent.com/grpc/grpc/master/etc/roots.pem',
+             "${env:KOKORO_GFILE_DIR}/roots.pem")
+        ${env:GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}="${env:KOKORO_GFILE_DIR}/roots.pem"
+        ${env:RUN_INTEGRATION_TESTS}="true"
+    }
+}
+
+$quickstart_args=@{
+    "storage"=@("${env:GOOGLE_CLOUD_PROJECT}", "${env:GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME}");
+    "bigtable"=@("${env:GOOGLE_CLOUD_PROJECT}", "${env:GOOGLE_CLOUD_CPP_BIGTABLE_TEST_INSTANCE_ID}", "quickstart")
+}
+
+ForEach($library in ("bigtable", "storage")) {
+    Set-Location "${project_root}/google/cloud/${library}/quickstart"
+    ForEach($_ in (1, 2, 3)) {
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "Fetch dependencies for ${library} [$_]"
+        bazel $common_flags fetch -- ...:all
+        if ($LastExitCode -eq 0) {
+            break
+        }
+    }
+
+    Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+        "Compiling quickstart for ${library}"
+    bazel $common_flags build ...
+    if ($LastExitCode) {
+        throw "bazel test failed with exit code ${LastExitCode}."
+    }
+
+    if ((Test-Path env:RUN_INTEGRATION_TESTS) -and ($env:RUN_INTEGRATION_TESTS -eq "true")) {
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "Running quickstart for ${library}"
+        bazel $common_flags run "--spawn_strategy=local" `
+            ":quickstart" -- $quickstart_args[${library}]
+        if ($LastExitCode) {
+            throw "Integration tests failed with exit code ${LastExitCode}."
+        }
+    }
+}
+Set-Location "${project_root}"
+
+# Shutdown the Bazel server to release any locks
+Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) Shutting down Bazel server"
+bazel $common_flags shutdown
+
+if ((-not $IsPR) -and $CacheConfigured -and $Has7z) {
+    Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) Updating Bazel cache"
+    # We use 7z because it knows how to handle locked files better than Unix
+    # tools like tar(1).
+    $archive_flags=@(
+        # Preserve hard links
+        "-snh",
+        # Preserve soft links
+        "-snl",
+        # Preserve full path
+        "-spf",
+        # Exclude directories named "install"
+        "-xr!install",
+        # Suppress errors
+        "-bse0",
+        # Suppress progress
+        "-bsp0",
+        # Suppress standard logging
+        "-bso0"
+    )
+    7z a "${download_dir}\${CACHE_BASENAME}.tar" "${bazel_root}" ${archive_flags}
+    if ($LastExitCode) {
+        # Ignore errors, caching failures should not break the build.
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "zipping cache contents failed with exit code ${LastExitCode}."
+    } else {
+        gcloud auth activate-service-account --key-file `
+            "${env:KOKORO_GFILE_DIR}/build-results-service-account.json"
+        Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+            "uploading Bazel cache."
+        gsutil -q cp "${download_dir}\${CACHE_BASENAME}.tar" `
+            "gs://${CACHE_FOLDER}/${CACHE_BASENAME}.tar"
+        if ($LastExitCode) {
+            Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+                "uploading cache failed exit code ${LastExitCode}."
+            Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) " `
+                "cache not updated, this will not cause a build failure."
+        }
+    }
+}
+
+Write-Host -ForegroundColor Yellow "`n$(Get-Date -Format o) DONE"
