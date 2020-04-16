ee35766782892fd6835bb39068ad4f66ae03fd7a
Carlos O'Ryan <coryan@google.com>
2020-04-16 07:55:22 -0400

    refactor: run bigtable_hello_instance_admin without scripts
    This was the last example for bigtable needing driver scripts, so I
    removed all the support scripts and call sites to them in the builds.
    Yay!

--- google/cloud/bigtable/ci/run_integration_tests.sh
+++ google/cloud/bigtable/ci/run_integration_tests.sh
@@ -23,15 +23,6 @@ source "${BINDIR}/../../../../ci/colors.sh"
 
 readonly BTDIR="google/cloud/bigtable"
 
-if [ -d ${BTDIR}/examples ]; then
-  (cd "${BTDIR}/examples" && "${BINDIR}/../examples/run_examples_emulator.sh")
-else
-  # For some builds (notably those without exceptions) the directory is not
-  # even created, so we have to skip it completely.
-  echo "${COLOR_YELLOW}[ SKIPPED  ]${COLOR_RESET} bigtable examples" \
-    " as the examples are not compiled for this build"
-fi
-
 # To improve coverage (and avoid bitrot), run the Bigtable benchmarks using the
 # embedded server.  The performance in the Travis and AppVeyor CI builds is not
 # consistent enough to use the results, but we want to detect crashes and ensure
--- google/cloud/bigtable/examples/bigtable_examples.bzl
+++ google/cloud/bigtable/examples/bigtable_examples.bzl
@@ -17,5 +17,15 @@
 """Automatically generated unit tests list - DO NOT EDIT."""
 
 bigtable_examples = [
+    "bigtable_grpc_credentials.cc",
+    "bigtable_hello_app_profile.cc",
     "bigtable_hello_instance_admin.cc",
+    "bigtable_hello_table_admin.cc",
+    "bigtable_hello_world.cc",
+    "bigtable_instance_admin_snippets.cc",
+    "data_async_snippets.cc",
+    "data_snippets.cc",
+    "instance_admin_async_snippets.cc",
+    "table_admin_async_snippets.cc",
+    "table_admin_snippets.cc",
 ]
