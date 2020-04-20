e97bc776da4f6792bfcd56cf2d8fb7964372241e
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-04-20 16:37:11 -0400

    ci: enable integration tests for macOS+Bazel (#3866)

--- ci/kokoro/macos/build-bazel.sh
+++ ci/kokoro/macos/build-bazel.sh
@@ -81,3 +81,62 @@ echo "================================================================"
 log_yellow "build all targets."
 "${BAZEL_BIN}" build \
   "${bazel_args[@]}" -- //google/cloud/...:all
+
+readonly CONFIG_DIR="${KOKORO_GFILE_DIR:-/private/var/tmp}"
+readonly INTEGRATION_TESTS_CONFIG="${PROJECT_ROOT}/ci/etc/integration-tests-config.sh"
+readonly TEST_KEY_FILE_JSON="${CONFIG_DIR}/kokoro-run-key.json"
+readonly TEST_KEY_FILE_P12="${CONFIG_DIR}/kokoro-run-key.p12"
+
+should_run_integration_tests() {
+  if [[ -r "${INTEGRATION_TESTS_CONFIG}" && -r \
+    "${GOOGLE_APPLICATION_CREDENTIALS}" && -r \
+    "${TEST_KEY_FILE_JSON}" && -r \
+    "${TEST_KEY_FILE_P12}" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+if should_run_integration_tests; then
+  echo
+  echo "================================================================"
+  log_yellow "running integration tests."
+
+  source "${INTEGRATION_TESTS_CONFIG}"
+  bazel_args+=(
+    # Common configuration
+    "--test_env=GRPC_DEFAULT_SSL_ROOTS_FILE_PATH=${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}"
+    "--test_env=GOOGLE_APPLICATION_CREDENTIALS=${CONFIG_DIR}/kokoro-run-key.json"
+    "--test_env=GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT}"
+    "--test_env=GOOGLE_CLOUD_CPP_AUTO_RUN_EXAMPLES=yes"
+
+    # Bigtable
+    "--test_env=GOOGLE_CLOUD_CPP_BIGTABLE_TEST_INSTANCE_ID=${GOOGLE_CLOUD_CPP_BIGTABLE_TEST_INSTANCE_ID}"
+    "--test_env=GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_A=${GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_A}"
+    "--test_env=GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_B=${GOOGLE_CLOUD_CPP_BIGTABLE_TEST_ZONE_B}"
+    "--test_env=GOOGLE_CLOUD_CPP_BIGTABLE_TEST_SERVICE_ACCOUNT=${GOOGLE_CLOUD_CPP_BIGTABLE_TEST_SERVICE_ACCOUNT}"
+    "--test_env=ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS=${ENABLE_BIGTABLE_ADMIN_INTEGRATION_TESTS:-no}"
+
+    # Storage
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME=${GOOGLE_CLOUD_CPP_STORAGE_TEST_BUCKET_NAME}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_DESTINATION_BUCKET_NAME=${GOOGLE_CLOUD_CPP_STORAGE_TEST_DESTINATION_BUCKET_NAME}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_REGION_ID=${GOOGLE_CLOUD_CPP_STORAGE_TEST_REGION_ID}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_TOPIC_NAME=${GOOGLE_CLOUD_CPP_STORAGE_TEST_TOPIC_NAME}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_SERVICE_ACCOUNT=${GOOGLE_CLOUD_CPP_STORAGE_TEST_SERVICE_ACCOUNT}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_SERVICE_ACCOUNT=${GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_SERVICE_ACCOUNT}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_CMEK_KEY=${GOOGLE_CLOUD_CPP_STORAGE_TEST_CMEK_KEY}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_KEYFILE=${PROJECT_ROOT}/google/cloud/storage/tests/test_service_account.not-a-test.json"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_CONFORMANCE_FILENAME=${PROJECT_ROOT}/google/cloud/storage/tests/v4_signatures.json"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_KEY_FILE_JSON=${TEST_KEY_FILE_JSON}"
+    "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_KEY_FILE_P12=${TEST_KEY_FILE_P12}"
+  )
+
+  "${BAZEL_BIN}" test \
+    "${bazel_args[@]}" \
+    "--test_tag_filters=bigtable-integration-tests,storage-integration-tests" \
+    -- //google/cloud/...:all \
+    -//google/cloud/bigtable/examples:bigtable_grpc_credentials \
+    -//google/cloud/storage/examples:storage_service_account_samples \
+    -//google/cloud/storage/tests:service_account_integration_test
+
+fi
