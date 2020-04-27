4c4455dfd08680e897e92665dce264d720230828
Marek Dopiera <marek@unoperate.com>
2020-04-28 00:00:03 +0200

    fix: use network to check if running inside Google
    This fixes #3304
    
    In order to tell if the code is running inside Google (GCE, GKE, Cloud
    Run, AppEngine flexible) we now simply try to obtain credentials from
    `metadata.google.internal`.  Any other attempts failed due to gVisor
    (used in Cloud Run) isolating the host operating system very well.
    
    If network is disabled or customized when run this won't work.
    
    This was tested on GCE, GKE, Cloud Run and AppEngine Flexible.

--- google/cloud/storage/internal/compute_engine_util.cc
+++ google/cloud/storage/internal/compute_engine_util.cc
@@ -14,13 +14,7 @@
 
 #include "google/cloud/storage/internal/compute_engine_util.h"
 #include "google/cloud/internal/getenv.h"
-#include "google/cloud/log.h"
 #include <string>
-#if _WIN32
-#include <Windows.h>
-#else  // On Linux
-#include <fstream>
-#endif  // _WIN32
 
 namespace google {
 namespace cloud {
@@ -35,72 +29,6 @@ std::string GceMetadataHostname() {
     return *maybe_hostname;
   }
   return "metadata.google.internal";
-}
-
-bool RunningOnComputeEngineVm() {
-  // Allow overriding this value for integration tests.
-  auto override_val = google::cloud::internal::GetEnv(GceCheckOverrideEnvVar());
-  if (override_val.has_value()) {
-    return std::string("1") == *override_val;
-  }
-
-#if _WIN32
-  // These values came from a GCE VM running Windows Server 2012 R2.
-  std::wstring const reg_key_path = L"SYSTEM\\HardwareConfig\\Current\\";
-  std::wstring const reg_key_name = L"SystemProductName";
-  std::wstring const gce_product_name = L"Google Compute Engine";
-
-  // Get the size of the string first to allocate our buffer. This includes
-  // enough space for the trailing NUL character that will be included.
-  DWORD buffer_size{};
-  auto rc = ::RegGetValueW(
-      HKEY_LOCAL_MACHINE, reg_key_path.c_str(), reg_key_name.c_str(),
-      RRF_RT_REG_SZ,
-      nullptr,        // We know the type will be REG_SZ.
-      nullptr,        // We're only fetching the size; no buffer given yet.
-      &buffer_size);  // Fetch the size in bytes of the value, if it exists.
-  if (rc != 0) {
-    return false;
-  }
-
-  // Allocate the buffer size and retrieve the product name string.
-  std::wstring buffer;
-  buffer.resize(static_cast<std::size_t>(buffer_size) / sizeof(wchar_t));
-  rc = ::RegGetValueW(
-      HKEY_LOCAL_MACHINE, reg_key_path.c_str(), reg_key_name.c_str(),
-      RRF_RT_REG_SZ,
-      nullptr,                         // We know the type will be REG_SZ.
-      static_cast<void*>(&buffer[0]),  // Fetch the string value this time.
-      &buffer_size);  // The wstring size in bytes, not including trailing NUL.
-  if (rc != 0) {
-    return false;
-  }
-
-  // Account for the trailing NUL character in the retrieved value, along with
-  // the additional NUL char appended to all wstring objects.
-  buffer_size = static_cast<std::size_t>(buffer_size) / sizeof(wchar_t);
-  if (buffer_size == 0) {
-    return false;
-  }
-  buffer_size--;
-  buffer.resize(static_cast<std::size_t>(buffer_size));
-
-  return buffer == gce_product_name;
-#else   // Running on Linux
-  // On Linux GCE VMs, we expect to see "Google Compute Engine" as the contents
-  // of the file at /sys/class/dmi/id/product_name.
-  std::string const gce_product_name = "Google Compute Engine";
-  std::string const product_name_file = "/sys/class/dmi/id/product_name";
-  std::ifstream is(product_name_file);
-  if (!is.is_open()) {
-    GCP_LOG(WARNING) << "Could not find file '" << product_name_file
-                     << "' when checking if running on GCE, returning false";
-    return false;
-  }
-  std::string first_line;
-  std::getline(is, first_line);
-  return first_line == gce_product_name;
-#endif  // _WIN32
 }
 
 }  // namespace internal
