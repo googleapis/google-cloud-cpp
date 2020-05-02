b69d55ece6a8a66f9ea6d515d0c4249f783b978e
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-05-02 18:35:18 -0400

    chore: ignore this test

--- ci/check-markdown.sh
+++ ci/check-markdown.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# THIS IS A TEST
+
 set -eu
 
 if [[ "${CHECK_MARKDOWN}" != "yes" ]]; then
