dede308931a1a71ee11213432b08d3f5bb3b53ec
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-05-04 17:39:39 -0400

    chore: give cmake targets unique names (#4006)
    Fixes: #4004

--- cmake/GoogleCloudCppCommon.cmake
+++ cmake/GoogleCloudCppCommon.cmake
@@ -176,3 +176,58 @@ function (google_cloud_cpp_install_headers target destination)
         install(FILES "${header}" DESTINATION "${destination}/${dir}")
     endforeach ()
 endfunction ()
+
+#
+# google_cloud_cpp_set_target_name : formats the prefix and path as a target
+#
+# The formatted target name looks like "<prefix>_<basename>" where <basename> is
+# computed from the path. A 4th argument may optionally be specified, which
+# should be the name of a variable in the parent's scope where the <basename>
+# should be set. This is useful only if the caller wants both the target name
+# and the basename.
+#
+# * target the name of the variable to be set in the parent scope to hold the
+#   target name.
+# * prefix a unique string to prepend to the target name. Usually this should be
+#   a string indicating the product, such as "pubsub" or "storage".
+# * path is the filename that should be used for the target.
+# * (optional) basename the name of a variable to be set in the parent scope
+#   containing the basename of the target.
+#
+function (google_cloud_cpp_set_target_name target prefix path)
+    string(REPLACE "/" "_" basename "${path}")
+    string(REPLACE ".cc" "" basename "${basename}")
+    set("${target}"
+        "${prefix}_${basename}"
+        PARENT_SCOPE)
+    # Optional 4th argument, will be set to the basename if present.
+    if (ARGC EQUAL 4)
+        set("${ARGV3}"
+            "${basename}"
+            PARENT_SCOPE)
+    endif ()
+endfunction ()
+
+#
+# google_cloud_cpp_add_executable : adds an executable w/ the given source and
+# prefix name
+#
+# Computes the target name using `google_cloud_cpp_set_target_name` (see above),
+# then adds an executable with a few common properties. Sets the `target` in the
+# caller's scope to the name of the computed target name.
+#
+# * target the name of the variable to be set in the parent scope to hold the
+#   target name.
+# * prefix a unique string to prepend to the target name. Usually this should be
+#   a string indicating the product, such as "pubsub" or "storage".
+# * path is the filename that should be used for the target.
+#
+function (google_cloud_cpp_add_executable target prefix source)
+    google_cloud_cpp_set_target_name(target_name "${prefix}" "${source}"
+                                     shortname)
+    add_executable("${target_name}" "${source}")
+    set_target_properties("${target_name}" PROPERTIES OUTPUT_NAME ${shortname})
+    set("${target}"
+        "${target_name}"
+        PARENT_SCOPE)
+endfunction ()
