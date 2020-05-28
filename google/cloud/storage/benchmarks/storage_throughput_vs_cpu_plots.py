da664fef8437842cac66eac22c940d5ec26389f9
Carlos O'Ryan <coryan@google.com>
2020-05-28 09:50:39 -0400

    Address review comments

--- google/cloud/storage/benchmarks/storage_throughput_vs_cpu_plots.py
+++ google/cloud/storage/benchmarks/storage_throughput_vs_cpu_plots.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2019 Google LLC
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,10 @@
 # limitations under the License.
 
 """Summarize the results from running storage_throughput_vs_cpu_benchmark."""
+
+# These funny `%%` comments help if you load this script in Spyder
+# (https://www.spyder-ide.org/). Each one starts a `cell` that you can
+# manually execute.
 
 # %%
 import argparse
