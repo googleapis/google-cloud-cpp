150185a0723bdf0db3c7aa1e836771e0eaa74d51
Carlos O'Ryan <coryan@google.com>
2020-06-08 09:13:11 -0400

    Use right enum to test Raw XML

--- google/cloud/storage/benchmarks/throughput_experiment.cc
+++ google/cloud/storage/benchmarks/throughput_experiment.cc
@@ -195,7 +195,7 @@ class DownloadObjectLibcurl : public ThroughputExperiment {
     auto* hnd = curl_easy_init();
     curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 102400L);
     std::string url;
-    if (api_ == ApiName::kApiXml) {
+    if (api_ == ApiName::kApiRawXml) {
       url = "https://storage.googleapis.com/" + bucket_name + "/" + object_name;
     } else {
       // For this benchmark it is not necessary to URL escape the object name.
