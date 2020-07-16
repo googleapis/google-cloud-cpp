f17705ed0f78b6fbfc1fedf6f4d3cd5d6ce17108
Carlos O'Ryan <coryan@google.com>
2020-07-16 01:16:39 -0400

    feat(pubsub): a wrapper for PubsubMessage
    Introduce `pubsub::Message`, a wrapper for the `PubsubMessage` protobuf.
    This wrapper provides more native C++ types to access the message
    properties, and a number of factory functions to create messages.

---
+++ google/cloud/pubsub/message.h
@@ -0,0 +1,188 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_PUBSUB_MESSAGE_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_PUBSUB_MESSAGE_H
+
+#include "google/cloud/pubsub/version.h"
+#include <google/pubsub/v1/pubsub.pb.h>
+#include <iosfwd>
+#include <map>
+#include <tuple>
+#include <vector>
+
+namespace google {
+namespace cloud {
+namespace pubsub {
+inline namespace GOOGLE_CLOUD_CPP_PUBSUB_NS {
+class Message;
+}  // namespace GOOGLE_CLOUD_CPP_PUBSUB_NS
+}  // namespace pubsub
+
+namespace pubsub_internal {
+inline namespace GOOGLE_CLOUD_CPP_PUBSUB_NS {
+::google::pubsub::v1::PubsubMessage const& ToProto(pubsub::Message const&);
+::google::pubsub::v1::PubsubMessage&& ToProto(pubsub::Message&&);
+pubsub::Message FromProto(::google::pubsub::v1::PubsubMessage);
+}  // namespace GOOGLE_CLOUD_CPP_PUBSUB_NS
+}  // namespace pubsub_internal
+
+namespace pubsub {
+inline namespace GOOGLE_CLOUD_CPP_PUBSUB_NS {
+
+/**
+ * The C++ representation for a Cloud Pub/Sub messages.
+ *
+ * Cloud Pub/Sub applications communicate to each other using messages. Note
+ * that messages must provide at least some data or some attributes. The `From*`
+ * factory functions are designed to encourage applications to always create
+ * valid messages.
+ *
+ * A #Message is immutable other than through move copy or assignment. Once a
+ * message is created its properties cannot be changed, though it is possible to
+ * create new messages by using the `Set*()` factory functions.
+ */
+class Message {
+ public:
+  /// Change the payload in @p m to @p data
+  static Message SetData(Message m, std::string data) {
+    m.proto_.set_data(std::move(data));
+    return m;
+  }
+
+  /// Change the attributes in @p m to those in the range [@p begin, @p end)
+  template <typename Iterator>
+  static Message SetAttributes(Message m, Iterator begin, Iterator end) {
+    for (auto kv = begin; kv != end; ++kv) {
+      using std::get;
+      (*m.proto_.mutable_attributes())[get<0>(*kv)] = get<1>(*kv);
+    }
+    return m;
+  }
+
+  /// Change the attributes in @p m to the values in @p v
+  static Message SetAttributes(
+      Message m, std::vector<std::pair<std::string, std::string>> v) {
+    using value_type =
+        google::protobuf::Map<std::string, std::string>::value_type;
+    for (auto& kv : v) {
+      m.proto_.mutable_attributes()->insert(
+          value_type(std::move(kv.first), std::move(kv.second)));
+    }
+    return m;
+  }
+
+  /// Change the attributes in @p m to the values in @p v
+  template <typename Pair>
+  static Message SetAttributes(Message m, std::vector<Pair> v) {
+    return SetAttributes(std::move(m), v.begin(), v.end());
+  }
+
+  /// Create a new message with @p data as its payload
+  static Message FromData(std::string data) {
+    return SetData(Message{}, std::move(data));
+  }
+
+  /// Create a new message with the attributes in the range [@p begin, @p end)
+  template <typename Iterator>
+  static Message FromAttributes(Iterator begin, Iterator end) {
+    return SetAttributes(Message{}, begin, end);
+  }
+
+  /// Create a new message with the attributes in @p v
+  static Message FromAttributes(
+      std::vector<std::pair<std::string, std::string>> v) {
+    return SetAttributes(Message{}, std::move(v));
+  }
+
+  /// Create a new message with the attributes in @p v
+  template <typename Pair>
+  static Message FromAttributes(std::vector<Pair> v) {
+    return SetAttributes(Message{}, std::move(v));
+  }
+
+  /// @name accessors
+  //@{
+  std::string const& data() const& { return proto_.data(); }
+  std::string&& data() && { return std::move(*proto_.mutable_data()); }
+  std::string const& message_id() const { return proto_.message_id(); }
+  std::string const& ordering_key() const { return proto_.ordering_key(); }
+  std::chrono::system_clock::time_point publish_time() const;
+  std::map<std::string, std::string> attributes() const {
+    std::map<std::string, std::string> r;
+    for (auto const& kv : proto_.attributes()) {
+      r.emplace(kv.first, kv.second);
+    }
+    return r;
+  }
+  //@}
+
+  /// @name Copy and move
+  //@{
+  Message(Message const&) = default;
+  Message& operator=(Message const&) = default;
+  Message(Message&&) = default;
+  Message& operator=(Message&&) = default;
+  //@}
+
+  /// @name Equality operators
+  //@{
+  friend bool operator==(Message const& a, Message const& b);
+  friend bool operator!=(Message const& a, Message const& b) {
+    return !(a == b);
+  }
+  //@}
+
+  /// Output in protobuf format, this is intended for debugging
+  friend std::ostream& operator<<(std::ostream& os, Message const& rhs);
+
+ private:
+  Message() = default;
+
+  Message(::google::pubsub::v1::PubsubMessage m) : proto_(std::move(m)) {}
+
+  friend Message pubsub_internal::FromProto(
+      ::google::pubsub::v1::PubsubMessage m);
+  friend ::google::pubsub::v1::PubsubMessage const& pubsub_internal::ToProto(
+      Message const& m);
+  friend ::google::pubsub::v1::PubsubMessage&& pubsub_internal::ToProto(
+      Message&& m);
+  google::pubsub::v1::PubsubMessage proto_;
+};
+
+}  // namespace GOOGLE_CLOUD_CPP_PUBSUB_NS
+}  // namespace pubsub
+
+namespace pubsub_internal {
+inline namespace GOOGLE_CLOUD_CPP_PUBSUB_NS {
+inline ::google::pubsub::v1::PubsubMessage const& ToProto(
+    pubsub::Message const& m) {
+  return m.proto_;
+}
+
+inline ::google::pubsub::v1::PubsubMessage&& ToProto(pubsub::Message&& m) {
+  return std::move(m.proto_);
+}
+
+inline pubsub::Message FromProto(::google::pubsub::v1::PubsubMessage m) {
+  return pubsub::Message(std::move(m));
+}
+
+}  // namespace GOOGLE_CLOUD_CPP_PUBSUB_NS
+}  // namespace pubsub_internal
+
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_PUBSUB_MESSAGE_H
---
+++ google/cloud/pubsub/message_test.cc
@@ -0,0 +1,173 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/pubsub/message.h"
+#include "google/cloud/testing_util/is_proto_equal.h"
+#include <google/protobuf/text_format.h>
+#include <gmock/gmock.h>
+#include <sstream>
+
+namespace google {
+namespace cloud {
+namespace pubsub {
+inline namespace GOOGLE_CLOUD_CPP_PUBSUB_NS {
+namespace {
+
+using ::google::cloud::testing_util::IsProtoEqual;
+using ::testing::HasSubstr;
+using ::testing::UnorderedElementsAre;
+
+TEST(Message, FromData) {
+  auto const m0 = Message::FromData("contents-0");
+  EXPECT_EQ("contents-0", m0.data());
+  EXPECT_TRUE(m0.attributes().empty());
+  EXPECT_TRUE(m0.ordering_key().empty());
+  EXPECT_TRUE(m0.message_id().empty());
+
+  auto const m1 = Message::FromData("contents-1");
+  EXPECT_EQ("contents-1", m1.data());
+
+  EXPECT_NE(m0, m1);
+  Message copy(m0);
+  EXPECT_EQ(m0, copy);
+  Message move(std::move(copy));
+  EXPECT_EQ(m0, move);
+}
+
+TEST(Message, FromAttributesIterator) {
+  std::map<std::string, std::string> const attributes(
+      {{"k1", "v1"}, {"k2", "v2"}});
+
+  auto const m0 = Message::FromAttributes(attributes.begin(), attributes.end());
+  EXPECT_TRUE(m0.data().empty());
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k1", "v1"),
+                                   std::make_pair("k2", "v2")));
+  EXPECT_TRUE(m0.ordering_key().empty());
+  EXPECT_TRUE(m0.message_id().empty());
+}
+
+TEST(Message, FromAttributesVectorStdPair) {
+  auto const m0 = Message::FromAttributes({{"k0", "v0"}, {"k1", "v1"}});
+  EXPECT_TRUE(m0.data().empty());
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k0", "v0"),
+                                   std::make_pair("k1", "v1")));
+  EXPECT_TRUE(m0.ordering_key().empty());
+  EXPECT_TRUE(m0.message_id().empty());
+}
+
+TEST(Message, FromAttributesVectorStdTuple) {
+  std::vector<std::tuple<std::string, std::string>> const attributes(
+      {{"k1", "v1"}, {"k2", "v2"}});
+  auto const m0 = Message::FromAttributes(attributes);
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k1", "v1"),
+                                   std::make_pair("k2", "v2")));
+}
+
+TEST(Message, SetData) {
+  auto const m0 = Message::SetData(Message::FromData("original"), "changed");
+  EXPECT_EQ("changed", m0.data());
+}
+
+TEST(Message, SetAttributesIterator) {
+  std::map<std::string, std::string> const attributes(
+      {{"k1", "v1"}, {"k2", "v2"}});
+  auto const m0 = Message::SetAttributes(Message::FromData("original"),
+                                         attributes.begin(), attributes.end());
+  EXPECT_EQ("original", m0.data());
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k1", "v1"),
+                                   std::make_pair("k2", "v2")));
+}
+
+TEST(Message, SetAttributesVectorStdPair) {
+  std::vector<std::pair<std::string, std::string>> const attributes(
+      {{"k1", "v1"}, {"k2", "v2"}});
+  auto const m0 =
+      Message::SetAttributes(Message::FromData("original"), attributes);
+  EXPECT_EQ("original", m0.data());
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k1", "v1"),
+                                   std::make_pair("k2", "v2")));
+}
+
+TEST(Message, SetAttributesVectorStdTuple) {
+  std::vector<std::tuple<std::string, std::string>> const attributes(
+      {{"k1", "v1"}, {"k2", "v2"}});
+  auto const m0 =
+      Message::SetAttributes(Message::FromData("original"), attributes);
+  EXPECT_EQ("original", m0.data());
+  EXPECT_THAT(m0.attributes(),
+              UnorderedElementsAre(std::make_pair("k1", "v1"),
+                                   std::make_pair("k2", "v2")));
+}
+
+TEST(Message, DataMove) {
+  auto m0 = Message::FromData("contents-0");
+  std::string const d = std::move(m0).data();
+  EXPECT_EQ("contents-0", d);
+}
+
+TEST(Message, FromProto) {
+  auto constexpr kText = R"pb(
+    data: "test-data"
+    attributes: { key: "key1" value: "label1" }
+    attributes: { key: "key0" value: "label0" }
+    message_id: "test-message-id"
+    publish_time { seconds: 123 nanos: 456000 }
+    ordering_key: "test-ordering-key"
+  )pb";
+  ::google::pubsub::v1::PubsubMessage expected;
+  EXPECT_TRUE(google::protobuf::TextFormat::ParseFromString(kText, &expected));
+  auto const m = pubsub_internal::FromProto(expected);
+
+  EXPECT_EQ("test-data", m.data());
+  EXPECT_THAT(m.attributes(),
+              UnorderedElementsAre(std::make_pair("key1", "label1"),
+                                   std::make_pair("key0", "label0")));
+  EXPECT_EQ("test-message-id", m.message_id());
+  auto const epoch = std::chrono::system_clock::from_time_t(0);
+  auto const expected_publish_time =
+      epoch + std::chrono::seconds(123) + std::chrono::nanoseconds(456000);
+  EXPECT_EQ(expected_publish_time, m.publish_time());
+  EXPECT_EQ("test-ordering-key", m.ordering_key());
+
+  auto const& actual_copy = pubsub_internal::ToProto(m);
+  EXPECT_THAT(actual_copy, IsProtoEqual(expected));
+
+  auto actual_move =
+      pubsub_internal::ToProto(pubsub_internal::FromProto(expected));
+  EXPECT_THAT(actual_move, IsProtoEqual(expected));
+}
+
+TEST(Message, OutputStream) {
+  Message const m = Message::SetAttributes(Message::FromData("test-only-data"),
+                                           {{"k0", "v0"}, {"k1", "v1"}});
+  std::ostringstream os;
+  os << m;
+  auto const actual = std::move(os).str();
+  EXPECT_THAT(actual, HasSubstr("test-only-data"));
+  EXPECT_THAT(actual, HasSubstr("k0"));
+  EXPECT_THAT(actual, HasSubstr("v0"));
+  EXPECT_THAT(actual, HasSubstr("k1"));
+  EXPECT_THAT(actual, HasSubstr("v1"));
+}
+
+}  // namespace
+}  // namespace GOOGLE_CLOUD_CPP_PUBSUB_NS
+}  // namespace pubsub
+}  // namespace cloud
+}  // namespace google
