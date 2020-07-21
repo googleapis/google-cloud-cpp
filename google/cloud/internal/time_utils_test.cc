2b671a727c34bc5e2f3c11b0f1bc72826024f994
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-07-21 12:44:01 -0400

    fix: timestamp proto encoding works before epoch
    Fixes: https://github.com/googleapis/google-cloud-cpp/issues/4389

--- google/cloud/internal/time_utils_test.cc
+++ google/cloud/internal/time_utils_test.cc
@@ -15,7 +15,6 @@
 #include "google/cloud/internal/time_utils.h"
 #include "google/cloud/testing_util/assert_ok.h"
 #include "google/cloud/testing_util/is_proto_equal.h"
-#include "absl/time/time.h"
 #include <gmock/gmock.h>
 #include <chrono>
 
@@ -27,24 +26,43 @@ namespace {
 
 using ::google::cloud::testing_util::IsProtoEqual;
 
+// A helper to create a Timestamp proto to make the tests in this file more
+// succinct and readable.
 google::protobuf::Timestamp MakeProto(std::int64_t sec, std::int32_t nsec) {
-  google::protobuf::Timestamp ts;
-  ts.set_seconds(sec);
-  ts.set_nanos(nsec);
-  return ts;
+  google::protobuf::Timestamp proto;
+  proto.set_seconds(sec);
+  proto.set_nanos(nsec);
+  return proto;
 }
 
-TEST(TimeUtils, ToProtoTimestamp) {
+// A helper to create an absl::Time from an RFC-3339 string.
+StatusOr<absl::Time> TimeFromString(std::string const& s) {
+  absl::Time t;
+  std::string err;
+  if (absl::ParseTime(absl::RFC3339_full, s, &t, &err)) return t;
+  return Status(StatusCode::kInvalidArgument, err);
+}
+
+TEST(TimeUtils, AbseilTime) {
   // Define a few constants and lambdas to make the test cases below read
   // nicely with minimal wrapping.
-  auto const epoch = std::chrono::system_clock::from_time_t(0);
-  auto const& sec = [](std::int64_t n) { return std::chrono::seconds(n); };
-  auto const& ns = [](std::int64_t n) { return std::chrono::nanoseconds(n); };
+  auto const epoch = absl::FromUnixSeconds(0);
+  auto const sec = [](std::int64_t n) { return absl::Seconds(n); };
+  auto const ns = [](std::int64_t n) { return absl::Nanoseconds(n); };
+
+  auto const min_time = TimeFromString("0001-01-01T00:00:00Z").value();
+  auto const max_time = TimeFromString("9999-12-31T23:59:59Z").value();
+  auto const min_proto = MakeProto(-62135596800, 0);
+  auto const max_proto = MakeProto(253402300799, 0);
+  auto const contemporary =
+      TimeFromString("2020-07-21T11:33:03.123456789Z").value();
 
   struct {
-    std::chrono::system_clock::time_point tp;
+    absl::Time t;
     google::protobuf::Timestamp expected;
-  } test_case[] = {
+  } round_trip_cases[] = {
+      {min_time, min_proto},
+      {epoch - sec(1), MakeProto(-1, 0)},
       {epoch - sec(1), MakeProto(-1, 0)},
       {epoch - sec(1) + ns(1), MakeProto(-1, 1)},
       {epoch - ns(1), MakeProto(-1, 999999999)},
@@ -52,45 +70,64 @@ TEST(TimeUtils, ToProtoTimestamp) {
       {epoch + ns(1), MakeProto(0, 1)},
       {epoch + sec(1), MakeProto(1, 0)},
       {epoch + sec(1) + ns(1), MakeProto(1, 1)},
+      {epoch + sec(123) + ns(456), MakeProto(123, 456)},
+      {contemporary, MakeProto(1595331183, 123456789)},
+      {max_time, max_proto},
   };
 
-  for (auto const& tc : test_case) {
-    SCOPED_TRACE("Time point: " + absl::FormatTime(absl::FromChrono(tc.tp)));
-    auto const p = ToProtoTimestamp(tc.tp);
+  for (auto const& tc : round_trip_cases) {
+    SCOPED_TRACE("Time: " + absl::FormatTime(tc.t));
+    auto const p = ToProtoTimestamp(tc.t);
     EXPECT_THAT(p, IsProtoEqual(tc.expected));
+    auto const t = ToAbslTime(p);
+    EXPECT_EQ(t, tc.t);
   }
+
+  // Tests that times before the min time are capped at the min time.
+  EXPECT_THAT(min_proto, IsProtoEqual(ToProtoTimestamp(absl::InfinitePast())));
+  EXPECT_THAT(min_proto, IsProtoEqual(ToProtoTimestamp(min_time - ns(1))));
+
+  // Tests that times after the max time are capped at the max time.
+  EXPECT_THAT(max_proto, IsProtoEqual(ToProtoTimestamp(max_time + ns(1))));
+  EXPECT_THAT(max_proto,
+              IsProtoEqual(ToProtoTimestamp(absl::InfiniteFuture())));
 }
 
-TEST(TimeUtils, ToChronoTimePoint) {
-  google::protobuf::Timestamp proto_timestamp;
-  proto_timestamp.set_seconds(867);
-  proto_timestamp.set_nanos(530900);
-  std::chrono::system_clock::time_point timepoint =
-      ToChronoTimePoint(proto_timestamp);
+TEST(TimeUtils, ChronoTime) {
+  // Define a few constants and lambdas to make the test cases below read
+  // nicely with minimal wrapping.
+  // Note: we use microseconds rather than nanoseconds here because we test on
+  // some platforms where the system clock only has microsecond resolution.
+  auto const epoch = std::chrono::system_clock::from_time_t(0);
+  auto const sec = [](std::int64_t n) { return std::chrono::seconds(n); };
+  auto const us = [](std::int64_t n) { return std::chrono::microseconds(n); };
+  auto const contemporary = absl::ToChronoTime(
+      TimeFromString("2020-07-21T11:33:03.123456000Z").value());
+  // Note: we do not test the proto min/max values here because we don't know
+  // if chrono's system_clock can represent that value (it likely can't).
 
-  std::chrono::system_clock::time_point expected =
-      std::chrono::system_clock::from_time_t(0) +
-      std::chrono::duration_cast<std::chrono::system_clock::duration>(
-          std::chrono::seconds(867) + std::chrono::nanoseconds(530900));
-  EXPECT_EQ(timepoint, expected);
-}
+  struct {
+    std::chrono::system_clock::time_point t;
+    google::protobuf::Timestamp expected;
+  } round_trip_cases[] = {
+      {epoch - sec(1), MakeProto(-1, 0)},
+      {epoch - sec(1) + us(1), MakeProto(-1, 1000)},
+      {epoch - us(1), MakeProto(-1, 999999000)},
+      {epoch, MakeProto(0, 0)},
+      {epoch + us(1), MakeProto(0, 1000)},
+      {epoch + sec(1), MakeProto(1, 0)},
+      {epoch + sec(1) + us(1), MakeProto(1, 1000)},
+      {epoch + sec(123) + us(456), MakeProto(123, 456000)},
+      {contemporary, MakeProto(1595331183, 123456000)},
+  };
 
-TEST(TimeUtils, ToAbslTime) {
-  google::protobuf::Timestamp proto_timestamp;
-  proto_timestamp.set_seconds(867);
-  proto_timestamp.set_nanos(530900);
-  auto const actual = ToAbslTime(proto_timestamp);
-  auto const expected = absl::FromUnixSeconds(867) + absl::Nanoseconds(530900);
-  EXPECT_EQ(expected, actual);
-}
-
-TEST(TimeUtils, ConvertAbslTimeToProtoTimestamp) {
-  google::protobuf::Timestamp expected;
-  expected.set_seconds(867);
-  expected.set_nanos(530900);
-  auto const t = absl::FromUnixSeconds(867) + absl::Nanoseconds(530900);
-  auto const actual = ToProtoTimestamp(t);
-  EXPECT_THAT(expected, IsProtoEqual(actual));
+  for (auto const& tc : round_trip_cases) {
+    SCOPED_TRACE("Time: " + absl::FormatTime(absl::FromChrono(tc.t)));
+    auto const p = ToProtoTimestamp(tc.t);
+    EXPECT_THAT(p, IsProtoEqual(tc.expected));
+    auto const t = ToChronoTimePoint(p);
+    EXPECT_EQ(t, tc.t);
+  }
 }
 
 }  // namespace
