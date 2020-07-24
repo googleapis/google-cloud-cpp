4e9be351434ee1fdf8d646c79413383631a6e8aa
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-07-24 17:57:30 -0400

    refactor: format_time_point now uses absl::Time

--- google/cloud/internal/format_time_point.cc
+++ google/cloud/internal/format_time_point.cc
@@ -13,49 +13,7 @@
 // limitations under the License.
 
 #include "google/cloud/internal/format_time_point.h"
-#include <array>
-#include <cstdio>
-#include <ctime>
-
-namespace {
-std::string FormatFractional(std::chrono::nanoseconds ns) {
-  if (ns.count() == 0) {
-    return "";
-  }
-
-  using std::chrono::milliseconds;
-  using std::chrono::nanoseconds;
-  using std::chrono::seconds;
-  auto constexpr kMaxNanosecondsDigits = 9;
-  auto constexpr kBufferSize = 16;
-  static_assert(kBufferSize > (kMaxNanosecondsDigits  // digits
-                               + 1                    // period
-                               + 1),                  // NUL terminator
-                "Buffer is not large enough for printing nanoseconds");
-  auto constexpr kNanosecondsPerMillisecond =
-      nanoseconds(milliseconds(1)).count();
-  auto constexpr kMillisecondsPerSecond = milliseconds(seconds(1)).count();
-
-  std::array<char, kBufferSize> buffer{};
-  // If the fractional seconds can be just expressed as milliseconds, do that,
-  // we do not want to print 1.123000000
-  auto d = std::lldiv(ns.count(), kNanosecondsPerMillisecond);
-  if (d.rem == 0) {
-    std::snprintf(buffer.data(), buffer.size(), ".%03lld", d.quot);
-    return buffer.data();
-  }
-  d = std::lldiv(ns.count(), kMillisecondsPerSecond);
-  if (d.rem == 0) {
-    std::snprintf(buffer.data(), buffer.size(), ".%06lld", d.quot);
-    return buffer.data();
-  }
-
-  std::snprintf(buffer.data(), buffer.size(), ".%09lld",
-                static_cast<long long>(ns.count()));
-  return buffer.data();
-}
-
-}  // namespace
+#include "absl/time/time.h"
 
 namespace google {
 namespace cloud {
@@ -63,64 +21,26 @@ inline namespace GOOGLE_CLOUD_CPP_NS {
 namespace internal {
 
 std::tm AsUtcTm(std::chrono::system_clock::time_point tp) {
-  std::time_t time = std::chrono::system_clock::to_time_t(tp);
-  std::tm tm{};
-  // The standard C++ function to convert time_t to a struct tm is not thread
-  // safe (it holds global storage), use some OS specific stuff here:
-#if _WIN32
-  gmtime_s(&tm, &time);
-#else
-  gmtime_r(&time, &tm);
-#endif  // _WIN32
-  return tm;
+  return absl::ToTM(absl::FromChrono(tp), absl::UTCTimeZone());
 }
 
-auto constexpr kTimestampFormatSize = 256;
-static_assert(kTimestampFormatSize > ((4 + 1)    // YYYY-
-                                      + (2 + 1)  // MM-
-                                      + 2        // DD
-                                      + 1        // T
-                                      + (2 + 1)  // HH:
-                                      + (2 + 1)  // MM:
-                                      + 2        // SS
-                                      + 1),      // Z
-              "Buffer size not large enough for YYYY-MM-DDTHH:MM:SSZ format");
-
 std::string FormatRfc3339(std::chrono::system_clock::time_point tp) {
-  std::tm tm = AsUtcTm(tp);
-  std::array<char, kTimestampFormatSize> buffer{};
-  std::strftime(buffer.data(), buffer.size(), "%Y-%m-%dT%H:%M:%S", &tm);
-
-  std::string result(buffer.data());
-  // Add the fractional seconds...
-  auto duration = tp.time_since_epoch();
-  using std::chrono::duration_cast;
-  auto fractional = duration_cast<std::chrono::nanoseconds>(
-      duration - duration_cast<std::chrono::seconds>(duration));
-  result += FormatFractional(fractional);
-  result += "Z";
-  return result;
+  auto constexpr kFormat = "%E4Y-%m-%dT%H:%M:%E*SZ";
+  auto const t = absl::FromChrono(tp);
+  return absl::FormatTime(kFormat, t, absl::UTCTimeZone());
 }
 
 std::string FormatV4SignedUrlTimestamp(
     std::chrono::system_clock::time_point tp) {
-  std::tm tm = AsUtcTm(tp);
-  std::array<char, kTimestampFormatSize> buffer{};
-  std::strftime(buffer.data(), buffer.size(), "%Y%m%dT%H%M%SZ", &tm);
-  return buffer.data();
+  auto constexpr kFormat = "%E4Y%m%dT%H%M%SZ";
+  auto const t = absl::FromChrono(tp);
+  return absl::FormatTime(kFormat, t, absl::UTCTimeZone());
 }
 
 std::string FormatV4SignedUrlScope(std::chrono::system_clock::time_point tp) {
-  std::tm tm = AsUtcTm(tp);
-
-  auto constexpr kDateFormatSize = 256;
-  static_assert(kDateFormatSize > (4      // YYYY
-                                   + 2    // MM
-                                   + 2),  // DD
-                "Buffer size not large enough for YYYYMMDD format");
-  std::array<char, kTimestampFormatSize> buffer{};
-  std::strftime(buffer.data(), buffer.size(), "%Y%m%d", &tm);
-  return buffer.data();
+  auto constexpr kFormat = "%E4Y%m%d";
+  auto const t = absl::FromChrono(tp);
+  return absl::FormatTime(kFormat, t, absl::UTCTimeZone());
 }
 
 }  // namespace internal
