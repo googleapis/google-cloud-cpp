db324db254b1ee943d7ac06d3c89f1b8b42e6775
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-07-27 17:30:56 -0400

    ci: ignore internal symbols in ABI checker

--- ci/kokoro/docker/check-abi.sh
+++ ci/kokoro/docker/check-abi.sh
@@ -55,7 +55,14 @@ check_library() {
   (
     cd "${BINARY_DIR}"
     zcat "${reference_file}" >"${old_dump_file}"
+    # We ignore all symbols in an "::internal::" namespace, because these are
+    # not part of our public API. We do this by specifying a regex that matches
+    # against the mangled symbol names. In this case, 8 is the number of
+    # characters in the string "internal", and it should again be followed by
+    # some other number indicating the length of the symbol within the
+    # "internal" namespace. See: https://en.wikipedia.org/wiki/Name_mangling
     abi-compliance-checker \
+      -skip-internal-symbols "8internal\d" \
       -src -l "${library}" -old "${old_dump_file}" -new "${new_dump_file}"
   )
   if [[ $? != 0 ]]; then
