ea93f6b7fcf744937ec9a691e49af29bb1c01cfc
Carlos O'Ryan <coryan@google.com>
2020-08-05 03:09:52 +0000

    Fix clang-3.8 build

--- super/external/nlohmann_json.cmake
+++ super/external/nlohmann_json.cmake
@@ -27,29 +27,6 @@ if (NOT TARGET nlohmann-json-project)
     set_external_project_build_parallel_level(PARALLEL)
     set_external_project_vars()
 
-    if ("${CMAKE_VERSION}" VERSION_LESS 3.8)
-        if (WIN32)
-            # patch may not be installed on Windows. It might be easier to
-            # upgrade CMake on that platform though.
-            message(FATAL_ERROR "Super builds on WIN32 require CMake >= 3.8."
-                                " Please upgrade your CMake version.")
-        endif ()
-        # nlohmann_json requires CMake >= 3.8, apparently to use the cxx_std_11
-        # property on its targets. We patch the CMakeLists.txt file to disable
-        # this feature. It is useful but using C++11 is a documented
-        # requirement.
-        message(
-            "nlohmann_json CMakeLists.txt file needs a patch ${CMAKE_CURRENT_LIST_DIR}/nlohmann_json.patch001"
-        )
-        set(GOOGLE_CLOUD_CPP_NLOHMANN_JSON_PATCH_COMMAND
-            "patch" "-p1" "<"
-            "${CMAKE_CURRENT_LIST_DIR}/nlohmann_json.patch001")
-    else ()
-        message(
-            "nlohmann_json CMakeLists.txt file will NOT need a patch ${CMAKE_VERSION}"
-        )
-        set(GOOGLE_CLOUD_CPP_NLOHMANN_JSON_PATCH_COMMAND "")
-    endif ()
     include(ExternalProject)
     ExternalProject_Add(
         nlohmann-json-project
@@ -64,7 +41,6 @@ if (NOT TARGET nlohmann-json-project)
                    -DCMAKE_INSTALL_RPATH=${GOOGLE_CLOUD_CPP_INSTALL_RPATH}
                    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
                    -DBUILD_TESTING=OFF
-        PATCH_COMMAND "${GOOGLE_CLOUD_CPP_NLOHMANN_JSON_PATCH_COMMAND}"
         BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> ${PARALLEL}
         LOG_DOWNLOAD ON
         LOG_CONFIGURE ON
