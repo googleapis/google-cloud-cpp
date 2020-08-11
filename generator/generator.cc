373c1d61079416bfd8ac56bac80e566e5ecd0900
Scott Hart <sdhart@google.com>
2020-08-11 14:40:40 -0400

    feature: add generator plugin entry point

---
+++ generator/generator.cc
@@ -0,0 +1,87 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "generator/generator.h"
+#include "google/cloud/status_or.h"
+#include "generator/internal/codegen_utils.h"
+//#include "generator/internal/service_generator.h"
+#include <google/api/client.pb.h>
+#include <string>
+#include <vector>
+
+namespace google {
+namespace cloud {
+namespace generator {
+
+// Temporary Stub for testing purposes.
+class ServiceGenerator {
+ public:
+  ServiceGenerator(google::protobuf::ServiceDescriptor const* service,
+                   google::protobuf::compiler::GeneratorContext*,
+                   std::map<std::string, std::string> const&)
+      : service_(service) {}
+
+  Status Generate() const {
+    if (service_->name() == "FailureService") {
+      return Status(StatusCode::kInternal, "Failed for testing.");
+    }
+    return {};
+  }
+
+ private:
+  google::protobuf::ServiceDescriptor const* service_;
+};
+
+bool Generator::Generate(google::protobuf::FileDescriptor const* file,
+                         std::string const& parameters,
+                         google::protobuf::compiler::GeneratorContext* context,
+                         std::string* error) const {
+  if (file->options().cc_generic_services()) {
+    *error =
+        "cpp codegen proto compiler plugin does not work with generic "
+        "services. To generate cpp codegen APIs, please set \""
+        "cc_generic_service = false\".";
+    return false;
+  }
+
+  StatusOr<std::vector<std::pair<std::string, std::string>>> command_line_args =
+      generator_internal::ProcessCommandLineArgs(parameters);
+  if (!command_line_args.ok()) {
+    *error = command_line_args.status().message();
+    return false;
+  }
+
+  std::vector<ServiceGenerator> services;
+  services.reserve(file->service_count());
+  for (int i = 0; i < file->service_count(); i++) {
+    services.emplace_back(
+        file->service(i), context,
+        std::map<std::string, std::string>(command_line_args->begin(),
+                                           command_line_args->end()));
+  }
+
+  for (auto const& service : services) {
+    auto result = service.Generate();
+    if (!result.ok()) {
+      *error = result.message();
+      return false;
+    }
+  }
+
+  return true;
+}
+
+}  // namespace generator
+}  // namespace cloud
+}  // namespace google
---
+++ generator/generator_test.cc
@@ -0,0 +1,106 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "generator/generator.h"
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+namespace generator {
+namespace {
+
+using google::protobuf::DescriptorPool;
+using google::protobuf::FileDescriptor;
+using google::protobuf::FileDescriptorProto;
+
+TEST(GeneratorTest, GenericService) {
+  DescriptorPool pool;
+  FileDescriptorProto generic_service_file;
+  generic_service_file.set_name("google/generic/v1/generic.proto");
+  generic_service_file.add_service()->set_name("GenericService");
+  generic_service_file.mutable_options()->set_cc_generic_services(true);
+  const FileDescriptor* generic_service_file_descriptor =
+      pool.BuildFile(generic_service_file);
+
+  Generator generator;
+  std::string actual_error;
+  std::string expected_error =
+      "cpp codegen proto compiler plugin does not work with generic "
+      "services. To generate cpp codegen APIs, please set \""
+      "cc_generic_service = false\".";
+  auto result = generator.Generate(generic_service_file_descriptor, {}, nullptr,
+                                   &actual_error);
+  EXPECT_FALSE(result);
+  EXPECT_EQ(actual_error, expected_error);
+}
+
+TEST(GeneratorTest, BadCommandLineArgs) {
+  DescriptorPool pool;
+  FileDescriptorProto service_file;
+  service_file.set_name("google/foo/v1/service.proto");
+  service_file.add_service()->set_name("Service");
+  service_file.mutable_options()->set_cc_generic_services(false);
+  const FileDescriptor* service_file_descriptor = pool.BuildFile(service_file);
+
+  Generator generator;
+  std::string actual_error;
+  std::string expected_error =
+      "--cpp_codegen_opt=product_path=<path> must be specified.";
+  auto result =
+      generator.Generate(service_file_descriptor, {}, nullptr, &actual_error);
+  EXPECT_FALSE(result);
+  EXPECT_EQ(actual_error, expected_error);
+}
+
+TEST(GeneratorTest, GenerateServicesSuccess) {
+  DescriptorPool pool;
+  FileDescriptorProto service_file;
+  service_file.set_name("google/foo/v1/service.proto");
+  service_file.add_service()->set_name("SuccessService");
+  service_file.mutable_options()->set_cc_generic_services(false);
+  const FileDescriptor* service_file_descriptor = pool.BuildFile(service_file);
+
+  std::string actual_error;
+  Generator generator;
+  auto result = generator.Generate(service_file_descriptor,
+                                   {"product_path=google/cloud/foo"}, nullptr,
+                                   &actual_error);
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(actual_error.empty());
+}
+
+TEST(GeneratorTest, GenerateServicesFailure) {
+  DescriptorPool pool;
+  FileDescriptorProto service_file;
+  service_file.set_name("google/foo/v1/service.proto");
+  service_file.add_service()->set_name("FailureService");
+  service_file.mutable_options()->set_cc_generic_services(false);
+  const FileDescriptor* service_file_descriptor = pool.BuildFile(service_file);
+
+  std::string actual_error;
+  std::string expected_error = "Failed for testing.";
+  Generator generator;
+  auto result = generator.Generate(service_file_descriptor,
+                                   {"product_path=google/cloud/foo"}, nullptr,
+                                   &actual_error);
+  EXPECT_FALSE(result);
+  EXPECT_EQ(actual_error, expected_error);
+}
+
+}  // namespace
+}  // namespace generator
+}  // namespace cloud
+}  // namespace google
