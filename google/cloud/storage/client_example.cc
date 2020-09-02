fc6c58a288287c3027aa8fe3232cf958fcc69dce
Tina Smirnova <tinanight98@gmail.com>
2020-09-02 16:44:46 +0300

    cc

--- google/cloud/storage/client_example.cc
+++ google/cloud/storage/client_example.cc
@@ -8,16 +8,15 @@ namespace storage {
 inline namespace STORAGE_CLIENT_NS {
 
 int main() {
-  const auto& client_options =
-      ClientOptions(oauth2::CreateAnonymousCredentials());
+  auto client_options = ClientOptions(oauth2::CreateAnonymousCredentials());
 
   Client client(client_options);
 
   auto stream = client.WriteObject("mock-bucket-name", "mock-object-name");
   stream << "Bring me to work!";
 
-  const auto& session_url = stream.resumable_session_id();
-  const auto& client_status =
+  auto const& session_url = stream.resumable_session_id();
+  auto const& client_status =
       client.DeleteResumableUpload(session_url, client_options);
 
   stream.Close();
