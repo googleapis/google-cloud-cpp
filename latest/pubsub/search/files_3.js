1a6cf3406528c5783a8076b396934bfe931e7d74
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-09-04 12:55:04 -0700

    Automatically generated documentation

--- latest/pubsub/search/files_3.js
+++ latest/pubsub/search/files_3.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['default_5fretry_5fpolicies_2ecc',['default_retry_policies.cc',['../default__retry__policies_8cc.html',1,'']]],
-  ['default_5fretry_5fpolicies_2eh',['default_retry_policies.h',['../default__retry__policies_8h.html',1,'']]]
+  ['default_5fretry_5fpolicies_2eh',['default_retry_policies.h',['../default__retry__policies_8h.html',1,'']]],
+  ['default_5fsubscription_5fbatch_5fsource_2ecc',['default_subscription_batch_source.cc',['../default__subscription__batch__source_8cc.html',1,'']]],
+  ['default_5fsubscription_5fbatch_5fsource_2eh',['default_subscription_batch_source.h',['../default__subscription__batch__source_8h.html',1,'']]],
+  ['default_5fsubscription_5fbatch_5fsource_5ftest_2ecc',['default_subscription_batch_source_test.cc',['../default__subscription__batch__source__test_8cc.html',1,'']]]
 ];
