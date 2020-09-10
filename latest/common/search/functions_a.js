65c40664d43f16225295b67606a39e25e6152cc6
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-09-10 12:26:41 -0700

    Automatically generated documentation

--- latest/common/search/functions_a.js
+++ latest/common/search/functions_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['main',['main',['../gcs2cbt_8cc.html#a0ddf1224851353fc92bfbff6f499fa97',1,'gcs2cbt.cc']]],
   ['make_5fready_5ffuture',['make_ready_future',['../namespacegoogle_1_1cloud_1_1v1.html#a2421390acdf2c6b05f2f4bb0c3b9549e',1,'google::cloud::v1::make_ready_future(T &amp;&amp;t)'],['../namespacegoogle_1_1cloud_1_1v1.html#aa42482de77940af49596eead9bdc158f',1,'google::cloud::v1::make_ready_future()']]],
   ['make_5fstatus_5for',['make_status_or',['../namespacegoogle_1_1cloud_1_1v1.html#adf3538accad2fc97d248127f7892d34d',1,'google::cloud::v1']]],
-  ['makedeadlinetimer',['MakeDeadlineTimer',['../classgoogle_1_1cloud_1_1v1_1_1CompletionQueue.html#a3dc0077cfca94f5e8532c42c2e25ca0c',1,'google::cloud::v1::CompletionQueue']]],
+  ['makedeadlinetimer',['MakeDeadlineTimer',['../classgoogle_1_1cloud_1_1v1_1_1CompletionQueue.html#ab6ddf6fdd2511dc008ffd70da0f1bc40',1,'google::cloud::v1::CompletionQueue']]],
   ['makekmskeyname',['MakeKmsKeyName',['../namespacegoogle_1_1cloud_1_1v1.html#a1581bd51abfa31a8e8782e3430c54528',1,'google::cloud::v1']]],
   ['makerelativetimer',['MakeRelativeTimer',['../classgoogle_1_1cloud_1_1v1_1_1CompletionQueue.html#a998c6ce6f8aca6e94a77ddccc1fa0956',1,'google::cloud::v1::CompletionQueue']]],
   ['makestatusfromrpcerror',['MakeStatusFromRpcError',['../namespacegoogle_1_1cloud_1_1v1.html#ac188af597da16f92d53e100218e75086',1,'google::cloud::v1::MakeStatusFromRpcError(grpc::Status const  &amp;status)'],['../namespacegoogle_1_1cloud_1_1v1.html#a08c5a91a5282e3a5d50425f6e6b21f45',1,'google::cloud::v1::MakeStatusFromRpcError(grpc::StatusCode code, std::string what)'],['../namespacegoogle_1_1cloud_1_1v1.html#a7c1c2b9f3727e0ce9841ed35022e3cba',1,'google::cloud::v1::MakeStatusFromRpcError(google::rpc::Status const  &amp;status)']]],
