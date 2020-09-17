2e81dbe3f29e21b6394adeee8d5348cc23c3e0d5
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-09-17 16:49:13 -0400

    test: use local httpbin for Bazel builds (#5076)
    The storage emulator also has a httpbin implementation, we use that
    implementation on the CMake-based builds, and should also use it for the
    Bazel-based builds.

--- google/cloud/storage/ci/run_integration_tests_emulator_bazel.sh
+++ google/cloud/storage/ci/run_integration_tests_emulator_bazel.sh
@@ -70,6 +70,7 @@ done
 # are missing too.
 testbench_args=(
   "--test_env=CLOUD_STORAGE_TESTBENCH_ENDPOINT=${CLOUD_STORAGE_TESTBENCH_ENDPOINT}"
+  "--test_env=HTTPBIN_ENDPOINT=${HTTPBIN_ENDPOINT}"
   "--test_env=GOOGLE_CLOUD_CPP_STORAGE_TEST_HMAC_SERVICE_ACCOUNT=fake-service-account-sign@example.com"
   "--test_env=GOOGLE_CLOUD_CPP_AUTO_RUN_EXAMPLES=yes"
 )
