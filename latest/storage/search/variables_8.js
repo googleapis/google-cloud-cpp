5df09fcdd3fd62a8081661d9cf6c6def1039e909
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-09-18 19:26:27 -0700

    Automatically generated documentation

--- latest/storage/search/variables_8.js
+++ latest/storage/search/variables_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['kadclink',['kAdcLink',['../namespacegoogle_1_1cloud_1_1storage_1_1v1_1_1oauth2.html#abd296e29980ba6c98b571b60ffca719d',1,'google::cloud::storage::v1::oauth2']]],
+  ['kdefaultendpoint',['kDefaultEndpoint',['../namespacegoogle_1_1cloud_1_1storage_1_1v1.html#ac66839404d5dc52f3e02411ba0363b0a',1,'google::cloud::storage::v1']]],
   ['kexpectedchardigits',['kExpectedCharDigits',['../namespacegoogle_1_1cloud_1_1storage_1_1v1.html#a01812d21d2120e979afd78bd09dda6e9',1,'google::cloud::storage::v1']]],
   ['key',['key',['../structgoogle_1_1cloud_1_1storage_1_1v1_1_1EncryptionKeyData.html#a707ddf02f9255743d23a1e41853835ee',1,'google::cloud::storage::v1::EncryptionKeyData']]],
   ['key_5fsha256',['key_sha256',['../structgoogle_1_1cloud_1_1storage_1_1v1_1_1CustomerEncryption.html#ab7de1b814f050c27a85e459c34ff77f3',1,'google::cloud::storage::v1::CustomerEncryption']]]
