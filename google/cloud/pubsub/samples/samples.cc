66bd51fbfeeab7eedcb05044a077de9b6ea26942
remyabel <70660631+remyabel@users.noreply.github.com>
2020-09-20 11:37:46 -0400

    feat(pubsub): Implement pubsub_dead_letter_create_subscription sample (#5101)

--- google/cloud/pubsub/samples/samples.cc
+++ google/cloud/pubsub/samples/samples.cc
@@ -13,7 +13,9 @@
 // limitations under the License.
 
 #include "google/cloud/pubsub/samples/pubsub_samples_common.h"
+#include "google/cloud/pubsub/subscriber.h"
 #include "google/cloud/pubsub/subscription_admin_client.h"
+#include "google/cloud/pubsub/subscription_mutation_builder.h"
 #include "google/cloud/pubsub/testing/random_names.h"
 #include "google/cloud/pubsub/topic_admin_client.h"
 #include "google/cloud/internal/getenv.h"
@@ -238,6 +240,44 @@ void CreatePushSubscription(
   }
   //! [END pubsub_create_push_subscription] [create-push-subscription]
   (std::move(client), argv.at(0), argv.at(1), argv.at(2), argv.at(3));
+}
+
+void CreateDeadLetterSubscription(
+    google::cloud::pubsub::SubscriptionAdminClient client,
+    std::vector<std::string> const& argv) {
+  //! [dead-letter-create-subscription]
+  // [START pubsub_dead_letter_create_subscription]
+  namespace pubsub = google::cloud::pubsub;
+  [](pubsub::SubscriptionAdminClient client, std::string const& project_id,
+     std::string const& topic_id, std::string const& subscription_id,
+     std::string const& dead_letter_topic_id,
+     int dead_letter_delivery_attempts) {
+    auto sub = client.CreateSubscription(
+        pubsub::Topic(project_id, std::move(topic_id)),
+        pubsub::Subscription(project_id, std::move(subscription_id)),
+        pubsub::SubscriptionMutationBuilder{}.set_dead_letter_policy(
+            pubsub::SubscriptionMutationBuilder::MakeDeadLetterPolicy(
+                pubsub::Topic(project_id, dead_letter_topic_id),
+                dead_letter_delivery_attempts)));
+    if (sub.status().code() == google::cloud::StatusCode::kAlreadyExists) {
+      std::cout << "The subscription already exists\n";
+      return;
+    }
+    if (!sub) throw std::runtime_error(sub.status().message());
+
+    std::cout << "The subscription was successfully created: "
+              << sub->DebugString() << "\n";
+
+    std::cout << "It will forward dead letter messages to: "
+              << sub->dead_letter_policy().dead_letter_topic() << "\n";
+
+    std::cout << "After " << sub->dead_letter_policy().max_delivery_attempts()
+              << " delivery attempts.\n";
+  }
+  // [END pubsub_dead_letter_create_subscription]
+  //! [dead-letter-create-subscription]
+  (std::move(client), argv.at(0), argv.at(1), argv.at(2), argv.at(3),
+   std::stoi(argv.at(4)));
 }
 
 void GetSubscription(google::cloud::pubsub::SubscriptionAdminClient client,
@@ -1099,6 +1139,8 @@ void AutoRun(std::vector<std::string> const& argv) {
   auto topic_id = RandomTopicId(generator);
   auto subscription_id = RandomSubscriptionId(generator);
   auto push_subscription_id = RandomSubscriptionId(generator);
+  auto dead_letter_subscription_id = RandomSubscriptionId(generator);
+  auto dead_letter_topic_id = "dead-letter-" + RandomTopicId(generator);
 
   auto snapshot_id = RandomSnapshotId(generator);
 
@@ -1165,6 +1207,26 @@ void AutoRun(std::vector<std::string> const& argv) {
   // Move push_subscription_id to prevent accidentally using it below.
   DeleteSubscription(subscription_admin_client,
                      {project_id, std::move(push_subscription_id)});
+
+  std::cout << "\nRunning CreateTopic() sample [3]" << std::endl;
+  CreateTopic(topic_admin_client, {project_id, dead_letter_topic_id});
+
+  // Hardcode this number as it does not really matter. The other samples pick
+  // something between 10 and 15.
+  auto constexpr kDeadLetterDeliveryAttempts = 15;
+
+  std::cout << "\nRunning CreateDeadLetterSubscription() sample" << std::endl;
+  CreateDeadLetterSubscription(
+      subscription_admin_client,
+      {project_id, topic_id, dead_letter_subscription_id, dead_letter_topic_id,
+       std::to_string(kDeadLetterDeliveryAttempts)});
+
+  std::cout << "\nRunning DeleteTopic() sample [1]" << std::endl;
+  DeleteTopic(topic_admin_client, {project_id, dead_letter_topic_id});
+
+  std::cout << "\nRunning DeleteSubscription() sample [2]" << std::endl;
+  DeleteSubscription(subscription_admin_client,
+                     {project_id, std::move(dead_letter_subscription_id)});
 
   std::cout << "\nRunning CreateSnapshot() sample [1]" << std::endl;
   CreateSnapshot(subscription_admin_client,
@@ -1278,16 +1340,16 @@ void AutoRun(std::vector<std::string> const& argv) {
   std::cout << "\nRunning DetachSubscription() sample" << std::endl;
   DetachSubscription(topic_admin_client, {project_id, subscription_id});
 
-  std::cout << "\nRunning DeleteSubscription() sample [2]" << std::endl;
-  DeleteSubscription(subscription_admin_client, {project_id, subscription_id});
-
   std::cout << "\nRunning DeleteSubscription() sample [3]" << std::endl;
   DeleteSubscription(subscription_admin_client, {project_id, subscription_id});
 
-  std::cout << "\nRunning DeleteTopic() sample [1]" << std::endl;
-  DeleteTopic(topic_admin_client, {project_id, topic_id});
+  std::cout << "\nRunning DeleteSubscription() sample [4]" << std::endl;
+  DeleteSubscription(subscription_admin_client, {project_id, subscription_id});
 
   std::cout << "\nRunning DeleteTopic() sample [2]" << std::endl;
+  DeleteTopic(topic_admin_client, {project_id, topic_id});
+
+  std::cout << "\nRunning DeleteTopic() sample [3]" << std::endl;
   DeleteTopic(topic_admin_client, {project_id, topic_id});
 }
 
@@ -1325,6 +1387,11 @@ int main(int argc, char* argv[]) {  // NOLINT(bugprone-exception-escape)
           "create-push-subscription",
           {"project-id", "topic-id", "subscription-id", "endpoint"},
           CreatePushSubscription),
+      CreateSubscriptionAdminCommand(
+          "create-dead-letter-subscription",
+          {"project-id", "topic-id", "subscription-id", "dead-letter-topic-id",
+           "dead-letter-delivery-attempts"},
+          CreateDeadLetterSubscription),
       CreateSubscriptionAdminCommand("get-subscription",
                                      {"project-id", "subscription-id"},
                                      GetSubscription),
