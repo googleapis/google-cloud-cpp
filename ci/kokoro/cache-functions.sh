5c0740d5cffaec53463d05d9a1834cae3e45d465
Carlos O'Ryan <coryan@google.com>
2020-09-22 03:17:12 +0000

    WIP - Fix formatting

--- ci/kokoro/cache-functions.sh
+++ ci/kokoro/cache-functions.sh
@@ -16,7 +16,7 @@
 readonly CACHE_KEYFILE="${KOKORO_GFILE_DIR:-/dev/shm}/build-results-service-account.json"
 
 cache_download_enabled() {
-  if [[ "${BUILD_NAME:-}" = "msan"  ]]; then
+  if [[ "${BUILD_NAME:-}" = "msan" ]]; then
     io::log "Skipping build cache for msan builds"
     return 1
   fi
