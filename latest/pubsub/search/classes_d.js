e44cc76c7927f83b60086c1e1d3938fa0e449370
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-10-15 15:41:43 -0700

    Automatically generated documentation

--- latest/pubsub/search/classes_d.js
+++ latest/pubsub/search/classes_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['seekparams',['SeekParams',['../structgoogle_1_1cloud_1_1pubsub_1_1v1_1_1SubscriptionAdminConnection_1_1SeekParams.html',1,'google::cloud::pubsub::v1::SubscriptionAdminConnection']]],
   ['snapshot',['Snapshot',['../classgoogle_1_1cloud_1_1pubsub_1_1v1_1_1Snapshot.html',1,'google::cloud::pubsub::v1']]],
-  ['snapshotmutationbuilder',['SnapshotMutationBuilder',['../classgoogle_1_1cloud_1_1pubsub_1_1v1_1_1SnapshotMutationBuilder.html',1,'google::cloud::pubsub::v1']]],
+  ['snapshotbuilder',['SnapshotBuilder',['../classgoogle_1_1cloud_1_1pubsub_1_1v1_1_1SnapshotBuilder.html',1,'google::cloud::pubsub::v1']]],
   ['status',['Status',['https://googleapis.dev/google-cloud-common/master/classgoogle_1_1cloud_1_1v1_1_1Status.html',1,'google::cloud::v1']]],
   ['statusor',['StatusOr',['https://googleapis.dev/google-cloud-common/master/classgoogle_1_1cloud_1_1v1_1_1StatusOr.html',1,'google::cloud::v1']]],
   ['subscribeparams',['SubscribeParams',['../structgoogle_1_1cloud_1_1pubsub_1_1v1_1_1SubscriberConnection_1_1SubscribeParams.html',1,'google::cloud::pubsub::v1::SubscriberConnection']]],
