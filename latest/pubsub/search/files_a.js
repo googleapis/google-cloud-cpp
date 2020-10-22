e93b0c7fab248914df44cd626ebd04c631fda8e2
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-10-21 18:37:45 -0700

    Automatically generated documentation

--- latest/pubsub/search/files_a.js
+++ latest/pubsub/search/files_a.js
@@ -2,10 +2,8 @@ var searchData=
 [
   ['message_2ecc',['message.cc',['../message_8cc.html',1,'']]],
   ['message_2eh',['message.h',['../message_8h.html',1,'']]],
-  ['message_5fbatcher_2eh',['message_batcher.h',['../message__batcher_8h.html',1,'']]],
   ['message_5ftest_2ecc',['message_test.cc',['../message__test_8cc.html',1,'']]],
   ['mock_5fack_5fhandler_2eh',['mock_ack_handler.h',['../mock__ack__handler_8h.html',1,'']]],
-  ['mock_5fmessage_5fbatcher_2eh',['mock_message_batcher.h',['../mock__message__batcher_8h.html',1,'']]],
   ['mock_5fpublisher_2ecc',['mock_publisher.cc',['../mock__publisher_8cc.html',1,'']]],
   ['mock_5fpublisher_5fconnection_2eh',['mock_publisher_connection.h',['../mock__publisher__connection_8h.html',1,'']]],
   ['mock_5fpublisher_5fstub_2eh',['mock_publisher_stub.h',['../mock__publisher__stub_8h.html',1,'']]],
