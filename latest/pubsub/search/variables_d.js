f0c190961c6beb455c5f258cb251a2872be8fdea
Google Cloud C++ Project Robot <google-cloud-cpp-bot@users.noreply.github.com>
2020-10-22 11:00:57 -0700

    Automatically generated documentation

--- latest/pubsub/search/variables_d.js
+++ latest/pubsub/search/variables_d.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['value_5f',['value_',['https://googleapis.dev/cpp/google-cloud-common/latest/classgoogle_1_1cloud_1_1v1_1_1StatusOr.html#a212fa48fdadd25c3a91ee1c123b0c89e',1,'google::cloud::v1::StatusOr']]],
-  ['version',['version',['https://googleapis.dev/cpp/google-cloud-common/latest/structgoogle_1_1cloud_1_1v1_1_1IamPolicy.html#ad7623682b6ad3d20a43dab59806633d7',1,'google::cloud::v1::IamPolicy']]]
+  ['update_5fmask',['update_mask',['../namespacegoogle.html#a56628d0dcba652374562df38efe1609a',1,'google::update_mask()'],['../subscription__builder__test_8cc.html#ad7ab45d03d717f8f1699c0d31ccd42a8',1,'update_mask():&#160;subscription_builder_test.cc']]]
 ];
