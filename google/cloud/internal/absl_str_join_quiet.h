2c7beb7feacd3d7394db515ebf3b42447873821b
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-10-26 23:46:31 -0400

    fix: pop the diagnostic stack to restore defaults (#5341)

--- google/cloud/internal/absl_str_join_quiet.h
+++ google/cloud/internal/absl_str_join_quiet.h
@@ -30,6 +30,6 @@
 #endif  // _MSC_VER
 #include "absl/strings/str_join.h"
 // TODO(#4501) - end
-#include "google/cloud/internal/diagnostics_push.inc"
+#include "google/cloud/internal/diagnostics_pop.inc"
 
 #endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_ABSL_STR_JOIN_QUIET_H
