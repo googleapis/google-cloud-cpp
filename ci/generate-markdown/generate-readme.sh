28c73a2ee304159b9770d2f19c1bdf21c8bf5b34
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-11-06 09:50:20 -0500

    WIP

--- ci/generate-markdown/generate-readme.sh
+++ ci/generate-markdown/generate-readme.sh
@@ -102,15 +102,6 @@ As an example, the following code snippet, taken from [Google Cloud
 Storage](google/cloud/storage/README.md), should give you a taste of what it's
 like to use one of these C++ libraries.
 
-## Support
-
-* This project supports Windows, macOS, Linux
-* This project supports C++11 (and higher) compilers (we test with GCC \>= 5.4, Clang >= 3.8, and MSVC \>= 2019)
-* This project supports Bazel and CMake builds; See the [Quickstart examples](https://github.com/googleapis/google-cloud-cpp#quickstart)
-* This project uses dependencies described in [doc/packaging.md](https://github.com/googleapis/google-cloud-cpp/blob/master/doc/packaging.md)
-* This project works with or without exceptions enabled
-* This project cuts [monthly releases](https://github.com/googleapis/google-cloud-cpp/releases) with detailed release notes
-
 ```cc
 EOF
 
@@ -120,6 +111,15 @@ sed -n '/^#/,$p' "${BINDIR}/../../google/cloud/storage/quickstart/quickstart.cc"
 
 cat <<"EOF"
 ```
+
+## Support
+
+* This project supports Windows, macOS, Linux
+* This project supports C++11 (and higher) compilers (we test with GCC \>= 5.4, Clang >= 3.8, and MSVC \>= 2019)
+* This project supports Bazel and CMake builds; See the [Quickstart examples](https://github.com/googleapis/google-cloud-cpp#quickstart)
+* This project uses dependencies described in [doc/packaging.md](https://github.com/googleapis/google-cloud-cpp/blob/master/doc/packaging.md)
+* This project works with or without exceptions enabled
+* This project cuts [monthly releases](https://github.com/googleapis/google-cloud-cpp/releases) with detailed release notes
 
 ## Contact us
 
