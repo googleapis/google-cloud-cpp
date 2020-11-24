d002c38a0cc43f9e6e75c8507d817cec51bd9355
Greg Miller <9447643+devjgm@users.noreply.github.com>
2020-11-23 22:05:54 -0500

    cleaned up

--- google/cloud/internal/pagination_range.h
+++ google/cloud/internal/pagination_range.h
@@ -21,6 +21,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -36,7 +37,26 @@ namespace internal {
  * pagination APIs. The application calls a `List*()` RPC which returns
  * a "page" of elements and a token, calling the same `List*()` RPC with the
  * token returns the next "page". We want to expose these APIs as input ranges
- * in the C++ client libraries. This class performs that work.
+ * in the C++ client libraries.
+ *
+ * To construct instances of `PaginationRange<T>`, use the
+ * `MakePaginationRange()` function defined below.
+ *
+ * @tparam T the type of the items, typically a proto describing the resources
+ */
+template <typename T>
+using PaginationRange = StreamRange<T>;
+
+/**
+ * Returns `T`s one at a time from pages of responses.
+ *
+ * This class is an implementation detail. An instance of this class is wrapped
+ * in a lambda and passed as the `StreamReader<T>` to the `PaginationRange<T>`
+ * constructor. This class is responsible for loading pages and returning the
+ * next `T`.
+ *
+ * Users should not use this class directly. Use the `MakePaginationRange()`
+ * function (defined below) instead.
  *
  * @tparam T the type of the items, typically a proto describing the resources
  * @tparam Request the type of the request object for the `List` RPC.
@@ -46,83 +66,103 @@ template <typename T, typename Request, typename Response>
 class PagedStreamReader {
  public:
   /**
-   * Create a new range to paginate over some elements.
+   * Create a new object.
    *
    * @param request the first request to start the iteration, the library may
    *    initialize this request with any filtering constraints.
    * @param loader makes the RPC request to fetch a new page of items.
-   * @param get_items extracts the items from the response using native C++
+   * @param extractor extracts the items from the response using native C++
    *     types (as opposed to the proto types used in `Response`).
    */
   PagedStreamReader(Request request,
-                    std::function<StatusOr<Response>(Request const& r)> loader,
-                    std::function<std::vector<T>(Response r)> get_items)
+                    std::function<StatusOr<Response>(Request const&)> loader,
+                    std::function<std::vector<T>(Response)> extractor)
       : request_(std::move(request)),
-        next_page_loader_(std::move(loader)),
-        get_items_(std::move(get_items)),
-        on_last_page_(false) {
-    current_ = current_page_.begin();
+        loader_(std::move(loader)),
+        extractor_(std::move(extractor)),
+        last_page_(false) {
+    current_ = page_.begin();
   }
 
   /**
    * Fetches (or returns if already fetched) the next object from the stream.
    *
-   * @return An iterator pointing to the next element in the stream. On error,
-   *   it returns an iterator that is different from `.end()`, but has an error
-   *   status. If the stream is exhausted, it returns the `.end()` iterator.
+   * @return the next available `T`, if one exists (or can be loaded). Returns
+   *   a non-OK `Status` to indicate an error, and an OK `Status` to indicate a
+   *   successful end of stream.
    */
   typename StreamReader<T>::result_type GetNext() {
-    if (current_page_.end() == current_) {
-      if (on_last_page_) return Status{};  // End of stream
-      request_.set_page_token(std::move(next_page_token_));
-      auto response = next_page_loader_(request_);
-      if (!response.ok()) {
-        next_page_token_.clear();
-        current_page_.clear();
-        on_last_page_ = true;
-        current_ = current_page_.begin();
-        return std::move(response).status();
-      }
-      next_page_token_ = std::move(*response->mutable_next_page_token());
-      current_page_ = get_items_(*std::move(response));
-      current_ = current_page_.begin();
-      if (next_page_token_.empty()) on_last_page_ = true;
-      if (current_page_.end() == current_) return Status{};  // End of stream
+    if (current_ == page_.end()) {
+      if (last_page_) return Status{};
+      request_.set_page_token(std::move(token_));
+      auto response = loader_(request_);
+      if (!response.ok()) return std::move(response).status();
+      token_ = std::move(*response->mutable_next_page_token());
+      if (token_.empty()) last_page_ = true;
+      page_ = extractor_(*std::move(response));
+      current_ = page_.begin();
+      if (current_ == page_.end()) return Status{};
     }
     return std::move(*current_++);
   }
 
  private:
   Request request_;
-  std::function<StatusOr<Response>(Request const& r)> next_page_loader_;
-  std::function<std::vector<T>(Response r)> get_items_;
-  std::vector<T> current_page_;
+  std::function<StatusOr<Response>(Request const&)> loader_;
+  std::function<std::vector<T>(Response)> extractor_;
+  std::vector<T> page_;
   typename std::vector<T>::iterator current_;
-  std::string next_page_token_;
-  bool on_last_page_;
+  std::string token_;
+  bool last_page_;
 };
 
-template <typename T>
-using PaginationRange = StreamRange<T>;
-
-template <typename Range, typename Request, typename Loader, typename Extractor,
-          typename ValueType = typename Range::value_type::value_type,
-          typename Response =
-              typename std::result_of<Loader(Request)>::type::value_type>
+/**
+ * A factory function for creating `PaginationRange<T>` instances.
+ *
+ * This function creates a `PaginationRange<T>` instance that will be fed from
+ * a `PagedStreamReader` (defined above).
+ *
+ * @par Example
+ *
+ * @code
+ *  auto loader = [](MyRequestProto const& r) -> StatusOr<MyResponseProto> {
+ *    ...
+ *  };
+ *  auto extractor = [](MyResponseProto r) -> std::vector<Foo> {
+ *    ...
+ *  };
+ *  using MyFooRange = PaginationRange<Foo>;
+ *  auto range = MakePaginationRange<MyFooRange>(
+ *      MyRequestProto{}, std::move(loader), std::move(extractor));
+ * @endcode
+ */
+template <typename Range, typename Request, typename Loader, typename Extractor>
 Range MakePaginationRange(Request request, Loader loader, Extractor extractor) {
-  auto reader =
-      std::make_shared<PagedStreamReader<ValueType, Request, Response>>(
-          std::move(request), std::move(loader), std::move(extractor));
-  return Range([reader]() mutable ->
-               typename StreamReader<ValueType>::result_type {
-                 return reader->GetNext();
-               });
+  using ValueType = typename Range::value_type::value_type;
+  using LoaderResult = typename std::result_of<Loader(Request)>::type;
+  using Response = typename LoaderResult::value_type;
+  using ExtractorResult = typename std::result_of<Extractor(Response)>::type;
+  // Some static asserts to make compiler errors easier to diagnose.
+  static_assert(std::is_same<Range, PaginationRange<ValueType>>::value,
+                "Expected Range is of type PaginationRange<ValueType>");
+  static_assert(std::is_same<LoaderResult, StatusOr<Response>>::value,
+                "Expected loader functor like StatusOr<Response>(Request)");
+  static_assert(std::is_same<ExtractorResult, std::vector<ValueType>>::value,
+                "Expected extractor functor like vector<ValueType>(Response)");
+  using ReaderType = PagedStreamReader<ValueType, Request, Response>;
+  auto reader = std::make_shared<ReaderType>(
+      std::move(request), std::move(loader), std::move(extractor));
+  return Range{[reader]() mutable { return reader->GetNext(); }};
 }
 
+/**
+ * A convenient function to make a `PaginationRange<T>` that contains a single
+ * error indicating "unimplemented".
+ */
 template <typename Range>
 Range MakeUnimplementedPaginationRange() {
-  return Range{[]() -> typename StreamReader<
-                        typename Range::value_type::value_type>::result_type {
+  using ValueType = typename Range::value_type::value_type;
+  return Range{[]() -> typename StreamReader<ValueType>::result_type {
     return Status{StatusCode::kUnimplemented, "needs-override"};
   }};
 }
