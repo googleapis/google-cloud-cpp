f796119078086c84efd2f26c551de69cddc35373
Scott Hart <sdhart@google.com>
2020-12-15 11:49:45 -0500

    feature(generator): generate multiple services from proto file (#5630)
    * feature(generator): generate multiple services from proto file

---
+++ generator/integration_tests/golden/iam_credentials_stub_test.cc
@@ -0,0 +1,133 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "generator/integration_tests/golden/internal/iam_credentials_stub.gcpcxx.pb.h"
+#include <gmock/gmock.h>
+#include <memory>
+
+using ::testing::_;
+using ::testing::Return;
+
+namespace google {
+namespace cloud {
+namespace golden_internal {
+inline namespace GOOGLE_CLOUD_CPP_NS {
+namespace {
+class MockGrpcIAMCredentialsStub : public ::google::test::admin::database::v1::
+                                       IAMCredentials::StubInterface {
+public:
+  ~MockGrpcIAMCredentialsStub() override = default;
+  MOCK_METHOD(
+      ::grpc::Status, GenerateAccessToken,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateAccessTokenRequest
+           &request,
+       ::google::test::admin::database::v1::GenerateAccessTokenResponse
+           *response),
+      (override));
+  MOCK_METHOD(
+      ::grpc::Status, GenerateIdToken,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateIdTokenRequest
+           &request,
+       ::google::test::admin::database::v1::GenerateIdTokenResponse *response),
+      (override));
+  MOCK_METHOD(
+      ::grpc::ClientAsyncResponseReaderInterface<
+          ::google::test::admin::database::v1::GenerateAccessTokenResponse> *,
+      AsyncGenerateAccessTokenRaw,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateAccessTokenRequest
+           &request,
+       ::grpc::CompletionQueue *cq),
+      (override));
+  MOCK_METHOD(
+      ::grpc::ClientAsyncResponseReaderInterface<
+          ::google::test::admin::database::v1::GenerateAccessTokenResponse> *,
+      PrepareAsyncGenerateAccessTokenRaw,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateAccessTokenRequest
+           &request,
+       ::grpc::CompletionQueue *cq),
+      (override));
+  MOCK_METHOD(
+      ::grpc::ClientAsyncResponseReaderInterface<
+          ::google::test::admin::database::v1::GenerateIdTokenResponse> *,
+      AsyncGenerateIdTokenRaw,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateIdTokenRequest
+           &request,
+       ::grpc::CompletionQueue *cq),
+      (override));
+  MOCK_METHOD(
+      ::grpc::ClientAsyncResponseReaderInterface<
+          ::google::test::admin::database::v1::GenerateIdTokenResponse> *,
+      PrepareAsyncGenerateIdTokenRaw,
+      (::grpc::ClientContext * context,
+       const ::google::test::admin::database::v1::GenerateIdTokenRequest
+           &request,
+       ::grpc::CompletionQueue *cq),
+      (override));
+};
+
+class IAMCredentialsStubTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    grpc_stub_ = std::unique_ptr<MockGrpcIAMCredentialsStub>(
+        new MockGrpcIAMCredentialsStub());
+  }
+
+  static grpc::Status GrpcTransientError() {
+    return grpc::Status(grpc::StatusCode::UNAVAILABLE, "try-again");
+  }
+  static Status TransientError() {
+    return Status(StatusCode::kUnavailable, "try-again");
+  }
+
+  std::unique_ptr<MockGrpcIAMCredentialsStub> grpc_stub_;
+};
+
+TEST_F(IAMCredentialsStubTest, GenerateAccessToken) {
+  grpc::Status status;
+  grpc::ClientContext context;
+  google::test::admin::database::v1::GenerateAccessTokenRequest request;
+  EXPECT_CALL(*grpc_stub_, GenerateAccessToken(&context, _, _))
+      .WillOnce(Return(status))
+      .WillOnce(Return(GrpcTransientError()));
+  DefaultIAMCredentialsStub stub(std::move(grpc_stub_));
+  auto success = stub.GenerateAccessToken(context, request);
+  EXPECT_TRUE(success.ok());
+  auto failure = stub.GenerateAccessToken(context, request);
+  EXPECT_EQ(failure.status(), TransientError());
+}
+
+TEST_F(IAMCredentialsStubTest, GenerateIdToken) {
+  grpc::Status status;
+  grpc::ClientContext context;
+  google::test::admin::database::v1::GenerateIdTokenRequest request;
+  EXPECT_CALL(*grpc_stub_, GenerateIdToken(&context, _, _))
+      .WillOnce(Return(status))
+      .WillOnce(Return(GrpcTransientError()));
+  DefaultIAMCredentialsStub stub(std::move(grpc_stub_));
+  auto success = stub.GenerateIdToken(context, request);
+  EXPECT_TRUE(success.ok());
+  auto failure = stub.GenerateIdToken(context, request);
+  EXPECT_EQ(failure.status(), TransientError());
+}
+
+} // namespace
+} // namespace GOOGLE_CLOUD_CPP_NS
+} // namespace golden_internal
+} // namespace cloud
+} // namespace google
