f0b71d15c2870cebac876db86e1bfc867abc3aaf
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-01-13 22:53:53 -0500

    fix(bigtable): crashes during client shutdown (#5701)

--- google/cloud/internal/async_connection_ready.h
+++ google/cloud/internal/async_connection_ready.h
@@ -15,16 +15,11 @@
 #ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_ASYNC_CONNECTION_READY_H
 #define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_ASYNC_CONNECTION_READY_H
 
-#include "google/cloud/async_operation.h"
 #include "google/cloud/future.h"
+#include "google/cloud/internal/completion_queue_impl.h"
 #include "google/cloud/status.h"
 #include "google/cloud/version.h"
 #include <grpcpp/channel.h>
-#include <grpcpp/completion_queue.h>
-#include <grpcpp/generic/async_generic_service.h>
-#include <grpcpp/generic/generic_stub.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
 #include <chrono>
 #include <memory>
 
@@ -40,30 +35,27 @@ namespace internal {
  * well be hidden away from the header, but is useful in
  * `FakeCompletionQueueImpl`.
  */
-class AsyncConnectionReadyFuture : public internal::AsyncGrpcOperation {
+class AsyncConnectionReadyFuture
+    : public std::enable_shared_from_this<AsyncConnectionReadyFuture> {
  public:
-  AsyncConnectionReadyFuture(std::shared_ptr<grpc::Channel> channel,
-                             std::chrono::system_clock::time_point deadline);
+  AsyncConnectionReadyFuture(
+      std::shared_ptr<google::cloud::internal::CompletionQueueImpl> cq,
+      std::shared_ptr<grpc::Channel> channel,
+      std::chrono::system_clock::time_point deadline);
 
-  void Start(grpc::CompletionQueue& cq, void* tag);
-  // There doesn't seem to be a way to cancel this operation:
-  // https://github.com/grpc/grpc/issues/3064
-  void Cancel() override {}
-  /// The future will be set to whether the state changed (false means timeout).
-  future<Status> GetFuture() { return promise_.get_future(); }
+  future<Status> Start();
 
  private:
-  bool Notify(bool ok) override;
-  // Returns whether to register for state change.
-  bool HandleSingleStateChange();
+  void Notify(bool ok);
 
-  std::shared_ptr<grpc::Channel> channel_;
-  std::chrono::system_clock::time_point deadline_;
+  // gRPC uses an anonymous type for the gRPC channel state enum :shrug:.
+  using ChannelStateType = decltype(GRPC_CHANNEL_READY);
+  void RunIteration(ChannelStateType state);
+
+  std::shared_ptr<google::cloud::internal::CompletionQueueImpl> const cq_;
+  std::shared_ptr<grpc::Channel> const channel_;
+  std::chrono::system_clock::time_point const deadline_;
   promise<Status> promise_;
-  // CompletionQueue and tag are memorized on start of the operation so that it
-  // can be re-registered in the completion queue for another state change.
-  grpc::CompletionQueue* cq_;
-  void* tag_;
 };
 
 }  // namespace internal
