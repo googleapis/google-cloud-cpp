4fc214400bf265a2a7504c7ccf91a16979a75477
Carlos O'Ryan <coryan@google.com>
2021-01-28 23:58:21 +0000

    Update the ABI checking scripts (and rename the baseline)

--- ci/kokoro/docker/build-in-docker-cmake.sh
+++ ci/kokoro/docker/build-in-docker-cmake.sh
@@ -462,7 +462,7 @@ if [[ "${TEST_INSTALL:-}" = "yes" ]]; then
       "google_cloud_cpp_common"
       "google_cloud_cpp_grpc_utils"
       "google_cloud_cpp_spanner"
-      "storage_client"
+      "google_cloud_cpp_storage"
       "google_cloud_cpp_pubsub"
     )
     echo "${libraries[@]}" | xargs -n 1 -P "${NCPU}" \
