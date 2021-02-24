c576bd2eb7552097fe6bbb674ebd3d285a9b50fa
Tommy Nguyen <remyabel@gmail.com>
2021-02-24 02:55:51 -0500

    cleanup: reenable DeleteResumableUpload test

--- google/cloud/storage/tests/object_integration_test.cc
+++ google/cloud/storage/tests/object_integration_test.cc
@@ -898,10 +898,6 @@ TEST_F(ObjectIntegrationTest, DeleteAccessControlFailure) {
 }
 
 TEST_F(ObjectIntegrationTest, DeleteResumableUpload) {
-  if (UsingGrpc()) {
-    // TODO(5030): DeleteResumableUpload doesn't work with gRPC yet.
-    GTEST_SKIP();
-  }
   StatusOr<Client> client = MakeIntegrationTestClient(
       absl::make_unique<LimitedErrorCountRetryPolicy>(1));
   ASSERT_STATUS_OK(client);
