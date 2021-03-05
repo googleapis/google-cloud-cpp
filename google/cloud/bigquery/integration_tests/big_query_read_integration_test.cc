142af4277125b5b454ac35c4f6b05a7f3c60f478
Scott Hart <sdhart@google.com>
2021-03-05 14:32:57 -0500

    feature(bigquery): storage library generation

---
+++ google/cloud/bigquery/integration_tests/big_query_read_integration_test.cc
@@ -0,0 +1,89 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/bigquery/big_query_read_client.gcpcxx.pb.h"
+#include "google/cloud/bigquery/internal/big_query_read_stub_factory.gcpcxx.pb.h"
+#include "google/cloud/internal/getenv.h"
+#include "google/cloud/log.h"
+#include "google/cloud/testing_util/assert_ok.h"
+#include "google/cloud/testing_util/scoped_log.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+namespace bigquery {
+inline namespace GOOGLE_CLOUD_CPP_GENERATED_NS {
+namespace {
+
+using ::testing::Contains;
+using ::testing::HasSubstr;
+
+class BigQueryReadIntegrationTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    connection_options_.enable_tracing("rpc");
+    retry_policy_ =
+        absl::make_unique<BigQueryReadLimitedErrorCountRetryPolicy>(1);
+    backoff_policy_ = absl::make_unique<ExponentialBackoffPolicy>(
+        std::chrono::seconds(1), std::chrono::seconds(1), 2.0);
+  }
+  std::vector<std::string> ClearLogLines() { return log_.ExtractLines(); }
+  BigQueryReadConnectionOptions connection_options_;
+  std::unique_ptr<BigQueryReadRetryPolicy> retry_policy_;
+  std::unique_ptr<BackoffPolicy> backoff_policy_;
+
+ private:
+  testing_util::ScopedLog log_;
+};
+
+TEST_F(BigQueryReadIntegrationTest, CreateReadSessionFailure) {
+  auto client = BigQueryReadClient(MakeBigQueryReadConnection(
+      connection_options_, retry_policy_->clone(), backoff_policy_->clone(),
+      MakeDefaultBigQueryReadConnectionIdempotencyPolicy()));
+  ::google::cloud::bigquery::storage::v1::CreateReadSessionRequest request;
+  auto response = client.CreateReadSession(request);
+  EXPECT_FALSE(response.status().ok());
+  auto const log_lines = ClearLogLines();
+  EXPECT_THAT(log_lines, Contains(HasSubstr("CreateReadSession")));
+}
+
+TEST_F(BigQueryReadIntegrationTest, ReadRowsFailure) {
+  auto client = BigQueryReadClient(MakeBigQueryReadConnection(
+      connection_options_, retry_policy_->clone(), backoff_policy_->clone(),
+      MakeDefaultBigQueryReadConnectionIdempotencyPolicy()));
+  ::google::cloud::bigquery::storage::v1::ReadRowsRequest request;
+  auto response = client.ReadRows(request);
+  auto begin = response.begin();
+  EXPECT_FALSE(begin == response.end());
+  auto const log_lines = ClearLogLines();
+  EXPECT_THAT(log_lines, Contains(HasSubstr("ReadRows")));
+}
+
+TEST_F(BigQueryReadIntegrationTest, SplitReadStreamFailure) {
+  auto client = BigQueryReadClient(MakeBigQueryReadConnection(
+      connection_options_, retry_policy_->clone(), backoff_policy_->clone(),
+      MakeDefaultBigQueryReadConnectionIdempotencyPolicy()));
+  ::google::cloud::bigquery::storage::v1::SplitReadStreamRequest request;
+  auto response = client.SplitReadStream(request);
+  EXPECT_FALSE(response.status().ok());
+  auto const log_lines = ClearLogLines();
+  EXPECT_THAT(log_lines, Contains(HasSubstr("SplitReadStream")));
+}
+
+}  // namespace
+}  // namespace GOOGLE_CLOUD_CPP_GENERATED_NS
+}  // namespace bigquery
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/bigquery/retry_traits.h
@@ -0,0 +1,39 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGQUERY_RETRY_TRAITS_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGQUERY_RETRY_TRAITS_H
+
+#include "google/cloud/status.h"
+#include "google/cloud/version.h"
+
+namespace google {
+namespace cloud {
+namespace bigquery_internal {
+inline namespace GOOGLE_CLOUD_CPP_GENERATED_NS {
+
+/// Define the gRPC status code semantics for retrying requests.
+struct BigQueryReadRetryTraits {
+  static inline bool IsPermanentFailure(google::cloud::Status const& status) {
+    return status.code() != StatusCode::kDeadlineExceeded &&
+           status.code() != StatusCode::kUnavailable;
+  }
+};
+
+}  // namespace GOOGLE_CLOUD_CPP_GENERATED_NS
+}  // namespace bigquery_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_BIGQUERY_RETRY_TRAITS_H
