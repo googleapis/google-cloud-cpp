8faaabddb97a1e7fb3c385c6ce77c7ec4d7744ec
Carlos O'Ryan <coryan@google.com>
2021-03-17 13:06:15 +0000

    Remove testing code

--- google/cloud/storage/tests/object_resumable_write_integration_test.cc
+++ google/cloud/storage/tests/object_resumable_write_integration_test.cc
@@ -197,7 +197,6 @@ TEST_F(ObjectResumableWriteIntegrationTest, WriteNotChunked) {
 
   auto status = client->DeleteObject(bucket_name_, object_name);
   EXPECT_STATUS_OK(status);
-  EXPECT_FALSE(status.ok()); // TODO(coryan) - DO NOT MERGE
 }
 
 TEST_F(ObjectResumableWriteIntegrationTest, WriteResumeFinalizedUpload) {
