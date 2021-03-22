7e9937a2f0932da49f4453691e6431f95bf5a90b
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-03-22 15:00:54 -0400

    feat(ci): add caching to GCB builds (#6084)
    Adds a ci/cloudbuild/cache.sh script that is responsible for save'ing and restore'ing our build caches to/from GCS.
    There are also two new build steps in the cloudbuild.yaml file that call this script at the appropriate times.
    
    NOTE: The ~/.cache/bazel directory is not currently cached. It was huge and it wasn't clear if it was beneficial. I'll look into
    fixing that later, or just using the normal remote build cache for bazel.
    
    This PR also creates a home directory mount of /h that's visible to all build steps so they can all read/write to the homedir.

---
+++ ci/cloudbuild/cache.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script saves a cache tarball to GCS, and restores one from GCS. It is
+# intended to be called from a build step in the `cloudbuild.yaml` file. It
+# executes in the context of build. Typically the "restore cache" step will be
+# called in one step, followed by the actual build of the code, followed by the
+# "save cache" step.
+#
+# Usage: ./cache <save|restore>
+#
+#   Options:
+#     --bucket_url=<url>    URL to the GCS bucket where the cache should be saved
+#     --key=<key>           The primary key to use when caching
+#     --fallback_key=<key>  If the primary key isn't found when restoring, try this key
+#     --path=<path>         A path to be backed up. May be specified multiple times.
+#     -h|--help             Print this help message
+
+set -euo pipefail
+
+source "$(dirname "$0")/../lib/init.sh"
+source module ci/lib/io.sh
+
+function print_usage() {
+  # Extracts the usage from the file comment starting at line 17.
+  sed -n '17,/^$/s/^# \?//p' "${PROGRAM_PATH}"
+}
+
+# Use getopt to parse and normalize all the args.
+PARSED="$(getopt -a \
+  --options="h" \
+  --longoptions="bucket_url:,key:,fallback_key:,path:,help" \
+  --name="${PROGRAM_NAME}" \
+  -- "$@")"
+eval set -- "${PARSED}"
+
+BUCKET_URL=""
+KEY=""
+FALLBACK_KEY=""
+PATHS=()
+while true; do
+  case "$1" in
+  --bucket_url)
+    BUCKET_URL="$2"
+    shift 2
+    ;;
+  --key)
+    KEY="$2"
+    shift 2
+    ;;
+  --fallback_key)
+    FALLBACK_KEY="$2"
+    shift 2
+    ;;
+  --path)
+    PATHS+=("$2")
+    shift 2
+    ;;
+  -h | --help)
+    print_usage
+    exit 0
+    ;;
+  --)
+    shift
+    break
+    ;;
+  esac
+done
+readonly BUCKET_URL
+readonly KEY
+readonly FALLBACK_KEY
+readonly PATHS
+
+if [[ $# -eq 0 ]]; then
+  echo "Missing action: save|restore"
+  print_usage
+  exit 1
+elif [[ -z "${BUCKET_URL}" ]]; then
+  echo "Missing --bucket_url="
+  print_usage
+  exit 1
+elif [[ -z "${KEY}" ]]; then
+  echo "Missing --key="
+  print_usage
+  exit 1
+fi
+
+readonly PRIMARY_CACHE_URL="${BUCKET_URL}/${KEY}/cache.tar.gz"
+
+function save_cache() {
+  # Filters PATHS to only those that exist.
+  paths=()
+  for p in "${PATHS[@]}"; do
+    test -r "${p}" && paths+=("${p}")
+  done
+  if ((${#paths[@]} == 0)); then
+    io::log "No paths to cache found."
+    return 0
+  fi
+  io::log "Saving ( ${paths[*]} ) to ${PRIMARY_CACHE_URL}"
+  tar -czf - "${paths[@]}" | gsutil cp - "${PRIMARY_CACHE_URL}"
+  gsutil stat "${PRIMARY_CACHE_URL}"
+}
+
+function restore_cache() {
+  local urls=("${PRIMARY_CACHE_URL}")
+  if [[ -n "${FALLBACK_KEY}" ]]; then
+    urls+=("${BUCKET_URL}/${FALLBACK_KEY}/cache.tar.gz")
+  fi
+  for url in "${urls[@]}"; do
+    if gsutil stat "${url}"; then
+      io::log "Fetching cache url ${url}"
+      gsutil cp "${url}" - | tar -zxf - || continue
+      break
+    fi
+  done
+  return 0
+}
+
+io::log "====> ${PROGRAM_NAME}: $*"
+readonly TIMEFORMAT="==> 🕑 ${PROGRAM_NAME} completed in %R seconds"
+time {
+  case "$1" in
+  save)
+    save_cache
+    ;;
+  restore)
+    restore_cache
+    ;;
+  *)
+    print_usage
+    ;;
+  esac
+}
