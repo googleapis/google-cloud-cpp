36d95b61e8cfa43fd0b65c116b94f1730f3a1bf7
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-03-23 12:13:59 -0400

    ci: add an xsan GCB build (#6090)
    Adds an xsan GCB build. This runs several sanitizers that are not already run elsewhere, such as
    -fsanitize=float-divide-by-zero and -fsanitize=nullability

---
+++ ci/cloudbuild/builds/xsan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+source "$(dirname "$0")/../../lib/init.sh"
+source module ci/cloudbuild/builds/lib/bazel.sh
+
+export CC=clang
+export CXX=clang++
+
+mapfile -t args < <(bazel::common_args)
+args+=("--config=xsan")
+args+=("--test_tag_filters=-integration-test")
+bazel test "${args[@]}" ...
