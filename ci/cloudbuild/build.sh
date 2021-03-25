e47291ccd77e09a99714dfb63b163b80c390e5bc
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-03-25 09:34:23 -0400

    ci: use Cloud ID rather than unix ID
    Manual build caches show up named like `manual-<user>`. The username
    part should use the gcloud account rather than the `id -un`.

--- ci/cloudbuild/build.sh
+++ ci/cloudbuild/build.sh
@@ -97,9 +97,10 @@ if [ "${DISTRO}" != "local" ]; then
     print_usage
     exit 1
   fi
+  account="$(gcloud config list account --format "value(core.account)")"
   subs="_DISTRO=${DISTRO}"
   subs+=",_BUILD_NAME=${BUILD_NAME}"
-  subs+=",_CACHE_TYPE=manual-$(id -un)"
+  subs+=",_CACHE_TYPE=manual-${account})"
   io::log "====> Submitting cloud build job for ${subs}"
   args=(
     "--config=ci/cloudbuild/cloudbuild.yaml"
