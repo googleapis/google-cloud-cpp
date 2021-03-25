b9eef89338acdcff6e026160d9103b36ed226cf6
Scott Hart <sdhart@google.com>
2021-03-25 16:17:19 -0400

    feature(bigquery): add quickstart

--- ci/etc/integration-tests-config.sh
+++ ci/etc/integration-tests-config.sh
@@ -53,6 +53,9 @@ export GOOGLE_CLOUD_CPP_SPANNER_TEST_SERVICE_ACCOUNT="spanner-iam-test-sa@${GOOG
 # Cloud Pub/Sub only needs GOOGLE_CLOUD_PROJECT
 export GOOGLE_CLOUD_CPP_PUBSUB_TEST_QUICKSTART_TOPIC="quickstart"
 
+# Cloud BigQuery only needs GOOGLE_CLOUD_PROJECT
+export GOOGLE_CLOUD_CPP_BIGQUERY_TEST_QUICKSTART_TABLE="projects/bigquery-public-data/datasets/usa_names/tables/usa_1910_current"
+
 # Cloud IAM configuration parameters
 export GOOGLE_CLOUD_CPP_IAM_TEST_SERVICE_ACCOUNT=${GOOGLE_CLOUD_CPP_STORAGE_TEST_SIGNING_SERVICE_ACCOUNT}
 export GOOGLE_CLOUD_CPP_IAM_INVALID_TEST_SERVICE_ACCOUNT="invalid-test-account@cloud-cpp-testing-resources.iam.gserviceaccount.com"
