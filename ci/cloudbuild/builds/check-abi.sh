googleapis/google-cloud-cpp

commit 5cc0e18da728869734640bac4f49b7abc8cccb60

ci(gcb): add check-abi build (#6314)
* ci(gcb): add check-abi build

This build does not yet include an "update abi" mode, but that'll come
in a later PR.

ci/cloudbuild/builds/check-abi.sh
@@ -1,0 +1,90 @@
+#!/bin/bash
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+source "$(dirname "$0")/../../lib/init.sh"
+source module ci/cloudbuild/builds/lib/cmake.sh
+
+export CC=gcc
+export CXX=g++
+
+INSTALL_PREFIX=/var/tmp/google-cloud-cpp
+# TODO(#6313): Compile with `-Og`, which abi-dumper wants.
+cmake -GNinja \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Debug -S . -B cmake-out
+cmake --build cmake-out
+cmake --build cmake-out --target install
+
+# Uses `abi-dumper` to dump the ABI for the given library, which should be
+# installed at the given prefix. This function will be called from a subshell,
+# so it cannot use other variables or functions (including io::log*).
+function dump_abi() {
+  local library="$1"
+  local prefix="$2"
+  echo "Dumping ${library} (may be slow)..."
+  abi-dumper "${prefix}/lib64/lib${library}.so" \
+    -public-headers "${prefix}/include" \
+    -lver "actual" \
+    -o "cmake-out/${library}.actual.abi.dump"
+}
+export -f dump_abi # enables this function to be called from a subshell
+
+libraries=(
+  "google_cloud_cpp_bigtable"
+  "google_cloud_cpp_common"
+  "google_cloud_cpp_grpc_utils"
+  "google_cloud_cpp_spanner"
+  "google_cloud_cpp_storage"
+  "google_cloud_cpp_pubsub"
+)
+
+# Run the dump_abi function for each library in parallel since its slow.
+echo "${libraries[@]}" | xargs -P "$(nproc)" -n 1 \
+  bash -c "dump_abi \$0 ${INSTALL_PREFIX}"
+
+# A count of the number of libraries that fail the abi compliance check.
+# This will become the script's exit code.
+errors=0
+
+for lib in "${libraries[@]}"; do
+  io::log_h2 "Checking ${lib}"
+  actual_dump_file="${lib}.actual.abi.dump"
+  expected_dump_file="${lib}.expected.abi.dump"
+  zcat "${PROJECT_ROOT}/ci/test-abi/${expected_dump_file}.gz" > \
+    "${expected_dump_file}"
+  # We ignore all symbols in internal namespaces, because these are not part
+  # of our public API. We do this by specifying a regex that matches against
+  # the mangled symbol names. For example, 8 is the number of characters in
+  # the string "internal", and it should again be followed by some other
+  # number indicating the length of the symbol within the "internal"
+  # namespace. See: https://en.wikipedia.org/wiki/Name_mangling
+  if ! abi-compliance-checker \
+    -skip-internal-symbols "(8internal|15pubsub_internal|16spanner_internal)\d" \
+    -src -l "${lib}" \
+    -old "cmake-out/${expected_dump_file}" \
+    -new "cmake-out/${actual_dump_file}"; then
+    io::log_red "ABI Compliance error: ${lib}"
+    ((++errors))
+    report="cmake-out/compat_reports/${lib}/expected_to_actual/src_compat_report.html"
+    io::log "Report file: ${report}"
+    w3m -dump "${report}"
+  fi
+done
+echo
+exit "${errors}"

ci/cloudbuild/dockerfiles/fedora.Dockerfile
@@ -20,7 +20,7 @@ ARG NCPU=4
 # for `google-cloud-cpp`. Install these packages and additional development
 # tools to compile the dependencies:
 RUN dnf makecache && \
-    dnf install -y abi-compliance-checker abi-dumper ccache \
+    dnf install -y abi-compliance-checker abi-dumper autoconf automake ccache \
         clang clang-analyzer clang-tools-extra \
         cmake diffutils doxygen findutils gcc-c++ git \
         grpc-devel grpc-plugins lcov libcxx-devel libcxxabi-devel \
@@ -137,6 +137,17 @@ RUN curl -sSL https://github.com/matus-chochlik/ctcache/archive/0ad2e227e8a981a9
     tar -xzf - --strip-components=1 && \
     cp clang-tidy /usr/local/bin/clang-tidy-wrapper && \
     cp clang-tidy-cache /usr/local/bin/clang-tidy-cache && \
+    cd /var/tmp && rm -fr build
+
+# Installs Universal Ctags (which is different than the default "Exuberant
+# Ctags"), which is needed by the ABI checker. See https://ctags.io/
+WORKDIR /var/tmp/build
+RUN curl -sSL https://github.com/universal-ctags/ctags/archive/refs/tags/p5.9.20210418.0.tar.gz | \
+    tar -xzf - --strip-components=1 && \
+    ./autogen.sh && \
+    ./configure --prefix=/usr/local && \
+    make && \
+    make install && \
     cd /var/tmp && rm -fr build
 
 # Install the Cloud SDK and some of the emulators. We use the emulators to run

ci/cloudbuild/triggers/check-abi-ci.yaml
@@ -1,0 +1,13 @@
+filename: ci/cloudbuild/cloudbuild.yaml
+github:
+  name: google-cloud-cpp
+  owner: googleapis
+  push:
+    branch: ^(master|main|v\d+\..*)$
+name: check-abi-ci
+substitutions:
+  _BUILD_NAME: check-abi
+  _DISTRO: fedora
+  _TRIGGER_TYPE: ci
+tags:
+- ci

ci/cloudbuild/triggers/check-abi-pr.yaml
@@ -1,0 +1,14 @@
+filename: ci/cloudbuild/cloudbuild.yaml
+github:
+  name: google-cloud-cpp
+  owner: googleapis
+  pullRequest:
+    branch: ^(master|main|v\d+\..*)$
+    commentControl: COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY
+name: check-abi-pr
+substitutions:
+  _BUILD_NAME: check-abi
+  _DISTRO: fedora
+  _TRIGGER_TYPE: pr
+tags:
+- pr

commit 0027c1ae8df344de2a4b63133004cb452955a6f4

ci(gcb): fix bug in check-abi
Oops. I introduced this bug at the last minute before sending my
previous PR for review. The bug was introduced in https://github.com/googleapis/google-cloud-cpp/pull/6314

ci/cloudbuild/builds/check-abi.sh
@@ -67,7 +67,7 @@ for lib in "${libraries[@]}"; do
   actual_dump_file="${lib}.actual.abi.dump"
   expected_dump_file="${lib}.expected.abi.dump"
   zcat "${PROJECT_ROOT}/ci/test-abi/${expected_dump_file}.gz" > \
-    "${expected_dump_file}"
+    "cmake-out/${expected_dump_file}"
   # We ignore all symbols in internal namespaces, because these are not part
   # of our public API. We do this by specifying a regex that matches against
   # the mangled symbol names. For example, 8 is the number of characters in