35c7a8de46919a9cd32e89d9a4d4405c85b76217
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-04-28 11:21:38 -0400

    ci(gcb): ui tweaks to the public logs index.html page (#6397)
    A few small UI tweaks to make the public logs HTML page a bit nicer looking.
    
    Example: https://storage.googleapis.com/cloud-cpp-community-publiclogs/logs/google-cloud-cpp/6397/2fb9207076ba450bd67750d50acaca6b8f24d4a1/index.html
    
    I'm no UI expert, so this could certainly be improved. Ideas to improve it:
    
    1. Have all pages use an external css file that we store in a single global place (maybe the top of the logs bucket), so we can tweak the UIs of all pages, including old ones, as necessary.
    2. We could change the log path to include the exact trigger name so that we can extract that and show users the exact command to locally reproduce a build
    3. Any other UI tweaks to make things look nice -- I'm already overstepping my bounds.

--- ci/cloudbuild/builds/log-linker.sh
+++ ci/cloudbuild/builds/log-linker.sh
@@ -33,14 +33,14 @@ io::log_h2 "Authenticating"
 gh auth login --with-token <<<"${LOG_LINKER_PAT}"
 
 console_link="https://console.cloud.google.com/cloud-build/builds?project=cloud-cpp-testing-resources&query=tags%3D%22${PR_NUMBER}%22"
-storage_link="http://storage.googleapis.com/cloud-cpp-community-publiclogs/logs/google-cloud-cpp/${PR_NUMBER}/${COMMIT_SHA}/index.html"
+storage_link="https://storage.googleapis.com/cloud-cpp-community-publiclogs/logs/google-cloud-cpp/${PR_NUMBER}/${COMMIT_SHA}/index.html"
 body="$(
   cat <<EOF
 **Google Cloud Build Logs**
 For commit: \`${COMMIT_SHA}\`
 
-* [Cloud Build Console](${console_link}) (for team members with access)
-* [Cloud Storage Logs Bucket](${storage_link}) (public access *coming soon*)
+* [Public Access Build Logs](${storage_link})
+* [Cloud Build Console](${console_link}) (requires auth)
 
 :information_source: NOTE: Kokoro logs are linked from "Details" below.
 EOF
--- ci/cloudbuild/notifiers/logs/function/function.cc
+++ ci/cloudbuild/notifiers/logs/function/function.cc
@@ -17,18 +17,57 @@
 #include <cppcodec/base64_rfc4648.hpp>
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include <libgen.h>
 #include <regex>
 #include <sstream>
 #include <stdexcept>
+#include <vector>
 
 auto constexpr kGcsPrefix = "https://storage.googleapis.com/";
+auto constexpr kPrPrefix =
+    "https://github.com/googleapis/google-cloud-cpp/pull/";
+auto constexpr kGCBPrefix =
+    "https://console.cloud.google.com/cloud-build/builds?project=";
 auto constexpr kAttempts = 4;
 
 using ::google::cloud::StatusCode;
 using ::google::cloud::functions::CloudEvent;
 namespace gcs = ::google::cloud::storage;
 
-void index_build_logs(CloudEvent event) {  // NOLINT
+namespace {
+
+// Returns an HTML anchor referencing the given URL with the optional name. If
+// `name` is not specified, the URL is used as the text.
+std::string Anchor(std::string const& url, std::string name = "") {
+  if (name.empty()) name = url;
+  return "<a href=\"" + url + "\">" + name + "</a>";
+}
+
+// Writes an HTML table w/ the data from the vector.
+void WriteTable(std::ostream& os,
+                std::vector<std::vector<std::string>> const& table,
+                std::vector<std::string> const& header = {}) {
+  os << "<table>\n";
+  if (!header.empty()) {
+    os << "<tr>";
+    for (auto const& col : header) {
+      os << "<th>" << col << "</th>";
+    }
+    os << "</tr>";
+  }
+  for (auto const& row : table) {
+    os << "<tr>";
+    for (auto const& col : row) {
+      os << "<td>" << col << "</td>";
+    }
+    os << "</tr>\n";
+  }
+  os << "</table>";
+}
+
+}  // namespace
+
+void IndexBuildLogs(CloudEvent event) {
   static auto client = [] {
     return gcs::Client::CreateDefaultClient().value();
   }();
@@ -42,8 +81,7 @@ void index_build_logs(CloudEvent event) {  // NOLINT
 
   if (event.data_content_type().value_or("") != "application/json") {
     std::cerr << nlohmann::json{{"severity", "error"},
-                     {"message", "expected application/json data"},
-                                }
+                                {"message", "expected application/json data"}}
                      .dump()
               << "\n";
     return;
@@ -72,47 +110,73 @@ void index_build_logs(CloudEvent event) {  // NOLINT
   auto const contents = nlohmann::json::parse(data);
   auto const trigger_type =
       contents["substitutions"].value("_TRIGGER_TYPE", "");
-  if (trigger_type == "manual") {
+  if (trigger_type != "pr") {
     std::cout << nlohmann::json{{"severity", "info"},
-                                {"message", "skip manual build"}}
+                                {"message", "skipping non-PR build"}}
                      .dump()
               << "\n";
     return;
   }
 
-  auto const pr_number = contents["substitutions"].value("_PR_NUMBER", "");
-  auto const commit_sha = contents["substitutions"].value("COMMIT_SHA", "");
+  auto const pr = contents["substitutions"].value("_PR_NUMBER", "");
+  auto const sha = contents["substitutions"].value("COMMIT_SHA", "");
+  auto const prefix = "logs/google-cloud-cpp/" + pr + "/" + sha + "/";
+  auto const project = contents["projectId"].get<std::string>();
 
-  auto const prefix =
-      "logs/google-cloud-cpp/" + pr_number + "/" + commit_sha + "/";
-
-  auto const link_prefix = kGcsPrefix + bucket_name;
-
-  static auto const kIsIndex =
+  static auto const kIndexRE =
       std::regex(R"re(/index\.html$)re", std::regex::optimize);
-  static auto const kIsStep =
-      std::regex(R"re(-step-[0-9]+\.txt)re", std::regex::optimize);
+  static auto const kLogfileRE =
+      std::regex(R"re(/log-[0-9a-f-]+\.txt$)re", std::regex::optimize);
+
+  // Each element vector should contain exactly two elements.
+  std::vector<std::vector<std::string>> v;
+  v.emplace_back(std::vector<std::string>{
+      "Repo", Anchor("https://github.com/googleapis/google-cloud-cpp")});
+  v.emplace_back(std::vector<std::string>{"Pull Request",
+                                          Anchor(kPrPrefix + pr, "#" + pr)});
+  v.emplace_back(std::vector<std::string>{
+      "Commit SHA", Anchor(kPrPrefix + pr + "/commits/" + sha, sha)});
+  v.emplace_back(std::vector<std::string>{
+      "GCB Console",
+      Anchor(kGCBPrefix + project + "&query=tags%3D%22" + pr + "%22",
+             "(requires auth)")});
 
   std::int64_t index_generation = 0;
   for (int i = 0; i != kAttempts; ++i) {
     std::ostringstream os;
     os << "<!DOCTYPE html>\n";
-    os << "<html><meta charset=\"utf-8\"><head>Logs for #" << pr_number << " @ "
-       << commit_sha << "</head>\n";
+    os << "<html>\n";
+    os << "<head><meta charset=\"utf-8\">";
+    os << "<style>\n";
+    os << "tr:nth-child(even) {background: #FFF}\n";
+    os << "tr:nth-child(odd) {background: #DDD}\n";
+    os << "</style></head>\n";
     os << "<body>\n";
-    os << "<ul>";
+    os << "<h1>Public Build Results</h1><hr/>";
+    WriteTable(os, v);
+    os << "<h2>Build logs</h2>";
+    std::vector<std::vector<std::string>> table;
+    std::vector<std::string> header{std::string{"Build"}, std::string{"Log"}};
     for (auto const& object :
          client.ListObjects(bucket_name, gcs::Prefix(prefix))) {
       if (!object) throw std::runtime_error(object.status().message());
-      if (std::regex_search(object->name(), kIsStep)) continue;
-      if (std::regex_search(object->name(), kIsIndex)) {
+      if (std::regex_search(object->name(), kIndexRE)) {
         index_generation = object->generation();
         continue;
       }
-      os << "<li><a href=\"" << kGcsPrefix + bucket_name + "/" + object->name()
-         << "\">" << object->name() << "</a></li>\n";
+      if (!std::regex_search(object->name(), kLogfileRE)) continue;
+      auto path = object->name();
+      table.emplace_back(std::vector<std::string>{
+          std::string{basename(dirname(path.data()))},
+          Anchor(kGcsPrefix + bucket_name + "/" + object->name(), "raw log")});
     }
-    os << "</ul>";
+    WriteTable(os, table, header);
+    os << "<hr/><p>NOTE: To debug a build on your local machine use the ";
+    os << "<code>ci/cloudbuild/build.sh</code> script, with its ";
+    os << "<a "
+          "href=\"https://github.com/googleapis/google-cloud-cpp/blob/master/"
+          "ci/cloudbuild/build.sh\">documentation here</a>.";
+    os << "</p>\n";
     os << "</body>\n";
     os << "</html>\n";
     // Use `IfGenerationMatch()` to prevent overwriting data. It is possible
