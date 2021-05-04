afc9d24e8ceb3a19826e4d2f533950460f3c6a18
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-05-04 17:40:21 -0400

    ci: install emulator dependencies using requirements file (#6457)

--- ci/cloudbuild/builds/lib/integration.sh
+++ ci/cloudbuild/builds/lib/integration.sh
@@ -26,6 +26,10 @@ source module ci/lib/io.sh
 source module ci/etc/integration-tests-config.sh
 source module ci/lib/io.sh
 
+# To run the integration tests we need to install the dependencies for the storage emulator
+export PATH="${HOME}/.local/bin:${PATH}"
+python3 -m pip install --user -r "${PROJECT_ROOT}/google/cloud/storage/emulator/requirements.txt"
+
 # Outputs a list of Bazel arguments that should be used when running
 # integration tests. These do not include the common `bazel::common_args`.
 #
