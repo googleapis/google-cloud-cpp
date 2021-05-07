6539fbb9d4e0b27eee67c598ee9985796e1f3466
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-05-07 17:49:33 -0400

    post full log URL, which is easier to copy-n-paste

--- ci/cloudbuild/notifiers/alerts/function/function.cc
+++ ci/cloudbuild/notifiers/alerts/function/function.cc
@@ -52,9 +52,9 @@ BuildStatus ParseBuildStatus(google::cloud::functions::CloudEvent event) {
 nlohmann::json MakeChatPayload(BuildStatus const& bs) {
   auto const trigger_name = bs.build["substitutions"].value("TRIGGER_NAME", "");
   auto const log_url = bs.build.value("logUrl", "");
-  auto text = fmt::format(
-      R""(Build failed: *{trigger_name}* (<{log_url}|Build Log>))"",
-      fmt::arg("trigger_name", trigger_name), fmt::arg("log_url", log_url));
+  auto text = fmt::format(R""(Build failed: *{trigger_name}* {log_url})"",
+                          fmt::arg("trigger_name", trigger_name),
+                          fmt::arg("log_url", log_url));
   return nlohmann::json{{"text", std::move(text)}};
 }
 
@@ -69,8 +69,8 @@ void HttpPost(std::string const& url, std::string const& data) {
   curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
   curl_easy_setopt(curl.get(), CURLOPT_HTTPHEADER, headers.get());
   curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDS, data.c_str());
-  CURLcode res = curl_easy_perform(curl.get());
-  if (res != CURLE_OK) throw std::runtime_error(curl_easy_strerror(res));
+  CURLcode code = curl_easy_perform(curl.get());
+  if (code != CURLE_OK) throw std::runtime_error(curl_easy_strerror(code));
 }
 
 }  // namespace
