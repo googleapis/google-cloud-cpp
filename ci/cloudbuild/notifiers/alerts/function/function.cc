155f0631b735eb0a436f95f8fce1427b641c2122
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-05-15 10:52:59 -0400

    fix unrelated typo that made this not compile

--- ci/cloudbuild/notifiers/alerts/function/function.cc
+++ ci/cloudbuild/notifiers/alerts/function/function.cc
@@ -93,6 +93,6 @@ void SendBuildAlerts(google::cloud::functions::CloudEvent event) {
   // Skips PR invocations and manually invoked builds (no trigger name).
   if (trigger_type == "pr" || trigger_name.empty()) return;
   auto const chat = MakeChatPayload(bs);
-  std::cout << nlohmann::json{{"severity", "INFO"}, {"chat" : chat}} << "\n";
+  std::cout << nlohmann::json{{"severity", "INFO"}, {"chat", chat}} << "\n";
   HttpPost(webhook, chat.dump());
 }
