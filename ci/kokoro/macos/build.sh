b55da83db94f5141beba572f480f2e15f47cefe2
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-06-09 17:03:40 -0400

    using brew outdated too

--- ci/kokoro/macos/build.sh
+++ ci/kokoro/macos/build.sh
@@ -107,7 +107,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 brew list --versions coreutils || brew install coreutils
 # Kokoro machines have an old and broken version of this SDK.
-brew upgrade google-cloud-sdk
+brew outdated google-cloud-sdk || brew upgrade google-cloud-sdk
 
 io::log_h1 "Starting Build: ${BUILD_NAME}"
 
