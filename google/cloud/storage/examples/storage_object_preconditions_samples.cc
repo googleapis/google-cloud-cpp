23e3729719eafc0ad54f8e60a881e1c4bf6316bc
Carlos O'Ryan <coryan@google.com>
2021-06-30 00:29:13 +0000

    Fix boilerplate

--- google/cloud/storage/examples/storage_object_preconditions_samples.cc
+++ google/cloud/storage/examples/storage_object_preconditions_samples.cc
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 #include "google/cloud/internal/getenv.h"
 #include <iostream>
 #include <map>
-#include <sstream>
 #include <string>
 #include <thread>
 
