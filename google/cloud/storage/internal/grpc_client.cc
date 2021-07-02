8f6afc3c9d503ebb634f189fd94f915c2e308b34
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-07-02 15:34:02 -0400

    fix: GrpcNumChannels option can be zero (#6926)
    Applications might set this option to zero or a negative number, this is
    almost always a bug or an accident. It seems reasonable to use `1` in
    such cases.

--- google/cloud/storage/internal/grpc_client.cc
+++ google/cloud/storage/internal/grpc_client.cc
@@ -21,15 +21,11 @@
 #include "google/cloud/storage/internal/storage_auth.h"
 #include "google/cloud/storage/internal/storage_round_robin.h"
 #include "google/cloud/storage/internal/storage_stub.h"
-#include "google/cloud/storage/oauth2/anonymous_credentials.h"
-#include "google/cloud/grpc_error_delegate.h"
 #include "google/cloud/grpc_options.h"
 #include "google/cloud/internal/big_endian.h"
 #include "google/cloud/internal/format_time_point.h"
 #include "google/cloud/internal/getenv.h"
 #include "google/cloud/internal/invoke_result.h"
-#include "google/cloud/internal/streaming_read_rpc.h"
-#include "google/cloud/internal/streaming_write_rpc.h"
 #include "google/cloud/internal/time_utils.h"
 #include "google/cloud/internal/unified_grpc_credentials.h"
 #include "google/cloud/log.h"
@@ -123,7 +119,7 @@ std::shared_ptr<StorageStub> CreateStorageStub(CompletionQueue cq,
                                                Options const& opts) {
   auto auth = CreateAuthenticationStrategy(std::move(cq), opts);
   std::vector<std::shared_ptr<StorageStub>> children(
-      opts.get<GrpcNumChannelsOption>());
+      (std::max)(1, opts.get<GrpcNumChannelsOption>()));
   int id = 0;
   std::generate(children.begin(), children.end(), [&id, &auth, opts] {
     return MakeDefaultStorageStub(CreateGrpcChannel(*auth, opts, id++));
