e7dc61f71ae7a8cb437c7c62f978568a5b49f231
Darren Bolduc <dbolduc@google.com>
2021-07-07 13:11:53 -0400

    test(bigtable): increase ClientOptions test coverage (#6945)
    Add missing unit tests for some of the `ClientOptions`. This will give us more confidence in our code when we transition the `ClientOptions` implementation to use `Options`

--- google/cloud/bigtable/client_options_test.cc
+++ google/cloud/bigtable/client_options_test.cc
@@ -14,11 +14,13 @@
 
 #include "google/cloud/bigtable/client_options.h"
 #include "google/cloud/bigtable/internal/client_options_defaults.h"
+#include "google/cloud/internal/background_threads_impl.h"
 #include "google/cloud/status.h"
 #include "google/cloud/testing_util/scoped_environment.h"
 #include "google/cloud/testing_util/status_matchers.h"
 #include "absl/types/optional.h"
 #include <gmock/gmock.h>
+#include <thread>
 
 namespace google {
 namespace cloud {
@@ -385,6 +387,83 @@ TEST(ClientOptionsTest, RefreshPeriod) {
   options.set_min_conn_refresh_period(ms(1000));
   EXPECT_EQ(1000, options.min_conn_refresh_period().count());
   EXPECT_EQ(5000, options.max_conn_refresh_period().count());
+}
+
+TEST(ClientOptionsTest, TracingComponents) {
+  google::cloud::testing_util::ScopedEnvironment tracing(
+      "GOOGLE_CLOUD_CPP_ENABLE_TRACING", "foo,bar");
+  auto options = ClientOptions();
+
+  // Check defaults
+  EXPECT_TRUE(options.tracing_enabled("foo"));
+  EXPECT_TRUE(options.tracing_enabled("bar"));
+  EXPECT_FALSE(options.tracing_enabled("baz"));
+
+  // Edit components
+  options.enable_tracing("baz");
+  EXPECT_TRUE(options.tracing_enabled("baz"));
+  options.disable_tracing("foo");
+  EXPECT_FALSE(options.tracing_enabled("foo"));
+}
+
+TEST(ClientOptionsTest, DefaultTracingOptionsNoEnv) {
+  google::cloud::testing_util::ScopedEnvironment tracing(
+      "GOOGLE_CLOUD_CPP_TRACING_OPTIONS", absl::nullopt);
+  auto options = ClientOptions().tracing_options();
+  EXPECT_EQ(TracingOptions(), options);
+}
+
+TEST(ClientOptionsTest, DefaultTracingOptionsEnv) {
+  google::cloud::testing_util::ScopedEnvironment tracing(
+      "GOOGLE_CLOUD_CPP_TRACING_OPTIONS", "single_line_mode=F");
+  auto options = ClientOptions().tracing_options();
+  EXPECT_FALSE(options.single_line_mode());
+}
+
+TEST(ClientOptionsTest, BackgroundThreadPoolSize) {
+  using ThreadPool =
+      ::google::cloud::internal::AutomaticallyCreatedBackgroundThreads;
+
+  auto options = ClientOptions();
+  // Check that the default value is 0 or 1. Both values result in the
+  // BackgroundThreadsFactory creating a ThreadPool with a single thread in it.
+  EXPECT_GE(1U, options.background_thread_pool_size());
+
+  auto background = options.background_threads_factory()();
+  auto* tp = dynamic_cast<ThreadPool*>(background.get());
+  ASSERT_NE(nullptr, tp);
+  EXPECT_EQ(1U, tp->pool_size());
+
+  options.set_background_thread_pool_size(5U);
+  EXPECT_EQ(5U, options.background_thread_pool_size());
+
+  background = options.background_threads_factory()();
+  tp = dynamic_cast<ThreadPool*>(background.get());
+  ASSERT_NE(nullptr, tp);
+  EXPECT_EQ(5U, tp->pool_size());
+}
+
+TEST(ClientOptionsTest, CustomBackgroundThreads) {
+  CompletionQueue cq;
+  auto options = ClientOptions().DisableBackgroundThreads(cq);
+  auto background = options.background_threads_factory()();
+
+  using ms = std::chrono::milliseconds;
+
+  // Schedule some work that cannot execute because there is no thread draining
+  // the completion queue.
+  promise<std::thread::id> p;
+  auto background_thread_id = p.get_future();
+  background->cq().RunAsync(
+      [&p](CompletionQueue&) { p.set_value(std::this_thread::get_id()); });
+  EXPECT_NE(std::future_status::ready, background_thread_id.wait_for(ms(10)));
+
+  // Verify we can create our own threads to drain the completion queue.
+  std::thread t([&cq] { cq.Run(); });
+  EXPECT_EQ(t.get_id(), background_thread_id.get());
+
+  cq.Shutdown();
+  t.join();
 }
 
 }  // namespace
