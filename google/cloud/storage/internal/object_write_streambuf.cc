b94bba9919fcbf4262b8ee9d0792fbd505a4c48d
Carlos O'Ryan <coryan@google.com>
2021-07-14 13:57:13 +0000

    cleanup(storage): prepare for import into google

--- google/cloud/storage/internal/object_write_streambuf.cc
+++ google/cloud/storage/internal/object_write_streambuf.cc
@@ -14,7 +14,7 @@
 
 #include "google/cloud/storage/internal/object_write_streambuf.h"
 #include "google/cloud/storage/internal/object_requests.h"
-#include "google/cloud/storage/version.h"
+#include "absl/memory/memory.h"
 #include <sstream>
 
 namespace google {
