5c6bb69f9ead84f62fe35d890e63ca0286288b83
Greg Miller <9447643+devjgm@users.noreply.github.com>
2021-08-03 08:56:41 -0400

    cleanup

--- release/changes.sh
+++ release/changes.sh
@@ -23,64 +23,72 @@
 
 set -eu
 
+# Lists all the changes between the given tag and HEAD.
+function list_changes() {
+  local since_tag="$1"
+  shift
+  git log --no-merges --format="format:%s" \
+    "${since_tag}"..HEAD upstream/main -- "$@"
+}
+
+# Removes changes that are likely uninteresting to customers. Changes that
+# indicate they're breaking with a `!:` are always kept. Pull request numbers
+# of the form `(#NNNN)` are turned into links.
 function filter_messages() {
   pr_url="https://github.com/googleapis/google-cloud-cpp/pull"
   for message in "$@"; do
     # Linkify "(#NNNN)" PR numbers.
     # shellcheck disable=SC2001
     message="$(sed -e "s,(#\([0-9]\+\)),([#\1][${pr_url}/\1]),g" <<<"${message}")"
-    # Include any message that says it's a breaking change, like "feat!: foo"
     if grep -qP "!:" <<<"${message}"; then
       echo "${message}"
       continue
     fi
-    # Skip PRs that likely won't interest our customers.
-    if grep -qP "^\*\s(ci|chore|test|testing|cleanup)\b" <<<"${message}"; then
+    # Likely uninteresting changes.
+    if grep -qP "^(ci|chore|test|testing|cleanup)\b" <<<"${message}"; then
       continue
     fi
     echo "${message}"
   done
 }
 
+# Prints the given changelog IFF it's not empty. The first argument is the
+# title for the section, the second argument is the URL to link the title to,
+# and the remaining arguments are the changelog messages to be bulleted after.
+function print_changelog() {
+  local title=$1
+  local url=$2
+  shift 2
+  if [[ $# -gt 0 ]]; then
+    printf "\n### [%s][%s]\n\n" "${title}" "${url}"
+    printf "* %s\n" "${changelog[@]}"
+  fi
+}
+
 # We'll look for changes between this tag (likely the last release) and HEAD.
 last_tag="$(git describe --tags --abbrev=0 upstream/main)"
 
-# The list of libraries to include in the CHANGELOG.md. The format is:
-#   <section title>:<repo path>
-libraries=(
-  "BigQuery:google/cloud/bigquery"
-  "Bigtable:google/cloud/bigtable"
-  "Firestore:google/cloud/firestore"
-  "IAM:google/cloud/iam"
-  "Pub/Sub:google/cloud/pubsub"
-  "Storage:google/cloud/storage"
-  "Spanner:google/cloud/spanner"
+# The format is: <title>,<path>[,<extra path> ...]
+sections=(
+  "BigQuery,google/cloud/bigquery"
+  "Bigtable,google/cloud/bigtable"
+  "Firestore,google/cloud/firestore"
+  "IAM,google/cloud/iam"
+  "Pub/Sub,google/cloud/pubsub"
+  "Storage,google/cloud/storage"
+  "Spanner,google/cloud/spanner"
 )
 
-# We'll add an entry to this array for each library path above.
-git_exclude=()
+# Adds a section for "Common Libraries", which excludes the previous sections.
+exclude=($(printf "%s\n" "${sections[@]}" | cut -f2 -d, | sed -e 's/^/:(exclude)/'))
+sections+=("Common Libraries,google/cloud,${exclude[*]}")
 
-for lib in "${libraries[@]}"; do
-  title="$(cut -f1 -d: <<<"${lib}")"
-  path="$(cut -f2 -d: <<<"${lib}")"
+for section in "${sections[@]}"; do
+  title="$(cut -f1 -d, <<<"${section}")"
+  path="$(cut -f2 -d, <<<"${section}")"
+  extra=($(cut -f3 -d, <<<"${section}"))
   url="https://github.com/googleapis/google-cloud-cpp/blob/main/${path}/README.md"
-  mapfile -t messages < <(git log --no-merges --format="format:* %s" \
-    "${last_tag}"..HEAD upstream/main -- "${path}")
+  mapfile -t messages < <(list_changes "${last_tag}" "${path}" "${extra[@]}")
   mapfile -t changelog < <(filter_messages "${messages[@]}")
-  if [[ ${#changelog[@]} -gt 0 ]]; then
-    printf "\n### [%s][%s]\n\n" "${title}" "${url}"
-    printf "%s\n" "${changelog[@]}"
-  fi
-  git_exclude+=(":(exclude)${path}")
+  print_changelog "${title}" "${url}" "${changelog[@]}"
 done
-
-# Prints the changelog for the "common" library by excluding changes for all
-# the libraries above.
-mapfile -t messages < <(git log --no-merges --format="format:* %s" \
-  "${last_tag}"..HEAD upstream/main -- "google/cloud" "${git_exclude[@]}")
-mapfile -t changelog < <(filter_messages "${messages[@]}")
-if [[ ${#changelog[@]} -gt 0 ]]; then
-  url="https://github.com/googleapis/google-cloud-cpp/blob/main/google/cloud/README.md"
-  printf "\n### [%s][%s]\n\n" "Common Libraries" "${url}"
-  printf "%s\n" "${changelog[@]}"
-fi
