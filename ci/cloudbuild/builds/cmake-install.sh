2a6d1ca25050e595c7464e5f4114b91f61d16de1
Carlos O'Ryan <coryan@google.com>
2021-08-09 21:13:20 +0000

    Fix demo builds and split GCS+gRPC

--- ci/cloudbuild/builds/cmake-install.sh
+++ ci/cloudbuild/builds/cmake-install.sh
@@ -160,12 +160,15 @@ done
 
 # Tests the installed artifacts by building and running the quickstarts.
 quickstart::build_cmake_and_make "${INSTALL_PREFIX}"
+quickstart::build_gcs_grpc_quickstart "${INSTALL_PREFIX}" "${PROJECT_ROOT}/google/cloud/storage/quickstart" "storage"
 quickstart::run_cmake_and_make "${INSTALL_PREFIX}"
+quickstart::run_gcs_grpc_quickstart "${INSTALL_PREFIX}"
 
 # Deletes all the installed artifacts, and installs only the runtime components
 # to verify that we can still execute the compiled quickstart programs.
 rm -rf "${INSTALL_PREFIX:?}"/{include,lib64}
 cmake --install cmake-out --component google_cloud_cpp_runtime
 quickstart::run_cmake_and_make "${INSTALL_PREFIX}"
+quickstart::run_gcs_grpc_quickstart "${INSTALL_PREFIX}"
 
 exit "${exit_code}"
