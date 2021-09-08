c044167d5eaef8fdd2164e34ce6e70648c229c81
Carlos O'Ryan <coryan@google.com>
2021-09-08 17:23:26 +0000

    Address review comments

--- google/cloud/storage/internal/grpc_configure_client_context_test.cc
+++ google/cloud/storage/internal/grpc_configure_client_context_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -74,7 +74,7 @@ TEST(GrpcConfigureClientContext, ApplyQueryParametersQuotaUserAndUserIp) {
       {ReadObjectRangeRequest("b", "o").set_option(
            QuotaUser("test-only-quota-user")),
        "test-only-quota-user"},
-  };
+\  };
 
   for (auto const& test : cases) {
     auto description = [&test] {
