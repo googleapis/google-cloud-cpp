90d7c480f4ef952b7c8c0e522b47a410f3e938a6
Carlos O'Ryan <coryan@google.com>
2021-10-18 16:36:25 +0000

    ci: remove macOS root CA workaround

--- ci/kokoro/macos/build.sh
+++ ci/kokoro/macos/build.sh
@@ -103,10 +103,6 @@ rm -f "${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}"
 curl -sSL --retry 10 -o "${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}" \
   https://pki.google.com/roots.pem
 
-# The CA roots are outdated in some Kokoro macOS machines. Use Google's
-# `root.pem` file for any remaining downloads.
-echo "cacert ${GRPC_DEFAULT_SSL_ROOTS_FILE_PATH}" >>"${HOME}/.curlrc"
-
 io::log_h1 "Downloading cache"
 readonly CACHE_BUCKET="${GOOGLE_CLOUD_CPP_KOKORO_RESULTS:-cloud-cpp-kokoro-results}"
 readonly CACHE_FOLDER="${CACHE_BUCKET}/build-cache/google-cloud-cpp/${BRANCH}"
