ac99b6499a8b39e914f3b176961de2be2b7d22dd
Carlos O'Ryan <coryan@google.com>
2021-10-19 16:35:27 +0000

    cleanup(storage): remove RPO workarounds

--- google/cloud/storage/tests/bucket_integration_test.cc
+++ google/cloud/storage/tests/bucket_integration_test.cc
@@ -104,9 +104,6 @@ TEST_F(BucketIntegrationTest, BasicCRUD) {
 
   StatusOr<BucketMetadata> get_meta = client->GetBucketMetadata(bucket_name);
   ASSERT_STATUS_OK(get_meta);
-  // TODO(#7403) - the default RPO value is changing during initial rollouts.
-  insert_meta->set_rpo("");
-  get_meta->set_rpo("");
   EXPECT_EQ(*insert_meta, *get_meta);
 
   // Create a request to update the metadata, change the storage class because
