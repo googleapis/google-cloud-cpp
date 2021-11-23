e3493fe05ac248a7144fe6a22d26e5b7199a3dd3
Carlos O'Ryan <coryan@google.com>
2021-11-23 08:49:59 -0500

    cleanup: use script to update external/googleapis (#7650)
    Use a script to update the list of `.proto` files use by each library,
    as well as the list of libraries each library depends on.
    
    The more complicated case was the `google-cloud-cpp::logging_type_protos`
    library, its name does not match the Bazel rules where it would generate
    `*logging_type_type_protos`. I introduced an `INTERFACE` library for
    backwards compatibility.
    
    In the bigquery library we are exposing both the `v1beta1/` and `v1/`
    versions of `connection.proto`. I will figure out how to clean that up
    in a future PR.

--- external/googleapis/update_libraries.sh
+++ external/googleapis/update_libraries.sh
@@ -16,24 +16,67 @@
 set -euo pipefail
 
 declare -A -r LIBRARIES=(
+  ["bigquery"]="$(
+    printf ",%s" \
+      "@com_google_googleapis//google/cloud/bigquery/v2:bigquery_proto" \
+      "@com_google_googleapis//google/cloud/bigquery/storage/v1:storage_proto" \
+      "@com_google_googleapis//google/cloud/bigquery/reservation/v1:reservation_proto" \
+      "@com_google_googleapis//google/cloud/bigquery/logging/v1:logging_proto" \
+      "@com_google_googleapis//google/cloud/bigquery/datatransfer/v1:datatransfer_proto" \
+      "@com_google_googleapis//google/cloud/bigquery/connection/v1:connection_proto"
+  )"
+  ["bigtable"]="$(
+    printf ",%s" \
+      "@com_google_googleapis//google/bigtable/v2:bigtable_proto" \
+      "@com_google_googleapis//google/bigtable/admin/v2:admin_proto"
+  )"
+  ["dialogflow"]="@com_google_googleapis//google/cloud/dialogflow/v2:dialogflow_proto"
+  ["iam"]="$(
+    printf ",%s" \
+      "@com_google_googleapis//google/iam/credentials/v1:credentials_proto" \
+      "@com_google_googleapis//google/iam/admin/v1:admin_proto"
+  )"
+  ["logging_type"]="@com_google_googleapis//google/logging/type:type_proto"
+  ["logging"]="@com_google_googleapis//google/logging/v2:logging_proto"
+  ["monitoring"]="@com_google_googleapis//google/monitoring/v3:monitoring_proto"
+  ["pubsub"]="@com_google_googleapis//google/pubsub/v1:pubsub_proto"
+  ["spanner"]="$(
+    printf ",%s" \
+      "@com_google_googleapis//google/spanner/v1:spanner_proto" \
+      "@com_google_googleapis//google/spanner/admin/instance/v1:instance_proto" \
+      "@com_google_googleapis//google/spanner/admin/database/v1:database_proto"
+  )"
+  ["speech"]="@com_google_googleapis//google/cloud/speech/v1:speech_proto"
+  ["storage"]="@com_google_googleapis//google/storage/v2:storage_proto"
   ["texttospeech"]="@com_google_googleapis//google/cloud/texttospeech/v1:texttospeech_proto"
 )
 
 for library in "${!LIBRARIES[@]}"; do
   IFS=',' read -r -a rules <<<"${LIBRARIES[$library]}"
-  : >"external/googleapis/protolists/${library}.list"
-  : >"external/googleapis/protodeps/${library}.deps"
+  files=(
+    "external/googleapis/protolists/${library}.list"
+    "external/googleapis/protodeps/${library}.deps"
+  )
+  for file in "${files[@]}"; do
+    : >"${file}"
+  done
   for rule in "${rules[@]}"; do
+    if [[ -z "${rule}" ]]; then continue; fi
     path="${rule%:*}"
-    bazel --batch query --noshow_progress --noshow_loading_progress \
+    echo "=== $library $rule $path"
+    bazel query --noshow_progress --noshow_loading_progress \
       "deps(${rule})" |
       grep "${path}" |
       grep -E '\.proto$' \
         >>"external/googleapis/protolists/${library}.list"
-    bazel --batch query --noshow_progress --noshow_loading_progress \
+    bazel query --noshow_progress --noshow_loading_progress \
       "deps(${rule})" |
       grep "@com_google_googleapis//" | grep _proto |
       grep -v "${path}" \
         >>"external/googleapis/protodeps/${library}.deps"
+  done
+  for file in "${files[@]}"; do
+    sort -u "${file}" >"${file}.sorted"
+    mv "${file}.sorted" "${file}"
   done
 done
