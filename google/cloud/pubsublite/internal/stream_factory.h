f25a205714b2f9795860601008d9de4041c6cdfd
Daniel Collins <dpcollins@google.com>
2021-12-01 13:32:35 -0500

    fixes

--- google/cloud/pubsublite/internal/stream_factory.h
+++ google/cloud/pubsublite/internal/stream_factory.h
@@ -37,8 +37,6 @@ using StreamFactory = std::function<std::unique_ptr<BidiStream<Request, Response
 
 using ClientMetadata = std::unordered_map<std::string, std::string>;
 
-namespace stream_factory_internal {
-
 inline std::unique_ptr<grpc::ClientContext> MakeGrpcClientContext(const ClientMetadata& metadata) {
     auto context = absl::make_unique<grpc::ClientContext>();
     for (const auto& kv : metadata) {
@@ -47,16 +45,13 @@ inline std::unique_ptr<grpc::ClientContext> MakeGrpcClientContext(const ClientMe
     return context;
 }
 
-}  // namespace stream_factory_internal
-
 inline StreamFactory<pubsublite::v1::PublishRequest, pubsublite::v1::PublishResponse> MakeStreamFactory(
     std::shared_ptr<pubsublite::v1::PublisherService::StubInterface> stub,
-    std::shared_ptr<google::cloud::internal::GrpcAuthenticationStrategy> auth,
     const google::cloud::CompletionQueue& cq, ClientMetadata metadata = {}) {
         return [=]{
             return internal::MakeStreamingReadWriteRpc<
                 pubsublite::v1::PublishRequest, pubsublite::v1::PublishResponse>(
-                    cq, stream_factory_internal::MakeGrpcClientContext(metadata),
+                    cq, MakeGrpcClientContext(metadata),
                     [stub](grpc::ClientContext* context, grpc::CompletionQueue* cq) {
                         return stub->PrepareAsyncPublish(context, cq);
                     });
@@ -65,12 +60,11 @@ inline StreamFactory<pubsublite::v1::PublishRequest, pubsublite::v1::PublishResp
 
 inline StreamFactory<pubsublite::v1::SubscribeRequest, pubsublite::v1::SubscribeResponse> MakeStreamFactory(
     std::shared_ptr<pubsublite::v1::SubscriberService::StubInterface> stub,
-    std::shared_ptr<google::cloud::internal::GrpcAuthenticationStrategy> auth,
     const google::cloud::CompletionQueue& cq, ClientMetadata metadata = {}) {
         return [=]{
             return google::cloud::internal::MakeStreamingReadWriteRpc<
                 pubsublite::v1::SubscribeRequest, pubsublite::v1::SubscribeResponse>(
-                    cq, stream_factory_internal::MakeGrpcClientContext(metadata),
+                    cq, MakeGrpcClientContext(metadata),
                     [stub](grpc::ClientContext* context, grpc::CompletionQueue* cq) {
                         return stub->PrepareAsyncSubscribe(context, cq);
                     });
@@ -81,12 +75,11 @@ inline StreamFactory<pubsublite::v1::SubscribeRequest, pubsublite::v1::Subscribe
 
 inline StreamFactory<pubsublite::v1::StreamingCommitCursorRequest, pubsublite::v1::StreamingCommitCursorResponse> MakeStreamFactory(
     std::shared_ptr<pubsublite::v1::CursorService::StubInterface> stub,
-    std::shared_ptr<google::cloud::internal::GrpcAuthenticationStrategy> auth,
     const google::cloud::CompletionQueue& cq, ClientMetadata metadata = {}) {
         return [=]{
             return google::cloud::internal::MakeStreamingReadWriteRpc<
                 pubsublite::v1::StreamingCommitCursorRequest, pubsublite::v1::StreamingCommitCursorResponse>(
-                    cq, stream_factory_internal::MakeGrpcClientContext(metadata),
+                    cq, MakeGrpcClientContext(metadata),
                     [stub](grpc::ClientContext* context, grpc::CompletionQueue* cq) {
                         return stub->PrepareAsyncStreamingCommitCursor(context, cq);
                     });
@@ -96,12 +89,11 @@ inline StreamFactory<pubsublite::v1::StreamingCommitCursorRequest, pubsublite::v
 
 inline StreamFactory<pubsublite::v1::PartitionAssignmentRequest, pubsublite::v1::PartitionAssignment> MakeStreamFactory(
     std::shared_ptr<pubsublite::v1::PartitionAssignmentService::StubInterface> stub,
-    std::shared_ptr<google::cloud::internal::GrpcAuthenticationStrategy> auth,
     const google::cloud::CompletionQueue& cq, ClientMetadata metadata = {}) {
         return [=]{
             return google::cloud::internal::MakeStreamingReadWriteRpc<
                 pubsublite::v1::PartitionAssignmentRequest, pubsublite::v1::PartitionAssignment>(
-                    cq, stream_factory_internal::MakeGrpcClientContext(metadata),
+                    cq, MakeGrpcClientContext(metadata),
                     [stub](grpc::ClientContext* context, grpc::CompletionQueue* cq) {
                         return stub->PrepareAsyncAssignPartitions(context, cq);
                     });
