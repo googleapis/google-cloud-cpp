da4d0c24c7cac7365a0b32e2274571f47371c39d
Carlos O'Ryan <coryan@google.com>
2021-12-08 13:24:56 +0000

    Address review comments

--- google/cloud/storage/ci/run_integration_tests_emulator_bazel.sh
+++ google/cloud/storage/ci/run_integration_tests_emulator_bazel.sh
@@ -30,14 +30,7 @@ shift
 BAZEL_VERB="$1"
 shift
 
-EMULATOR_SHA="$(find "${HOME}/.local/" -type f -name '*.py' -print0 |
-  xargs -0 sha256sum | sort | sha256sum -)"
-readonly EMULATOR_SHA
-
-bazel_test_args=(
-  # Run the tests again if the emulator has changed
-  --test_env=CLOUD_STORAGE_EMULATOR_SHA="${EMULATOR_SHA}"
-)
+bazel_test_args=()
 excluded_targets=()
 
 # Separate caller-provided excluded targets (starting with "-//..."), so that
@@ -92,9 +85,25 @@ printf '{"name": "%s"}' "${GOOGLE_CLOUD_CPP_STORAGE_TEST_DESTINATION_BUCKET_NAME
   curl -X POST -H "Content-Type: application/json" --data-binary @- \
     "${CLOUD_STORAGE_EMULATOR_ENDPOINT}/storage/v1/b?project=${GOOGLE_CLOUD_PROJECT}"
 
+# This is just the SHA for the *description* of the testbench, it includes its
+# version and other info, but no details about the contents.
 TESTBENCH_SHA="$(pip show googleapis-storage-testbench | sha256sum)"
+# With these two commands we extract the SHA of the testbench contents. It
+# excludes the contents of its deps (say gRPC, or Flask), those are unlikely
+# to affect the results of the test.
+TESTBENCH_LOCATION="$(pip show googleapis-storage-testbench | sed -n 's/^Location: //p')"
+TESTBENCH_CONTENTS_SHA="$(
+  cd "${TESTBENCH_LOCATION}"
+  pip show --files googleapis-storage-testbench |
+    sed -n '/^Files:/,$p' |
+    grep '\.py$' |
+    xargs sha256sum |
+    sort |
+    sha256sum -
+)"
 emulator_args=(
   "--test_env=TESTBENCH_SHA=${TESTBENCH_SHA}"
+  "--test_env=TESTBENCH_CONTENTS_SHA=${TESTBENCH_CONTENTS_SHA}"
   "--test_env=CLOUD_STORAGE_EMULATOR_ENDPOINT=${CLOUD_STORAGE_EMULATOR_ENDPOINT}"
   "--test_env=CLOUD_STORAGE_GRPC_ENDPOINT=${CLOUD_STORAGE_GRPC_ENDPOINT}"
   "--test_env=HTTPBIN_ENDPOINT=${HTTPBIN_ENDPOINT}"
