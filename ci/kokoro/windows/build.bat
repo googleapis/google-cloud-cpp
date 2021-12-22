e8d8adebf461e9a87ef3c2e14cbe44959aeb4965
Carlos O'Ryan <coryan@google.com>
2021-12-22 10:48:40 -0800

    cleanup: cleanup http vs. https in ci/ (#7777)

--- ci/kokoro/windows/build.bat
+++ ci/kokoro/windows/build.bat
@@ -4,7 +4,7 @@
 @REM you may not use this file except in compliance with the License.
 @REM You may obtain a copy of the License at
 @REM
-@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM     https://www.apache.org/licenses/LICENSE-2.0
 @REM
 @REM Unless required by applicable law or agreed to in writing, software
 @REM distributed under the License is distributed on an "AS IS" BASIS,
