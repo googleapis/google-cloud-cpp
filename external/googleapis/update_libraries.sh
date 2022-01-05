3e96219d9c99e33de088c27135f7de1020206218
Carlos O'Ryan <coryan@google.com>
2022-01-05 19:14:43 +0000

    feat(scheduler): generate scheduler library

--- external/googleapis/update_libraries.sh
+++ external/googleapis/update_libraries.sh
@@ -41,6 +41,7 @@ declare -A -r LIBRARIES=(
   ["monitoring"]="@com_google_googleapis//google/monitoring/v3:monitoring_proto"
   ["pubsub"]="@com_google_googleapis//google/pubsub/v1:pubsub_proto"
   ["pubsublite"]="@com_google_googleapis//google/cloud/pubsublite/v1:pubsublite_proto"
+  ["scheduler"]="@com_google_googleapis//google/cloud/scheduler/v1:scheduler_proto"
   ["secretmanager"]="$(
     printf ",%s" \
       "@com_google_googleapis//google/cloud/secretmanager/v1:secretmanager_proto" \
