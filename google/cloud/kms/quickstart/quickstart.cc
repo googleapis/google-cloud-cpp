ab134a0662d19aff5730044b4ecc48565abcaf10
Carlos O'Ryan <coryan@google.com>
2022-01-05 20:14:58 +0000

    Run the generators and format the output

---
+++ google/cloud/kms/quickstart/quickstart.cc
@@ -0,0 +1,34 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/kms/ EDIT HERE .h"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 4) {
+    std::cerr << "Usage: " << argv[0] << " project-id \n";
+    return 1;
+  }
+
+  namespace kms = ::google::cloud::kms;
+  auto client = kms::Client(kms::MakeConnection(/* EDIT HERE */));
+
+  // EDIT HERE: add some code
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  return 1;
+}
