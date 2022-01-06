a7669707c6ddc56292f467c955dd2b8ac76da921
Carlos O'Ryan <coryan@google.com>
2022-01-06 14:53:44 +0000

    Manually update READMEs, quickstart, and top-level stuff

--- google/cloud/accessapproval/quickstart/quickstart.cc
+++ google/cloud/accessapproval/quickstart/quickstart.cc
@@ -12,21 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cloud/accessapproval/ EDIT HERE .h"
+#include "google/cloud/accessapproval/access_approval_client.h"
+#include "google/cloud/project.h"
 #include <iostream>
 #include <stdexcept>
 
 int main(int argc, char* argv[]) try {
-  if (argc != 4) {
-    std::cerr << "Usage: " << argv[0] << " project-id \n";
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
     return 1;
   }
 
   namespace accessapproval = ::google::cloud::accessapproval;
-  auto client =
-      accessapproval::Client(accessapproval::MakeConnection(/* EDIT HERE */));
-
-  // EDIT HERE: add some code
+  auto client = accessapproval::AccessApprovalClient(
+      accessapproval::MakeAccessApprovalConnection());
+  auto const project = google::cloud::Project(argv[1]);
+  for (auto a : client.ListApprovalRequests(project.FullName())) {
+    if (!a) throw std::runtime_error(a.status().message());
+    std::cout << a->DebugString() << "\n";
+  }
 
   return 0;
 } catch (std::exception const& ex) {
