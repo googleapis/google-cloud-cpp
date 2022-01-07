1bf11ac5c1965581c060bc8632a1d1451d7372a5
Darren Bolduc <dbolduc@google.com>
2022-01-07 11:58:25 -0500

    feat(billing): generate library (#7836)

---
+++ google/cloud/billing/quickstart/quickstart.cc
@@ -0,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/billing/cloud_billing_client.h"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 1) {
+    std::cerr << "Usage: " << argv[0] << "\n";
+    return 1;
+  }
+
+  namespace billing = ::google::cloud::billing;
+  auto client =
+      billing::CloudBillingClient(billing::MakeCloudBillingConnection());
+
+  for (auto a : client.ListBillingAccounts()) {
+    if (!a) throw std::runtime_error(a.status().message());
+    std::cout << a->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  return 1;
+}
