221bbffef02b96052681fc7b5df7f806e880f325
Carlos O'Ryan <coryan@google.com>
2022-01-14 18:41:58 -0500

    refactor(GCS+gRPC): move stub factory function (#7952)
    Split the factory function for `StorageStub` (with decorators) to its
    own `.h` and `.cc` files. This will help as I add additional decorators
    and member functions.

--- google/cloud/storage/internal/grpc_client.cc
+++ google/cloud/storage/internal/grpc_client.cc
@@ -21,18 +21,13 @@
 #include "google/cloud/storage/internal/grpc_object_request_parser.h"
 #include "google/cloud/storage/internal/grpc_resumable_upload_session.h"
 #include "google/cloud/storage/internal/resumable_upload_session.h"
-#include "google/cloud/storage/internal/sha256_hash.h"
-#include "google/cloud/storage/internal/storage_auth.h"
-#include "google/cloud/storage/internal/storage_round_robin.h"
-#include "google/cloud/storage/internal/storage_stub.h"
+#include "google/cloud/storage/internal/storage_stub_factory.h"
 #include "google/cloud/grpc_options.h"
 #include "google/cloud/internal/big_endian.h"
 #include "google/cloud/internal/getenv.h"
 #include "google/cloud/internal/invoke_result.h"
 #include "google/cloud/internal/unified_grpc_credentials.h"
 #include "google/cloud/log.h"
-#include "absl/strings/str_split.h"
-#include "absl/time/time.h"
 #include <crc32c/crc32c.h>
 #include <grpcpp/grpcpp.h>
 #include <algorithm>
@@ -44,18 +39,7 @@ namespace storage {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 namespace internal {
 
-using ::google::cloud::internal::GrpcAuthenticationStrategy;
 using ::google::cloud::internal::MakeBackgroundThreadsFactory;
-
-auto constexpr kDirectPathConfig = R"json({
-    "loadBalancingConfig": [{
-      "grpclb": {
-        "childPolicy": [{
-          "pick_first": {}
-        }]
-      }
-    }]
-  })json";
 
 int DefaultGrpcNumChannels() {
   auto constexpr kMinimumChannels = 4;
@@ -85,59 +69,6 @@ Options DefaultOptionsGrpc(Options options) {
     options.set<GrpcNumChannelsOption>(DefaultGrpcNumChannels());
   }
   return options;
-}
-
-std::shared_ptr<grpc::Channel> CreateGrpcChannel(
-    GrpcAuthenticationStrategy& auth, Options const& options, int channel_id) {
-  grpc::ChannelArguments args;
-  auto const& config = options.get<storage_experimental::GrpcPluginOption>();
-  if (config.empty() || config == "default" || config == "none") {
-    // Just configure for the regular path.
-    args.SetInt("grpc.channel_id", channel_id);
-    return auth.CreateChannel(options.get<EndpointOption>(), std::move(args));
-  }
-  std::set<absl::string_view> settings = absl::StrSplit(config, ',');
-  auto const dp = settings.count("dp") != 0 || settings.count("alts") != 0;
-  if (dp || settings.count("pick-first-lb") != 0) {
-    args.SetServiceConfigJSON(kDirectPathConfig);
-  }
-  if (dp || settings.count("enable-dns-srv-queries") != 0) {
-    args.SetInt("grpc.dns_enable_srv_queries", 1);
-  }
-  if (settings.count("disable-dns-srv-queries") != 0) {
-    args.SetInt("grpc.dns_enable_srv_queries", 0);
-  }
-  if (settings.count("exclusive") != 0) {
-    args.SetInt("grpc.channel_id", channel_id);
-  }
-  if (settings.count("alts") != 0) {
-    grpc::experimental::AltsCredentialsOptions alts_opts;
-    return grpc::CreateCustomChannel(
-        options.get<EndpointOption>(),
-        grpc::CompositeChannelCredentials(
-            grpc::experimental::AltsCredentials(alts_opts),
-            grpc::GoogleComputeEngineCredentials()),
-        std::move(args));
-  }
-  return auth.CreateChannel(options.get<EndpointOption>(), std::move(args));
-}
-
-std::shared_ptr<StorageStub> CreateStorageStub(CompletionQueue cq,
-                                               Options const& opts) {
-  auto auth = google::cloud::internal::CreateAuthenticationStrategy(
-      std::move(cq), opts);
-  std::vector<std::shared_ptr<StorageStub>> children(
-      (std::max)(1, opts.get<GrpcNumChannelsOption>()));
-  int id = 0;
-  std::generate(children.begin(), children.end(), [&id, &auth, opts] {
-    return MakeDefaultStorageStub(CreateGrpcChannel(*auth, opts, id++));
-  });
-  std::shared_ptr<StorageStub> stub =
-      std::make_shared<StorageRoundRobin>(std::move(children));
-  if (auth->RequiresConfigureContext()) {
-    stub = std::make_shared<StorageAuth>(std::move(auth), std::move(stub));
-  }
-  return stub;
 }
 
 std::shared_ptr<GrpcClient> GrpcClient::Create(Options opts) {
---
+++ google/cloud/storage/internal/storage_stub_factory.cc
@@ -0,0 +1,106 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/storage_stub_factory.h"
+#include "google/cloud/storage/grpc_plugin.h"
+#include "google/cloud/storage/internal/storage_auth.h"
+#include "google/cloud/storage/internal/storage_round_robin.h"
+#include "google/cloud/grpc_options.h"
+#include "google/cloud/internal/algorithm.h"
+#include "google/cloud/internal/unified_grpc_credentials.h"
+#include "google/cloud/log.h"
+#include "absl/strings/str_split.h"
+#include <grpcpp/grpcpp.h>
+
+namespace google {
+namespace cloud {
+namespace storage {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace internal {
+namespace {
+auto constexpr kDirectPathConfig = R"json({
+    "loadBalancingConfig": [{
+      "grpclb": {
+        "childPolicy": [{
+          "pick_first": {}
+        }]
+      }
+    }]
+  })json";
+
+std::shared_ptr<grpc::Channel> CreateGrpcChannel(
+    google::cloud::internal::GrpcAuthenticationStrategy& auth,
+    Options const& options, int channel_id) {
+  grpc::ChannelArguments args;
+  auto const& config = options.get<storage_experimental::GrpcPluginOption>();
+  if (config.empty() || config == "default" || config == "none") {
+    // Just configure for the regular path.
+    args.SetInt("grpc.channel_id", channel_id);
+    return auth.CreateChannel(options.get<EndpointOption>(), std::move(args));
+  }
+  std::set<absl::string_view> settings = absl::StrSplit(config, ',');
+  auto const dp = settings.count("dp") != 0 || settings.count("alts") != 0;
+  if (dp || settings.count("pick-first-lb") != 0) {
+    args.SetServiceConfigJSON(kDirectPathConfig);
+  }
+  if (dp || settings.count("enable-dns-srv-queries") != 0) {
+    args.SetInt("grpc.dns_enable_srv_queries", 1);
+  }
+  if (settings.count("disable-dns-srv-queries") != 0) {
+    args.SetInt("grpc.dns_enable_srv_queries", 0);
+  }
+  if (settings.count("exclusive") != 0) {
+    args.SetInt("grpc.channel_id", channel_id);
+  }
+  if (settings.count("alts") != 0) {
+    grpc::experimental::AltsCredentialsOptions alts_opts;
+    return grpc::CreateCustomChannel(
+        options.get<EndpointOption>(),
+        grpc::CompositeChannelCredentials(
+            grpc::experimental::AltsCredentials(alts_opts),
+            grpc::GoogleComputeEngineCredentials()),
+        std::move(args));
+  }
+  return auth.CreateChannel(options.get<EndpointOption>(), std::move(args));
+}
+
+}  // namespace
+
+std::shared_ptr<StorageStub> CreateStorageStub(
+    google::cloud::CompletionQueue cq, Options const& options) {
+  auto auth = google::cloud::internal::CreateAuthenticationStrategy(
+      std::move(cq), options);
+
+  std::vector<std::shared_ptr<StorageStub>> children(
+      (std::max)(1, options.get<GrpcNumChannelsOption>()));
+  int id = 0;
+  std::generate(children.begin(), children.end(), [&id, &auth, options] {
+    auto channel = CreateGrpcChannel(*auth, options, id++);
+    return MakeDefaultStorageStub(std::move(channel));
+  });
+
+  std::shared_ptr<StorageStub> stub =
+      std::make_shared<StorageRoundRobin>(std::move(children));
+
+  if (auth->RequiresConfigureContext()) {
+    stub = std::make_shared<StorageAuth>(std::move(auth), std::move(stub));
+  }
+  return stub;
+}
+
+}  // namespace internal
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace storage
+}  // namespace cloud
+}  // namespace google
