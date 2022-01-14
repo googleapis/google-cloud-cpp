11b56eb15cefabaafcfd2d5390c875ab8271a8d9
Daniel Collins <dpcollins@google.com>
2022-01-14 12:37:03 -0500

    fix coverage again

--- ci/cloudbuild/builds/coverage.sh
+++ ci/cloudbuild/builds/coverage.sh
@@ -26,9 +26,8 @@ export CXX=g++
 mapfile -t args < <(bazel::common_args)
 args+=("--instrumentation_filter=/google/cloud[/:],/generator[/:]")
 args+=("--instrument_test_targets")
-# We need to build with gcc which has many more warnings of dubious
-# value in -Wall
-args+=("--copt=-Wno-all")
+# We need to build with gcc and some dependencies are not gcc warning clean
+args+=("--copt=-Wno-error")
 bazel coverage "${args[@]}" --test_tag_filters=-integration-test ...
 GOOGLE_CLOUD_CPP_SPANNER_SLOW_INTEGRATION_TESTS="instance"
 mapfile -t integration_args < <(integration::bazel_args)
