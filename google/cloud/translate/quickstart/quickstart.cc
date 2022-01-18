d208ebadac6fe5e09b3a66ae4d34a6f72838387e
Carlos O'Ryan <coryan@google.com>
2022-01-18 15:05:28 +0000

    Manually update READMEs, quickstart, and top-level stuff

--- google/cloud/translate/quickstart/quickstart.cc
+++ google/cloud/translate/quickstart/quickstart.cc
@@ -12,24 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cloud/translate/ EDIT HERE .h"
+#include "google/cloud/translate/translation_client.h"
 #include "google/cloud/project.h"
 #include <iostream>
 #include <stdexcept>
 
+auto constexpr kText = R"""(
+Four score and seven years ago our fathers brought forth on this
+continent, a new nation, conceived in Liberty, and dedicated to
+the proposition that all men are created equal.)""";
+
 int main(int argc, char* argv[]) try {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " project-id\n";
+  if (argc < 2 || argc > 3) {
+    std::cerr << "Usage: " << argv[0] << " project-id "
+              << "[target-language (default: es-419)]\n";
     return 1;
   }
 
   namespace translate = ::google::cloud::translate;
-  auto client = translate::Client(translate::MakeConnection(/* EDIT HERE */));
+  auto client = translate::TranslationServiceClient(
+      translate::MakeTranslationServiceConnection());
 
   auto const project = google::cloud::Project(argv[1]);
-  for (auto r : client.List /*EDIT HERE*/ (project.FullName())) {
-    if (!r) throw std::runtime_error(r.status().message());
-    std::cout << r->DebugString() << "\n";
+  auto const target = std::string{argc >= 3 ? argv[2] : "es-419"};
+  auto response =
+      client.TranslateText(project.FullName(), target, {std::string{kText}});
+  if (!response) throw std::runtime_error(response.status().message());
+
+  for (auto const& translation : response->translations()) {
+    std::cout << translation.translated_text() << "\n";
   }
 
   return 0;
