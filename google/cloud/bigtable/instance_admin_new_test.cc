be590f9306c7fde14b906791eb751da256129e21
dbolduc <dbolduc@google.com>
2022-01-21 22:39:17 -0500

    impl(bigtable): implement connection mechanism in legacy InstanceAdmin

--- google/cloud/bigtable/instance_admin_new_test.cc
+++ google/cloud/bigtable/instance_admin_new_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 Google Inc.
+// Copyright 2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,42 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "google/cloud/bigtable/admin/mocks/mock_bigtable_instance_admin_connection.h"
 #include "google/cloud/bigtable/instance_admin.h"
-#include "google/cloud/bigtable/testing/mock_instance_admin_client.h"
 #include <gmock/gmock.h>
 
 namespace google {
 namespace cloud {
 namespace bigtable {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+// Helper class for checking that the legacy API still functions correctly
+class InstanceAdminTester {
+ public:
+  static std::shared_ptr<bigtable_admin::BigtableInstanceAdminConnection>
+  Connection(InstanceAdmin const& admin) {
+    return admin.connection_;
+  }
+};
+
 namespace {
 
-using ::testing::ReturnRef;
-
-using MockAdminClient =
-    ::google::cloud::bigtable::testing::MockInstanceAdminClient;
+using MockConnection =
+    ::google::cloud::bigtable_admin_mocks::MockBigtableInstanceAdminConnection;
 
 std::string const kProjectId = "the-project";
 
 /// A fixture for the bigtable::InstanceAdmin tests.
 class InstanceAdminTest : public ::testing::Test {
  protected:
-  void SetUp() override {
-    EXPECT_CALL(*client_, project()).WillRepeatedly(ReturnRef(kProjectId));
-  }
-
-  std::shared_ptr<MockAdminClient> client_ =
-      std::make_shared<MockAdminClient>();
+  std::shared_ptr<MockConnection> connection_ =
+      std::make_shared<MockConnection>();
 };
 
-/// @test Verify basic functionality in the `bigtable::InstanceAdmin` class.
-TEST_F(InstanceAdminTest, Default) {
-  InstanceAdmin tested(client_);
-  EXPECT_EQ("the-project", tested.project_id());
+TEST_F(InstanceAdminTest, Project) {
+  Project p(kProjectId);
+  InstanceAdmin tested(MakeInstanceAdminClient(p.project_id()));
+  EXPECT_EQ(p.project_id(), tested.project_id());
+  EXPECT_EQ(p.FullName(), tested.project_name());
 }
 
 TEST_F(InstanceAdminTest, CopyConstructor) {
-  InstanceAdmin source(client_);
+  auto source = InstanceAdmin(connection_, kProjectId);
   std::string const& expected = source.project_id();
   // NOLINTNEXTLINE(performance-unnecessary-copy-initialization)
   InstanceAdmin copy(source);
@@ -55,40 +60,44 @@ TEST_F(InstanceAdminTest, CopyConstructor) {
 }
 
 TEST_F(InstanceAdminTest, MoveConstructor) {
-  InstanceAdmin source(client_);
+  auto source = InstanceAdmin(connection_, kProjectId);
   std::string expected = source.project_id();
   InstanceAdmin copy(std::move(source));
   EXPECT_EQ(expected, copy.project_id());
 }
 
 TEST_F(InstanceAdminTest, CopyAssignment) {
-  std::shared_ptr<MockAdminClient> other_client =
-      std::make_shared<MockAdminClient>();
-  std::string other_project = "other-project";
-  EXPECT_CALL(*other_client, project())
-      .WillRepeatedly(ReturnRef(other_project));
+  std::shared_ptr<MockConnection> other_client =
+      std::make_shared<MockConnection>();
 
-  InstanceAdmin source(client_);
+  auto source = InstanceAdmin(connection_, kProjectId);
   std::string const& expected = source.project_id();
-  InstanceAdmin dest(other_client);
+  auto dest = InstanceAdmin(other_client, "other-project");
   EXPECT_NE(expected, dest.project_id());
   dest = source;
   EXPECT_EQ(expected, dest.project_id());
 }
 
 TEST_F(InstanceAdminTest, MoveAssignment) {
-  std::shared_ptr<MockAdminClient> other_client =
-      std::make_shared<MockAdminClient>();
-  std::string other_project = "other-project";
-  EXPECT_CALL(*other_client, project())
-      .WillRepeatedly(ReturnRef(other_project));
+  std::shared_ptr<MockConnection> other_client =
+      std::make_shared<MockConnection>();
 
-  InstanceAdmin source(client_);
+  auto source = InstanceAdmin(connection_, kProjectId);
   std::string expected = source.project_id();
-  InstanceAdmin dest(other_client);
+  auto dest = InstanceAdmin(other_client, "other-project");
   EXPECT_NE(expected, dest.project_id());
   dest = std::move(source);
   EXPECT_EQ(expected, dest.project_id());
+}
+
+TEST_F(InstanceAdminTest, LegacyConstructorSharesConnection) {
+  auto admin_client = MakeInstanceAdminClient("test-project");
+  auto admin_1 = InstanceAdmin(admin_client);
+  auto admin_2 = InstanceAdmin(admin_client);
+  auto conn_1 = InstanceAdminTester::Connection(admin_1);
+  auto conn_2 = InstanceAdminTester::Connection(admin_2);
+
+  EXPECT_EQ(conn_1, conn_2);
 }
 
 }  // namespace
