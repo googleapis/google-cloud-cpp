3feb6766627e6f3968a05434b12c38e55f2d70ec
Bradley White <14679271+devbww@users.noreply.github.com>
2022-01-22 14:25:34 -0500

    fix(spanner): apply policy options in (generated) client ctor
    Apply any retry/backoff/polling policies in the client options
    instead of the connection options.  This makes more logical
    sense (particularly so when we consider per-operation options),
    and is actually required until we fix #8054.
    
    Fixes #8037.

--- google/cloud/spanner/admin/integration_tests/backup_integration_test.cc
+++ google/cloud/spanner/admin/integration_tests/backup_integration_test.cc
@@ -69,7 +69,8 @@ class BackupIntegrationTest
  public:
   BackupIntegrationTest()
       : generator_(google::cloud::internal::MakeDefaultPRNG()),
-        database_admin_client_(spanner_admin::MakeDatabaseAdminConnection(
+        database_admin_client_(
+            spanner_admin::MakeDatabaseAdminConnection(),
             Options{}
                 .set<spanner_admin::DatabaseAdminRetryPolicyOption>(
                     spanner_admin::DatabaseAdminLimitedTimeRetryPolicy(
@@ -84,7 +85,7 @@ class BackupIntegrationTest
                         LimitedTimeRetryPolicy(std::chrono::hours(3)),
                         ExponentialBackoffPolicy(std::chrono::seconds(1),
                                                  std::chrono::minutes(1), 2.0))
-                        .clone()))) {}
+                        .clone())) {}
 
  protected:
   google::cloud::internal::DefaultPRNG generator_;
