327298eb2e4f6646f86fcac41e608e2f6e11c582
Scott Hart <sdhart@google.com>
2022-01-24 17:01:48 -0500

    impl(rest): add HttpPayload class

---
+++ google/cloud/testing_util/mock_rest_utils.h
@@ -0,0 +1,42 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_TESTING_UTIL_MOCK_REST_UTILS_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_TESTING_UTIL_MOCK_REST_UTILS_H
+
+#include "google/cloud/credentials.h"
+#include "google/cloud/internal/http_payload.h"
+#include "google/cloud/version.h"
+#include <gmock/gmock.h>
+
+namespace google {
+namespace cloud {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace testing_util {
+
+class MockHttpPayload : public rest_internal::HttpPayload {
+ public:
+  ~MockHttpPayload() override = default;
+  MOCK_METHOD(StatusOr<std::size_t>, Read, (absl::Span<char> buffer),
+              (override));
+  MOCK_METHOD((std::multimap<std::string, std::string>), Trailers, (),
+              (const, override));
+};
+
+}  // namespace testing_util
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_TESTING_UTIL_MOCK_REST_UTILS_H
