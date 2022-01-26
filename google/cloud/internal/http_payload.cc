d72c1fe8f15c463f23c8cce778216b3a11acd051
Scott Hart <sdhart@google.com>
2022-01-25 23:41:45 -0500

    impl(rest): add HttpPayload class (#8083)
    * impl(rest): add HttpPayload class

---
+++ google/cloud/internal/http_payload.cc
@@ -0,0 +1,52 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/http_payload.h"
+#include "google/cloud/internal/curl_impl.h"
+#include "google/cloud/log.h"
+#include "absl/strings/ascii.h"
+#include "absl/strings/str_split.h"
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+CurlHttpPayload::CurlHttpPayload(std::unique_ptr<CurlImpl> impl,
+                                 Options options)
+    : impl_(std::move(impl)), options_(std::move(options)) {}
+
+StatusOr<std::size_t> CurlHttpPayload::Read(absl::Span<char> buffer) {
+  return impl_->Read(buffer);
+}
+
+StatusOr<std::string> ReadAll(std::unique_ptr<HttpPayload> payload,
+                              std::size_t read_size) {
+  std::string output_buffer;
+  // Allocate buf on the heap as large values of read_size could exceed stack
+  // size.
+  auto buf = absl::make_unique<char[]>(read_size);
+  StatusOr<std::size_t> read_status;
+  do {
+    read_status = payload->Read({&buf[0], read_size});
+    if (!read_status.ok()) return std::move(read_status).status();
+    output_buffer.append(buf.get(), read_status.value());
+  } while (read_status.value() > 0);
+  return output_buffer;
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/internal/http_payload.h
@@ -0,0 +1,75 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_HTTP_PAYLOAD_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_HTTP_PAYLOAD_H
+
+#include "google/cloud/options.h"
+#include "google/cloud/status_or.h"
+#include "google/cloud/version.h"
+#include "absl/types/span.h"
+#include <map>
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+class CurlImpl;
+
+// Allows the payload and trailers of an HTTP response to be read.
+class HttpPayload {
+ public:
+  static constexpr std::size_t kDefaultReadSize = 1024 * 1024;
+  virtual ~HttpPayload() = default;
+
+  // Always reads up to N bytes (as specified in buffer) from the payload and
+  // write tp the provided buffer. Read can be called multiple times in order to
+  // read the entire payload.
+  // Returns number of bytes actually read into buffer from the payload.
+  virtual StatusOr<std::size_t> Read(absl::Span<char> buffer) = 0;
+};
+
+// Implementation using libcurl.
+class CurlHttpPayload : public HttpPayload {
+ public:
+  ~CurlHttpPayload() override = default;
+
+  CurlHttpPayload(CurlHttpPayload const&) = delete;
+  CurlHttpPayload(CurlHttpPayload&&) = default;
+  CurlHttpPayload& operator=(CurlHttpPayload const&) = delete;
+  CurlHttpPayload& operator=(CurlHttpPayload&&) = default;
+
+  StatusOr<std::size_t> Read(absl::Span<char> buffer) override;
+
+ private:
+  friend class CurlRestResponse;
+  CurlHttpPayload(std::unique_ptr<CurlImpl> impl, Options options);
+
+  std::unique_ptr<CurlImpl> impl_;
+  Options options_;
+};
+
+// This function makes one or more HttpPayload::Read calls and writes all the
+// bytes from the payload to a buffer it allocates.
+StatusOr<std::string> ReadAll(
+    std::unique_ptr<HttpPayload> payload,
+    std::size_t read_size = HttpPayload::kDefaultReadSize);
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_HTTP_PAYLOAD_H
