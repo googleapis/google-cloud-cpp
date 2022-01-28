9e4eaa97e75dd53fe01e60964e295ae5e4736f19
Greg Miller <9447643+devjgm@users.noreply.github.com>
2022-01-28 14:52:25 -0500

    feat(functions): generate library (#8149)
    * feat(functions): generate library
    
    * Run generators and format their outputs
    
    * Manually update READMEs, quickstart, and top-level stuff

---
+++ google/cloud/functions/quickstart/quickstart.cc
@@ -0,0 +1,42 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/functions/cloud_functions_client.h"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
+    return 1;
+  }
+
+  namespace functions = ::google::cloud::functions;
+  auto client = functions::CloudFunctionsServiceClient(
+      functions::MakeCloudFunctionsServiceConnection());
+
+  auto project_id = std::string(argv[1]);
+  auto request = google::cloud::functions::v1::ListFunctionsRequest{};
+  request.set_parent("projects/" + project_id + "locations/-");
+
+  for (auto r : client.ListFunctions(std::move(request))) {
+    if (!r) throw std::runtime_error(r.status().message());
+    std::cout << r->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  return 1;
+}
