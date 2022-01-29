c86f6c33d7d0ed73bad929996fa57e9a5134f0b1
Greg Miller <9447643+devjgm@users.noreply.github.com>
2022-01-29 10:23:50 -0500

    feat(redis): generate library (#8155)
    Fixes: #8154

---
+++ google/cloud/redis/quickstart/quickstart.cc
@@ -0,0 +1,40 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/redis/cloud_redis_client.h"
+#include "google/cloud/project.h"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
+    return 1;
+  }
+
+  namespace redis = ::google::cloud::redis;
+  auto client = redis::CloudRedisClient(redis::MakeCloudRedisConnection());
+
+  auto const project_id = std::string(argv[1]);
+  auto const parent = "projects/" + project_id + "locations/-";
+  for (auto r : client.ListInstances(parent)) {
+    if (!r) throw std::runtime_error(r.status().message());
+    std::cout << r->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  return 1;
+}
