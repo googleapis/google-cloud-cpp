8d75ac25f215037e53e8aa1e467134869d9d9bd9
Bradley White <14679271+devbww@users.noreply.github.com>
2022-02-05 15:33:51 -0500

    ci: pass the --disable-pip-version-check flag to "pip install"
    To continue the effort to eliminate uninteresting build output,
    and particularly when it is highlighted somehow, quash the "You
    are using pip version X; however, version Y is available" warning.
    
    Also, we might as well skip installing the docuploader package
    when we're going to skip uploading the docs.

--- ci/cloudbuild/builds/lib/integration.sh
+++ ci/cloudbuild/builds/lib/integration.sh
@@ -22,14 +22,14 @@ if ((CI_CLOUDBUILD_BUILDS_LIB_INTEGRATION_SH__++ != 0)); then
   return 0
 fi # include guard
 
-source module ci/lib/io.sh
 source module ci/etc/integration-tests-config.sh
 source module ci/lib/io.sh
 
 # To run the integration tests we need to install the dependencies for the storage emulator
 export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m pip uninstall -y --quiet googleapis-storage-testbench
-python3 -m pip install --upgrade --user --quiet "git+https://github.com/googleapis/storage-testbench@v0.14.0"
+python3 -m pip install --upgrade --user --quiet --disable-pip-version-check \
+  "git+https://github.com/googleapis/storage-testbench@v0.14.0"
 
 # Some of the tests will need a valid roots.pem file.
 rm -f /dev/shm/roots.pem
