7fcaf7f05435960479f142d2496a4bee73d37aba
Carlos O'Ryan <coryan@google.com>
2022-02-05 18:21:29 -0500

    ci: reduce logging in cmake-install build (#8265)

--- ci/cloudbuild/builds/cmake-install.sh
+++ ci/cloudbuild/builds/cmake-install.sh
@@ -173,14 +173,15 @@ while IFS= read -r -d '' f; do
   fi
 done < <(find "${INSTALL_PREFIX}" -type f -print0)
 
+mapfile -t ctest_args < <(ctest::common_args)
 for repo_root in "ci/verify_current_targets" "ci/verify_deprecated_targets"; do
   out_dir="cmake-out/$(basename "${repo_root}")-out"
   rm -f "${out_dir}/CMakeCache.txt"
   io::log_h2 "Verifying CMake targets in repo root: ${repo_root}"
-  cmake -GNinja -DCMAKE_PREFIX_PATH="${INSTALL_PREFIX}" \
+  cmake --log-level WARNING -GNinja -DCMAKE_PREFIX_PATH="${INSTALL_PREFIX}" \
     -S "${repo_root}" -B "${out_dir}" -Wno-dev
   cmake --build "${out_dir}"
-  cmake --build "${out_dir}" --target test
+  env -C "${out_dir}" ctest "${ctest_args[@]}"
 done
 
 # Tests the installed artifacts by building and running the quickstarts.
