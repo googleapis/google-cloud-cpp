64d7784fe0d8f7eb6538c54a37adb3baa0e484c6
Carlos O'Ryan <coryan@google.com>
2022-02-05 15:46:27 +0000

    fix(pubsublite): correct quickstart and enable in CI
    The quickstart had a number of problems, starting with using the wrong
    endpoint.  Pub/Sub Lite happens to be zonal, it requires a zone as a
    location argument, and it uses regional endpoints.

---
+++ google/cloud/pubsublite/endpoint.cc
@@ -0,0 +1,35 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/pubsublite/endpoint.h"
+#include <cctype>
+
+namespace google {
+namespace cloud {
+namespace pubsublite {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+StatusOr<std::string> EndpointFromZone(std::string const& zone_id) {
+  auto const n = zone_id.size();
+  if (n < 3 || !std::isalpha(zone_id[n - 1]) || zone_id[n - 2] != '-') {
+    return Status(StatusCode::kInvalidArgument,
+                  "expected a zone id in <region>-[a-z] format");
+  }
+  return zone_id.substr(0, n - 1) + "pubsublite.googleapis.com";
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace pubsublite
+}  // namespace cloud
+}  // namespace google
