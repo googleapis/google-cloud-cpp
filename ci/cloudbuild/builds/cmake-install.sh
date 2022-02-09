7ddbb6d26f5667e919260e0db14c9b2d878dde51
Carlos O'Ryan <coryan@google.com>
2022-02-09 13:21:44 -0500

    feat(bigquerydatatransfer): generate library (#8317)

--- ci/cloudbuild/builds/cmake-install.sh
+++ ci/cloudbuild/builds/cmake-install.sh
@@ -60,8 +60,6 @@ expected_dirs+=(
   ./include/google/appengine/logging
   ./include/google/appengine/logging/v1
   ./include/google/bigtable/v2
-  ./include/google/cloud/bigquery/datatransfer
-  ./include/google/cloud/bigquery/datatransfer/v1
   ./include/google/cloud/bigquery/logging
   ./include/google/cloud/bigquery/logging/v1
   ./include/google/cloud/bigquery/v2
