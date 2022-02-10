7d963059bed1e4575b9b86c38d311c9a5f67659b
Carlos O'Ryan <coryan@google.com>
2022-02-10 17:18:09 -0500

    test(resourcemanager): enable quickstart in CI (#8337)

--- ci/etc/integration-tests-config.sh
+++ ci/etc/integration-tests-config.sh
@@ -93,3 +93,6 @@ export GOOGLE_CLOUD_CPP_TEST_HELLO_WORLD_HTTP_URL=""
 # To run google/cloud/gameservices' quickstart
 export GOOGLE_CLOUD_CPP_GAMESERVICES_TEST_LOCATION="global"
 export GOOGLE_CLOUD_CPP_GAMESERVICES_TEST_REALM="test-realm"
+
+# To run google/cloud/resourcemanager's quickstart, this is the Cloud C++ team community folder
+export GOOGLE_CLOUD_CPP_RESOURCEMANAGER_TEST_FOLDER="204009073908"
