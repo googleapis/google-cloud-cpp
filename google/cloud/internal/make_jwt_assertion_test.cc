5f0c67402c859aecdfc5587531bc9aef779c6c8e
Scott Hart <sdhart@google.com>
2022-02-11 18:35:12 -0500

    impl(oauth2): port service account credential prerequisites (#8343)
    * impl(oauth2): port service account credential prerequisites

---
+++ google/cloud/internal/make_jwt_assertion_test.cc
@@ -0,0 +1,98 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/make_jwt_assertion.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include "absl/strings/str_split.h"
+#include <gmock/gmock.h>
+#include <nlohmann/json.hpp>
+
+namespace google {
+namespace cloud {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace internal {
+namespace {
+
+using ::google::cloud::testing_util::IsOk;
+
+// This is an invalidated private key. It was created using the Google Cloud
+// Platform console, but then the key (and service account) were deleted.
+auto constexpr kWellFormattedKey = R"""(-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCltiF2oP3KJJ+S
+tTc1McylY+TuAi3AdohX7mmqIjd8a3eBYDHs7FlnUrFC4CRijCr0rUqYfg2pmk4a
+6TaKbQRAhWDJ7XD931g7EBvCtd8+JQBNWVKnP9ByJUaO0hWVniM50KTsWtyX3up/
+fS0W2R8Cyx4yvasE8QHH8gnNGtr94iiORDC7De2BwHi/iU8FxMVJAIyDLNfyk0hN
+eheYKfIDBgJV2v6VaCOGWaZyEuD0FJ6wFeLybFBwibrLIBE5Y/StCrZoVZ5LocFP
+T4o8kT7bU6yonudSCyNMedYmqHj/iF8B2UN1WrYx8zvoDqZk0nxIglmEYKn/6U7U
+gyETGcW9AgMBAAECggEAC231vmkpwA7JG9UYbviVmSW79UecsLzsOAZnbtbn1VLT
+Pg7sup7tprD/LXHoyIxK7S/jqINvPU65iuUhgCg3Rhz8+UiBhd0pCH/arlIdiPuD
+2xHpX8RIxAq6pGCsoPJ0kwkHSw8UTnxPV8ZCPSRyHV71oQHQgSl/WjNhRi6PQroB
+Sqc/pS1m09cTwyKQIopBBVayRzmI2BtBxyhQp9I8t5b7PYkEZDQlbdq0j5Xipoov
+9EW0+Zvkh1FGNig8IJ9Wp+SZi3rd7KLpkyKPY7BK/g0nXBkDxn019cET0SdJOHQG
+DiHiv4yTRsDCHZhtEbAMKZEpku4WxtQ+JjR31l8ueQKBgQDkO2oC8gi6vQDcx/CX
+Z23x2ZUyar6i0BQ8eJFAEN+IiUapEeCVazuxJSt4RjYfwSa/p117jdZGEWD0GxMC
++iAXlc5LlrrWs4MWUc0AHTgXna28/vii3ltcsI0AjWMqaybhBTTNbMFa2/fV2OX2
+UimuFyBWbzVc3Zb9KAG4Y7OmJQKBgQC5324IjXPq5oH8UWZTdJPuO2cgRsvKmR/r
+9zl4loRjkS7FiOMfzAgUiXfH9XCnvwXMqJpuMw2PEUjUT+OyWjJONEK4qGFJkbN5
+3ykc7p5V7iPPc7Zxj4mFvJ1xjkcj+i5LY8Me+gL5mGIrJ2j8hbuv7f+PWIauyjnp
+Nx/0GVFRuQKBgGNT4D1L7LSokPmFIpYh811wHliE0Fa3TDdNGZnSPhaD9/aYyy78
+LkxYKuT7WY7UVvLN+gdNoVV5NsLGDa4cAV+CWPfYr5PFKGXMT/Wewcy1WOmJ5des
+AgMC6zq0TdYmMBN6WpKUpEnQtbmh3eMnuvADLJWxbH3wCkg+4xDGg2bpAoGAYRNk
+MGtQQzqoYNNSkfus1xuHPMA8508Z8O9pwKU795R3zQs1NAInpjI1sOVrNPD7Ymwc
+W7mmNzZbxycCUL/yzg1VW4P1a6sBBYGbw1SMtWxun4ZbnuvMc2CTCh+43/1l+FHe
+Mmt46kq/2rH2jwx5feTbOE6P6PINVNRJh/9BDWECgYEAsCWcH9D3cI/QDeLG1ao7
+rE2NcknP8N783edM07Z/zxWsIsXhBPY3gjHVz2LDl+QHgPWhGML62M0ja/6SsJW3
+YvLLIc82V7eqcVJTZtaFkuht68qu/Jn1ezbzJMJ4YXDYo1+KFi+2CAGR06QILb+I
+lUtj+/nH3HDQjM4ltYfTPUg=
+-----END PRIVATE KEY-----
+)""";
+
+TEST(MakeJWTAssertionNoThrow, Basic) {
+  auto header = nlohmann::json{
+      {"alg", "HS256"}, {"typ", "JWT"}, {"kid", "test-key-name"}};
+  auto payload = nlohmann::json{
+      {"iss", "--invalid--@developer.gserviceaccount.com"},
+      {"sub", "--invalid--@developer.gserviceaccount.com"},
+      {"aud", "https//not-a-service.googleapis.com"},
+      {"iat", "1511900000"},
+      {"exp", "1511903600"},
+  };
+  auto const assertion =
+      MakeJWTAssertionNoThrow(header.dump(), payload.dump(), kWellFormattedKey);
+  ASSERT_THAT(assertion, IsOk());
+
+  std::vector<std::string> components = absl::StrSplit(*assertion, '.');
+  EXPECT_EQ(components.size(), 3);
+}
+
+TEST(MakeJWTAssertionNoThrow, InvalidKey) {
+  auto header = nlohmann::json{
+      {"alg", "HS256"}, {"typ", "JWT"}, {"kid", "test-key-name"}};
+  auto payload = nlohmann::json{
+      {"iss", "--invalid--@developer.gserviceaccount.com"},
+      {"sub", "--invalid--@developer.gserviceaccount.com"},
+      {"aud", "https//not-a-service.googleapis.com"},
+      {"iat", "1511900000"},
+      {"exp", "1511903600"},
+  };
+  auto const assertion =
+      MakeJWTAssertionNoThrow(header.dump(), payload.dump(), "invalid-key");
+  ASSERT_THAT(assertion, Not(IsOk()));
+}
+
+}  // namespace
+}  // namespace internal
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/internal/openssl_util.cc
@@ -0,0 +1,152 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/openssl_util.h"
+#include "google/cloud/internal/base64_transforms.h"
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
+#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <openssl/opensslv.h>
+#include <openssl/pem.h>
+#include <memory>
+
+namespace google {
+namespace cloud {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace internal {
+
+namespace {
+// The name of the function to free an EVP_MD_CTX changed in OpenSSL 1.1.0.
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L)  // Older than version 1.1.0.
+inline std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_destroy)>
+GetDigestCtx() {
+  return std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_destroy)>(
+      EVP_MD_CTX_create(), &EVP_MD_CTX_destroy);
+};
+#else
+inline std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_free)> GetDigestCtx() {
+  return std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_free)>(
+      EVP_MD_CTX_new(), &EVP_MD_CTX_free);
+};
+#endif
+}  // namespace
+
+StatusOr<std::vector<std::uint8_t>> SignUsingSha256(
+    std::string const& str, std::string const& pem_contents) {
+  auto digest_ctx = GetDigestCtx();
+  if (!digest_ctx) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not create context for OpenSSL digest. ");
+  }
+
+  EVP_MD const* digest_type = EVP_sha256();
+  if (digest_type == nullptr) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not find specified digest in OpenSSL. ");
+  }
+
+  auto pem_buffer = std::unique_ptr<BIO, decltype(&BIO_free)>(
+      BIO_new_mem_buf(pem_contents.data(),
+                      static_cast<int>(pem_contents.length())),
+      &BIO_free);
+  if (!pem_buffer) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not create PEM buffer. ");
+  }
+
+  auto private_key = std::unique_ptr<EVP_PKEY, decltype(&EVP_PKEY_free)>(
+      PEM_read_bio_PrivateKey(
+          pem_buffer.get(),
+          nullptr,  // EVP_PKEY **x
+          nullptr,  // pem_password_cb *cb -- a custom callback.
+          // void *u -- this represents the password for the PEM (only
+          // applicable for formats such as PKCS12 (.p12 files) that use
+          // a password, which we don't currently support.
+          nullptr),
+      &EVP_PKEY_free);
+  if (!private_key) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not parse PEM to get private key ");
+  }
+
+  int const digest_sign_success_code = 1;
+  if (digest_sign_success_code !=
+      EVP_DigestSignInit(digest_ctx.get(),
+                         nullptr,  // `EVP_PKEY_CTX **pctx`
+                         digest_type,
+                         nullptr,  // `ENGINE *e`
+                         private_key.get())) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not initialize PEM digest. ");
+  }
+
+  if (digest_sign_success_code !=
+      EVP_DigestSignUpdate(digest_ctx.get(), str.data(), str.length())) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not update PEM digest. ");
+  }
+
+  std::size_t signed_str_size = 0;
+  // Calling this method with a nullptr buffer will populate our size var
+  // with the resulting buffer's size. This allows us to then call it again,
+  // with the correct buffer and size, which actually populates the buffer.
+  if (digest_sign_success_code !=
+      EVP_DigestSignFinal(digest_ctx.get(),
+                          nullptr,  // unsigned char *sig
+                          &signed_str_size)) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not finalize PEM digest (1/2). ");
+  }
+
+  std::vector<unsigned char> signed_str(signed_str_size);
+  if (digest_sign_success_code != EVP_DigestSignFinal(digest_ctx.get(),
+                                                      signed_str.data(),
+                                                      &signed_str_size)) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials: "
+                  "could not finalize PEM digest (2/2). ");
+  }
+
+  return StatusOr<std::vector<unsigned char>>(
+      {signed_str.begin(), signed_str.end()});
+}
+
+StatusOr<std::vector<std::uint8_t>> UrlsafeBase64Decode(
+    std::string const& str) {
+  if (str.empty()) return std::vector<std::uint8_t>{};
+  std::string b64str = str;
+  std::replace(b64str.begin(), b64str.end(), '-', '+');
+  std::replace(b64str.begin(), b64str.end(), '_', '/');
+  // To restore the padding there are only two cases:
+  //    https://en.wikipedia.org/wiki/Base64#Decoding_Base64_without_padding
+  if (b64str.length() % 4 == 2) {
+    b64str.append("==");
+  } else if (b64str.length() % 4 == 3) {
+    b64str.append("=");
+  }
+  return Base64DecodeToBytes(b64str);
+}
+
+}  // namespace internal
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace cloud
+}  // namespace google
