b9965b1964dc4f4eec83791d58742efd2b0c9604
Scott Hart <sdhart@google.com>
2022-02-11 19:47:01 -0500

    impl(oauth2): add service account credentials

---
+++ google/cloud/internal/oauth2_service_account_credentials.cc
@@ -0,0 +1,217 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/oauth2_service_account_credentials.h"
+#include "google/cloud/internal/absl_str_join_quiet.h"
+#include "google/cloud/internal/make_jwt_assertion.h"
+#include "google/cloud/internal/openssl_util.h"
+#include "google/cloud/internal/rest_response.h"
+#include <nlohmann/json.hpp>
+
+namespace google {
+namespace cloud {
+namespace oauth2_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountCredentials(
+    std::string const& content, std::string const& source,
+    std::string const& default_token_uri) {
+  auto credentials = nlohmann::json::parse(content, nullptr, false);
+  if (credentials.is_discarded()) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials,"
+                  "parsing failed on data loaded from " +
+                      source);
+  }
+  std::string const private_key_id_key = "private_key_id";
+  std::string const private_key_key = "private_key";
+  std::string const token_uri_key = "token_uri";
+  std::string const client_email_key = "client_email";
+  for (auto const& key : {private_key_key, client_email_key}) {
+    if (credentials.count(key) == 0) {
+      return Status(StatusCode::kInvalidArgument,
+                    "Invalid ServiceAccountCredentials, the " +
+                        std::string(key) +
+                        " field is missing on data loaded from " + source);
+    }
+    if (credentials.value(key, "").empty()) {
+      return Status(StatusCode::kInvalidArgument,
+                    "Invalid ServiceAccountCredentials, the " +
+                        std::string(key) +
+                        " field is empty on data loaded from " + source);
+    }
+  }
+  // The token_uri field may be missing, but may not be empty:
+  if (credentials.count(token_uri_key) != 0 &&
+      credentials.value(token_uri_key, "").empty()) {
+    return Status(StatusCode::kInvalidArgument,
+                  "Invalid ServiceAccountCredentials, the " +
+                      std::string(token_uri_key) +
+                      " field is empty on data loaded from " + source);
+  }
+  return ServiceAccountCredentialsInfo{
+      credentials.value(client_email_key, ""),
+      credentials.value(private_key_id_key, ""),
+      credentials.value(private_key_key, ""),
+      // Some credential formats (e.g. gcloud's ADC file) don't contain a
+      // "token_uri" attribute in the JSON object.  In this case, we try using
+      // the default value.
+      credentials.value(token_uri_key, default_token_uri),
+      /*scopes*/ {},
+      /*subject*/ {}};
+}
+
+std::pair<std::string, std::string> AssertionComponentsFromInfo(
+    ServiceAccountCredentialsInfo const& info,
+    std::chrono::system_clock::time_point now) {
+  nlohmann::json assertion_header = {{"alg", "RS256"}, {"typ", "JWT"}};
+  if (!info.private_key_id.empty()) {
+    assertion_header["kid"] = info.private_key_id;
+  }
+
+  // Scopes must be specified in a comma-delimited string.
+  std::string scope_str;
+  if (!info.scopes) {
+    scope_str = GoogleOAuthScopeCloudPlatform();
+  } else {
+    absl::StrAppend(&scope_str, absl::StrJoin(*(info.scopes), ","));
+  }
+
+  auto expiration = now + GoogleOAuthAccessTokenLifetime();
+  // As much as possible, do the time arithmetic using the std::chrono types.
+  // Convert to an integer only when we are dealing with timestamps since the
+  // epoch. Note that we cannot use `time_t` directly because that might be a
+  // floating point.
+  auto const now_from_epoch =
+      static_cast<std::intmax_t>(std::chrono::system_clock::to_time_t(now));
+  auto const expiration_from_epoch = static_cast<std::intmax_t>(
+      std::chrono::system_clock::to_time_t(expiration));
+  nlohmann::json assertion_payload = {
+      {"iss", info.client_email},
+      {"scope", scope_str},
+      {"aud", info.token_uri},
+      {"iat", now_from_epoch},
+      // Resulting access token should expire after one hour.
+      {"exp", expiration_from_epoch}};
+  if (info.subject) {
+    assertion_payload["sub"] = *(info.subject);
+  }
+
+  // Note: we don't move here as it would prevent copy elision.
+  return std::make_pair(assertion_header.dump(), assertion_payload.dump());
+}
+
+std::string MakeJWTAssertion(std::string const& header,
+                             std::string const& payload,
+                             std::string const& pem_contents) {
+  return internal::MakeJWTAssertionNoThrow(header, payload, pem_contents)
+      .value();
+}
+
+std::vector<std::pair<std::string, std::string>>
+CreateServiceAccountRefreshPayload(
+    ServiceAccountCredentialsInfo const& info,
+    std::pair<std::string, std::string> grant_type,
+    std::chrono::system_clock::time_point now) {
+  auto assertion_components = AssertionComponentsFromInfo(info, now);
+  std::vector<std::pair<std::string, std::string>> payload = {
+      std::move(grant_type)};
+  payload.emplace_back(
+      "assertion",
+      MakeJWTAssertion(assertion_components.first, assertion_components.second,
+                       info.private_key));
+  return payload;
+}
+
+StatusOr<RefreshingCredentialsWrapper::TemporaryToken>
+ParseServiceAccountRefreshResponse(rest_internal::RestResponse& response,
+                                   std::chrono::system_clock::time_point now) {
+  auto status_code = response.StatusCode();
+  auto payload = rest_internal::ReadAll(std::move(response).ExtractPayload());
+  if (!payload.ok()) return std::move(payload).status();
+  auto access_token = nlohmann::json::parse(*payload, nullptr, false);
+  if (access_token.is_discarded() || access_token.count("access_token") == 0 ||
+      access_token.count("expires_in") == 0 ||
+      access_token.count("token_type") == 0) {
+    auto error_payload =
+        *payload +
+        "Could not find all required fields in response (access_token,"
+        " expires_in, token_type).";
+    return AsStatus(status_code, error_payload);
+  }
+
+  // Response should have the attributes "access_token", "expires_in", and
+  // "token_type".
+  std::string header_value = access_token.value("token_type", "");
+  header_value += ' ';
+  header_value += access_token.value("access_token", "");
+  auto expires_in =
+      std::chrono::seconds(access_token.value("expires_in", int(0)));
+  auto new_expiration = now + expires_in;
+  return RefreshingCredentialsWrapper::TemporaryToken{
+      std::make_pair("Authorization", header_value), new_expiration};
+}
+
+ServiceAccountCredentials::ServiceAccountCredentials(
+    ServiceAccountCredentialsInfo info, Options options,
+    std::unique_ptr<rest_internal::RestClient> rest_client,
+    CurrentTimeFn current_time_fn)
+    : info_(std::move(info)),
+      current_time_fn_(std::move(current_time_fn)),
+      rest_client_(std::move(rest_client)),
+      options_(std::move(options)) {
+  if (!rest_client_) {
+    rest_client_ =
+        rest_internal::GetDefaultRestClient(info_.token_uri, options_);
+  }
+}
+
+StatusOr<std::pair<std::string, std::string>>
+ServiceAccountCredentials::AuthorizationHeader() {
+  std::unique_lock<std::mutex> lock(mu_);
+  return refreshing_creds_.AuthorizationHeader(current_time_fn_(),
+                                               [this] { return Refresh(); });
+}
+
+StatusOr<std::vector<std::uint8_t>> ServiceAccountCredentials::SignBlob(
+    std::string const& signing_account, std::string const& blob) const {
+  if (signing_account != info_.client_email) {
+    return Status(
+        StatusCode::kInvalidArgument,
+        "The current_credentials cannot sign blobs for " + signing_account);
+  }
+  return internal::SignUsingSha256(blob, info_.private_key);
+}
+
+StatusOr<RefreshingCredentialsWrapper::TemporaryToken>
+ServiceAccountCredentials::Refresh() {
+  rest_internal::RestRequest request;
+  std::pair<std::string, std::string> grant_type(
+      "grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
+  auto payload =
+      CreateServiceAccountRefreshPayload(info_, grant_type, current_time_fn_());
+  StatusOr<std::unique_ptr<rest_internal::RestResponse>> response =
+      rest_client_->Post(request, payload);
+  if (!response.ok()) return std::move(response).status();
+  std::unique_ptr<rest_internal::RestResponse> real_response =
+      std::move(response.value());
+  if (real_response->StatusCode() >= 300)
+    return AsStatus(std::move(*real_response));
+  return ParseServiceAccountRefreshResponse(*real_response, current_time_fn_());
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace oauth2_internal
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/internal/oauth2_service_account_credentials.h
@@ -0,0 +1,168 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_SERVICE_ACCOUNT_CREDENTIALS_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_SERVICE_ACCOUNT_CREDENTIALS_H
+
+#include "google/cloud/internal/oauth2_credential_constants.h"
+#include "google/cloud/internal/oauth2_credentials.h"
+#include "google/cloud/internal/oauth2_refreshing_credentials_wrapper.h"
+#include "google/cloud/internal/rest_client.h"
+#include "google/cloud/optional.h"
+#include "google/cloud/status_or.h"
+#include "google/cloud/version.h"
+#include "absl/types/optional.h"
+#include <chrono>
+#include <mutex>
+#include <string>
+#include <vector>
+
+namespace google {
+namespace cloud {
+namespace oauth2_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+/// Object to hold information used to instantiate an ServiceAccountCredentials.
+struct ServiceAccountCredentialsInfo {
+  std::string client_email;
+  std::string private_key_id;
+  std::string private_key;
+  std::string token_uri;
+  // If no set is supplied, a default set of scopes will be used.
+  absl::optional<std::set<std::string>> scopes;
+  // See https://developers.google.com/identity/protocols/OAuth2ServiceAccount.
+  absl::optional<std::string> subject;
+};
+
+/// Parses the contents of a JSON keyfile into a ServiceAccountCredentialsInfo.
+StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountCredentials(
+    std::string const& content, std::string const& source,
+    std::string const& default_token_uri = GoogleOAuthRefreshEndpoint());
+
+/// Parses a refresh response JSON string and uses the current time to create a
+/// TemporaryToken.
+StatusOr<RefreshingCredentialsWrapper::TemporaryToken>
+ParseServiceAccountRefreshResponse(rest_internal::RestResponse& response,
+                                   std::chrono::system_clock::time_point now);
+
+/**
+ * Splits a ServiceAccountCredentialsInfo into header and payload components
+ * and uses the current time to make a JWT assertion.
+ *
+ * @see
+ * https://cloud.google.com/endpoints/docs/frameworks/java/troubleshoot-jwt
+ *
+ * @see https://tools.ietf.org/html/rfc7523
+ */
+std::pair<std::string, std::string> AssertionComponentsFromInfo(
+    ServiceAccountCredentialsInfo const& info,
+    std::chrono::system_clock::time_point now);
+
+/**
+ * Given a key and a JSON header and payload, creates a JWT assertion string.
+ *
+ * @see https://tools.ietf.org/html/rfc7519
+ */
+std::string MakeJWTAssertion(std::string const& header,
+                             std::string const& payload,
+                             std::string const& pem_contents);
+
+/// Uses a ServiceAccountCredentialsInfo and the current time to construct a
+/// JWT assertion. The assertion combined with the grant type is used to create
+/// the refresh payload.
+std::vector<std::pair<std::string, std::string>>
+CreateServiceAccountRefreshPayload(
+    ServiceAccountCredentialsInfo const& info,
+    std::pair<std::string, std::string> grant_type,
+    std::chrono::system_clock::time_point now);
+
+/**
+ * Wrapper class for Google OAuth 2.0 service account credentials.
+ *
+ * Takes a ServiceAccountCredentialsInfo and obtains access tokens from the
+ * Google Authorization Service as needed.  Instances of this class should
+ * usually be created via the convenience methods declared in
+ * google/cloud/credentials.h.
+ *
+ * An HTTP Authorization header, with an access token as its value,
+ * can be obtained by calling the AuthorizationHeader() method; if the current
+ * access token is invalid or nearing expiration, this will class will first
+ * obtain a new access token before returning the Authorization header string.
+
+ * @see https://developers.google.com/identity/protocols/OAuth2ServiceAccount
+ * for an overview of using service accounts with Google's OAuth 2.0 system.
+ *
+ * @see https://cloud.google.com/storage/docs/reference/libraries for details on
+ * how to obtain and get started with service account credentials.
+ */
+class ServiceAccountCredentials : public oauth2_internal::Credentials {
+ public:
+  using CurrentTimeFn =
+      std::function<std::chrono::time_point<std::chrono::system_clock>()>;
+
+  /**
+   * Creates an instance of ServiceAccountCredentials.
+   *
+   * @param rest_client a dependency injection point. It makes it possible to
+   *     mock internal REST types. This should generally not be overridden
+   *     except for testing.
+   * @param current_time_fn a dependency injection point to fetch the current
+   *     time. This should generally not be overridden except for testing.
+   */
+  explicit ServiceAccountCredentials(
+      ServiceAccountCredentialsInfo info, Options options = {},
+      std::unique_ptr<rest_internal::RestClient> rest_client = nullptr,
+      CurrentTimeFn current_time_fn = std::chrono::system_clock::now);
+
+  /**
+   * Returns a key value pair for an "Authorization" header.
+   */
+  StatusOr<std::pair<std::string, std::string>> AuthorizationHeader() override;
+
+  /**
+   * Create a RSA SHA256 signature of the blob using the Credential object.
+   *
+   * @param signing_account the desired service account which should sign
+   *   @p blob. If not set, uses this object's account. If set, it must match
+   *   this object's service account.
+   * @param blob the string to sign. Note that sometimes the application must
+   *   Base64-encode the data before signing.
+   * @return the signed blob as raw bytes. An error if the @p signing_account
+   *     does not match the email for the credential's account.
+   */
+  StatusOr<std::vector<std::uint8_t>> SignBlob(
+      std::string const& signing_account,
+      std::string const& blob) const override;
+
+  std::string AccountEmail() const override { return info_.client_email; }
+
+  std::string KeyId() const override { return info_.private_key_id; }
+
+ private:
+  StatusOr<RefreshingCredentialsWrapper::TemporaryToken> Refresh();
+
+  ServiceAccountCredentialsInfo info_;
+  CurrentTimeFn current_time_fn_;
+  std::unique_ptr<rest_internal::RestClient> rest_client_;
+  Options options_;
+  mutable std::mutex mu_;
+  RefreshingCredentialsWrapper refreshing_creds_;
+};
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace oauth2_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_SERVICE_ACCOUNT_CREDENTIALS_H
