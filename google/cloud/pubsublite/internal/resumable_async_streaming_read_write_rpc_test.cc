14a7e9df3b4073f502b7e1d79802bc1d3aa72161
Adarsh Suresh <adarshsuresh@google.com>
2022-02-22 22:01:23 +0000

    initializer fail then good

--- google/cloud/pubsublite/internal/resumable_async_streaming_read_write_rpc_test.cc
+++ google/cloud/pubsublite/internal/resumable_async_streaming_read_write_rpc_test.cc
@@ -279,6 +279,70 @@ TEST_F(ResumableAsyncReadWriteStreamingRpcTestBase,
   finish.get();
 }
 
+TEST_F(ResumableAsyncReadWriteStreamingRpcTestBase,
+       InitializerFailureThenGood) {
+  InSequence seq;
+
+  auto stream1 = absl::make_unique<StrictMock<AsyncReaderWriter>>();
+  auto& stream1_ref = *stream1;
+
+  auto backoff_policy = absl::make_unique<StrictMock<MockBackoffPolicy>>();
+  auto& backoff_policy_ref = *backoff_policy;
+  EXPECT_CALL(*backoff_policy_, clone)
+      .WillOnce(Return(ByMove(std::move(backoff_policy))));
+
+  auto mock_retry_policy = absl::make_unique<StrictMock<MockRetryPolicy>>();
+  auto& retry_policy_ref = *mock_retry_policy;
+  EXPECT_CALL(retry_policy_factory_, Call)
+      .WillOnce(Return(ByMove(std::move(mock_retry_policy))));
+
+  EXPECT_CALL(stream_factory_, Call)
+      .WillOnce(Return(ByMove(std::move(stream1))));
+
+  EXPECT_CALL(stream1_ref, Start)
+      .WillOnce(Return(ByMove(make_ready_future(true))));
+  promise<StatusOr<AsyncReadWriteStreamReturnType>> initializer_promise;
+  EXPECT_CALL(initializer_, Call)
+      .WillOnce(Return(ByMove(initializer_promise.get_future())));
+
+  auto status_future = stream_->Start();
+
+  EXPECT_CALL(retry_policy_ref, IsExhausted).WillOnce(Return(false));
+  EXPECT_CALL(retry_policy_ref, OnFailure(kFailStatus)).WillOnce(Return(true));
+  EXPECT_CALL(backoff_policy_ref, OnCompletion)
+      .WillOnce(Return(std::chrono::milliseconds(7)));
+  EXPECT_CALL(sleeper_, Call).WillOnce(ZeroSleep());
+
+  auto stream2 = absl::make_unique<StrictMock<AsyncReaderWriter>>();
+  auto& stream2_ref = *stream2;
+  EXPECT_CALL(stream_factory_, Call)
+      .WillOnce(Return(ByMove(std::move(stream2))));
+  EXPECT_CALL(stream2_ref, Start)
+      .WillOnce(Return(ByMove(make_ready_future(true))));
+
+  EXPECT_CALL(initializer_, Call).WillOnce(InitializeInline());
+
+  initializer_promise.set_value(
+      StatusOr<AsyncReadWriteStreamReturnType>(kFailStatus));
+
+  EXPECT_CALL(stream2_ref, Read)
+      .WillOnce(Return(
+          ByMove(make_ready_future(absl::make_optional(kBasicResponse)))));
+  auto response0 = stream_->Read().get();
+  ASSERT_TRUE(response0.has_value());
+  EXPECT_EQ(*response0, kBasicResponse);
+  EXPECT_EQ(
+      status_future.wait_for(ms(kFutureWaitMs)),
+      std::future_status::timeout);  // start future doesn't finish until
+                                     // permanent error or `Finish`is called
+
+  EXPECT_CALL(stream2_ref, Finish)
+      .WillOnce(Return(ByMove(make_ready_future(Status()))));
+  auto finish = stream_->Finish();
+  finish.get();
+  EXPECT_THAT(status_future.get(), IsOk());
+}
+
 class ResumableAsyncReadWriteStreamingRpcTest
     : public ResumableAsyncReadWriteStreamingRpcTestBase {
  protected:
