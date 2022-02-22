a8c0964a6d905115864f7fc2946580ece1948989
Carlos O'Ryan <coryan@google.com>
2022-02-22 23:11:26 +0000

    doc(storage): improve `{Bucket,Object}Metadata` docs
    The documentation was not rendering correctly. It was reusing the
    description from one function to many unrelated functions. In addition,
    the groups were also rendering incorrectly.
    
    After re-reading the Doxygen documentation it seems that
    
    ```cc
    /// @name Group description
    ///@{
    ... grouped member functions
    ///@}
    ```
    
    Is the "Right Thing":tm:

--- google/cloud/storage/object_metadata.h
+++ google/cloud/storage/object_metadata.h
@@ -88,6 +88,13 @@ inline bool operator>=(CustomerEncryption const& lhs,
 
 /**
  * Represents the metadata for a Google Cloud Storage Object.
+ *
+ * Note that all modifiers just change the local representation of the Object's
+ * metadata.  Applications should use `Client::PatchObject()`, or a similar
+ * operation, to actually modify the metadata stored by GCS.
+ *
+ * @see https://cloud.google.com/storage/docs/json_api/v1/objects for a more
+ *     detailed description of each attribute and their effects.
  */
 class ObjectMetadata : private internal::CommonMetadata<ObjectMetadata> {
  public:
@@ -95,78 +102,149 @@ class ObjectMetadata : private internal::CommonMetadata<ObjectMetadata> {
 
   // Please keep these in alphabetical order, that make it easier to verify we
   // have actually implemented all of them.
+
+  /// The access control list for this object.
   std::vector<ObjectAccessControl> const& acl() const { return acl_; }
+
+  /// The access control list for this object.
   std::vector<ObjectAccessControl>& mutable_acl() { return acl_; }
+
+  /// Change the access control list.
   ObjectMetadata& set_acl(std::vector<ObjectAccessControl> acl) {
     acl_ = std::move(acl);
     return *this;
   }
 
+  /// The name of the bucket containing this object.
   std::string const& bucket() const { return bucket_; }
 
+  /// The cacheControl attribute.
   std::string const& cache_control() const { return cache_control_; }
+
+  /// Set the cacheControl attributes.
   ObjectMetadata& set_cache_control(std::string cache_control) {
     cache_control_ = std::move(cache_control);
     return *this;
   }
 
+  /// The number of components, for objects built using `ComposeObject()`.
   std::int32_t component_count() const { return component_count_; }
 
+  /// The `contentDisposition` attribute.
   std::string content_disposition() const { return content_disposition_; }
+
+  /// Change the `contentDisposition` attribute.
   ObjectMetadata& set_content_disposition(std::string value) {
     content_disposition_ = std::move(value);
     return *this;
   }
 
+  /// The `contentEncoding` attribute.
   std::string content_encoding() const { return content_encoding_; }
+
+  /// Change the `contentEncoding` attribute.
   ObjectMetadata& set_content_encoding(std::string value) {
     content_encoding_ = std::move(value);
     return *this;
   }
 
+  /// The `contentLanguage` attribute.
   std::string content_language() const { return content_language_; }
+
+  /// Change the `contentLanguage` attribute.
   ObjectMetadata& set_content_language(std::string value) {
     content_language_ = std::move(value);
     return *this;
   }
 
+  /// The `contentType` attribute.
   std::string content_type() const { return content_type_; }
+
+  /// Change the `contentLanguage` attribute.
   ObjectMetadata& set_content_type(std::string value) {
     content_type_ = std::move(value);
     return *this;
   }
 
+  /// The `CRC32C` checksum for the object contents.
   std::string const& crc32c() const { return crc32c_; }
 
+  /// Returns `true` if the object uses CSEK (Customer-Supplied Encryption
+  /// Keys).
   bool has_customer_encryption() const {
     return customer_encryption_.has_value();
   }
+
+  /**
+   * Returns the CSEK metadata (algorithm and key SHA256).
+   *
+   * It is undefined behavior to call this member function if
+   * `has_customer_encryption() == false`.
+   */
   CustomerEncryption const& customer_encryption() const {
     return customer_encryption_.value();
   }
 
+  /// The `Etag` attribute.
   using CommonMetadata::etag;
 
+  /// The `eventBasedHold` attribute.
   bool event_based_hold() const { return event_based_hold_; }
+
+  /// Changes the `eventBasedHold` attribute.
   ObjectMetadata& set_event_based_hold(bool v) {
     event_based_hold_ = v;
     return *this;
   }
 
+  /**
+   * The object generation.
+   *
+   * In buckets with object versioning enabled, each object may have multiple
+   * generations. Each generation data (the object contents) is immutable, but
+   * the metadata associated with each generation can be changed.
+   */
   std::int64_t generation() const { return generation_; }
 
+  /// The `id` attribute (the object name)
   using CommonMetadata::id;
+
+  /// The `kind` attribute, that is, `storage#object`.
   using CommonMetadata::kind;
 
+  /**
+   * The name of the KMS (Key Management Service) key used in this object.
+   *
+   * This is empty for objects not using CMEK (Customer Managed Encryption
+   * Keys).
+   */
   std::string const& kms_key_name() const { return kms_key_name_; }
+
+  /// The MD5 hash of the object contents. Can be empty.
   std::string const& md5_hash() const { return md5_hash_; }
+
+  /// The HTTPS link to access the object contents.
   std::string const& media_link() const { return media_link_; }
 
-  //@{
-  /// @name Accessors and modifiers to the `metadata` labels.
+  /**
+   * @name Accessors and modifiers for metadata metadata.
+   *
+   * The object metadata contains a user-defined set of `key`, `value` pairs,
+   * which are also called "metadata". Applications can use these fields to
+   * add custom annotations to each object.
+   */
+  ///@{
+  /// Returns `true` if the key is present in the object metadata entries.
   bool has_metadata(std::string const& key) const {
     return metadata_.end() != metadata_.find(key);
   }
+
+  /**
+   * Returns the value of @p key in the Object's metadata entries.
+   *
+   * It is undefined behavior to call `metadata(key)` if `has_metadata(key) ==
+   * false`.
+   */
   std::string const& metadata(std::string const& key) const {
     return metadata_.at(key);
   }
@@ -192,56 +270,99 @@ class ObjectMetadata : private internal::CommonMetadata<ObjectMetadata> {
     return *this;
   }
 
+  /// Returns all the Object's metadata entries,
   std::map<std::string, std::string> const& metadata() const {
     return metadata_;
   }
-  std::map<std::string, std::string>& mutable_metadata() { return metadata_; }
-  //@}
 
+  /// Returns all the Object's metadata entries.
+  std::map<std::string, std::string>& mutable_metadata() { return metadata_; }
+  ///@}
+
+  /// Returns `true` if the object has a `owner` attributes.
   using CommonMetadata::has_owner;
+
+  /**
+   * The generation of the object metadata.
+   *
+   * Note that changes to the object metadata (e.g. changing the `cacheControl`
+   * attribute) increases the metageneration, but does not change the object
+   * generation.
+   */
   using CommonMetadata::metageneration;
+
+  /// The object name, including bucket and generation.
   using CommonMetadata::name;
+
+  /**
+   * The object's owner attributes.
+   *
+   * It is undefined behavior to call this member function if
+   * `has_owner() == false`.
+   */
   using CommonMetadata::owner;
 
+  /// The retention expiration time, or the system clock's epoch, if not set.
   std::chrono::system_clock::time_point retention_expiration_time() const {
     return retention_expiration_time_;
   }
 
+  /// An HTTPS link to the object metadata.
   using CommonMetadata::self_link;
 
+  /// The size of the object's data.
   std::uint64_t size() const { return size_; }
 
+  /// The `storageClass` attribute.
   using CommonMetadata::storage_class;
+
+  /// Changes the `storageClass` attribute.
   ObjectMetadata& set_storage_class(std::string v) {
     CommonMetadata::set_storage_class(std::move(v));
     return *this;
   }
 
+  /// The `temporaryHold` attribute.
   bool temporary_hold() const { return temporary_hold_; }
+
+  /// Changes the `temporaryHold` attribute.
   ObjectMetadata& set_temporary_hold(bool v) {
     temporary_hold_ = v;
     return *this;
   }
 
+  /// The object creation timestamp.
   using CommonMetadata::time_created;
 
+  /// The object's deletion timestamp.
   std::chrono::system_clock::time_point time_deleted() const {
     return time_deleted_;
   }
+
+  /// The timestamp for the last storage class change.
   std::chrono::system_clock::time_point time_storage_class_updated() const {
     return time_storage_class_updated_;
   }
 
+  /// The timestamp for the last object *metadata* update.
   using CommonMetadata::updated;
 
+  /// Returns `true` if the object has a `customTime` attribute.
   bool has_custom_time() const { return custom_time_.has_value(); }
+
+  /// Returns the object's `customTime` or the system clock's epoch.
   std::chrono::system_clock::time_point custom_time() const {
     return custom_time_.value_or(std::chrono::system_clock::time_point{});
   }
+
+  /// Changes the `customTime` attribute.
   ObjectMetadata& set_custom_time(std::chrono::system_clock::time_point v) {
     custom_time_ = v;
     return *this;
   }
+
+  /// Reset (clears) the `customTime` attribute. `has_custom_time()` returns
+  /// `false` after calling this function.
   ObjectMetadata& reset_custom_time() {
     custom_time_.reset();
     return *this;
