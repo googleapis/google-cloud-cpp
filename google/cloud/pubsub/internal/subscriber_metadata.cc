e15d76976dec7b25837c682f9f4364c6463242c2
Carlos O'Ryan <coryan@google.com>
2022-02-25 21:15:18 +0000

    feat(pubsub): support `AuthorityOption`
    By default use `pubsub.googleapis.com` as the `authority` in all
    requests, regardless of the `EndpointOption` value.  This improves the
    support for PSC and VPC-SC.

--- google/cloud/pubsub/internal/subscriber_metadata.cc
+++ google/cloud/pubsub/internal/subscriber_metadata.cc
@@ -147,6 +147,9 @@ void SubscriberMetadata::SetMetadata(grpc::ClientContext& context,
     context.AddMetadata("x-goog-user-project",
                         options.get<UserProjectOption>());
   }
+  if (options.has<AuthorityOption>()) {
+    context.set_authority(options.get<AuthorityOption>());
+  }
 }
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
