b334ef837f0cf48d8e705b1af783c3670acb95ea
Bradley White <14679271+devbww@users.noreply.github.com>
2022-04-01 03:48:31 -0400

    ci(spanner): temporarily tolerate another failure in CreateBackup()
    Tolerate (cleanup and skip) `CreateBackup()` integration tests that fail
    with "terminated by polling policy" deadline-exceeded errors, until #8616
    can be resolved.

--- google/cloud/spanner/integration_tests/backup_extra_integration_test.cc
+++ google/cloud/spanner/integration_tests/backup_extra_integration_test.cc
@@ -367,6 +367,17 @@ TEST_F(BackupExtraIntegrationTest, BackupRestoreWithCMEK) {
       GTEST_SKIP();
     }
   }
+  {
+    // TODO(#8616): Remove this when we know how to deal with the issue.
+    auto matcher = testing_util::StatusIs(
+        StatusCode::kDeadlineExceeded,
+        testing::HasSubstr("terminated by polling policy"));
+    testing::StringMatchResultListener listener;
+    if (matcher.impl().MatchAndExplain(backup, &listener)) {
+      EXPECT_STATUS_OK(database_admin_client_.DropDatabase(db));
+      GTEST_SKIP();
+    }
+  }
   ASSERT_STATUS_OK(backup);
   EXPECT_TRUE(backup->has_encryption_info());
   if (backup->has_encryption_info()) {
