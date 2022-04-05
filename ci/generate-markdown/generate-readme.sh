81edf1124857459f82b5d36188cb2db0b0acff8b
Carlos O'Ryan <coryan@google.com>
2022-04-05 06:39:24 -0700

    doc: improve GitHub pages (#8688)

--- ci/generate-markdown/generate-readme.sh
+++ ci/generate-markdown/generate-readme.sh
@@ -16,15 +16,22 @@
 
 set -euo pipefail
 
+if ! type sponge >/dev/null 2>&1; then
+  echo "This script requires sponge(1) to produce its output."
+  echo "Please install this tool and try again."
+  exit 1
+fi
+
+file="README.md"
 (
-  sed '/<!-- inject-quickstart-start -->/q' "README.md"
+  sed '/<!-- inject-quickstart-start -->/q' "${file}"
   echo '```cc'
   # Dumps the contents of quickstart.cc starting at the first #include, so we
   # skip the license header comment.
   sed -n -e '/END .*quickstart/,$d' -e '/^#/,$p' "google/cloud/storage/quickstart/quickstart.cc"
   echo '```'
-  sed -n '/<!-- inject-quickstart-end -->/,$p' "README.md"
-) >"README.md.tmp"
+  sed -n '/<!-- inject-quickstart-end -->/,$p' "${file}"
+) | sponge "${file}"
 
 (
   mapfile -t libraries < <(bazelisk --batch query \
@@ -32,13 +39,12 @@ set -euo pipefail
     'kind(cc_library, //:all) except filter("experimental|mocks|common|grpc_utils", kind(cc_library, //:all))' |
     sed -e 's;//:;;' |
     LC_ALL=C sort)
-  sed '/<!-- inject-GA-libraries-start -->/q' "README.md.tmp"
+  sed '/<!-- inject-GA-libraries-start -->/q' "${file}"
   for library in "${libraries[@]}"; do
     description="$(sed -n '1 s/# \(.*\) C++ Client Library/\1/p' "google/cloud/${library}/README.md")"
-    printf '* [%s](google/cloud/%s/README.md) [[quickstart]](google/cloud/%s/quickstart/README.md)\n' \
-      "${description}" "${library}" "${library}"
+    printf '* [%s](google/cloud/%s/README.md)\n' "${description}" "${library}"
+    printf '  [[quickstart]](google/cloud/%s/quickstart/README.md)\n' "${library}"
+    printf '  [[reference]](https://googleapis.dev/cpp/google-cloud-%s/latest)\n' "${library}"
   done
-  sed -n '/<!-- inject-GA-libraries-end -->/,$p' "README.md.tmp"
-) >"README.md"
-
-rm README.md.tmp
+  sed -n '/<!-- inject-GA-libraries-end -->/,$p' "${file}"
+) | sponge "${file}"
