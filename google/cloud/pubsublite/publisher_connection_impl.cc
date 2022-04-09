bc38349c86215d59f791c7cef452b63c104126fd
Adarsh Suresh <adarshsuresh@google.com>
2022-04-09 17:02:54 +0000

    get

--- google/cloud/pubsublite/publisher_connection_impl.cc
+++ google/cloud/pubsublite/publisher_connection_impl.cc
@@ -33,8 +33,7 @@ PublisherConnectionImpl::PublisherConnectionImpl(
 }
 
 PublisherConnectionImpl::~PublisherConnectionImpl() {
-  // isn't safe b/c object should only be destroyed after future is satisfied
-  service_composite_.Shutdown();
+  service_composite_.Shutdown().get();
 }
 
 future<StatusOr<std::string>> PublisherConnectionImpl::Publish(
