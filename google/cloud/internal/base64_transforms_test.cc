fdb3cfd47b7b4f4ac22c3a8f916caff168fab355
Adarsh Suresh <adarshsuresh@google.com>
2022-04-14 00:04:00 +0000

    add test

--- google/cloud/internal/base64_transforms_test.cc
+++ google/cloud/internal/base64_transforms_test.cc
@@ -104,12 +104,15 @@ TEST(Base64, WikiExample) {
 
   Base64Encoder enc;
   for (auto c : plain) enc.PushBack(c);
-  auto const actual = std::move(enc).FlushAndPad();
+  auto actual = std::move(enc).FlushAndPad();
   EXPECT_EQ(actual, expected);
   EXPECT_STATUS_OK(ValidateBase64String(actual));
   Base64Decoder dec(actual);
   auto const decoded = std::string{dec.begin(), dec.end()};
   EXPECT_EQ(plain, decoded);
+  // test PushBack(absl::string_view)
+  enc = Base64Encoder{};
+  enc.PushBack(plain);
 }
 
 TEST(Base64, ValidateBase64StringFailures) {
