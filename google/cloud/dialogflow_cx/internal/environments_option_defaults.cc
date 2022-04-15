0a0fb4ef548164f91d8f48da3eef44c7c79d3878
Darren Bolduc <dbolduc@google.com>
2022-04-15 11:23:48 -0400

    cleanup(dialogflow): endpoint env vars (#8763)

--- google/cloud/dialogflow_cx/internal/environments_option_defaults.cc
+++ google/cloud/dialogflow_cx/internal/environments_option_defaults.cc
@@ -34,8 +34,8 @@ auto constexpr kBackoffScaling = 2.0;
 
 Options EnvironmentsDefaultOptions(Options options) {
   options = google::cloud::internal::PopulateCommonOptions(
-      std::move(options), "GOOGLE_CLOUD_CPP_ENVIRONMENTS_ENDPOINT", "",
-      "dialogflow.googleapis.com");
+      std::move(options), "GOOGLE_CLOUD_CPP_DIALOGFLOW_ENVIRONMENT_ENDPOINT",
+      "", "dialogflow.googleapis.com");
   options =
       google::cloud::internal::PopulateGrpcOptions(std::move(options), "");
   if (!options.has<dialogflow_cx::EnvironmentsRetryPolicyOption>()) {
