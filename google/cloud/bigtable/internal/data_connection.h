5762ac5415043aac6566ecd314aaa34091cae96e
dbolduc <dbolduc@google.com>
2022-05-03 15:23:00 -0400

    address review comments

--- google/cloud/bigtable/internal/data_connection.h
+++ google/cloud/bigtable/internal/data_connection.h
@@ -30,6 +30,17 @@ namespace cloud {
 namespace bigtable_internal {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
+/**
+ * A connection to the Cloud Bigtable Data API.
+ *
+ * This interface defines pure-virtual methods for each of the user-facing
+ * overload sets in `Table`. This allows users to inject custom behavior (e.g.,
+ * with a Google Mock object) in a `Table` object for use in their own tests.
+ *
+ * To create a concrete instance, see `MakeDataConnection()`.
+ *
+ * For mocking, see `bigtable_mocks::MockDataConnection`.
+ */
 class DataConnection {
  public:
   virtual ~DataConnection() = 0;
@@ -37,6 +48,30 @@ class DataConnection {
   virtual Options options() { return Options{}; }
 };
 
+/**
+ * Returns a `DataConnection` object that can be used for interacting with
+ * the Cloud Bigtable Data API.
+ *
+ * The returned connection object should not be used directly; instead it
+ * should be given to a `Table` instance, and methods should be invoked on
+ * `Table`.
+ *
+ * The optional @p opts argument may be used to configure aspects of the
+ * returned `DataConnection`. Expected options are any of the types in the
+ * following option lists.
+ *
+ * - `google::cloud::CommonOptionList`
+ * - `google::cloud::GrpcOptionList`
+ * - `google::cloud::bigtable::ClientOptionsList`
+ * - `google::cloud::bigtable::DataPolicyOptionList`
+ *
+ * @note Unrecognized options will be ignored. To debug issues with options set
+ *     `GOOGLE_CLOUD_CPP_ENABLE_CLOG=yes` in the environment and unexpected
+ *     options will be logged.
+ *
+ * @param opts (optional) Configure the `DataConnection` created by this
+ * function.
+ */
 std::shared_ptr<DataConnection> MakeDataConnection(Options options = {});
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
