abcdd093507322d1f2e8b50a5badb95bc852bc0c
dbolduc <dbolduc@google.com>
2022-05-16 18:31:16 +0000

    Run generators and format their outputs

---
+++ google/cloud/bms/quickstart/quickstart.cc
@@ -0,0 +1,39 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/bms/ EDIT HERE .h"
+#include "google/cloud/project.h"
+#include <iostream>
+#include <stdexcept>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
+    return 1;
+  }
+
+  namespace bms = ::google::cloud::bms;
+  auto client = bms::Client(bms::MakeConnection(/* EDIT HERE */));
+
+  auto const project = google::cloud::Project(argv[1]);
+  for (auto r : client.List /*EDIT HERE*/ (project.FullName())) {
+    if (!r) throw std::runtime_error(r.status().message());
+    std::cout << r->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (std::exception const& ex) {
+  std::cerr << "Standard exception raised: " << ex.what() << "\n";
+  return 1;
+}
