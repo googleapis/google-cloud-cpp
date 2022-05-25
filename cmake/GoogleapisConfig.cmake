c2dbd4089ab26a1618656a313c97e0564e5229b2
Carlos O'Ryan <coryan@google.com>
2022-05-25 12:32:39 +0000

    chore: update googleapis SHA circa 2022-05-25

--- cmake/GoogleapisConfig.cmake
+++ cmake/GoogleapisConfig.cmake
@@ -17,11 +17,11 @@
 # Give application developers a hook to configure the version and hash
 # downloaded from GitHub.
 set(GOOGLE_CLOUD_CPP_GOOGLEAPIS_COMMIT_SHA
-    "cfe613f3dd717b137f2384b2a1a685c2f48f1d05"
+    "02cdbfd362275b3573184c41cd76f6924954bc36"
     CACHE STRING "Configure the commit SHA (or tag) for the googleapis protos.")
 mark_as_advanced(GOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA)
 set(GOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA256
-    "176247ca11c90f41be1267dffe90a5e2a5d8f232de2c56bbc24373c2d52a79a2"
+    "6c29b05c3f42a778a6b0e2b04a01f2c39c85cc2264ab0df31f89f2d39da5ca67"
     CACHE STRING "Configure the SHA256 checksum of the googleapis tarball.")
 mark_as_advanced(GOOGLE_CLOUD_CPP_GOOGLEAPIS_SHA256)
 
