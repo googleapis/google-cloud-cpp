16e8cc929eaa6c58e3b2e3c2a1c894dbc5f2dc19
Carlos O'Ryan <coryan@google.com>
2022-05-27 17:57:37 +0000

    Address review comments

--- ci/cloudbuild/builds/coverage.sh
+++ ci/cloudbuild/builds/coverage.sh
@@ -83,14 +83,11 @@ io::log_h2 "Uploading ${MERGED_COVERAGE} to codecov.io"
 io::log "Flags: ${codecov_args[*]}"
 TIMEFORMAT="==> 🕑 codecov.io upload done in %R seconds"
 time {
-  # Verifies the codecov bash uploader before executing it.
+  # Verifies the codecov uploader before executing it.
   curl -sSL -o /var/tmp/codecov https://uploader.codecov.io/v0.2.3/linux/codecov
   sha256sum="648b599397548e4bb92429eec6391374c2cbb0edb835e3b3f03d4281c011f401"
-  codecov_url="https://raw.githubusercontent.com/codecov/codecov-bash/1b4b96ac38946b20043b3ca3bad88d95462259b6/codecov"
-  codecov_script="$(curl -s "${codecov_url}")"
   if ! sha256sum -c <(echo "${sha256sum} -") </var/tmp/codecov; then
-    io::log_h2 "ERROR: Invalid sha256sum for codecov_script:"
-    echo "${codecov_script}"
+    io::log_h2 "ERROR: Invalid sha256sum for codecov program"
     exit 1
   fi
   chmod +x /var/tmp/codecov
