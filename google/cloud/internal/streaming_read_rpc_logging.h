8fc8cf281d3b837afe25550af42c32acecab33e0
Bradley White <14679271+devbww@users.noreply.github.com>
2022-06-07 22:41:38 -0400

    cleanup(common): correct the direction on some request/response logging
    Our logging convention is "func() << request" and "func() >> response".

--- google/cloud/internal/streaming_read_rpc_logging.h
+++ google/cloud/internal/streaming_read_rpc_logging.h
@@ -49,13 +49,13 @@ class StreamingReadRpcLogging : public StreamingReadRpc<ResponseType> {
 
   void Cancel() override {
     auto const prefix = std::string(__func__) + "(" + request_id_ + ")";
-    GCP_LOG(DEBUG) << prefix << "() >> (void)";
+    GCP_LOG(DEBUG) << prefix << "() << (void)";
     reader_->Cancel();
     GCP_LOG(DEBUG) << prefix << "() >> (void)";
   }
   absl::variant<Status, ResponseType> Read() override {
     auto const prefix = std::string(__func__) + "(" + request_id_ + ")";
-    GCP_LOG(DEBUG) << prefix << "() >> (void)";
+    GCP_LOG(DEBUG) << prefix << "() << (void)";
     auto result = reader_->Read();
     GCP_LOG(DEBUG) << prefix << "() >> "
                    << absl::visit(ResultVisitor(tracing_options_), result);
