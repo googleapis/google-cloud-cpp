0d305f01148aa103d1b79484d1403d108f2f1cb9
Carlos O'Ryan <coryan@google.com>
2022-06-13 17:55:09 +0000

    feat(GCS+gRPC): per-operation options for `AsyncClient`

--- google/cloud/storage/async_client.h
+++ google/cloud/storage/async_client.h
@@ -16,6 +16,7 @@
 #define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_ASYNC_CLIENT_H
 
 #include "google/cloud/storage/internal/async_connection.h"
+#include "google/cloud/storage/internal/group_options.h"
 #include "google/cloud/storage/internal/object_requests.h"
 #include "google/cloud/storage/version.h"
 #include "google/cloud/background_threads.h"
@@ -28,6 +29,78 @@ GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
 /**
  * A client for Google Cloud Storage offering asynchronous operations.
+ *
+ * @note This class is experimental, it is subject to change without notice.
+ *
+ * @par Optional Request Options
+ * Most of the member functions in this class can receive optional request
+ * options. For example, the default when deleting an object is to delete the
+ * latest version:
+ *
+ * @code
+ * auto pending = gcs.DeleteObject("my-bucket", "my-object");
+ * @endcode
+ *
+ * Some applications may want to delete an specific versions. In this case
+ * just provide the `Generation` request option:
+ *
+ * @code
+ * auto pending = gcs.DeleteObject(
+ *     "my-bucket", "my-object", gcs::Generation(generation));
+ * @endcode
+ *
+ * Each function documents the types accepted as optional request options. These
+ * parameters can be specified in any order. Specifying a request option that is
+ * not applicable to a member function results in a compile-time error.
+ *
+ * All operations support the following common request options:
+ *
+ * - `Fields`: return a [partial response], which includes only the desired
+ *   fields.
+ * - `QuotaUser`: attribute the request to this specific label for quota
+ *   purposes.
+ * - `UserProject`: change the request costs (if applicable) to this GCP
+ *   project.
+ * - `CustomHeader`: include a custom header with the request. These are
+ *   typically used for testing, though they are sometimes helpful if
+ *   environments where HTTPS traffic is mediated by a proxy.
+ * - `UserIp`: attribute the request to this specific IP address for quota
+ *   purpose. Not recommended, prefer `QuotaUser` instead.
+ *
+ * [partial response]:
+ * https://cloud.google.com/storage/docs/json_api#partial-response
+ *
+ * @par Per-operation Overrides
+ *
+ * In addition to the request options, which are passed on to the service to
+ * modify the request, you can specify options that override the local behavior
+ * of the library.  For example, you can override the local retry policy:
+ *
+ * @code
+ * auto pending = gcs.DeleteObject(
+ *     "my-bucket", "my-object",
+ *      google::cloud::Options{}
+ *          .set<gcs::RetryPolicyOption>(
+ *              gcs::LimitedErrorCountRetryPolicy(5).clone()));
+ * @endcode
+ *
+ * @par Retry, Backoff, and Idempotency Policies
+ *
+ * The library automatically retries requests that fail with transient errors,
+ * and follows the [recommended practice][exponential-backoff] to backoff
+ * between retries.
+ *
+ * The default policies are to continue retrying for up to 15 minutes, and to
+ * use truncated (at 5 minutes) exponential backoff, doubling the maximum
+ * backoff period between retries. Likewise, the idempotency policy is
+ * configured to retry all operations.
+ *
+ * The application can override these policies when constructing objects of this
+ * class. The documentation for the constructors show examples of this in
+ * action.
+ *
+ * [exponential-backoff]:
+ * https://cloud.google.com/storage/docs/exponential-backoff
  */
 class AsyncClient {
  public:
@@ -42,6 +115,7 @@ class AsyncClient {
    *     Valid types for this operation include `Generation`,
    *     `IfGenerationMatch`, `IfGenerationNotMatch`, `IfMetagenerationMatch`,
    *     `IfMetagenerationNotMatch`, and `UserProject`.
+   *     See the class description for common request options.
    *
    * @par Idempotency
    * This operation is only idempotent if:
@@ -52,7 +126,8 @@ class AsyncClient {
   future<Status> DeleteObject(std::string const& bucket_name,
                               std::string const& object_name,
                               RequestOptions&&... options) {
-    internal::OptionsSpan span(connection_->options());
+    google::cloud::internal::OptionsSpan const span(
+        SpanOptions(std::forward<RequestOptions>(options)...));
     storage::internal::DeleteObjectRequest request(bucket_name, object_name);
     request.set_multiple_options(std::forward<RequestOptions>(options)...);
     return connection_->AsyncDeleteObject(request);
@@ -63,6 +138,12 @@ class AsyncClient {
   explicit AsyncClient(
       std::shared_ptr<google::cloud::BackgroundThreads> background,
       std::shared_ptr<storage_internal::AsyncConnection> connection);
+
+  template <typename... RequestOptions>
+  google::cloud::Options SpanOptions(RequestOptions&&... o) const {
+    return google::cloud::storage::internal::GroupOptions(
+        connection_->options(), std::forward<RequestOptions>(o)...);
+  }
 
   std::shared_ptr<google::cloud::BackgroundThreads> background_;
   std::shared_ptr<storage_internal::AsyncConnection> connection_;
