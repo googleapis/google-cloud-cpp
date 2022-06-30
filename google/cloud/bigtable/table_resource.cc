0f6e9476f079fab76919dfb48d20ee5e8b8eb784
dbolduc <dbolduc@google.com>
2022-06-30 11:29:38 -0400

    address review comments

--- google/cloud/bigtable/table_resource.cc
+++ google/cloud/bigtable/table_resource.cc
@@ -26,8 +26,9 @@ TableResource::TableResource(InstanceResource instance, std::string table_id)
 
 TableResource::TableResource(std::string project_id, std::string instance_id,
                              std::string table_id)
-    : instance_(Project(std::move(project_id)), std::move(instance_id)),
-      table_id_(std::move(table_id)) {}
+    : TableResource(InstanceResource(Project(std::move(project_id)),
+                                     std::move(instance_id)),
+                    std::move(table_id)) {}
 
 std::string TableResource::FullName() const {
   return instance_.FullName() + "/tables/" + table_id_;
