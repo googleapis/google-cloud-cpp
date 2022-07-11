6b3cc03ec1a0b773785cf3c4dcf84d5065f12fa9
Carlos O'Ryan <coryan@google.com>
2022-07-11 13:22:14 +0000

    feat(rest): reduce contention in `PooledCurlHandleFactory`
    Use separate mutexes for each component. Move as much work as possible
    outside the critical sections. And finally amortize the cost of
    releasing the handles to one thread.
    
    Of these changes the most important seems to be moving the work to
    fetch the local IP address outside the critical sections, as this may
    involve a DNS lookup. The rest are at least not harmful, and could
    help when under pressure.
    
    I added a benchmark.  I ran this to 32,768 simultaneous threads using
    the pool. It works and only takes a few milliseconds, but this is not
    a number of threads we can safely run in the CI environment.

--- google/cloud/internal/curl_handle_factory.cc
+++ google/cloud/internal/curl_handle_factory.cc
@@ -15,6 +15,8 @@
 #include "google/cloud/internal/curl_handle_factory.h"
 #include "google/cloud/credentials.h"
 #include "google/cloud/internal/curl_options.h"
+#include <algorithm>
+#include <iterator>
 
 namespace google {
 namespace cloud {
@@ -79,31 +81,23 @@ void DefaultCurlHandleFactory::SetCurlOptions(CURL* handle) {
 
 PooledCurlHandleFactory::PooledCurlHandleFactory(std::size_t maximum_size,
                                                  Options const& o)
-    : maximum_size_(maximum_size) {
-  if (o.has<CARootsFilePathOption>()) cainfo_ = o.get<CARootsFilePathOption>();
-  if (o.has<CAPathOption>()) capath_ = o.get<CAPathOption>();
-}
+    : maximum_size_(maximum_size), cainfo_(CAInfo(o)), capath_(CAPath(o)) {}
 
-PooledCurlHandleFactory::~PooledCurlHandleFactory() {
-  for (auto* h : handles_) {
-    curl_easy_cleanup(h);
-  }
-  for (auto* m : multi_handles_) {
-    curl_multi_cleanup(m);
-  }
-}
+PooledCurlHandleFactory::~PooledCurlHandleFactory() = default;
 
 CurlPtr PooledCurlHandleFactory::CreateHandle() {
-  std::unique_lock<std::mutex> lk(mu_);
+  std::unique_lock<std::mutex> lk(handles_mu_);
   if (!handles_.empty()) {
-    auto* handle = handles_.back();
-    // Clear all the options in the handle so we do not leak its previous state.
-    (void)curl_easy_reset(handle);
+    auto handle = std::move(handles_.back());
     handles_.pop_back();
-    CurlPtr curl(handle, &curl_easy_cleanup);
-    SetCurlOptions(curl.get());
-    return curl;
+    lk.unlock();
+    // Clear all the options in the handle, so we do not leak its previous
+    // state.
+    (void)curl_easy_reset(handle.get());
+    SetCurlOptions(handle.get());
+    return handle;
   }
+  lk.unlock();
   auto curl = MakeCurlPtr();
   SetCurlOptions(curl.get());
   return curl;
@@ -111,43 +105,60 @@ CurlPtr PooledCurlHandleFactory::CreateHandle() {
 
 void PooledCurlHandleFactory::CleanupHandle(CurlPtr h) {
   if (!h) return;
-  std::unique_lock<std::mutex> lk(mu_);
+  // Querying the local IP can be expensive, as it may require a DNS lookup.
+  // We should not perform such operations while holding a lock.
   char* ip;
   auto res = curl_easy_getinfo(h.get(), CURLINFO_LOCAL_IP, &ip);
   if (res == CURLE_OK && ip != nullptr) {
+    std::unique_lock<std::mutex> lk(last_client_ip_address_mu_);
     last_client_ip_address_ = ip;
   }
-  while (handles_.size() >= maximum_size_) {
-    auto* tmp = handles_.front();
-    handles_.erase(handles_.begin());
-    curl_easy_cleanup(tmp);
+  // Use a temporary data structure to release any excess handles *after* the
+  // lock is released.
+  std::vector<CurlPtr> released;
+  std::unique_lock<std::mutex> lk(handles_mu_);
+  // If needed, release several handles to make room, amortizing the cost when
+  // many threads are releasing handles at the same time.
+  if (handles_.size() >= maximum_size_) {
+    auto const keep_count = maximum_size_ / 2;
+    auto const release_count = handles_.size() - keep_count;
+    released.reserve(release_count);
+    auto const end = std::next(handles_.begin(), release_count);
+    std::move(handles_.begin(), end, std::back_inserter(released));
+    handles_.erase(handles_.begin(), end);
   }
-  handles_.push_back(h.get());
-  // The handles_ vector now has ownership, so release it.
-  (void)h.release();
+  handles_.push_back(std::move(h));
 }
 
 CurlMulti PooledCurlHandleFactory::CreateMultiHandle() {
-  std::unique_lock<std::mutex> lk(mu_);
+  std::unique_lock<std::mutex> lk(multi_handles_mu_);
   if (!multi_handles_.empty()) {
-    auto* m = multi_handles_.back();
+    auto m = std::move(multi_handles_.back());
     multi_handles_.pop_back();
-    return CurlMulti(m, &curl_multi_cleanup);
+    lk.unlock();
+    return m;
   }
+  lk.unlock();
   return CurlMulti(curl_multi_init(), &curl_multi_cleanup);
 }
 
 void PooledCurlHandleFactory::CleanupMultiHandle(CurlMulti m) {
   if (!m) return;
-  std::unique_lock<std::mutex> lk(mu_);
-  while (multi_handles_.size() >= maximum_size_) {
-    auto* tmp = multi_handles_.front();
-    multi_handles_.erase(multi_handles_.begin());
-    curl_multi_cleanup(tmp);
+  // Use a temporary data structure to release any excess handles *after* the
+  // lock is released.
+  std::vector<CurlMulti> released;
+  std::unique_lock<std::mutex> lk(multi_handles_mu_);
+  // If needed, release several handles to make room, amortizing the cost when
+  // many threads are releasing handles at the same time.
+  if (multi_handles_.size() >= maximum_size_) {
+    auto const keep_count = maximum_size_ / 2;
+    auto const release_count = multi_handles_.size() - keep_count;
+    released.reserve(release_count);
+    auto const end = std::next(multi_handles_.begin(), release_count);
+    std::move(multi_handles_.begin(), end, std::back_inserter(released));
+    multi_handles_.erase(multi_handles_.begin(), end);
   }
-  multi_handles_.push_back(m.get());
-  // The multi_handles_ vector now has ownership, so release it.
-  (void)m.release();
+  multi_handles_.push_back(std::move(m));
 }
 
 void PooledCurlHandleFactory::SetCurlOptions(CURL* handle) {
@@ -157,6 +168,16 @@ void PooledCurlHandleFactory::SetCurlOptions(CURL* handle) {
   if (capath_) {
     SetCurlStringOption(handle, CURLOPT_CAPATH, capath_->c_str());
   }
+}
+
+absl::optional<std::string> PooledCurlHandleFactory::CAInfo(Options const& o) {
+  if (!o.has<CARootsFilePathOption>()) return absl::nullopt;
+  return o.get<CARootsFilePathOption>();
+}
+
+absl::optional<std::string> PooledCurlHandleFactory::CAPath(Options const& o) {
+  if (!o.has<CAPathOption>()) return absl::nullopt;
+  return o.get<CAPathOption>();
 }
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
---
+++ google/cloud/internal/curl_handle_factory_benchmark.cc
@@ -0,0 +1,72 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/curl_handle_factory.h"
+#include <benchmark/benchmark.h>
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace {
+
+class PoolFixture : public ::benchmark::Fixture {
+ public:
+  PoolFixture() : pool_(128) {}
+
+  CurlHandleFactory& pool() { return pool_; }
+
+ private:
+  PooledCurlHandleFactory pool_;
+};
+
+bool CreateAndCleanup(CurlHandleFactory& factory, benchmark::State& state) {
+  auto h = factory.CreateHandle();
+  if (!h) ++state.counters["CreateHandleError"];
+  auto m = factory.CreateMultiHandle();
+  if (!m) ++state.counters["CreateMultiHandleError"];
+  auto const success = h && m;
+  factory.CleanupMultiHandle(std::move(m));
+  factory.CleanupHandle(std::move(h));
+  return success;
+}
+
+BENCHMARK_DEFINE_F(PoolFixture, Burst)(benchmark::State& state) {
+  for (auto _ : state) {
+    if (!CreateAndCleanup(pool(), state)) {
+      state.SkipWithError("error creating handles");
+      break;
+    }
+  }
+}
+BENCHMARK_REGISTER_F(PoolFixture, Burst)->ThreadRange(1, 1 << 8)->UseRealTime();
+
+BENCHMARK_DEFINE_F(PoolFixture, Linear)(benchmark::State& state) {
+  for (auto _ : state) {
+    for (auto i = 0; i != state.range(0); ++i) {
+      CreateAndCleanup(pool(), state);
+    }
+  }
+  state.SetComplexityN(state.range(0));
+}
+BENCHMARK_REGISTER_F(PoolFixture, Linear)
+    ->RangeMultiplier(2)
+    ->Ranges({{1, 1 << 10}})
+    ->Complexity(benchmark::oN);
+
+}  // namespace
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
