38dc2120e8350165bd7bbfb2b134ea22cee5e6cf
Darren Bolduc <dbolduc@google.com>
2022-08-16 13:55:31 -0400

    cleanup(bigtable): reclaim vertical space (#9687)

--- google/cloud/bigtable/internal/readrowsparser.cc
+++ google/cloud/bigtable/internal/readrowsparser.cc
@@ -63,12 +63,8 @@ void ReadRowsParser::HandleChunk(ReadRowsResponse_CellChunk chunk,
 
   if (cell_first_chunk_) {
     cell_.timestamp = chunk.timestamp_micros();
-  }
-
-  std::move(chunk.mutable_labels()->begin(), chunk.mutable_labels()->end(),
-            std::back_inserter(cell_.labels));
-
-  if (cell_first_chunk_) {
+    std::move(chunk.mutable_labels()->begin(), chunk.mutable_labels()->end(),
+              std::back_inserter(cell_.labels));
     // Most common case, move the value
     using std::swap;
     swap(*chunk.mutable_value(), cell_.value);
