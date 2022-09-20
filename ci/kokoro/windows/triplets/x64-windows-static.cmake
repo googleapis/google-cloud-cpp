6a85c48d0ec6ff3458b5072ce0843479b23da32e
Carlos O'Ryan <coryan@google.com>
2022-09-20 11:44:04 -0400

    ci: use custom vcpkg triplets for Windows+CMake
    `vcpkg` uses its own algorithm to find MSVC. The documented way to
    change this algorithm is to create a custom triplet.

---
+++ ci/kokoro/windows/triplets/x64-windows-static.cmake
@@ -0,0 +1,4 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_VISUAL_STUDIO_PATH "C:\\Program Files (x86)\\Microsoft Visual Studio\\$ENV{MSVC_VERSION}\\BuildTools")
---
+++ ci/kokoro/windows/triplets/x86-windows.cmake
@@ -0,0 +1,4 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_VISUAL_STUDIO_PATH "C:\\Program Files (x86)\\Microsoft Visual Studio\\$ENV{MSVC_VERSION}\\BuildTools")
