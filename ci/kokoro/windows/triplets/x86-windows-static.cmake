b291edeb9a42b422dd4372094f8b025bbb7f29e7
Carlos O'Ryan <coryan@google.com>
2022-09-21 00:07:35 +0000

    Fix formatting

--- ci/kokoro/windows/triplets/x86-windows-static.cmake
+++ ci/kokoro/windows/triplets/x86-windows-static.cmake
@@ -1,4 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_VISUAL_STUDIO_PATH "C:\\Program Files (x86)\\Microsoft Visual Studio\\$ENV{MSVC_VERSION}\\BuildTools")
+set(VCPKG_VISUAL_STUDIO_PATH
+    "C:\\Program Files (x86)\\Microsoft Visual Studio\\$ENV{MSVC_VERSION}\\BuildTools"
+)
