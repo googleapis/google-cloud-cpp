593432e1b69e9cef89df7931d15919219163daab
Carlos O'Ryan <coryan@google.com>
2022-10-31 20:19:17 +0000

    Address review comments

--- google/cloud/bigtable/examples/client_samples.cc
+++ google/cloud/bigtable/examples/client_samples.cc
@@ -69,83 +69,6 @@ void TableWithServiceAccount(std::vector<std::string> const& argv) {
   (argv.at(0), argv.at(1), argv.at(2), argv.at(3));
 }
 
-void TableAdminClientSetEndpoint(std::vector<std::string> const& argv) {
-  namespace examples = ::google::cloud::testing_util;
-  if (!argv.empty()) {
-    throw examples::Usage{"table-admin-client-set-endpoint"};
-  }
-  //! [table-admin-client-set-endpoint]
-  namespace admin = ::google::cloud::bigtable_admin;
-  []() {
-    auto options = google::cloud::Options{}.set<google::cloud::EndpointOption>(
-        "private.googleapis.com");
-    return admin::BigtableTableAdminClient(
-        admin::MakeBigtableTableAdminConnection(options));
-  }
-  //! [table-admin-client-set-endpoint]
-  ();
-}
-
-void TableAdminClientWithServiceAccount(std::vector<std::string> const& argv) {
-  namespace examples = ::google::cloud::testing_util;
-  if (argv.size() != 1 || argv[0] == "--help") {
-    throw examples::Usage{"table-admin-client-with-service-account"};
-  }
-  //! [table-admin-client-with-service-account]
-  namespace admin = ::google::cloud::bigtable_admin;
-  [](std::string const& keyfile) {
-    auto is = std::ifstream(keyfile);
-    is.exceptions(std::ios::badbit);
-    auto contents = std::string(std::istreambuf_iterator<char>(is.rdbuf()), {});
-    auto options =
-        google::cloud::Options{}.set<google::cloud::UnifiedCredentialsOption>(
-            google::cloud::MakeServiceAccountCredentials(contents));
-    return admin::BigtableTableAdminClient(
-        admin::MakeBigtableTableAdminConnection(options));
-  }
-  //! [table-admin-client-with-service-account]
-  (argv.at(0));
-}
-
-void InstanceAdminClientSetEndpoint(std::vector<std::string> const& argv) {
-  namespace examples = ::google::cloud::testing_util;
-  if (!argv.empty()) {
-    throw examples::Usage{"instance-admin-client-set-endpoint"};
-  }
-  //! [instance-admin-client-set-endpoint]
-  namespace admin = ::google::cloud::bigtable_admin;
-  []() {
-    auto options = google::cloud::Options{}.set<google::cloud::EndpointOption>(
-        "private.googleapis.com");
-    return admin::BigtableInstanceAdminClient(
-        admin::MakeBigtableInstanceAdminConnection(options));
-  }
-  //! [instance-admin-client-set-endpoint]
-  ();
-}
-
-void InstanceAdminClientWithServiceAccount(
-    std::vector<std::string> const& argv) {
-  namespace examples = ::google::cloud::testing_util;
-  if (argv.size() != 1 || argv[0] == "--help") {
-    throw examples::Usage{"instance-admin-client-with-service-account"};
-  }
-  //! [instance-admin-client-with-service-account]
-  namespace admin = ::google::cloud::bigtable_admin;
-  [](std::string const& keyfile) {
-    auto is = std::ifstream(keyfile);
-    is.exceptions(std::ios::badbit);
-    auto contents = std::string(std::istreambuf_iterator<char>(is.rdbuf()), {});
-    auto options =
-        google::cloud::Options{}.set<google::cloud::UnifiedCredentialsOption>(
-            google::cloud::MakeServiceAccountCredentials(contents));
-    return admin::BigtableInstanceAdminClient(
-        admin::MakeBigtableInstanceAdminConnection(options));
-  }
-  //! [instance-admin-client-with-service-account]
-  (argv.at(0));
-}
-
 void AutoRun(std::vector<std::string> const& argv) {
   using ::google::cloud::internal::GetEnv;
   namespace examples = ::google::cloud::testing_util;
@@ -171,20 +94,6 @@ void AutoRun(std::vector<std::string> const& argv) {
   std::cout << "\nRunning TableWithServiceAccount() sample" << std::endl;
   TableWithServiceAccount({project_id, instance_id, table_id, keyfile});
 
-  std::cout << "\nRunning TableAdminClientSetEndpoint() sample" << std::endl;
-  TableAdminClientSetEndpoint({});
-
-  std::cout << "\nRunning TableAdminClientWithServiceAccount() sample"
-            << std::endl;
-  TableAdminClientWithServiceAccount({keyfile});
-
-  std::cout << "\nRunning InstanceAdminClientSetEndpoint() sample" << std::endl;
-  InstanceAdminClientSetEndpoint({});
-
-  std::cout << "\nRunning InstanceAdminClientWithServiceAccount() sample"
-            << std::endl;
-  InstanceAdminClientWithServiceAccount({keyfile});
-
   std::cout << "\nAutoRun done" << std::endl;
 }
 
@@ -194,12 +103,6 @@ int main(int argc, char* argv[]) {  // NOLINT(bugprone-exception-escape)
   google::cloud::testing_util::Example example({
       {"table-set-endpoint", TableSetEndpoint},
       {"table-with-service-account", TableWithServiceAccount},
-      {"table-admin-client-set-endpoint", TableAdminClientSetEndpoint},
-      {"table-admin-client-with-service-account",
-       TableAdminClientWithServiceAccount},
-      {"instance-admin-client-set-endpoint", InstanceAdminClientSetEndpoint},
-      {"instance-admin-client-with-service-account",
-       InstanceAdminClientWithServiceAccount},
       {"auto", AutoRun},
   });
   return example.Run(argc, argv);
