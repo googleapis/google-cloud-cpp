a5a59735cd45020db7ba189a03e0c3aa9caa0680
Carlos O'Ryan <coryan@google.com>
2022-11-09 22:56:45 +0000

    Run generators and format their outputs

---
+++ google/cloud/connectors/quickstart/quickstart.cc
@@ -0,0 +1,38 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/connectors/ EDIT HERE .h"
+#include "google/cloud/project.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) try {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " project-id\n";
+    return 1;
+  }
+
+  namespace connectors = ::google::cloud::connectors;
+  auto client = connectors::Client(connectors::MakeConnection(/* EDIT HERE */));
+
+  auto const project = google::cloud::Project(argv[1]);
+  for (auto r : client.List /*EDIT HERE*/ (project.FullName())) {
+    if (!r) throw std::move(r).status();
+    std::cout << r->DebugString() << "\n";
+  }
+
+  return 0;
+} catch (google::cloud::Status const& status) {
+  std::cerr << "google::cloud::Status thrown: " << status << "\n";
+  return 1;
+}
