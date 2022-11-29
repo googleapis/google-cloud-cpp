0b328169f29e30f45e60dfa5024616729e0bf1d8
Carlos O'Ryan <coryan@google.com>
2022-11-29 22:53:29 +0000

    impl(common): token exchange for external accounts
    To support external accounts we need to convert the "subject token"
    returned by the external account source (e.g. an Azure metadata service
    or a GitHub Actions OIDC source) into "access tokens" returned by
    GCP's "Security Token Service".  In this change we introduce the code
    to perform such exchanges, including a small integration test on
    GitHub actions.

---
+++ google/cloud/internal/oauth2_external_account_credentials.h
@@ -0,0 +1,63 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_EXTERNAL_ACCOUNT_CREDENTIALS_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_EXTERNAL_ACCOUNT_CREDENTIALS_H
+
+#include "google/cloud/internal/oauth2_credentials.h"
+#include "google/cloud/internal/oauth2_external_account_token_source.h"
+#include "google/cloud/internal/rest_client.h"
+#include "google/cloud/options.h"
+#include "google/cloud/version.h"
+#include <functional>
+#include <memory>
+
+namespace google {
+namespace cloud {
+namespace oauth2_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+struct ExternalAccountInfo {
+  std::string audience;
+  std::string subject_token_type;
+  std::string token_url;
+  ExternalAccountTokenSource token_source;
+};
+
+class ExternalAccountCredentials : public oauth2_internal::Credentials {
+ public:
+  using HttpClientFactory =
+      std::function<std::unique_ptr<rest_internal::RestClient>(Options const&)>;
+
+  ExternalAccountCredentials(ExternalAccountInfo info,
+                             HttpClientFactory client_factory,
+                             Options options = {});
+  ~ExternalAccountCredentials() override = default;
+
+  StatusOr<internal::AccessToken> GetToken(
+      std::chrono::system_clock::time_point tp) override;
+  StatusOr<std::pair<std::string, std::string>> AuthorizationHeader() override;
+
+ private:
+  ExternalAccountInfo info_;
+  HttpClientFactory client_factory_;
+  Options options_;
+};
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace oauth2_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_OAUTH2_EXTERNAL_ACCOUNT_CREDENTIALS_H
