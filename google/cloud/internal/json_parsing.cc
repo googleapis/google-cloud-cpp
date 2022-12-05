929a75cfe7fbb7ed54d8b3baeeeac0daab526d54
Carlos O'Ryan <coryan@google.com>
2022-12-05 18:59:37 +0000

    refactor: generalize JSON parsing helpers
    The filename gave the impression that these functions were only useful
    for external account parsing.

--- google/cloud/internal/json_parsing.cc
+++ google/cloud/internal/json_parsing.cc
@@ -12,62 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_EXTERNAL_ACCOUNT_PARSING_H
-#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_EXTERNAL_ACCOUNT_PARSING_H
-
-#include "google/cloud/internal/error_metadata.h"
-#include "google/cloud/status_or.h"
-#include "google/cloud/version.h"
-#include "absl/strings/string_view.h"
-#include <nlohmann/json.hpp>
-#include <string>
+#include "google/cloud/internal/json_parsing.h"
+#include "google/cloud/internal/absl_str_cat_quiet.h"
+#include "google/cloud/internal/make_status.h"
 
 namespace google {
 namespace cloud {
 namespace oauth2_internal {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
-/// Returns the string value for `json[name]` (which must exist) or a
-/// descriptive error.
 StatusOr<std::string> ValidateStringField(nlohmann::json const& json,
                                           absl::string_view name,
                                           absl::string_view object_name,
-                                          internal::ErrorContext const& ec);
+                                          internal::ErrorContext const& ec) {
+  auto it = json.find(std::string{name});
+  if (it == json.end()) return MissingFieldError(name, object_name, ec);
+  if (!it->is_string()) return InvalidTypeError(name, object_name, ec);
+  return it->get<std::string>();
+}
 
-/// Returns the string value for `json[name]`, a default value if it does not
-/// exist, or a descriptive error if it exists but it is not a string.
 StatusOr<std::string> ValidateStringField(nlohmann::json const& json,
                                           absl::string_view name,
                                           absl::string_view object_name,
                                           absl::string_view default_value,
-                                          internal::ErrorContext const& ec);
+                                          internal::ErrorContext const& ec) {
+  auto it = json.find(std::string{name});
+  if (it == json.end()) return std::string{default_value};
+  if (!it->is_string()) return InvalidTypeError(name, object_name, ec);
+  return it->get<std::string>();
+}
 
-/// Returns the std::int32_t value for `json[name]` (which must exist) or a
-/// descriptive error.
 StatusOr<std::int32_t> ValidateIntField(nlohmann::json const& json,
                                         absl::string_view name,
                                         absl::string_view object_name,
-                                        internal::ErrorContext const& ec);
+                                        internal::ErrorContext const& ec) {
+  auto it = json.find(std::string{name});
+  if (it == json.end()) return MissingFieldError(name, object_name, ec);
+  if (!it->is_number_integer()) return InvalidTypeError(name, object_name, ec);
+  return it->get<std::int32_t>();
+}
 
-/// Returns the std::int32_t value for `json[name]`, a default value if it does
-/// not exist, or a descriptive error if it exists but it is not an integer.
 StatusOr<std::int32_t> ValidateIntField(nlohmann::json const& json,
                                         absl::string_view name,
                                         absl::string_view object_name,
                                         std::int32_t default_value,
-                                        internal::ErrorContext const& ec);
+                                        internal::ErrorContext const& ec) {
+  auto it = json.find(std::string{name});
+  if (it == json.end()) return default_value;
+  if (!it->is_number_integer()) return InvalidTypeError(name, object_name, ec);
+  return it->get<std::int32_t>();
+}
 
-/// Use when a JSON field cannot be found but is required.
 Status MissingFieldError(absl::string_view name, absl::string_view object_name,
-                         internal::ErrorContext const& ec);
+                         internal::ErrorContext const& ec) {
+  return InvalidArgumentError(
+      absl::StrCat("cannot find `", name, "` field in `", object_name, "`"),
+      GCP_ERROR_INFO().WithContext(ec));
+}
 
-/// Use when a JSON field type does not match the expected type.
 Status InvalidTypeError(absl::string_view name, absl::string_view object_name,
-                        internal::ErrorContext const& ec);
+                        internal::ErrorContext const& ec) {
+  return InvalidArgumentError(absl::StrCat("invalid type for `", name,
+                                           "` field in `", object_name, "`"),
+                              GCP_ERROR_INFO().WithContext(ec));
+}
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
 }  // namespace oauth2_internal
 }  // namespace cloud
 }  // namespace google
-
-#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_INTERNAL_EXTERNAL_ACCOUNT_PARSING_H
