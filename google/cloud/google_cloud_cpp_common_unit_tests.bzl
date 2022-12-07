853fbeb86f0869f4c1eadcf8eb9e59b1a9a563f8
Darren Bolduc <dbolduc@google.com>
2022-12-07 12:26:35 -0500

    impl(otel): take dependency on library (#10387)

--- google/cloud/google_cloud_cpp_common_unit_tests.bzl
+++ google/cloud/google_cloud_cpp_common_unit_tests.bzl
@@ -40,6 +40,7 @@ google_cloud_cpp_common_unit_tests = [
     "internal/invoke_result_test.cc",
     "internal/log_impl_test.cc",
     "internal/make_status_test.cc",
+    "internal/opentelemetry_test.cc",
     "internal/pagination_range_test.cc",
     "internal/parse_rfc3339_test.cc",
     "internal/populate_common_options_test.cc",
---
+++ google/cloud/internal/opentelemetry_test.cc
@@ -0,0 +1,33 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+#include <gmock/gmock.h>
+#include <opentelemetry/trace/default_span.h>
+#include <opentelemetry/version.h>
+
+namespace {
+
+using ::testing::IsEmpty;
+using ::testing::Not;
+
+TEST(OpenTelemetry, IsUsable) {
+  auto version = std::string{OPENTELEMETRY_VERSION};
+  EXPECT_THAT(version, Not(IsEmpty()));
+  auto span = opentelemetry::trace::DefaultSpan::GetInvalid();
+  EXPECT_EQ(span.ToString(), "DefaultSpan");
+}
+
+}  // namespace
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
