7668eaa368c102b5cf91324a1070e4f0ea946da6
dbolduc <dbolduc@google.com>
2023-01-10 05:06:32 +0000

    shame on me

--- generator/internal/dern.cc
+++
@@ -1,5 +0,0 @@
-/// Returns `child` if tracing is not enabled.
-std::shared_ptr<$product_namespace$::$connection_class_name$>
-MaybeMake$tracing_connection_class_name$(
-    std::shared_ptr<$product_namespace$::$connection_class_name$>,
-    Options options);
