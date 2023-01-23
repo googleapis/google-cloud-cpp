9a86f23f968fa9b7c7dd01014183dc7e52fb5178
Bradley White <14679271+devbww@users.noreply.github.com>
2023-01-23 13:17:27 -0500

    cleanup(doc): reset group/other permissions after mdformat (#10635)
    The in-place update performed by `mdformat` does not preserve
    the file mode, so do something simple to (probably) compensate.

--- ci/cloudbuild/builds/checkers.sh
+++ ci/cloudbuild/builds/checkers.sh
@@ -203,10 +203,14 @@ time {
   done
 }
 
+# mdformat does `tempfile.mkstemp(); ...; os.replace(tmp_path, path)`,
+# which results in the new .md file having mode 0600. So, run a second
+# pass to reset the group/other permissions to something more reasonable.
 printf "%-50s" "Running markdown formatter:" >&2
 time {
   # See `.mdformat.toml` for the configuration parameters.
   git_files -z -- '*.md' | xargs -r -P "$(nproc)" -n 1 -0 mdformat
+  git_files -z -- '*.md' | xargs -r -0 chmod go=u-w
 }
 
 printf "%-50s" "Running doxygen landing-page updates:" >&2
