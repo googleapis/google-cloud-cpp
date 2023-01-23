7648d2dcec03fa017c2276b56eb54c7f01348edc
Carlos O'Ryan <coryan@google.com>
2023-01-23 17:27:17 +0000

    impl(docfx): create scaffold for docfx
    We will need to convert the Doxygen XML output into DocFX to support
    reference documents at `cloud.google.com`. This just creates the basic
    structure to host this tool.

---
+++ docfx/parse_arguments.cc
@@ -0,0 +1,38 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "docfx/parse_arguments.h"
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+namespace {
+
+std::string Usage(std::string const& cmd) {
+  std::ostringstream os;
+  os << "Usage: " << cmd << " <infile> <library>";
+  return std::move(os).str();
+}
+
+}  // namespace
+
+Config ParseArguments(std::vector<std::string> const& args) {
+  if (args.size() == 2 && args[1] == "--help") {
+    std::cout << Usage(args[0]) << "\n";
+    std::exit(0);
+  }
+  if (args.size() != 3) throw std::runtime_error(Usage(args[0]));
+  return Config{args[1], args[2]};
+}
---
+++ docfx/parse_arguments_test.cc
@@ -0,0 +1,67 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "docfx/parse_arguments.h"
+#include <gmock/gmock.h>
+#include <stdexcept>
+#include <string_view>
+
+namespace {
+
+TEST(ParseArguments, Basic) {
+  auto const actual = ParseArguments({"cmd", "input-file", "library"});
+  EXPECT_EQ(actual.input_filename, "input-file");
+  EXPECT_EQ(actual.library, "library");
+}
+
+TEST(ParseArguments, Help) {
+  auto const arguments = std::vector<std::string>({"cmd", "--help"});
+  EXPECT_EXIT(
+      ParseArguments(arguments), [](int status) { return status == 0; },
+      // The usage message goes to stdout, the error log should be empty:
+      "");
+}
+
+TEST(ParseArguments, NoArguments) {
+  EXPECT_THROW(
+      try { ParseArguments({"cmd"}); } catch (std::exception const& ex) {
+        EXPECT_EQ(ex.what(), std::string_view("Usage: cmd <infile> <library>"));
+        throw;
+      },
+      std::runtime_error);
+}
+
+TEST(ParseArguments, TooFewArguments) {
+  EXPECT_THROW(
+      try {
+        ParseArguments({"cmd", "1"});
+      } catch (std::exception const& ex) {
+        EXPECT_EQ(ex.what(), std::string_view("Usage: cmd <infile> <library>"));
+        throw;
+      },
+      std::runtime_error);
+}
+
+TEST(ParseArguments, TooManyArguments) {
+  EXPECT_THROW(
+      try {
+        ParseArguments({"cmd", "1", "2", "3"});
+      } catch (std::exception const& ex) {
+        EXPECT_EQ(ex.what(), std::string_view("Usage: cmd <infile> <library>"));
+        throw;
+      },
+      std::runtime_error);
+}
+
+}  // namespace
