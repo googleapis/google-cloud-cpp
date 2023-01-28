c257db47df7b6fd76f3f266cd40b35e63a3749d2
Carlos O'Ryan <coryan@google.com>
2023-01-28 16:39:52 +0000

    feat(generator): link references in service comments

--- generator/internal/format_class_comments_test.cc
+++ generator/internal/format_class_comments_test.cc
@@ -25,7 +25,10 @@ namespace {
 
 using ::testing::AllOf;
 using ::testing::Contains;
+using ::testing::EndsWith;
+using ::testing::HasSubstr;
 using ::testing::NotNull;
+using ::testing::StartsWith;
 
 class FormatClassCommentsTest
     : public generator_testing::DescriptorPoolFixture {};
@@ -50,6 +53,8 @@ service Service {
   ASSERT_THAT(service, NotNull());
 
   auto const actual = FormatClassCommentsFromServiceComments(*service);
+  // Verify it has exactly one trailing `///` comment.
+  EXPECT_THAT(actual, AllOf(EndsWith("\n///"), Not(EndsWith("\n///\n///"))));
   auto const lines = std::vector<std::string>{absl::StrSplit(actual, '\n')};
   EXPECT_THAT(lines, AllOf(Contains("/// A brief description of the service."),
                            Contains("/// A longer description of the service."),
@@ -80,6 +85,55 @@ service Service {
       lines, AllOf(Contains("/// ServiceClient"), Contains("/// @par Equality"),
                    Contains("/// @par Performance"),
                    Contains("/// @par Thread Safety")));
+}
+
+TEST_F(FormatClassCommentsTest, WithReferences) {
+  auto constexpr kContents = R"""(
+syntax = "proto3";
+import "test/v1/common.proto";
+package test.v1;
+
+// A brief description of the service.
+//
+// This references the [Request][test.v1.Request] message, another service
+// [OtherService][test.v1.OtherService], and even a field
+// [Resource.parent][test.v1.Resource.parent]
+service Service {
+    rpc Method(Request) returns (Response) {}
+}
+
+// Just to test references.
+service OtherService {}
+message Resource {
+    string parent = 1;
+}
+)""";
+
+  ASSERT_THAT(FindFile("test/v1/common.proto"), NotNull());
+  ASSERT_TRUE(AddProtoFile("test/v1/service.proto", kContents));
+  auto const* service = pool().FindServiceByName("test.v1.Service");
+  ASSERT_THAT(service, NotNull());
+
+  auto const actual = FormatClassCommentsFromServiceComments(*service);
+  // Verify the first reference is separated by an empty line and that it ends
+  // with a single `///` comment.
+  EXPECT_THAT(actual, AllOf(HasSubstr("///\n/// [test.v1.OtherService]: "),
+                            EndsWith("\n///"), Not(EndsWith("\n///\n///"))));
+  auto const lines = std::vector<std::string>{absl::StrSplit(actual, '\n')};
+  EXPECT_THAT(
+      lines,
+      AllOf(Contains("/// A brief description of the service."),
+            Contains(StartsWith(
+                "/// [test.v1.Request]: "
+                "@googleapis_link_reference{test/v1/common.proto#L")),
+            Contains(StartsWith(
+                "/// [test.v1.OtherService]: "
+                "@googleapis_link_reference{test/v1/service.proto#L")),
+            Contains(StartsWith(
+                "/// [test.v1.Resource.parent]: "
+                "@googleapis_link_reference{test/v1/service.proto#L")),
+            Contains("/// @par Equality"), Contains("/// @par Performance"),
+            Contains("/// @par Thread Safety")));
 }
 
 }  // namespace
