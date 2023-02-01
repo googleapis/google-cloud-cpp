0f311d3006b8122f21bf0cb460e536551db6e209
Jaya Srinivasan <jsrinivasan@google.com>
2023-02-01 22:42:03 +0000

    Initial files for the Bigquery V2 Job Api as per the design in go/bq-odbc-client-design

---
+++ google/cloud/bigquery/v2/minimal/internal/job_rest_stub_factory.cc
@@ -0,0 +1,28 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Implementation of internal interface for Bigquery V2 Job resource.
+// Source: //depot/google3/google/cloud/bigquery/v2/job.proto
+
+#include "google/cloud/bigquery/v2/minimal/internal/job_rest_stub_factory.h"
+
+namespace google {
+namespace cloud {
+namespace bigquery_v2_minimal {
+namespace internal {
+
+}  // internal
+}  // namespace bigquery_v2_minimal
+}  // namespace cloud
+}  // namespace google
