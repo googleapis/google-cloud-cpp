63e6dc4c4fbfdd0e0711f3a61c53fffdf2f4b55f
Jaya Srinivasan <jsrinivasan@google.com>
2023-03-08 23:20:18 +0000

    impl(bigquery): Remaining implementation files for GetJob api. Connection tests are stubbed out. Detailed connection tests with mocks will be added in the subsequent PR.

---
+++ google/cloud/bigquery/v2/minimal/internal/job_connection.cc
@@ -0,0 +1,53 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/bigquery/v2/minimal/internal/job_connection.h"
+#include "google/cloud/bigquery/v2/minimal/internal/job_rest_connection_impl.h"
+#include "google/cloud/bigquery/v2/minimal/internal/job_rest_stub_factory.h"
+#include "google/cloud/background_threads.h"
+#include "google/cloud/common_options.h"
+#include "google/cloud/credentials.h"
+#include "google/cloud/internal/rest_background_threads_impl.h"
+#include <memory>
+
+namespace google {
+namespace cloud {
+namespace bigquery_v2_minimal_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+BigQueryJobConnection::~BigQueryJobConnection() = default;
+
+StatusOr<GetJobResponse> BigQueryJobConnection::GetJob(GetJobRequest const&) {
+  return Status(StatusCode::kUnimplemented, "not implemented");
+}
+
+std::shared_ptr<BigQueryJobConnection> MakeBigQueryJobConnection(
+    Options options) {
+  internal::CheckExpectedOptions<CommonOptionList, UnifiedCredentialsOptionList,
+                                 BigQueryJobPolicyOptionList>(options,
+                                                              __func__);
+  options = BigQueryJobDefaultOptions(std::move(options));
+  auto background = absl::make_unique<
+      rest_internal::AutomaticallyCreatedRestBackgroundThreads>();
+
+  auto job_rest_stub = CreateDefaultBigQueryJobRestStub(options);
+
+  return std::make_shared<BigQueryJobRestConnectionImpl>(
+      std::move(background), std::move(job_rest_stub), std::move(options));
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace bigquery_v2_minimal_internal
+}  // namespace cloud
+}  // namespace google
