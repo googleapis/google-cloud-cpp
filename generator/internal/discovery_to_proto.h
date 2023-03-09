296e1d9f23089f49d032d924ba38fab0d014feb6
Scott Hart <sdhart@google.com>
2023-03-09 17:37:27 -0500

    impl(generator): extract resources from Discovery Document

--- generator/internal/discovery_to_proto.h
+++ generator/internal/discovery_to_proto.h
@@ -15,9 +15,11 @@
 #ifndef GOOGLE_CLOUD_CPP_GENERATOR_INTERNAL_DISCOVERY_TO_PROTO_H
 #define GOOGLE_CLOUD_CPP_GENERATOR_INTERNAL_DISCOVERY_TO_PROTO_H
 
+#include "generator/internal/discovery_resource.h"
 #include "generator/internal/discovery_type_vertex.h"
 #include "google/cloud/status_or.h"
 #include <nlohmann/json.hpp>
+#include <map>
 #include <string>
 
 namespace google {
@@ -26,6 +28,10 @@ namespace generator_internal {
 
 StatusOr<std::map<std::string, DiscoveryTypeVertex>> ExtractTypesFromSchema(
     nlohmann::json const& discovery_doc);
+
+std::map<std::string, DiscoveryResource> ExtractResourcesFromSchema(
+    nlohmann::json const& discovery_doc, std::string const& default_hostname,
+    std::string const& base_path);
 
 Status GenerateProtosFromDiscoveryDoc(std::string const& url,
                                       std::string const& protobuf_proto_path,
