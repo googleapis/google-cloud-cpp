985901b6c633a49aaf8879888fb4f82538483cb4
dbolduc <dbolduc@google.com>
2023-03-17 17:32:03 -0400

    address review comments; fix clang otel disabled

--- google/cloud/internal/opentelemetry.cc
+++ google/cloud/internal/opentelemetry.cc
@@ -99,15 +99,16 @@ std::function<void(std::chrono::milliseconds)> MakeTracedSleeper(
   return sleeper;
 }
 
-// NOLINTNEXTLINE(misc-unused-value-param)
-void AddSpanAttribute(std::string const& key, std::string const& value) {
 #ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+void AddSpanAttribute(std::string const& key, std::string const& value) {
   if (TracingEnabled(CurrentOptions())) {
     auto span = opentelemetry::trace::Tracer::GetCurrentSpan();
-    span->SetAttribute(std::move(key), std::move(value));
+    span->SetAttribute(key, value);
   }
-#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
 }
+#else
+void AddSpanAttribute(std::string const&, std::string const&) {}
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
 
 }  // namespace internal
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
