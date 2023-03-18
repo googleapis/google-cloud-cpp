c796ee1c28d9cd049bac71fbe47c56c9d7f85084
Carlos O'Ryan <coryan@google.com>
2023-03-18 16:59:37 +0000

    cleanup(GCS+gRPC): use generated round robin decorator

--- google/cloud/storage/google_cloud_cpp_storage_grpc.cmake
+++ google/cloud/storage/google_cloud_cpp_storage_grpc.cmake
@@ -87,8 +87,8 @@ else ()
         internal/storage_logging_decorator.h
         internal/storage_metadata_decorator.cc
         internal/storage_metadata_decorator.h
-        internal/storage_round_robin.cc
-        internal/storage_round_robin.h
+        internal/storage_round_robin_decorator.cc
+        internal/storage_round_robin_decorator.h
         internal/storage_stub.cc
         internal/storage_stub.h
         internal/storage_stub_factory.cc
@@ -208,7 +208,6 @@ if (BUILD_TESTING AND GOOGLE_CLOUD_CPP_STORAGE_ENABLE_GRPC)
         internal/grpc_sign_blob_request_parser_test.cc
         internal/grpc_split_write_object_data_test.cc
         internal/grpc_synthetic_self_link_test.cc
-        internal/storage_round_robin_test.cc
         internal/storage_stub_factory_test.cc)
 
     foreach (fname ${storage_client_grpc_unit_tests})
