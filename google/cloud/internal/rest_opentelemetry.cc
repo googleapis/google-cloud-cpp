31b2328b700991527304f5145d7bc86b3e006698
Carlos O'Ryan <coryan@google.com>
2023-04-08 14:58:51 -0400

    impl(rest): basic otel span for HTTP requests (#11209)

---
+++ google/cloud/internal/rest_opentelemetry.cc
@@ -0,0 +1,67 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+#include "google/cloud/internal/rest_opentelemetry.h"
+#include "google/cloud/internal/absl_str_cat_quiet.h"
+#include "google/cloud/internal/opentelemetry.h"
+#include "google/cloud/internal/opentelemetry_options.h"
+#include "google/cloud/options.h"
+#include "absl/strings/match.h"
+#include <opentelemetry/context/propagation/global_propagator.h>
+#include <opentelemetry/context/propagation/text_map_propagator.h>
+#include <opentelemetry/trace/provider.h>
+#include <opentelemetry/trace/semantic_conventions.h>
+#include <opentelemetry/trace/span_metadata.h>
+#include <opentelemetry/trace/span_startoptions.h>
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> MakeSpanHttp(
+    RestRequest const& request, opentelemetry::nostd::string_view method) {
+  namespace sc = opentelemetry::trace::SemanticConventions;
+  opentelemetry::trace::StartSpanOptions options;
+  options.kind = opentelemetry::trace::SpanKind::kClient;
+  auto span =
+      internal::GetTracer(internal::CurrentOptions())
+          ->StartSpan(absl::StrCat("HTTP/", absl::string_view{method.data(),
+                                                              method.size()}),
+                      {{sc::kNetTransport, sc::NetTransportValues::kIpTcp},
+                       {sc::kHttpMethod, method},
+                       {sc::kHttpUrl, request.path()}},
+                      options);
+  for (auto const& kv : request.headers()) {
+    if (kv.second.empty()) {
+      span->SetAttribute("http.header." + kv.first, "");
+      continue;
+    }
+    if (absl::EqualsIgnoreCase(kv.first, "authorization")) {
+      span->SetAttribute("http.header.authorization",
+                         kv.second.front().substr(0, 32));
+      continue;
+    }
+    span->SetAttribute("http.header." + kv.first, kv.second.front());
+  }
+  return span;
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
---
+++ google/cloud/internal/rest_opentelemetry_test.cc
@@ -0,0 +1,75 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+#include "google/cloud/internal/rest_opentelemetry.h"
+#include "google/cloud/internal/opentelemetry_options.h"
+#include "google/cloud/testing_util/opentelemetry_matchers.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include <gmock/gmock.h>
+#include <opentelemetry/context/propagation/global_propagator.h>
+#include <opentelemetry/trace/semantic_conventions.h>
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace {
+
+using ::google::cloud::testing_util::InstallSpanCatcher;
+using ::google::cloud::testing_util::SpanAttribute;
+using ::google::cloud::testing_util::SpanHasAttributes;
+using ::google::cloud::testing_util::SpanHasInstrumentationScope;
+using ::google::cloud::testing_util::SpanKindIsClient;
+using ::google::cloud::testing_util::SpanNamed;
+using ::testing::AllOf;
+using ::testing::ElementsAre;
+using ::testing::Gt;
+
+TEST(RestOpentelemetry, MakeSpanHttp) {
+  namespace sc = ::opentelemetry::trace::SemanticConventions;
+  auto span_catcher = InstallSpanCatcher();
+
+  auto constexpr kUrl = "https://storage.googleapis.com/storage/v1/b/my-bucket";
+  RestRequest request(kUrl);
+  auto const secret =
+      std::string{"Bearer secret-0123456789aaaaabbbbbcccccddddd"};
+  ASSERT_THAT(secret.size(), Gt(32));
+  request.AddHeader("Authorization", secret);
+  request.AddHeader("X-Goog-Foo", "bar");
+
+  auto span = MakeSpanHttp(request, "GET");
+  span->End();
+
+  auto spans = span_catcher->GetSpans();
+  EXPECT_THAT(
+      spans, ElementsAre(AllOf(
+                 SpanHasInstrumentationScope(), SpanKindIsClient(),
+                 SpanNamed("HTTP/GET"),
+                 SpanHasAttributes(
+                     SpanAttribute<std::string>(sc::kNetTransport,
+                                                sc::NetTransportValues::kIpTcp),
+                     SpanAttribute<std::string>(sc::kHttpMethod, "GET"),
+                     SpanAttribute<std::string>(sc::kHttpUrl, kUrl),
+                     SpanAttribute<std::string>("http.header.authorization",
+                                                secret.substr(0, 32))))));
+}
+
+}  // namespace
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
