b0a037ce8806aa8ae7dc2ae6ad1bd745859e702e
Carlos O'Ryan <coryan@google.com>
2023-04-11 14:27:55 -0400

    impl(GCS+gRPC): support OSS version of ctype=CORD (#11185)
    The OSS version of ctype=CORD will not support the `mutable_*()`
    accessor (in our case that is `mutable_content()`). One needs to use
    `set_content()` and `content()` exclusively. The workarounds to access
    the private member functions fail to compile (even if unused), so we
    need a compile-time flag and some heavy documentation.

---
+++ doc/ctype-cord-workarounds.md
@@ -0,0 +1,85 @@
+# Enabling ctype=CORD workarounds for older Protobuf versions
+
+The `[ctype = CORD]` annotation in a proto file changes the C++ representation
+of `bytes` (and `string`) fields from `std::string` to `absl::Cord`. This
+annotation is critical to get good performance with GCS and gRPC.
+
+Protobuf does not support `[ctype = CORD]` until 4.23.x (aka v23.x). In older
+versions the protobuf compiler "hides" the accessors and modifiers for fields
+with this annotation. The accessors and modifiers become `private` in the
+generated C++ classes.
+
+We can use some workarounds to access these private member functions, but these
+workarounds do not compile when Protobuf supports `[ctype = CORD]`.
+
+We cannot require Protobuf >= v23 until the 3.x release of `google-cloud-cpp`,
+as that would be a breaking change. We need to support compilation with or
+without support for `[ctype = CORD]`.
+
+We need to disable the workarounds using a `#if` / `#endif` block. Protobuf does
+not provide any pre-processor macros to detect if it supports `[ctype = CORD]`.
+We need to provide our own C++ pre-processor macros for this purpose.
+
+While we can try to chose a good default for these macros, these defaults will
+be incorrect in specific environments.
+
+The `google-cloud-cpp` code uses a `static_assert()` to detect if the macros are
+set incorrectly, and prints an error message pointing to this document.
+
+## Enabling ctype=CORD workarounds with CMake
+
+If you are using CMake as your build system the
+`GOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND` CMake option enables the
+workaround. When compiling with older versions of Protobuf add
+`-DGOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND=ON` to your CMake configuration
+line. For example:
+
+```
+cmake -S . -B ... -DGOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND=ON
+```
+
+Use \`\`-DGOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND=OFF\` with Protobuf >= v23:
+
+```
+cmake -S . -B ... -DGOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND=OFF
+```
+
+If you are not building `google-cloud-cpp` from source, then you are probably
+using a package manager, such as [vcpkg](https://vcpkg.io)
+or [Conda](https://conda.io). The `google-cloud-cpp` recipes in the package
+manager should match the Protobuf version in that package manager. If they
+configuration is incorrect, please file a bug with the package maintainers.
+
+## Enabling ctype=CORD workarounds with Bazel
+
+If you are using Bazel as your build system the
+`//:enable-ctype-cord-workaround` Bazel flag enables the workaround. This flag
+must be used with whatever name *you* gave to the `google-cloud-cpp` repository
+in your `WORKSPACE` file. Set this flag to `true` if using older versions of
+Protobuf. For example:
+
+```
+bazel build --@google_cloud_cpp//:enable-ctype-cord-workaround=true ...
+```
+
+Set this flag to `false` if using Protobuf >= v23. For example:
+
+```
+bazel build --@google_cloud_cpp//:enable-ctype-cord-workaround=false ...
+```
+
+## Defaults during the transition period
+
+As of 2023-04-10 the proto files for the Google Cloud Storage service do not
+contain any `[ctype = CORD]` annotations. The default will be to disable
+the workarounds until this annotation is added. As long as the annotation is
+not present, this is the correct default with **any** Protobuf version.
+
+Once the `[ctype = CORD]` annotation is included in the proto files then the
+correct default depends on the Protobuf version. If `google-cloud-cpp` is using
+Protobuf \< v23 when the annotation is included in the proto files, the
+`google-cloud-cpp` will enable the workarounds by default.
+
+It is very likely that the default value for this option is incorrect during
+the transition period. This is cumbersome, but unavoidable until Protobuf >= v23
+is widely deployed.
--- google/cloud/storage/internal/grpc_ctype_cord_workaround.h
+++ google/cloud/storage/internal/grpc_ctype_cord_workaround.h
@@ -17,8 +17,10 @@
 
 #include "google/cloud/internal/type_traits.h"
 #include "google/cloud/version.h"
+#include <google/protobuf/message.h>
 #include <google/storage/v2/storage.pb.h>
 #include <string>
+#include <type_traits>
 
 namespace google {
 namespace cloud {
@@ -44,50 +46,128 @@ struct ContentTypeImpl<
 using ContentType =
     typename ContentTypeImpl<google::storage::v2::ChecksummedData>::type;
 
+template <typename P, typename AlwaysVoid = void>
+struct HasPublicContent : public std::false_type {};
+
+template <typename P>
+struct HasPublicContent<
+    P, google::cloud::internal::void_t<decltype(std::declval<P>().content())>>
+    : public std::true_type {};
+
+// See /doc/ctype-cord-workarounds for details.
+#if GOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND
+
+static_assert(!HasPublicContent<google::storage::v2::ChecksummedData>::value,
+              "The workarounds for [ctype = CORD] are enabled, but not needed."
+              "\nPlease disable these workarounds as described in:"
+              "\n    https://github.com/googleapis/google-cloud-cpp"
+              "/blob/main/doc/ctype-cord-workaround.md"
+              "\n");
+
+static_assert(std::is_same<std::string, ContentType>::value,
+              "The workarounds for [ctype = CORD] are enabled, but not needed."
+              "\nThis will result in compile-time errors."
+              "\nPlease disable these workarounds as described in:"
+              "\n    https://github.com/googleapis/google-cloud-cpp"
+              "/blob/main/doc/ctype-cord-workaround.md"
+              "\n");
+
 // Workaround for older Protobuf versions without `[ctype = CORD]` support.
-//
-// In older versions of Protobuf adding the `[ctype = CORD]` annotation moves
-// the field accessors and modifiers to the `private:` section of the generated
-// C++ class. That makes the field unusable but there is a workaround. One can
-// bypass the C++ member access control mechanism using these tricks.
-template <typename T, typename T::type M>
+template <typename Tag, typename Tag::type M>
 struct BypassPrivateControl {
-  friend constexpr typename T::type GetMemberPointer(T) { return M; }
+  friend constexpr typename Tag::type GetMemberPointer(Tag) { return M; }
 };
 
 // A tag to gain access to the `mutable_content()` accessor.
 struct GetMutableContentTag {
-  using type = ContentType* (google::storage::v2::ChecksummedData::*)();
+  using type = std::string* (google::storage::v2::ChecksummedData::*)();
   friend constexpr type GetMemberPointer(GetMutableContentTag);
+};
+
+// A tag to gain access to the `content()` accessor.
+struct GetContentTag {
+  using type =
+      std::string const& (google::storage::v2::ChecksummedData::*)() const;
+  friend constexpr type GetMemberPointer(GetContentTag);
 };
 
 extern template struct BypassPrivateControl<
     GetMutableContentTag,
     &google::storage::v2::ChecksummedData::mutable_content>;
-
-struct GetContentTag {
-  using type =
-      ContentType const& (google::storage::v2::ChecksummedData::*)() const;
-  friend constexpr type GetMemberPointer(GetContentTag);
-};
-
 extern template struct BypassPrivateControl<
     GetContentTag, &google::storage::v2::ChecksummedData::content>;
 
-inline ContentType StealMutableContent(
-    google::storage::v2::ChecksummedData& d) {
-  return std::move(*(d.*GetMemberPointer(GetMutableContentTag{}))());
-}
-
-inline ContentType const& GetContent(
+inline std::string const& GetContent(
     google::storage::v2::ChecksummedData const& d) {
   return (d.*GetMemberPointer(GetContentTag{}))();
 }
 
 inline void SetMutableContent(google::storage::v2::ChecksummedData& d,
-                              ContentType value) {
+                              std::string value) {
   *(d.*GetMemberPointer(GetMutableContentTag{}))() = std::move(value);
 }
+
+inline std::string StealMutableContent(
+    google::storage::v2::ChecksummedData& d) {
+  return std::move(*(d.*GetMemberPointer(GetMutableContentTag{}))());
+}
+
+#else  // GOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND == TRUE
+
+static_assert(
+    HasPublicContent<google::storage::v2::ChecksummedData>::value,
+    "The workarounds for [ctype = CORD] are disabled, but are required."
+    "\nThis will result in compile-time errors"
+    "\nPlease enable these workarounds as described in:"
+    "\n    https://github.com/googleapis/google-cloud-cpp"
+    "/blob/main/doc/ctype-cord-workaround.md"
+    "\n");
+
+// The OSS version of [ctype = CORD] will not support `mutable_content()`. That
+// means we can only use `set_content()` when `ContentType == absl::Cord`.
+// But for `ContentType == std::string` we really want to use `mutable_*()` to
+// steal the contents with zero copy. This extra layer of SFINAE deals with
+// this (hopefully the last) layer of complexity.
+template <typename T, typename C = std::string>
+struct AccessContent {
+  static C const& Get(T const& d) {
+    return d.content();
+    ;
+  }
+
+  static void Set(T& d, C value) { *(d.mutable_content()) = std::move(value); }
+
+  static C Steal(T& d) { return std::move(*d.mutable_content()); }
+};
+
+template <typename T>
+struct AccessContent<T, absl::Cord> {
+  static absl::Cord const& Get(T const& d) { return d.content(); }
+
+  static void Set(T& d, absl::Cord value) { d.set_content(std::move(value)); }
+
+  static absl::Cord Steal(T& d) { return d.content(); }
+};
+
+inline ContentType const& GetContent(
+    google::storage::v2::ChecksummedData const& d) {
+  return AccessContent<google::storage::v2::ChecksummedData, ContentType>::Get(
+      d);
+}
+
+inline void SetMutableContent(google::storage::v2::ChecksummedData& d,
+                              ContentType value) {
+  AccessContent<google::storage::v2::ChecksummedData, ContentType>::Set(
+      d, std::move(value));
+}
+
+inline ContentType StealMutableContent(
+    google::storage::v2::ChecksummedData& d) {
+  return AccessContent<google::storage::v2::ChecksummedData,
+                       ContentType>::Steal(d);
+}
+
+#endif  // GOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
 }  // namespace storage_internal
