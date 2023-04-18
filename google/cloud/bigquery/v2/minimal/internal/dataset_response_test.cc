765a67190e6aa77e95449f62317daa2bcd6b7c81
Jaya Srinivasan <jsrinivasan@google.com>
2023-04-18 21:46:02 +0000

    impl(bigquery): Response objects with unit tests for Dataset apis

---
+++ google/cloud/bigquery/v2/minimal/internal/dataset_response_test.cc
@@ -0,0 +1,170 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/bigquery/v2/minimal/internal/dataset_response.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include <gmock/gmock.h>
+#include <sstream>
+
+namespace google {
+namespace cloud {
+namespace bigquery_v2_minimal_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+using ::google::cloud::testing_util::StatusIs;
+using ::testing::HasSubstr;
+
+TEST(GetDatasetResponseTest, Success) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"kind": "d-kind",
+          "etag": "d-tag",
+          "id": "d-id",
+          "self_link": "d-selfLink",
+          "friendly_name": "d-friendly-name",
+          "dataset_reference": {"project_id": "p-id", "dataset_id": "d-id"}
+    })";
+  auto const response =
+      GetDatasetResponse::BuildFromHttpResponse(http_response);
+  ASSERT_STATUS_OK(response);
+  EXPECT_FALSE(response->http_response.payload.empty());
+  EXPECT_EQ(response->dataset.kind, "d-kind");
+  EXPECT_EQ(response->dataset.etag, "d-tag");
+  EXPECT_EQ(response->dataset.id, "d-id");
+  EXPECT_EQ(response->dataset.self_link, "d-selfLink");
+  EXPECT_EQ(response->dataset.friendly_name, "d-friendly-name");
+  EXPECT_EQ(response->dataset.dataset_reference.project_id, "p-id");
+  EXPECT_EQ(response->dataset.dataset_reference.dataset_id, "d-id");
+}
+
+TEST(GetDatasetResponseTest, EmptyPayload) {
+  BigQueryHttpResponse http_response;
+  auto const response =
+      GetDatasetResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response, StatusIs(StatusCode::kInternal,
+                                 HasSubstr("Empty payload in HTTP response")));
+}
+
+TEST(GetDatasetResponseTest, InvalidJson) {
+  BigQueryHttpResponse http_response;
+  http_response.payload = "Help! I am not json";
+  auto const response =
+      GetDatasetResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response,
+              StatusIs(StatusCode::kInternal,
+                       HasSubstr("Error parsing Json from response payload")));
+}
+
+TEST(GetDatasetResponseTest, InvalidDataset) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"kind": "dkind",
+          "etag": "dtag",
+          "id": "jd123",
+          "self_link": "dselfLink"})";
+  auto const response =
+      GetDatasetResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response, StatusIs(StatusCode::kInternal,
+                                 HasSubstr("Not a valid Json Dataset object")));
+}
+
+TEST(ListDatasetsResponseTest, Success) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"etag": "tag-1",
+          "kind": "kind-1",
+          "next_page_token": "npt-123",
+          "datasets": [
+              {
+                "id": "1",
+                "kind": "kind-2",
+                "dataset_reference": {"project_id": "p123", "dataset_id": "d123"},
+
+                "friendly_name": "friendly-name",
+                "location": "location",
+                "type": "DEFAULT"
+              }
+  ]})";
+  auto const list_datasets_response =
+      ListDatasetsResponse::BuildFromHttpResponse(http_response);
+  ASSERT_STATUS_OK(list_datasets_response);
+  EXPECT_FALSE(list_datasets_response->http_response.payload.empty());
+  EXPECT_EQ(list_datasets_response->kind, "kind-1");
+  EXPECT_EQ(list_datasets_response->etag, "tag-1");
+  EXPECT_EQ(list_datasets_response->next_page_token, "npt-123");
+
+  auto const datasets = list_datasets_response->datasets;
+  ASSERT_EQ(datasets.size(), 1);
+  EXPECT_EQ(datasets[0].id, "1");
+  EXPECT_EQ(datasets[0].kind, "kind-2");
+  EXPECT_EQ(datasets[0].friendly_name, "friendly-name");
+  EXPECT_EQ(datasets[0].dataset_reference.project_id, "p123");
+  EXPECT_EQ(datasets[0].dataset_reference.dataset_id, "d123");
+  EXPECT_EQ(datasets[0].location, "location");
+  EXPECT_EQ(datasets[0].type, "DEFAULT");
+}
+
+TEST(ListDatasetsResponseTest, EmptyPayload) {
+  BigQueryHttpResponse http_response;
+  auto const response =
+      ListDatasetsResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response, StatusIs(StatusCode::kInternal,
+                                 HasSubstr("Empty payload in HTTP response")));
+}
+
+TEST(ListDatasetsResponseTest, InvalidJson) {
+  BigQueryHttpResponse http_response;
+  http_response.payload = "Invalid";
+  auto const response =
+      ListDatasetsResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response,
+              StatusIs(StatusCode::kInternal,
+                       HasSubstr("Error parsing Json from response payload")));
+}
+
+TEST(ListDatasetsResponseTest, InvalidDatasetList) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"kind": "dkind",
+          "etag": "dtag"})";
+  auto const response =
+      ListDatasetsResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response,
+              StatusIs(StatusCode::kInternal,
+                       HasSubstr("Not a valid Json DatasetList object")));
+}
+
+TEST(ListDatasetsResponseTest, InvalidListFormatDataset) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"etag": "tag-1",
+          "kind": "kind-1",
+          "next_page_token": "npt-123",
+          "datasets": [
+              {
+                "id": "1",
+                "kind": "kind-2"
+              }
+  ]})";
+  auto const response =
+      ListDatasetsResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response,
+              StatusIs(StatusCode::kInternal,
+                       HasSubstr("Not a valid Json ListFormatDataset object")));
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace bigquery_v2_minimal_internal
+}  // namespace cloud
+}  // namespace google
