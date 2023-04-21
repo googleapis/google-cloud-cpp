001ef901e2d3864d8b49f7b37f07f2d1886bbe8a
Carlos O'Ryan <coryan@google.com>
2023-04-21 13:54:12 -0400

    impl(otel): set the tracing scope on HTTP requests (#11332)

--- google/cloud/internal/tracing_rest_client_test.cc
+++ google/cloud/internal/tracing_rest_client_test.cc
@@ -40,6 +40,7 @@ using ::google::cloud::testing_util::SpanHasInstrumentationScope;
 using ::google::cloud::testing_util::SpanKindIsClient;
 using ::google::cloud::testing_util::SpanNamed;
 using ::testing::AllOf;
+using ::testing::AtLeast;
 using ::testing::ElementsAreArray;
 using ::testing::Eq;
 using ::testing::NotNull;
@@ -105,6 +106,48 @@ TEST(TracingRestClient, Delete) {
                         "http.response.header.x-test-header-2", "value2"))),
           // Read span on the HttpPayload
           SpanNamed("Read"), SpanNamed("Read")));
+}
+
+TEST(TracingRestClient, HasScope) {
+  auto span_catcher = InstallSpanCatcher();
+
+  auto impl = std::make_unique<MockRestClient>();
+  EXPECT_CALL(*impl, Get).WillOnce([](RestContext&, RestRequest const&) {
+    // Inject an attribute to the current span, which should be the request
+    // span.
+    auto span = opentelemetry::trace::Tracer::GetCurrentSpan();
+    span->SetAttribute("test.attribute", "test.value");
+    auto response = std::make_unique<MockRestResponse>();
+    EXPECT_CALL(*response, StatusCode)
+        .WillRepeatedly(Return(HttpStatusCode::kOk));
+    EXPECT_CALL(*response, Headers).Times(AtLeast(1));
+    EXPECT_CALL(std::move(*response), ExtractPayload).WillOnce([] {
+      return MakeMockHttpPayloadSuccess(MockContents());
+    });
+    return std::unique_ptr<RestResponse>(std::move(response));
+  });
+
+  auto constexpr kUrl = "https://storage.googleapis.com/storage/v1/b/my-bucket";
+  RestRequest request(kUrl);
+
+  auto client = MakeTracingRestClient(std::move(impl));
+  rest_internal::RestContext context;
+  auto r = client->Get(context, request);
+  ASSERT_STATUS_OK(r);
+  auto response = *std::move(r);
+  ASSERT_THAT(response, NotNull());
+  EXPECT_THAT(response->StatusCode(), Eq(HttpStatusCode::kOk));
+  auto contents = ReadAll(std::move(*response).ExtractPayload());
+  EXPECT_THAT(contents, IsOkAndHolds(MockContents()));
+
+  auto spans = span_catcher->GetSpans();
+  EXPECT_THAT(spans,
+              UnorderedElementsAre(
+                  AllOf(SpanNamed("HTTP/GET"), SpanHasInstrumentationScope(),
+                        SpanKindIsClient(),
+                        SpanHasAttributes(SpanAttribute<std::string>(
+                            "test.attribute", "test.value"))),
+                  SpanNamed("Read"), SpanNamed("Read")));
 }
 
 }  // namespace
