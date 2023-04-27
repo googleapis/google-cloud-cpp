de86e4620b50b0b3543b1d03011cea08be32201b
Carlos O'Ryan <coryan@google.com>
2023-04-27 15:22:18 +0000

    Address review comments

--- google/cloud/storage/internal/retry_client_test.cc
+++ google/cloud/storage/internal/retry_client_test.cc
@@ -13,15 +13,13 @@
 // limitations under the License.
 
 #include "google/cloud/storage/internal/retry_client.h"
+#include "google/cloud/storage/internal/tracing_client.h"
 #include "google/cloud/storage/testing/canonical_errors.h"
 #include "google/cloud/storage/testing/mock_client.h"
-#include "google/cloud/testing_util/chrono_literals.h"
-#include "google/cloud/testing_util/status_matchers.h"
-#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
-#include "google/cloud/storage/internal/tracing_client.h"
 #include "google/cloud/internal/opentelemetry_options.h"
+#include "google/cloud/testing_util/chrono_literals.h"
 #include "google/cloud/testing_util/opentelemetry_matchers.h"
-#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+#include "google/cloud/testing_util/status_matchers.h"
 #include <gmock/gmock.h>
 
 namespace google {
