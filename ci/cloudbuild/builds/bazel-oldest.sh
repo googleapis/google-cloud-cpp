d9dfb15498b36e269424aad11091832849f36bca
Carlos O'Ryan <coryan@google.com>
2023-04-29 12:11:35 -0400

    ci: extend Bazel 4.2 testing for one release (#11443)
    This will simplify adoption by one of our customers and ease the
    transition to GCS+gRPC.

--- ci/cloudbuild/builds/bazel-oldest.sh
+++ ci/cloudbuild/builds/bazel-oldest.sh
@@ -16,7 +16,7 @@
 
 set -euo pipefail
 
-export USE_BAZEL_VERSION=5.4.0
+export USE_BAZEL_VERSION=4.2.4
 
 source "$(dirname "$0")/../../lib/init.sh"
 source module ci/cloudbuild/builds/lib/bazel.sh
