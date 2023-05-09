25c2faccf65cf39aff7e276459742ef6a1b4011b
Anna Levenberg <alevenb@google.com>
2023-05-09 15:29:15 +0000

    Checker fix

--- ci/install-cloud-sdk.sh
+++ ci/install-cloud-sdk.sh
@@ -26,10 +26,10 @@ ARCH="$(uname -m)"
 if [[ "${ARCH}" == "aarch64" ]]; then
   # The tarball uses this name
   ARCH="arm"
-else 
-  # For x86_64, uname -p might give unknown 
+else
+  # For x86_64, uname -p might give unknown
   ARCH="$(uname -m)"
-fi 
+fi
 
 readonly ARCH
 
