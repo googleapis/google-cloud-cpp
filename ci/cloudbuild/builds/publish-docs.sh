9dbc672b66f083869840c8ff07b131c626f869f8
Carlos O'Ryan <coryan@google.com>
2023-05-12 13:03:51 +0000

    ci: upload more library documentation

--- ci/cloudbuild/builds/publish-docs.sh
+++ ci/cloudbuild/builds/publish-docs.sh
@@ -158,7 +158,7 @@ for feature in "${FEATURE_LIST[@]}"; do
   if [[ "${feature}" == "experimental-storage-grpc" ]]; then continue; fi
   if [[ "${feature}" == "grafeas" ]]; then continue; fi
   # TODO(#11430) - slowly change this limit until all libraries are published
-  if [[ "${feature}" < "z" ]]; then continue; fi
+  if [[ "${feature}" < "v" ]]; then continue; fi
   upload_docfx "google-cloud-${feature}" "cmake-out/google/cloud/${feature}/docfx"
 done
 
