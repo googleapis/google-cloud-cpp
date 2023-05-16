baaa3301f1f9b6057670192077278898a060f70a
Jeffrey Rennie <rennie@google.com>
2023-05-16 14:59:20 -0700

    fix: lint error

--- ci/kokoro/release/publish.sh
+++ ci/kokoro/release/publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Record the current commit hash.
-git -C github/google-cloud-cpp log -1 --format=%H > commit-hash.txt
+git -C github/google-cloud-cpp log -1 --format=%H >commit-hash.txt
