17fa7994759c43eecea0e1e10a0f2e034bada375
dbolduc <dbolduc@google.com>
2023-06-02 15:18:01 -0400

    fix windows quickstart cmake

--- ci/kokoro/windows/builds/quickstart-cmake.ps1
+++ ci/kokoro/windows/builds/quickstart-cmake.ps1
@@ -50,7 +50,9 @@ function Get-Vcpkg-Features {
             # TODO(#8145) - does not compile on Windows.
             "asset",
             # TODO(#8125) - does not compile on Windows.
-            "channel" -contains $_) } |
+            "channel",
+            # TODO(#11772) - deprecated library. Need to remove from vcpkg.
+            "debugger" -contains $_) } |
         # These are convenience features to refactor dependencies; they do not have quickstarts.
         Where-Object { -not ("googleapis", "grpc-common", "grafeas" -contains $_) }
 }
