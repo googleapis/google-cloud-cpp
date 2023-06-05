bd63c5e88ead9c6136305d26c2cedcfcecef8fa2
Mike Schwartz <mfschwartz@google.com>
2023-06-05 17:45:15 -0600

    Add option to set AuthorityOption

--- google/cloud/storage/benchmarks/throughput_options.cc
+++ google/cloud/storage/benchmarks/throughput_options.cc
@@ -14,6 +14,7 @@
 
 #include "google/cloud/storage/benchmarks/throughput_options.h"
 #include "google/cloud/storage/options.h"
+#include "google/cloud/common_options.h"
 #include "google/cloud/grpc_options.h"
 #include "absl/strings/str_split.h"
 #include "absl/time/time.h"
@@ -351,6 +352,11 @@ google::cloud::StatusOr<ThroughputOptions> ParseThroughputOptions(
        "sets the endpoint for gRPC+DirectPath-based benchmarks",
        [&options](std::string const& val) {
          options.direct_path_options.set<EndpointOption>(val);
+       }},
+      {"--gprc-authority-hostname",
+       "sets the ALTS call host for gRPC+DirectPath-based benchmarks",
+       [&options](std::string const& val) {
+         options.direct_path_options.set<AuthorityOption>(val);
        }},
       {"--transfer-stall-timeout",
        "configure `storage::TransferStallTimeoutOption`: the maximum time"
