ffab97b197b9e45fcb00ad4cde9d9b3baa17068a
Carlos O'Ryan <coryan@google.com>
2023-06-10 13:57:59 +0000

    impl(essentialcontacts): merge mishap
    I merge the code without rebasing and regenerating while a separate PR
    changed the generator.

--- google/cloud/essentialcontacts/v1/internal/essential_contacts_stub_factory.cc
+++ google/cloud/essentialcontacts/v1/internal/essential_contacts_stub_factory.cc
@@ -54,7 +54,8 @@ CreateDefaultEssentialContactsServiceStub(google::cloud::CompletionQueue cq,
     stub = std::make_shared<EssentialContactsServiceAuth>(std::move(auth),
                                                           std::move(stub));
   }
-  stub = std::make_shared<EssentialContactsServiceMetadata>(std::move(stub));
+  stub = std::make_shared<EssentialContactsServiceMetadata>(
+      std::move(stub), std::multimap<std::string, std::string>{});
   if (internal::Contains(options.get<TracingComponentsOption>(), "rpc")) {
     GCP_LOG(INFO) << "Enabled logging for gRPC calls";
     stub = std::make_shared<EssentialContactsServiceLogging>(
