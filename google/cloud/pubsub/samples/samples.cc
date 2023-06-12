ec69aa5be3c12c1dfe091e36860888b940df9897
Anna Levenberg <alevenb@google.com>
2023-06-12 15:36:57 +0000

    fix merge errors

--- google/cloud/pubsub/samples/samples.cc
+++ google/cloud/pubsub/samples/samples.cc
@@ -38,8 +38,6 @@ namespace {
 using ::google::cloud::pubsub::examples::CleanupSchemas;
 using ::google::cloud::pubsub::examples::
     CommitSchemaRevisionsForRollbackSchemaTesting;
-using ::google::cloud::pubsub::examples::
-    CommitSchemaRevisionsForRollbackSchemaTesting;
 using ::google::cloud::pubsub::examples::RandomSchemaId;
 using ::google::cloud::pubsub::examples::RandomSnapshotId;
 using ::google::cloud::pubsub::examples::RandomSubscriptionId;
@@ -2078,12 +2076,6 @@ void AutoRunAvro(
 
   std::cout << "\nRunning RollbackSchema sample" << std::endl;
   RollbackSchema(schema_admin, {project_id, avro_schema_id, revision_id});
-
-  std::cout << "\nRunning DeleteSchemaRevision sample" << std::endl;
-  DeleteSchemaRevision(schema_admin, {project_id, avro_schema_id, revision_id});
-
-  // std::cout << "\nRunning RollbackSchema sample" << std::endl;
-  // RollbackSchema(schema_admin, {project_id, avro_schema_id, revision_id});
 
   std::cout << "\nRunning DeleteSchemaRevision sample" << std::endl;
   DeleteSchemaRevision(schema_admin, {project_id, avro_schema_id, revision_id});
