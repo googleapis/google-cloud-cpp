65b4b03444e96ce147b5fcb38cd993966f773762
dbolduc <dbolduc@google.com>
2023-06-12 19:19:55 +0000

    generated changes

--- google/cloud/compute/network_endpoint_groups/v1/mocks/mock_network_endpoint_groups_connection.h
+++ google/cloud/compute/network_endpoint_groups/v1/mocks/mock_network_endpoint_groups_connection.h
@@ -41,7 +41,7 @@ GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
  * While the example showcases types from the BigQuery library, the underlying
  * principles apply for any pair of `*Client` and `*Connection`.
  *
- * [bq-mock]: @googleapis_dev_link{bigquery,bigquery-read-mock.html}
+ * [bq-mock]: @cloud_cpp_docs_link{bigquery,bigquery-read-mock}
  */
 class MockNetworkEndpointGroupsConnection
     : public compute_network_endpoint_groups_v1::
