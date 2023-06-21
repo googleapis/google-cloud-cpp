f7ccdf7ed10bad1f18d1463d2607b36b6204bdc7
Jaya Srinivasan <jsrinivasan@google.com>
2023-06-21 17:20:23 +0000

    impl(bigquery): InsertJobResponse implementation with unit tests

--- google/cloud/bigquery/v2/minimal/internal/job_response_test.cc
+++ google/cloud/bigquery/v2/minimal/internal/job_response_test.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "google/cloud/bigquery/v2/minimal/internal/job_response.h"
+#include "google/cloud/bigquery/v2/minimal/testing/job_test_utils.h"
 #include "google/cloud/testing_util/status_matchers.h"
 #include <gmock/gmock.h>
 #include <sstream>
@@ -21,6 +22,8 @@ namespace google {
 namespace cloud {
 namespace bigquery_v2_minimal_internal {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+using ::google::cloud::bigquery_v2_minimal_testing::MakeJob;
 
 using ::google::cloud::rest_internal::HttpStatusCode;
 using ::google::cloud::testing_util::StatusIs;
@@ -890,6 +893,191 @@ TEST(ListJobsResponseTest, DebugString) {
     payload: REDACTED
   }
 })");
+}
+
+TEST(InsertJobResponseTest, Success) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"configuration":{"dry_run":true,"job_timeout_ms":10)"
+      R"(,"job_type":"QUERY")"
+      R"(,"labels":{"label-key1":"label-val1"})"
+      R"(,"query_config":{"allow_large_results":true)"
+      R"(,"clustering":{"fields":["clustering-field-1")"
+      R"(,"clustering-field-2"]})"
+      R"(,"connection_properties":[{"key":"conn-prop-key")"
+      R"(,"value":"conn-prop-val"}])"
+      R"(,"continuous":true,"create_disposition":"job-create-disposition")"
+      R"(,"create_session":true,"default_dataset":{"dataset_id":"1")"
+      R"(,"project_id":"2"},"destination_encryption_configuration":{)"
+      R"("kms_key_name":"encryption-key-name"},"destination_table":{")"
+      R"(dataset_id":"1","project_id":"2","table_id":"3"})"
+      R"(,"flatten_results":true)"
+      R"(,"maximum_bytes_billed":0,"parameter_mode":"job-param-mode")"
+      R"(,"preserve_nulls":true,"priority":"job-priority","query":"select 1;")"
+      R"(,"query_parameters":[{"name":"query-parameter-name","parameter_type":{)"
+      R"("array_type":{"struct_types":[{"description":"array-struct-description")"
+      R"(,"name":"array-struct-name","type":{"struct_types":[])"
+      R"(,"type":"array-struct-type"}}],"type":"array-type"})"
+      R"(,"struct_types":[{"description":"qp-struct-description")"
+      R"(,"name":"qp-struct-name","type":{"struct_types":[])"
+      R"(,"type":"qp-struct-type"}}])"
+      R"(,"type":"query-parameter-type"},"parameter_value":{)"
+      R"("array_values":[{"array_values":[{"array_values":[],"struct_values":{)"
+      R"("array-map-key":{"array_values":[],"struct_values":{})"
+      R"(,"value":"array-map-value"}})"
+      R"(,"value":"array-val-2"}],"struct_values":{},"value":"array-val-1"}])"
+      R"(,"struct_values":{"qp-map-key":{"array_values":[],"struct_values":{})"
+      R"(,"value":"qp-map-value"}},"value":"query-parameter-value"}}])"
+      R"(,"range_partitioning":{"field":"rp-field-1","range":{"end":"range-end")"
+      R"(,"interval":"range-interval","start":"range-start"}})"
+      R"(,"schema_update_options":["job-update-options"])"
+      R"(,"script_options":{)"
+      R"("key_result_statement":{"value":"FIRST_SELECT"})"
+      R"(,"statement_byte_budget":10)"
+      R"(,"statement_timeout_ms":10},"system_variables":{"types":{)"
+      R"("sql-struct-type-key-1":{"sub_type":{"fields":[{)"
+      R"("name":"f1-sql-struct-type-int64"}]})"
+      R"(,"sub_type_index":2,"type_kind":{"value":"INT64"}})"
+      R"(,"sql-struct-type-key-2":{"sub_type":{"fields":[{)"
+      R"("name":"f2-sql-struct-type-string"}]},"sub_type_index":2)"
+      R"(,"type_kind":{"value":"STRING"}},"sql-struct-type-key-3":{)"
+      R"("sub_type":{"sub_type":{"fields":[{"name":"f2-sql-struct-type-string"}]})"
+      R"(,"sub_type_index":2,"type_kind":{"value":"STRING"}},"sub_type_index":1)"
+      R"(,"type_kind":{"value":"STRING"}}},"values":{"fields":{"bool-key":{)"
+      R"("kind_index":3,"value_kind":true},"double-key":{"kind_index":1)"
+      R"(,"value_kind":3.4},"string-key":{"kind_index":2,"value_kind":"val3"}}}})"
+      R"(,"time_partitioning":{"expiration_time":0,"field":"tp-field-1")"
+      R"(,"type":"tp-field-type"},"use_legacy_sql":true,"use_query_cache":true)"
+      R"(,"write_disposition":"job-write-disposition"}},"etag":"etag","id":"1")"
+      R"(,"kind":"Job","reference":{"job_id":"2","location":"us-east")"
+      R"(,"project_id":"1"},"self_link":"self-link","statistics":{)"
+      R"("completion_ratio":1234.1234,"creation_time":10,"data_masking_applied":true)"
+      R"(,"end_time":10,"final_execution_duration":10,"job_query_stats":{)"
+      R"("billing_tier":1234,"cache_hit":true,"dcl_target_dataset":{"dataset_id":"1")"
+      R"(,"project_id":"2"},"dcl_target_table":{"dataset_id":"1","project_id":"2")"
+      R"(,"table_id":"3"},"dcl_target_view":{"dataset_id":"1","project_id":"2")"
+      R"(,"table_id":"3"},"ddl_affected_row_access_policy_count":1234)"
+      R"(,"ddl_destination_table":{"dataset_id":"1","project_id":"2","table_id":"3"})"
+      R"(,"ddl_operation_performed":"ddl_operation_performed")"
+      R"(,"ddl_target_dataset":{"dataset_id":"1","project_id":"2"})"
+      R"(,"ddl_target_routine":{"dataset_id":"1","project_id":"2","routine_id":"3"})"
+      R"(,"ddl_target_row_access_policy":{"dataset_id":"1","policy_id":"3")"
+      R"(,"project_id":"1234","table_id":"2"},"ddl_target_table":{"dataset_id":"1")"
+      R"(,"project_id":"2","table_id":"3"},"dml_stats":{"deleted_row_count":1234)"
+      R"(,"inserted_row_count":1234,"updated_row_count":1234})"
+      R"(,"estimated_bytes_processed":1234,"materialized_view_statistics":{)"
+      R"("materialized_view":[{"chosen":true,"estimated_bytes_saved":1234)"
+      R"(,"rejected_reason":{"value":"BASE_TABLE_DATA_CHANGE"},"table_reference":{)"
+      R"("dataset_id":"1","project_id":"2","table_id":"3"}}]})"
+      R"(,"metadata_cache_statistics":{"table_metadata_cache_usage":[{)"
+      R"("explanation":"test-table-metadata","table_reference":{"dataset_id":"1")"
+      R"(,"project_id":"2","table_id":"3"},"unused_reason":{)"
+      R"("value":"EXCEEDED_MAX_STALENESS"}}]},"num_dml_affected_rows":1234)"
+      R"(,"performance_insights":{"avg_previous_execution_time":10)"
+      R"(,"stage_performance_change_insights":{"input_data_change":{)"
+      R"("records_read_diff_percentage":12.119999885559082},"stage_id":1234})"
+      R"(,"stage_performance_standalone_insights":{"insufficient_shuffle_quota":true)"
+      R"(,"slot_contention":true,"stage_id":1234}},"query_plan":[{)"
+      R"("completed_parallel_inputs":1234,"compute_avg_time_spent":10)"
+      R"(,"compute_max_time_spent":10,"compute_mode":{"value":"BIGQUERY"})"
+      R"(,"compute_ratio_avg":1234.1234,"compute_ratio_max":1234.1234,"end_time":10)"
+      R"(,"id":1234,"input_stages":[1234],"name":"test-explain")"
+      R"(,"parallel_inputs":1234,"read_avg_time_spent":10,"read_max_time_spent":10)"
+      R"(,"read_ratio_avg":1234.1234,"read_ratio_max":1234.1234,"records_read":1234)"
+      R"(,"records_written":1234,"shuffle_output_bytes":1234)"
+      R"(,"shuffle_output_bytes_spilled":1234,"slot_time":10)"
+      R"(,"start_time":10,"status":"explain-status","steps":[{)"
+      R"("kind":"sub-step-kind","substeps":["sub-step-1"]}])"
+      R"(,"wait_avg_time_spent":10)"
+      R"(,"wait_max_time_spent":10,"wait_ratio_avg":1234.1234)"
+      R"(,"wait_ratio_max":1234.1234,"write_avg_time_spent":10)"
+      R"(,"write_max_time_spent":10,"write_ratio_avg":1234.1234)"
+      R"(,"write_ratio_max":1234.1234}],"referenced_routines":[{)"
+      R"("dataset_id":"1","project_id":"2","routine_id":"3"}])"
+      R"(,"referenced_tables":[{"dataset_id":"1","project_id":"2")"
+      R"(,"table_id":"3"}],"schema":{"fields":[{"categories":{)"
+      R"("names":[]},"collation":"","data_classification_tags":{)"
+      R"("names":[]},"default_value_expression":"")"
+      R"(,"description":"","fields":{"fields":[]})"
+      R"(,"is_measure":true,"max_length":0,"mode":"fmode","name":"fname-1")"
+      R"(,"policy_tags":{"names":[]},"precision":0,"range_element_type":{)"
+      R"("type":""},"rounding_mode":{"value":""},"scale":0,"type":""}]})"
+      R"(,"search_statistics":{"index_unused_reasons":[{"base_table":{)"
+      R"("dataset_id":"1","project_id":"2","table_id":"3"},"code":{)"
+      R"("value":"BASE_TABLE_TOO_SMALL"},"index_name":"test-index")"
+      R"(,"message":""}],"index_usage_mode":{"value":"PARTIALLY_USED"}})"
+      R"(,"statement_type":"statement_type","timeline":[{"active_units":1234)"
+      R"(,"completed_units":1234,"elapsed_time":10)"
+      R"(,"estimated_runnable_units":1234,"pending_units":1234)"
+      R"(,"total_slot_time":10}],"total_bytes_billed":1234)"
+      R"(,"total_bytes_processed":1234)"
+      R"(,"total_bytes_processed_accuracy":"total_bytes_processed_accuracy")"
+      R"(,"total_partitions_processed":1234,"total_slot_time":10)"
+      R"(,"transferred_bytes":1234,"undeclared_query_parameters":[{)"
+      R"("name":"query-parameter-name","parameter_type":{"array_type":{)"
+      R"("struct_types":[{"description":"array-struct-description")"
+      R"(,"name":"array-struct-name","type":{"struct_types":[])"
+      R"(,"type":"array-struct-type"}}],"type":"array-type"})"
+      R"(,"struct_types":[{"description":"qp-struct-description")"
+      R"(,"name":"qp-struct-name","type":{"struct_types":[])"
+      R"(,"type":"qp-struct-type"}}],"type":"query-parameter-type"})"
+      R"(,"parameter_value":{"array_values":[{"array_values":[{"array_values":[])"
+      R"(,"struct_values":{"array-map-key":{"array_values":[],"struct_values":{})"
+      R"(,"value":"array-map-value"}},"value":"array-val-2"}])"
+      R"(,"struct_values":{},"value":"array-val-1"}])"
+      R"(,"struct_values":{"qp-map-key":{"array_values":[],"struct_values":{})"
+      R"(,"value":"qp-map-value"}},"value":"query-parameter-value"}}]})"
+      R"(,"num_child_jobs":1234,"parent_job_id":"parent-job-123")"
+      R"(,"quota_deferments":["quota-defer-1"])"
+      R"(,"reservation_id":"reservation-id-123","row_level_security_applied":true)"
+      R"(,"script_statistics":{"evaluation_kind":{"value":"STATEMENT"})"
+      R"(,"stack_frames":[{"end_column":1234,"end_line":1234)"
+      R"(,"procedure_id":"proc-id","start_column":1234,"start_line":1234)"
+      R"(,"text":"stack-frame-text"}]},"session_id":"session-id-123")"
+      R"(,"start_time":10,"total_bytes_processed":1234)"
+      R"(,"total_modified_partitions":1234,"total_slot_time":10)"
+      R"(,"transaction_id":"transaction-id-123"},"status":{"error_result":{)"
+      R"("location":"","message":"","reason":""},"errors":[])"
+      R"(,"state":"DONE"},"user_email":"a@b.com"})";
+
+  auto const insert_job_response =
+      InsertJobResponse::BuildFromHttpResponse(http_response);
+  ASSERT_STATUS_OK(insert_job_response);
+  EXPECT_FALSE(insert_job_response->http_response.payload.empty());
+
+  auto expected_job = MakeJob();
+  auto actual_job = insert_job_response->job;
+
+  bigquery_v2_minimal_testing::AssertEquals(expected_job, actual_job);
+}
+
+TEST(InsertJobResponseTest, EmptyPayload) {
+  BigQueryHttpResponse http_response;
+  auto const response = InsertJobResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response, StatusIs(StatusCode::kInternal,
+                                 HasSubstr("Empty payload in HTTP response")));
+}
+
+TEST(InsertJobResponseTest, InvalidJson) {
+  BigQueryHttpResponse http_response;
+  http_response.payload = "Help! I am not json";
+  auto const response = InsertJobResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response,
+              StatusIs(StatusCode::kInternal,
+                       HasSubstr("Error parsing Json from response payload")));
+}
+
+TEST(InsertJobResponseTest, InvalidJob) {
+  BigQueryHttpResponse http_response;
+  http_response.payload =
+      R"({"kind": "jkind",
+          "etag": "jtag",
+          "id": "j123",
+          "self_link": "jselfLink",
+          "user_email": "juserEmail"})";
+  auto const response = InsertJobResponse::BuildFromHttpResponse(http_response);
+  EXPECT_THAT(response, StatusIs(StatusCode::kInternal,
+                                 HasSubstr("Not a valid Json Job object")));
 }
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
