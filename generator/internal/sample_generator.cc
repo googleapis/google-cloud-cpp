a2ce19f369d4805029221d7128bf5003dcf3d405
Carlos O'Ryan <coryan@google.com>
2023-06-21 16:59:01 +0000

    Address review comments

--- generator/internal/sample_generator.cc
+++ generator/internal/sample_generator.cc
@@ -143,7 +143,7 @@ void SetRetryPolicy(std::vector<std::string> const& argv) {
     google::cloud::ExperimentalTag{}, connection);
 
   // You can override any of the policies in a new client. This new client
-  // will share the policies from c1 (or c2) *except* from the retry policy.
+  // will share the policies from c1 (or c2) *except* for the retry policy.
   auto c3 = google::cloud::$product_namespace$::$client_class_name$(
     google::cloud::ExperimentalTag{}, connection,
     google::cloud::Options{}.set<google::cloud::$product_namespace$::$retry_policy_name$Option>(
