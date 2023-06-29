40a4c3603790a20ed90fa89f6ff1a5d35407362f
Carlos O'Ryan <coryan@google.com>
2023-06-29 15:01:09 +0000

    ci: retry integration tests on failure

--- ci/cloudbuild/builds/lib/integration.sh
+++ ci/cloudbuild/builds/lib/integration.sh
@@ -148,6 +148,9 @@ function integration::bazel_with_emulators() {
 
   local verb="$1"
   local args=("${@:2}")
+  # Integration tests are inherently flaky. Make up to three attempts to get the
+  # test passing.
+  args+=(--flaky_test_attempts=3)
 
   production_integration_tests=(
     # gRPC Utils integration tests
@@ -271,6 +274,9 @@ function integration::ctest_with_emulators() {
 
   local cmake_out="$1"
   mapfile -t ctest_args < <(ctest::common_args)
+  # The integration tests are inherently flaky.  Try them 3 times before
+  # declaring a failure.
+  ctest_args+=(--repeat until-pass:3)
 
   io::log_h2 "Running Pub/Sub integration tests (with emulator)"
   "google/cloud/pubsub/ci/${EMULATOR_SCRIPT}" \
