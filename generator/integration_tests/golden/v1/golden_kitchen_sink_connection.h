f69ee74b0b98a319f0334b5dd104303524e9619b
Carlos O'Ryan <coryan@google.com>
2023-06-29 22:49:07 +0000

    Update golden files

--- generator/integration_tests/golden/v1/golden_kitchen_sink_connection.h
+++ generator/integration_tests/golden/v1/golden_kitchen_sink_connection.h
@@ -38,15 +38,108 @@ namespace cloud {
 namespace golden_v1 {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
-using GoldenKitchenSinkRetryPolicy = ::google::cloud::internal::TraitBasedRetryPolicy<
-    golden_v1_internal::GoldenKitchenSinkRetryTraits>;
 
-using GoldenKitchenSinkLimitedTimeRetryPolicy = ::google::cloud::internal::LimitedTimeRetryPolicy<
-    golden_v1_internal::GoldenKitchenSinkRetryTraits>;
+/// The retry policy for `GoldenKitchenSinkConnection`.
+class GoldenKitchenSinkRetryPolicy : public ::google::cloud::RetryPolicy {
+  public:
+    /// Creates a new instance with the initial state, as-if no errors had been
+    /// handled.
+    virtual std::unique_ptr<GoldenKitchenSinkRetryPolicy> clone() const = 0;
+};
 
-using GoldenKitchenSinkLimitedErrorCountRetryPolicy =
-    ::google::cloud::internal::LimitedErrorCountRetryPolicy<
-        golden_v1_internal::GoldenKitchenSinkRetryTraits>;
+/**
+ * A retry policy for `GoldenKitchenSinkConnection` that stops retrying after a
+ * prescribed number of transient errors (or the first non-transient error).
+ *
+ * @note You can set the number of errors to 0 (or 1) to disable the retry loop.
+ */
+class GoldenKitchenSinkLimitedErrorCountRetryPolicy : public GoldenKitchenSinkRetryPolicy {
+ public:
+  GoldenKitchenSinkLimitedErrorCountRetryPolicy(int maximum_failures)
+    : impl_(maximum_failures) {}
+
+  GoldenKitchenSinkLimitedErrorCountRetryPolicy(
+      GoldenKitchenSinkLimitedErrorCountRetryPolicy&& rhs) noexcept
+    : GoldenKitchenSinkLimitedErrorCountRetryPolicy(rhs.maximum_failures()) {}
+  GoldenKitchenSinkLimitedErrorCountRetryPolicy(
+      GoldenKitchenSinkLimitedErrorCountRetryPolicy const& rhs) noexcept
+    : GoldenKitchenSinkLimitedErrorCountRetryPolicy(rhs.maximum_failures()) {}
+
+  int maximum_failures() const { return impl_.maximum_failures(); }
+
+  bool OnFailure(Status const& status) override {
+    return impl_.OnFailure(status);
+  }
+  bool IsExhausted() const override { return impl_.IsExhausted(); }
+  bool IsPermanentFailure(Status const& status) const override {
+    return impl_.IsPermanentFailure(status);
+  }
+  std::unique_ptr<GoldenKitchenSinkRetryPolicy> clone() const override {
+    return std::make_unique<GoldenKitchenSinkLimitedErrorCountRetryPolicy>(
+        maximum_failures());
+  }
+
+  // This is provided only for backwards compatibility.
+  using BaseType = GoldenKitchenSinkRetryPolicy;
+ private:
+  google::cloud::internal::LimitedErrorCountRetryPolicy<golden_v1_internal::GoldenKitchenSinkRetryTraits> impl_;
+};
+
+/// A retry policy for `GoldenKitchenSinkConnection` that stops retrying after
+/// some wall clock time has elapsed.
+class GoldenKitchenSinkLimitedTimeRetryPolicy : public GoldenKitchenSinkRetryPolicy {
+ public:
+  /**
+   * Constructor given a `std::chrono::duration<>` object.
+   *
+   * @tparam DurationRep a placeholder to match the `Rep` tparam for @p
+   *     duration's type. The semantics of this template parameter are
+   *     documented in `std::chrono::duration<>` (in brief, the underlying
+   *     arithmetic type used to store the number of ticks), for our purposes it
+   *     is simply a formal parameter.
+   * @tparam DurationPeriod a placeholder to match the `Period` tparam for @p
+   *     duration's type. The semantics of this template parameter are
+   *     documented in `std::chrono::duration<>` (in brief, the length of the
+   *     tick in seconds, expressed as a `std::ratio<>`), for our purposes it is
+   *     simply a formal parameter.
+   * @param maximum_duration the maximum time allowed before the policy expires,
+   *     while the application can express this time in any units they desire,
+   *     the class truncates to milliseconds.
+   *
+   * @see https://en.cppreference.com/w/cpp/chrono/duration for more information
+   *     about `std::chrono::duration`.
+   */
+  template <typename DurationRep, typename DurationPeriod>
+  explicit GoldenKitchenSinkLimitedTimeRetryPolicy(
+      std::chrono::duration<DurationRep, DurationPeriod> maximum_duration)
+    : impl_(maximum_duration) {}
+
+  GoldenKitchenSinkLimitedTimeRetryPolicy(GoldenKitchenSinkLimitedTimeRetryPolicy&& rhs) noexcept
+    : GoldenKitchenSinkLimitedTimeRetryPolicy(rhs.maximum_duration()) {}
+  GoldenKitchenSinkLimitedTimeRetryPolicy(GoldenKitchenSinkLimitedTimeRetryPolicy const& rhs) noexcept
+    : GoldenKitchenSinkLimitedTimeRetryPolicy(rhs.maximum_duration()) {}
+
+  std::chrono::milliseconds maximum_duration() const {
+    return impl_.maximum_duration();
+  }
+
+  bool OnFailure(Status const& status) override {
+    return impl_.OnFailure(status);
+  }
+  bool IsExhausted() const override { return impl_.IsExhausted(); }
+  bool IsPermanentFailure(Status const& status) const override {
+    return impl_.IsPermanentFailure(status);
+  }
+  std::unique_ptr<GoldenKitchenSinkRetryPolicy> clone() const override {
+    return std::make_unique<GoldenKitchenSinkLimitedTimeRetryPolicy>(
+        maximum_duration());
+  }
+
+  // This is provided only for backwards compatibility.
+  using BaseType = GoldenKitchenSinkRetryPolicy;
+ private:
+  google::cloud::internal::LimitedTimeRetryPolicy<golden_v1_internal::GoldenKitchenSinkRetryTraits> impl_;
+};
 
 /**
  * The `GoldenKitchenSinkConnection` object for `GoldenKitchenSinkClient`.
--- generator/integration_tests/golden/v1/golden_rest_only_connection.h
+++ generator/integration_tests/golden/v1/golden_rest_only_connection.h
@@ -34,15 +34,108 @@ namespace cloud {
 namespace golden_v1 {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
-using GoldenRestOnlyRetryPolicy = ::google::cloud::internal::TraitBasedRetryPolicy<
-    golden_v1_internal::GoldenRestOnlyRetryTraits>;
 
-using GoldenRestOnlyLimitedTimeRetryPolicy = ::google::cloud::internal::LimitedTimeRetryPolicy<
-    golden_v1_internal::GoldenRestOnlyRetryTraits>;
+/// The retry policy for `GoldenRestOnlyConnection`.
+class GoldenRestOnlyRetryPolicy : public ::google::cloud::RetryPolicy {
+  public:
+    /// Creates a new instance with the initial state, as-if no errors had been
+    /// handled.
+    virtual std::unique_ptr<GoldenRestOnlyRetryPolicy> clone() const = 0;
+};
 
-using GoldenRestOnlyLimitedErrorCountRetryPolicy =
-    ::google::cloud::internal::LimitedErrorCountRetryPolicy<
-        golden_v1_internal::GoldenRestOnlyRetryTraits>;
+/**
+ * A retry policy for `GoldenRestOnlyConnection` that stops retrying after a
+ * prescribed number of transient errors (or the first non-transient error).
+ *
+ * @note You can set the number of errors to 0 (or 1) to disable the retry loop.
+ */
+class GoldenRestOnlyLimitedErrorCountRetryPolicy : public GoldenRestOnlyRetryPolicy {
+ public:
+  GoldenRestOnlyLimitedErrorCountRetryPolicy(int maximum_failures)
+    : impl_(maximum_failures) {}
+
+  GoldenRestOnlyLimitedErrorCountRetryPolicy(
+      GoldenRestOnlyLimitedErrorCountRetryPolicy&& rhs) noexcept
+    : GoldenRestOnlyLimitedErrorCountRetryPolicy(rhs.maximum_failures()) {}
+  GoldenRestOnlyLimitedErrorCountRetryPolicy(
+      GoldenRestOnlyLimitedErrorCountRetryPolicy const& rhs) noexcept
+    : GoldenRestOnlyLimitedErrorCountRetryPolicy(rhs.maximum_failures()) {}
+
+  int maximum_failures() const { return impl_.maximum_failures(); }
+
+  bool OnFailure(Status const& status) override {
+    return impl_.OnFailure(status);
+  }
+  bool IsExhausted() const override { return impl_.IsExhausted(); }
+  bool IsPermanentFailure(Status const& status) const override {
+    return impl_.IsPermanentFailure(status);
+  }
+  std::unique_ptr<GoldenRestOnlyRetryPolicy> clone() const override {
+    return std::make_unique<GoldenRestOnlyLimitedErrorCountRetryPolicy>(
+        maximum_failures());
+  }
+
+  // This is provided only for backwards compatibility.
+  using BaseType = GoldenRestOnlyRetryPolicy;
+ private:
+  google::cloud::internal::LimitedErrorCountRetryPolicy<golden_v1_internal::GoldenRestOnlyRetryTraits> impl_;
+};
+
+/// A retry policy for `GoldenRestOnlyConnection` that stops retrying after
+/// some wall clock time has elapsed.
+class GoldenRestOnlyLimitedTimeRetryPolicy : public GoldenRestOnlyRetryPolicy {
+ public:
+  /**
+   * Constructor given a `std::chrono::duration<>` object.
+   *
+   * @tparam DurationRep a placeholder to match the `Rep` tparam for @p
+   *     duration's type. The semantics of this template parameter are
+   *     documented in `std::chrono::duration<>` (in brief, the underlying
+   *     arithmetic type used to store the number of ticks), for our purposes it
+   *     is simply a formal parameter.
+   * @tparam DurationPeriod a placeholder to match the `Period` tparam for @p
+   *     duration's type. The semantics of this template parameter are
+   *     documented in `std::chrono::duration<>` (in brief, the length of the
+   *     tick in seconds, expressed as a `std::ratio<>`), for our purposes it is
+   *     simply a formal parameter.
+   * @param maximum_duration the maximum time allowed before the policy expires,
+   *     while the application can express this time in any units they desire,
+   *     the class truncates to milliseconds.
+   *
+   * @see https://en.cppreference.com/w/cpp/chrono/duration for more information
+   *     about `std::chrono::duration`.
+   */
+  template <typename DurationRep, typename DurationPeriod>
+  explicit GoldenRestOnlyLimitedTimeRetryPolicy(
+      std::chrono::duration<DurationRep, DurationPeriod> maximum_duration)
+    : impl_(maximum_duration) {}
+
+  GoldenRestOnlyLimitedTimeRetryPolicy(GoldenRestOnlyLimitedTimeRetryPolicy&& rhs) noexcept
+    : GoldenRestOnlyLimitedTimeRetryPolicy(rhs.maximum_duration()) {}
+  GoldenRestOnlyLimitedTimeRetryPolicy(GoldenRestOnlyLimitedTimeRetryPolicy const& rhs) noexcept
+    : GoldenRestOnlyLimitedTimeRetryPolicy(rhs.maximum_duration()) {}
+
+  std::chrono::milliseconds maximum_duration() const {
+    return impl_.maximum_duration();
+  }
+
+  bool OnFailure(Status const& status) override {
+    return impl_.OnFailure(status);
+  }
+  bool IsExhausted() const override { return impl_.IsExhausted(); }
+  bool IsPermanentFailure(Status const& status) const override {
+    return impl_.IsPermanentFailure(status);
+  }
+  std::unique_ptr<GoldenRestOnlyRetryPolicy> clone() const override {
+    return std::make_unique<GoldenRestOnlyLimitedTimeRetryPolicy>(
+        maximum_duration());
+  }
+
+  // This is provided only for backwards compatibility.
+  using BaseType = GoldenRestOnlyRetryPolicy;
+ private:
+  google::cloud::internal::LimitedTimeRetryPolicy<golden_v1_internal::GoldenRestOnlyRetryTraits> impl_;
+};
 
 /**
  * The `GoldenRestOnlyConnection` object for `GoldenRestOnlyClient`.
