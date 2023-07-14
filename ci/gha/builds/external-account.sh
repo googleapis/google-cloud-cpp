faacee182b0b838507d95e9a0d671cf88eb6e783
Carlos O'Ryan <coryan@google.com>
2023-07-14 16:11:18 -0400

    ci(gha): restructure external accounts build (#12095)
    Restructure the external-accounts build to support additional builds in
    the near future. All the steps to prepare the VM running the build are
    in the workflow file. The steps to compile the code are in a shell
    script, which follows the structure of analogous scripts for the
    GCB-based builds.
    
    This build is still "special" in that it runs one integration test from
    the workflow file. We may want to refactor that in the future.

---
+++ ci/gha/builds/external-account.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+source "$(dirname "$0")/../../lib/init.sh"
+source module ci/gha/builds/lib/linux.sh
+source module ci/gha/builds/lib/cmake.sh
+
+mapfile -t args < <(cmake::common_args)
+mapfile -t vcpkg_args < <(cmake::vcpkg_args)
+mapfile -t ctest_args < <(ctest::common_args)
+
+# This is a build to test External Accounts. This is a feature to use accounts
+# from providers other than Google to access Google services. In this case we
+# are using "GitHub Actions" as the provider.
+# The External Accounts feature is sometimes known as Workload Identity
+# Federation, and sometimes BYOID (Bring Your Own ID).
+features=(
+  # Enable the smallest set of libraries libraries that will compile gRPC and
+  # REST-based authentication components
+  storage
+  iam
+)
+enable=$(printf ";%s" "${features[@]}")
+enable=${enable:1}
+
+io::log_h1 "Starting Build"
+TIMEFORMAT="==> 🕑 CMake configuration done in %R seconds"
+time {
+  io::run cmake "${args[@]}" "${vcpkg_args[@]}" -DGOOGLE_CLOUD_CPP_ENABLE="${enable}"
+}
+
+TIMEFORMAT="==> 🕑 CMake build done in %R seconds"
+time {
+  # Compile only the integration test we need for this build
+  io::run cmake --build cmake-out --target common_internal_external_account_integration_test
+}
---
+++ ci/gha/builds/lib/cmake.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Make our include guard clean against set -o nounset.
+test -n "${CI_CLOUDBUILD_BUILDS_LIB_CMAKE_SH__:-}" || declare -i CI_CLOUDBUILD_BUILDS_LIB_CMAKE_SH__=0
+if ((CI_CLOUDBUILD_BUILDS_LIB_CMAKE_SH__++ != 0)); then
+  return 0
+fi # include guard
+
+source module ci/lib/io.sh
+
+io::log_h1 "CMake Info"
+printf "%10s %s\n" "cmake:" "$(cmake --version || echo not available)"
+printf "%10s %s\n" "ninja:" "$(ninja --version || echo not available)"
+printf "%10s %s\n" "sccache:" "$(sccache --version || echo not available)"
+
+if command -v sccache >/dev/null 2>&1; then
+  io::log "Clearing sccache stats"
+  sccache --zero-stats
+  function show_stats_handler() {
+    io::log "Final sccache stats"
+    sccache --show-stats
+  }
+  trap show_stats_handler EXIT
+fi
+
+function cmake::common_args() {
+  local binary="cmake-out"
+  if [[ $# -ge 1 ]]; then
+    binary="$1"
+  fi
+  local args
+  args=(
+    -DGOOGLE_CLOUD_CPP_ENABLE_CCACHE=OFF
+    -DGOOGLE_CLOUD_CPP_ENABLE_WERROR=ON
+    -GNinja
+    -S .
+    -B "${binary}"
+  )
+  if command -v sccache >/dev/null 2>&1; then
+    args+=(
+      -DCMAKE_CXX_COMPILER_LAUNCHER=sccache
+    )
+  fi
+  printf "%s\n" "${args[@]}"
+}
+
+function cmake::vcpkg_args() {
+  local args
+  args=(
+    -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+    # The version of vcpkg we are using ships with Protobuf v21.x, the
+    # workarounds are required until v23.x.
+    -DGOOGLE_CLOUD_CPP_ENABLE_CTYPE_CORD_WORKAROUND=ON
+  )
+  printf "%s\n" "${args[@]}"
+}
+
+function ctest::common_args() {
+  local args
+  args=(
+    # Print the full output on failures
+    --output-on-failure
+    # Run many tests in parallel, use -j for compatibility with old versions
+    -j "$(os::cpus)"
+    # Make the output shorter on interactive tests
+    --progress
+  )
+  printf "%s\n" "${args[@]}"
+}
