aa07e051962d374db54c6d18b463ef49692233eb
dbolduc <dbolduc@google.com>
2023-07-20 07:23:12 +0000

    pick up ##12172

--- google/cloud/speech/v1/internal/adaptation_logging_decorator.cc
+++ google/cloud/speech/v1/internal/adaptation_logging_decorator.cc
@@ -29,10 +29,10 @@ GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
 AdaptationLogging::AdaptationLogging(std::shared_ptr<AdaptationStub> child,
                                      TracingOptions tracing_options,
-                                     std::set<std::string> components)
+                                     std::set<std::string> const& components)
     : child_(std::move(child)),
       tracing_options_(std::move(tracing_options)),
-      components_(std::move(components)) {}
+      stream_logging_(components.find("rpc-streams") != components.end()) {}
 
 StatusOr<google::cloud::speech::v1::PhraseSet>
 AdaptationLogging::CreatePhraseSet(
--- google/cloud/speech/v1/internal/adaptation_logging_decorator.h
+++ google/cloud/speech/v1/internal/adaptation_logging_decorator.h
@@ -36,7 +36,7 @@ class AdaptationLogging : public AdaptationStub {
   ~AdaptationLogging() override = default;
   AdaptationLogging(std::shared_ptr<AdaptationStub> child,
                     TracingOptions tracing_options,
-                    std::set<std::string> components);
+                    std::set<std::string> const& components);
 
   StatusOr<google::cloud::speech::v1::PhraseSet> CreatePhraseSet(
       grpc::ClientContext& context,
@@ -88,7 +88,7 @@ class AdaptationLogging : public AdaptationStub {
  private:
   std::shared_ptr<AdaptationStub> child_;
   TracingOptions tracing_options_;
-  std::set<std::string> components_;
+  bool stream_logging_;
 };  // AdaptationLogging
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
