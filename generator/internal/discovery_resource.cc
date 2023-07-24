1666f0f498c2f5249e866034e09f0c50f3fb14b1
Scott Hart <sdhart@google.com>
2023-07-24 20:15:20 +0000

    impl(generator): emit implicit api http paths for compute (#12202)

--- generator/internal/discovery_resource.cc
+++ generator/internal/discovery_resource.cc
@@ -101,8 +101,7 @@ std::string DiscoveryResource::FormatUrlPath(std::string const& path) {
     current = open + 1;
     auto close = path.find('}', current);
     absl::StrAppend(
-        &output, CamelCaseToSnakeCase(path.substr(current, close - current)),
-        "=", CamelCaseToSnakeCase(path.substr(current, close - current)));
+        &output, CamelCaseToSnakeCase(path.substr(current, close - current)));
     current = close;
   }
   absl::StrAppend(&output, path.substr(current));
