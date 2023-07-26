d63c25f1e8d13cb4aa40d34e7788add8bbde7f34
dbolduc <dbolduc@google.com>
2023-07-26 21:15:17 +0000

    cleanup(docs): preposition nit

--- generator/internal/sample_generator.cc
+++ generator/internal/sample_generator.cc
@@ -164,7 +164,7 @@ void SetRetryPolicy(std::vector<std::string> const& argv) {
   auto c2 = google::cloud::$product_namespace$::$client_class_name$(connection);
 
   // You can override any of the policies in a new client. This new client
-  // will share the policies from c1 (or c2) *except* from the retry policy.
+  // will share the policies from c1 (or c2) *except* for the retry policy.
   auto c3 = google::cloud::$product_namespace$::$client_class_name$(
     connection, google::cloud::Options{}.set<google::cloud::$product_namespace$::$retry_policy_name$Option>(
       google::cloud::$product_namespace$::$limited_time_retry_policy_name$(std::chrono::minutes(5)).clone()));
