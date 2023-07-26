3d71df39f1e57bc1951d6841318e7a494054c7db
Scott Hart <sdhart@google.com>
2023-07-26 13:15:37 -0400

    impl(generator): use previous gen protos to determine field numbers

--- generator/internal/discovery_type_vertex.h
+++ generator/internal/discovery_type_vertex.h
@@ -78,6 +78,7 @@ class DiscoveryTypeVertex {
       nlohmann::json const& v, std::string const& field_name);
 
   struct MessageProperties {
+    std::vector<std::string> lines;
     std::set<int> reserved_numbers;
     int next_available_field_number;
   };
@@ -89,26 +90,28 @@ class DiscoveryTypeVertex {
       google::protobuf::Descriptor const& message_descriptor);
 
   // Formats the properties of the json into proto message fields.
-  StatusOr<std::vector<std::string>> FormatProperties(
+  StatusOr<MessageProperties> FormatProperties(
       std::map<std::string, DiscoveryTypeVertex> const& types,
-      std::string const& message_name, std::string const& file_package_name,
-      nlohmann::json const& json, int indent_level) const;
+      std::string const& message_name,
+      std::string const& qualified_message_name,
+      std::string const& file_package_name, nlohmann::json const& json,
+      int indent_level) const;
 
   StatusOr<std::string> FormatMessage(
       std::map<std::string, DiscoveryTypeVertex> const& types,
-      std::string const& name, std::string const& package_name,
-
-      nlohmann::json const& json, int indent_level) const;
+      std::string const& name, std::string const& qualified_name,
+      std::string const& package_name, nlohmann::json const& json,
+      int indent_level) const;
 
   // Formats any field options as indicated by the field_json.
   static std::string FormatFieldOptions(std::string const& field_name,
                                         nlohmann::json const& field_json);
 
   // Determines the correct field_number to use for the specified field.
-  static StatusOr<int> GetFieldNumber(std::string const& message_name,
-                                      std::string const& field_name,
-                                      std::string const& field_type,
-                                      int field_number);
+  static StatusOr<int> GetFieldNumber(
+      google::protobuf::Descriptor const* message_descriptor,
+      std::string const& field_name, std::string const& field_type,
+      int candidate_field_number);
 
   // Emits the protobuf message definition for this type.
   StatusOr<std::string> JsonToProtobufMessage(
@@ -121,6 +124,7 @@ class DiscoveryTypeVertex {
   std::string name_;
   std::string package_name_;
   nlohmann::json json_;
+  google::protobuf::DescriptorPool const* const descriptor_pool_;
   std::set<std::string> needs_;
   std::set<std::string> needed_by_;
 };
