0f257bf89af307c2c6ba2e3d1cc2915513c20bdb
jsrinnn <114950032+jsrinnn@users.noreply.github.com>
2023-07-30 17:46:01 -0700

    impl(bigquery): Response object for GetQueryResults api (#12249)

--- google/cloud/bigquery/v2/minimal/internal/job_response.cc
+++ google/cloud/bigquery/v2/minimal/internal/job_response.cc
@@ -304,6 +304,105 @@ void from_json(nlohmann::json const& j, QueryResults& q) {
   SafeGetTo(q.dml_stats, j, "dmlStats");
 }
 
+void to_json(nlohmann::json& j, GetQueryResults const& q) {
+  j = nlohmann::json{{"kind", q.kind},
+                     {"etag", q.etag},
+                     {"pageToken", q.page_token},
+                     {"totalRows", q.total_rows},
+                     {"totalBytesProcessed", q.total_bytes_processed},
+                     {"numDmlAffectedRows", q.num_dml_affected_rows},
+                     {"jobComplete", q.job_complete},
+                     {"cacheHit", q.cache_hit},
+                     {"schema", q.schema},
+                     {"jobReference", q.job_reference},
+                     {"rows", q.rows},
+                     {"errors", q.errors}};
+}
+void from_json(nlohmann::json const& j, GetQueryResults& q) {
+  SafeGetTo(q.kind, j, "kind");
+  SafeGetTo(q.etag, j, "etag");
+  SafeGetTo(q.page_token, j, "pageToken");
+  SafeGetTo(q.total_rows, j, "totalRows");
+  SafeGetTo(q.total_bytes_processed, j, "totalBytesProcessed");
+  SafeGetTo(q.num_dml_affected_rows, j, "numDmlAffectedRows");
+  SafeGetTo(q.job_complete, j, "jobComplete");
+  SafeGetTo(q.cache_hit, j, "cacheHit");
+  SafeGetTo(q.schema, j, "schema");
+  SafeGetTo(q.job_reference, j, "jobReference");
+  SafeGetTo(q.rows, j, "rows");
+  SafeGetTo(q.errors, j, "errors");
+}
+
+std::string GetQueryResults::DebugString(absl::string_view name,
+                                         TracingOptions const& options,
+                                         int indent) const {
+  return internal::DebugFormatter(name, options, indent)
+      .StringField("kind", kind)
+      .StringField("etag", etag)
+      .StringField("page_token", page_token)
+      .Field("total_rows", total_rows)
+      .Field("total_bytes_processed", total_bytes_processed)
+      .Field("num_dml_affected_rows", num_dml_affected_rows)
+      .Field("job_complete", job_complete)
+      .Field("cache_hit", cache_hit)
+      .Field("rows", rows)
+      .Field("errors", errors)
+      .SubMessage("schema", schema)
+      .SubMessage("job_reference", job_reference)
+      .Build();
+}
+
+std::string GetQueryResultsResponse::DebugString(absl::string_view name,
+                                                 TracingOptions const& options,
+                                                 int indent) const {
+  return internal::DebugFormatter(name, options, indent)
+      .SubMessage("http_response", http_response)
+      .SubMessage("get_query_results", get_query_results)
+      .Build();
+}
+
+StatusOr<GetQueryResultsResponse>
+GetQueryResultsResponse::BuildFromHttpResponse(
+    BigQueryHttpResponse const& http_response) {
+  auto json = parse_json(http_response.payload);
+  if (!json) return std::move(json).status();
+
+  GetQueryResults get_query_results;
+  get_query_results.kind = json->value("kind", "");
+  get_query_results.etag = json->value("etag", "");
+  get_query_results.page_token = json->value("pageToken", "");
+  get_query_results.total_rows = json->at("totalRows").get<std::uint64_t>();
+  get_query_results.total_bytes_processed =
+      json->at("totalBytesProcessed").get<std::int64_t>();
+  get_query_results.num_dml_affected_rows =
+      json->at("numDmlAffectedRows").get<std::int64_t>();
+
+  get_query_results.job_complete = json->at("jobComplete").get<bool>();
+  get_query_results.cache_hit = json->at("cacheHit").get<bool>();
+
+  get_query_results.schema = json->at("schema").get<TableSchema>();
+  get_query_results.job_reference =
+      json->at("jobReference").get<JobReference>();
+
+  for (auto const& kv : json->at("rows").items()) {
+    auto const& json_struct_obj = kv.value();
+    auto const& row = json_struct_obj.get<Struct>();
+    get_query_results.rows.push_back(row);
+  }
+
+  for (auto const& kv : json->at("errors").items()) {
+    auto const& json_error_proto_obj = kv.value();
+    auto const& error = json_error_proto_obj.get<ErrorProto>();
+    get_query_results.errors.push_back(error);
+  }
+
+  GetQueryResultsResponse response;
+  response.http_response = http_response;
+  response.get_query_results = get_query_results;
+
+  return response;
+}
+
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
 }  // namespace bigquery_v2_minimal_internal
 }  // namespace cloud
