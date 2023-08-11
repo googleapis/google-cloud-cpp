64f8d784ee3d178d2a5555509756483406b2ca3b
Carlos O'Ryan <coryan@google.com>
2023-08-11 11:14:40 -0400

    docs(GCS+gRPC): sample showing transport detection (#12367)

--- google/cloud/storage/examples/storage_grpc_samples.cc
+++ google/cloud/storage/examples/storage_grpc_samples.cc
@@ -23,6 +23,8 @@
 #include "google/cloud/options.h"
 
 //! [grpc-includes] [END storage_grpc_quickstart]
+#include <iostream>
+#include <map>
 
 namespace {
 namespace examples = ::google::cloud::storage::examples;
@@ -101,6 +103,69 @@ void GrpcClientWithProjectCommand(std::vector<std::string> argv) {
   GrpcClientWithProject(argv[0]);
 }
 
+void GrpcReportTransportCommand(std::vector<std::string> argv) {
+  if (argv.size() != 2 || argv[0] == "--help") {
+    throw examples::Usage("grpc-report-transport <config> <project-id>");
+  }
+  auto client = [&] {
+    namespace gcs = ::google::cloud::storage;
+#if GOOGLE_CLOUD_CPP_STORAGE_HAVE_GRPC
+    namespace g = ::google::cloud;
+    if (argv[0] == "GRPC") {
+      return google::cloud::storage_experimental::DefaultGrpcClient();
+    }
+    if (argv[0] == "DP") {
+      // Some documentation calls this `DirectPath`
+      return google::cloud::storage_experimental::DefaultGrpcClient(
+          g::Options{}.set<g::EndpointOption>(
+              "google-c2p:///storage.googleapis.com"));
+    }
+#endif  // GOOGLE_CLOUD_CPP_STORAGE_HAVE_GRPC
+    return gcs::Client();
+  }();
+
+  //! [report-transport]
+  namespace g = ::google::cloud;
+  namespace gcs = ::google::cloud::storage;
+  [](gcs::Client& client, std::string const& bucket_name) {
+    auto constexpr kText = R"""(Hello World!)""";
+    auto constexpr kObjectName = "hello-world.txt";
+
+    // Reports the transport used for a transfer. This can be useful when
+    // troubleshooting the application and VM config. One may want to verify
+    // that the client library is using DirectPath instead of falling back to
+    // plain gRPC.
+    auto transport = [](std::multimap<std::string, std::string> const& headers)
+        -> std::string {
+      auto l = headers.find(":curl-peer");
+      if (l != headers.end()) return "HTTP";
+      l = headers.lower_bound(":grpc-context-peer");
+      if (l == headers.end()) return "UNKNOWN";
+      auto const& peer = l->second;
+      if (peer.rfind("ipv6:[2001:4860:8040:", 0) == 0) return "DP";  //! NOLINT
+      if (peer.rfind("ipv4:34.126.", 0) == 0) return "DP";           //! NOLINT
+      return "GRPC";
+    };
+
+    auto os = client.WriteObject(bucket_name, kObjectName);
+    os << kText;
+    os.Close();
+    auto object = os.metadata();
+    if (!object) throw std::move(object).status();
+    std::cout << "Object successfully uploaded using the "
+              << transport(os.headers()) << " transport\n";
+
+    auto is = client.ReadObject(bucket_name, kObjectName,
+                                gcs::Generation(object->generation()));
+    std::string const actual(std::istreambuf_iterator<char>{is}, {});
+    if (is.bad()) throw g::Status(is.status());
+    std::cout << "Object successfully downloaded using the "
+              << transport(is.headers()) << " transport\n";
+  }
+  //! [report-transport]
+  (client, argv.at(1));
+}
+
 void AutoRun(std::vector<std::string> const& argv) {
   if (!argv.empty()) throw examples::Usage{"auto"};
   examples::CheckEnvironmentVariablesAreSet({
@@ -122,6 +187,12 @@ void AutoRun(std::vector<std::string> const& argv) {
 
   std::cout << "Running GrpcClientWithProject() example" << std::endl;
   GrpcClientWithProjectCommand({project_id});
+
+  std::cout << "Running GrpcReportTransport() example [1]" << std::endl;
+  GrpcReportTransportCommand({"HTTP", bucket_name});
+
+  std::cout << "Running GrpcReportTransport() example [2]" << std::endl;
+  GrpcReportTransportCommand({"GRPC", bucket_name});
 }
 
 }  // namespace
@@ -131,6 +202,7 @@ int main(int argc, char* argv[]) {
       {"grpc-read-write", GrpcReadWriteCommand},
       {"grpc-client-with-dp", GrpcClientWithDPCommand},
       {"grpc-client-with-project", GrpcClientWithProjectCommand},
+      {"grpc-report-transport", GrpcReportTransportCommand},
       {"auto", AutoRun},
   });
   return example.Run(argc, argv);
