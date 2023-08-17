ec67c615f4b29db1a294b36984dee1eba777933d
Anna Levenberg <alevenb@google.com>
2023-08-17 11:06:48 -0400

    respond to comments

--- google/cloud/internal/rest_carrier.cc
+++ google/cloud/internal/rest_carrier.cc
@@ -32,7 +32,7 @@ opentelemetry::nostd::string_view RestCarrier::Get(
 
 void RestCarrier::Set(opentelemetry::nostd::string_view key,
                       opentelemetry::nostd::string_view value) noexcept {
-  internal::NoExceptAction([this, &key, &value] {
+  internal::NoExceptAction([this, key, value] {
     context_.AddHeader(std::string(key.data(), key.size()),
                        std::string(value.data(), value.size()));
   });
