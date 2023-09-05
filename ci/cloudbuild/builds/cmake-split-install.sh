f06c53253b54bc64184e9af77610b428b6838b85
Scott Hart <sdhart@google.com>
2023-09-05 14:42:20 -0400

    impl(compute): install compute libs per service

--- ci/cloudbuild/builds/cmake-split-install.sh
+++ ci/cloudbuild/builds/cmake-split-install.sh
@@ -52,14 +52,25 @@ mapfile -t feature_cmake_args < <(cmake::common_args cmake-out/features)
 io::run cmake "${feature_cmake_args[@]}" "${install_args[@]}" \
   -DCMAKE_PREFIX_PATH="${INSTALL_PREFIX}" \
   -DGOOGLE_CLOUD_CPP_USE_INSTALLED_COMMON=ON \
-  -DGOOGLE_CLOUD_CPP_ENABLE="__ga_libraries__,-bigtable,-iam,-pubsub,-storage,-spanner"
+  -DGOOGLE_CLOUD_CPP_ENABLE="__ga_libraries__,-bigtable,-iam,-pubsub,-storage,-spanner,-compute"
 io::run cmake --build cmake-out/features
 io::run cmake --install cmake-out/features --prefix "${INSTALL_PREFIX}"
+
+io::log_h2 "Building and installing Compute"
+# We need a custom build directory
+mapfile -t feature_cmake_args < <(cmake::common_args cmake-out/compute)
+io::run cmake "${feature_cmake_args[@]}" "${install_args[@]}" \
+  -DCMAKE_PREFIX_PATH="${INSTALL_PREFIX}" \
+  -DGOOGLE_CLOUD_CPP_USE_INSTALLED_COMMON=ON \
+  -DGOOGLE_CLOUD_CPP_ENABLE="compute"
+io::run cmake --build cmake-out/compute
+io::run cmake --install cmake-out/compute --prefix "${INSTALL_PREFIX}"
 
 # Tests the installed artifacts by building all the quickstarts.
 # shellcheck disable=SC2046
 mapfile -t feature_list < <(cmake -DCMAKE_MODULE_PATH="${PWD}/cmake" -P cmake/print-ga-libraries.cmake 2>&1)
 FEATURES=$(printf ";%s" "${feature_list[@]}")
+FEATURES="${FEATURES};compute"
 FEATURES="${FEATURES:1}"
 cmake -G Ninja \
   -S "${PROJECT_ROOT}/ci/verify_quickstart" \
