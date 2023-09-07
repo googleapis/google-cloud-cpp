962ec11d157bd08bd0783d2f1107b0539b77c4cc
Carlos O'Ryan <coryan@google.com>
2023-09-07 12:41:07 +0000

    cleanup(GCS+gRPC): remove async StartResumableUpload
    The new design does not need this function.

--- google/cloud/storage/tests/async_client_integration_test.cc
+++ google/cloud/storage/tests/async_client_integration_test.cc
@@ -127,23 +127,6 @@ TEST_F(AsyncClientIntegrationTest, ComposeObject) {
   EXPECT_EQ(*read.object_metadata, *composed);
 }
 
-TEST_F(AsyncClientIntegrationTest, WriteObject) {
-  auto client = MakeIntegrationTestClient();
-  ASSERT_STATUS_OK(client);
-
-  auto o1 = MakeRandomObjectName();
-  auto o2 = MakeRandomObjectName();
-
-  auto async = MakeAsyncClient();
-  auto pending0 = async.StartResumableUpload(bucket_name(), o1);
-  auto pending1 = async.StartResumableUpload(bucket_name(), o2);
-
-  for (auto* p : {&pending1, &pending0}) {
-    auto response = p->get();
-    EXPECT_STATUS_OK(response);
-  }
-}
-
 }  // namespace
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
 }  // namespace storage_experimental
