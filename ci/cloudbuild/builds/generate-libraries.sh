3ecd2077dd04f081f32a21294fcbd2dd5f0e2051
Scott Hart <sdhart@google.com>
2023-09-11 19:12:37 -0400

    ci: execute clang-format twice in generate-libraries

--- ci/cloudbuild/builds/generate-libraries.sh
+++ ci/cloudbuild/builds/generate-libraries.sh
@@ -83,6 +83,10 @@ if [ -z "${GENERATE_GOLDEN_ONLY}" ]; then
     xargs -P "$(nproc)" -n 1 -0 clang-format -i
   git ls-files -z -- '*.h' '*.cc' '*.proto' |
     xargs -r -P "$(nproc)" -n 50 -0 sed -i 's/[[:blank:]]\+$//'
+  # TODO(#12621): Remove this second execution of clang-format when this issue
+  # is resolved.
+  git ls-files -z -- '*.h' '*.cc' '*.proto' |
+    xargs -P "$(nproc)" -n 1 -0 clang-format -i
 else
   io::log_red "Only formatting generated golden code."
   git ls-files -z -- 'generator/integration_tests/golden/**/*.h' \
