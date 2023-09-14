b58189262250374257855bf09487ec5f2d42ddec
Anna Levenberg <alevenb@google.com>
2023-09-14 11:59:03 -0400

    impl(pubsub): create batching tracing connection

---
+++ google/cloud/pubsub/internal/batching_publisher_tracing_connection.cc
@@ -0,0 +1,86 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/pubsub/internal/batching_publisher_tracing_connection.h"
+#include "google/cloud/pubsub/publisher_connection.h"
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+#include "google/cloud/internal/opentelemetry.h"
+#include <opentelemetry/trace/context.h>
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+
+namespace google {
+namespace cloud {
+namespace pubsub_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+namespace {
+
+class BatchingPublisherTracingConnection : public pubsub::PublisherConnection {
+ public:
+  explicit BatchingPublisherTracingConnection(
+      std::shared_ptr<pubsub::PublisherConnection> child)
+      : child_(std::move(child)) {}
+
+  ~BatchingPublisherTracingConnection() override = default;
+
+  future<StatusOr<std::string>> Publish(PublishParams p) override {
+    auto span =
+        internal::MakeSpan("pubsub::BatchingPublisherConnection::Publish");
+    auto result = child_->Publish(std::move(p));
+    internal::EndSpan(*span);
+    return result;
+  }
+
+  void Flush(FlushParams p) override {
+    auto span =
+        internal::MakeSpan("pubsub::BatchingPublisherConnection::Flush");
+    child_->Flush(std::move(p));
+    internal::EndSpan(*span);
+  }
+
+  void ResumePublish(ResumePublishParams p) override {
+    auto span = internal::MakeSpan(
+        "pubsub::BatchingPublisherTracingConnection::ResumePublish");
+    child_->ResumePublish(std::move(p));
+    internal::EndSpan(*span);
+  }
+
+ private:
+  std::shared_ptr<pubsub::PublisherConnection> child_;
+};
+
+}  // namespace
+
+std::shared_ptr<pubsub::PublisherConnection>
+MakeBatchingPublisherTracingConnection(
+    std::shared_ptr<pubsub::PublisherConnection> connection) {
+  return std::make_shared<BatchingPublisherTracingConnection>(
+      std::move(connection));
+}
+
+#else  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+
+std::shared_ptr<pubsub::PublisherConnection>
+MakeBatchingPublisherTracingConnection(
+    std::shared_ptr<pubsub::PublisherConnection> connection) {
+  return connection;
+}
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace pubsub_internal
+}  // namespace cloud
+}  // namespace google
---
+++ google/cloud/pubsub/internal/batching_publisher_tracing_connection_test.cc
@@ -0,0 +1,120 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
+
+#include "google/cloud/pubsub/internal/flow_controlled_publisher_tracing_connection.h"
+#include "google/cloud/pubsub/mocks/mock_publisher_connection.h"
+#include "google/cloud/pubsub/publisher_connection.h"
+#include "google/cloud/internal/opentelemetry.h"
+#include "google/cloud/opentelemetry_options.h"
+#include "google/cloud/testing_util/opentelemetry_matchers.h"
+#include "google/cloud/testing_util/status_matchers.h"
+#include <gmock/gmock.h>
+namespace google {
+namespace cloud {
+namespace pubsub_internal {
+namespace {
+
+using ::google::cloud::pubsub::PublisherConnection;
+using ::google::cloud::pubsub_internal::MakeBatchingPublisherTracingConnection;
+using ::google::cloud::pubsub_mocks::MockPublisherConnection;
+using ::google::cloud::testing_util::InstallSpanCatcher;
+using ::google::cloud::testing_util::OTelAttribute;
+using ::google::cloud::testing_util::SpanHasAttributes;
+using ::google::cloud::testing_util::SpanHasInstrumentationScope;
+using ::google::cloud::testing_util::SpanKindIsClient;
+using ::google::cloud::testing_util::SpanNamed;
+using ::google::cloud::testing_util::SpanWithStatus;
+using ::google::cloud::testing_util::StatusIs;
+using ::google::cloud::testing_util::ThereIsAnActiveSpan;
+
+TEST(BatchingPublisherTracingConnectionTest, PublishSpan) {
+  auto span_catcher = InstallSpanCatcher();
+  auto mock = std::make_shared<MockPublisherConnection>();
+  EXPECT_CALL(*mock, Publish)
+      .WillOnce([&](pubsub::PublisherConnection::PublishParams const&) {
+        return google::cloud::make_ready_future(
+            google::cloud::StatusOr<std::string>("test-id-0"));
+      });
+  auto connection = MakeBatchingPublisherTracingConnection(std::move(mock));
+
+  auto response = connection
+                      ->Publish({pubsub::MessageBuilder{}
+                                     .SetData("test-data-0")
+                                     .SetOrderingKey("ordering-key-0")
+                                     .Build()})
+                      .get();
+
+  EXPECT_THAT(response, StatusIs(StatusCode::kOk));
+  EXPECT_THAT(
+      span_catcher->GetSpans(),
+      ElementsAre(AllOf(
+          SpanHasInstrumentationScope(), SpanKindIsClient(),
+          SpanNamed("pubsub::BatchingPublisherConnection::Publish"),
+          SpanWithStatus(opentelemetry::trace::StatusCode::kOk),
+          SpanHasAttributes(OTelAttribute<int>("gcloud.status_code", 0)))));
+}
+
+TEST(BatchingPublisherTracingConnectionTest, FlushSpan) {
+  auto span_catcher = InstallSpanCatcher();
+  auto mock = std::make_shared<MockPublisherConnection>();
+  EXPECT_CALL(*mock, Flush).Times(1);
+  auto connection = MakeBatchingPublisherTracingConnection(std::move(mock));
+
+  connection->Flush(PublisherConnection::FlushParams{});
+
+  EXPECT_THAT(
+      span_catcher->GetSpans(),
+      ElementsAre(AllOf(
+          SpanHasInstrumentationScope(), SpanKindIsClient(),
+          SpanNamed("pubsub::BatchingPublisherConnection::Flush"),
+          SpanWithStatus(opentelemetry::trace::StatusCode::kOk),
+          SpanHasAttributes(OTelAttribute<int>("gcloud.status_code", 0)))));
+}
+
+TEST(FlowControlledPublisher, ResumePublishSpan) {
+  auto span_catcher = InstallSpanCatcher();
+  auto mock = std::make_shared<MockPublisherConnection>();
+  EXPECT_CALL(*mock, ResumePublish).Times(1);
+  auto connection = MakeBatchingPublisherTracingConnection(std::move(mock));
+
+  connection->ResumePublish(PublisherConnection::ResumePublishParams{});
+
+  EXPECT_THAT(
+      span_catcher->GetSpans(),
+      ElementsAre(AllOf(
+          SpanHasInstrumentationScope(), SpanKindIsClient(),
+          SpanNamed("pubsub::BatchingPublisherConnection::ResumePublish"),
+          SpanWithStatus(opentelemetry::trace::StatusCode::kOk),
+          SpanHasAttributes(OTelAttribute<int>("gcloud.status_code", 0)))));
+}
+
+TEST(MakeBatchingPublisherTracingConnectionTest, CreateTracingConnection) {
+  auto span_catcher = InstallSpanCatcher();
+  auto mock = std::make_shared<MockPublisherConnection>();
+  EXPECT_CALL(*mock, Flush).WillOnce([] {
+    EXPECT_TRUE(ThereIsAnActiveSpan());
+  });
+  auto connection = MakeBatchingPublisherTracingConnection(std::move(mock));
+
+  connection->Flush(PublisherConnection::FlushParams{});
+}
+
+}  // namespace
+}  // namespace pubsub_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_HAVE_OPENTELEMETRY
