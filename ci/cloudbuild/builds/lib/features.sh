fd4210819753eda6236c9de3e670606db507e7c1
Darren Bolduc <dbolduc@google.com>
2023-09-15 09:49:52 -0400

    cleanup: iam_policy never became a feature (#12651)

--- ci/cloudbuild/builds/lib/features.sh
+++ ci/cloudbuild/builds/lib/features.sh
@@ -60,7 +60,7 @@ function features::libraries() {
 function features::list_full() {
   local feature_list
   mapfile -t feature_list < <(features::libraries)
-  feature_list+=(experimental-opentelemetry experimental-storage-grpc grafeas iam_policy)
+  feature_list+=(experimental-opentelemetry experimental-storage-grpc grafeas)
   printf "%s\n" "${feature_list[@]}" | sort -u
 }
 
