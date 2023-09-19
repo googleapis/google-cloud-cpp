ea7ccff798ec6e498fbab04a64a642ff64ce494b
dbolduc <dbolduc@google.com>
2023-09-19 02:49:25 +0000

    generated changes

--- google/cloud/compute/region_instance_groups/v1/internal/region_instance_groups_rest_stub_factory.cc
+++ google/cloud/compute/region_instance_groups/v1/internal/region_instance_groups_rest_stub_factory.cc
@@ -41,7 +41,7 @@ std::shared_ptr<RegionInstanceGroupsRestStub>
 CreateDefaultRegionInstanceGroupsRestStub(Options const& options) {
   Options opts = options;
   if (!opts.has<UnifiedCredentialsOption>()) {
-    opts.set<UnifiedCredentialsOption>(MakeGoogleDefaultCredentials());
+    opts.set<UnifiedCredentialsOption>(MakeGoogleDefaultCredentials(options));
   }
   if (!opts.has<rest_internal::LongrunningEndpointOption>()) {
     opts.set<rest_internal::LongrunningEndpointOption>(
