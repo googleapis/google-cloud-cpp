c94a958630f3eb9d2902ecb0c8d2b7cdbbd80b38
Anna Levenberg <alevenb@google.com>
2023-10-19 15:06:14 -0400

    address comment

--- generator/internal/format_class_comments.h
+++ generator/internal/format_class_comments.h
@@ -22,6 +22,9 @@ namespace google {
 namespace cloud {
 namespace generator_internal {
 
+/// The function does not just use service.name() and takes @p service_name in
+/// case there exists a service_name_mapping argument that is renaming the
+/// service.
 std::string FormatClassCommentsFromServiceComments(
     google::protobuf::ServiceDescriptor const& service,
     std::string const& service_name);
