a3ba8b5d825e989db0fbc41e7d43ecd6263461a4
Carlos O'Ryan <coryan@google.com>
2023-11-02 18:53:56 +0000

    impl(GCS+gRPC): `WriterConnectionFinalized`
    Sometimes the application may try to resume an upload that was already
    finalized. This introduces an implementation of the
    `storage_experimental::WriterConnection` interface that is suited for
    that case.

---
+++ google/cloud/storage/internal/async/writer_connection_finalized.h
@@ -0,0 +1,67 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_ASYNC_WRITER_CONNECTION_FINALIZED_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_ASYNC_WRITER_CONNECTION_FINALIZED_H
+
+#include "google/cloud/storage/async_writer_connection.h"
+#include "google/cloud/version.h"
+
+namespace google {
+namespace cloud {
+namespace storage_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+/**
+ * Implement the `AsyncWriterConnection` interface for resumed, but finalized,
+ * uploads.
+ *
+ * Applications may resume an upload that was already finalized. For example,
+ * the application may resume all pending uploads when it starts, using some
+ * kind of database to keep the pending uploads. Such an application may
+ * finalize an upload, crash or be terminated before having an opportunity to
+ * update its database, and then try to resume the upload only to discover it
+ * is finalized.
+ *
+ * In this case we want to return an implementation of `AsyncWriterConnection`
+ * that contains the result of a finalized upload, but cannot
+ */
+class AsyncWriterConnectionFinalized
+    : public storage_experimental::AsyncWriterConnection {
+ public:
+  explicit AsyncWriterConnectionFinalized(std::string upload_id,
+                                          storage::ObjectMetadata metadata);
+  ~AsyncWriterConnectionFinalized() override;
+
+  void Cancel() override;
+
+  std::string UploadId() const override;
+  absl::variant<std::int64_t, storage::ObjectMetadata> PersistedState()
+      const override;
+
+  future<Status> Write(storage_experimental::WritePayload payload) override;
+  future<StatusOr<storage::ObjectMetadata>> Finalize(
+      storage_experimental::WritePayload) override;
+
+ private:
+  std::string upload_id_;
+  storage::ObjectMetadata metadata_;
+};
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace storage_internal
+}  // namespace cloud
+}  // namespace google
+
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_INTERNAL_ASYNC_WRITER_CONNECTION_FINALIZED_H
