3d65dcdd2f42bd20e5f86a22c9af56727c07df44
Anna Levenberg <alevenb@google.com>
2023-11-03 12:46:58 -0400

    fix

--- google/cloud/pubsub/internal/publisher_tracing_connection_test.cc
+++ google/cloud/pubsub/internal/publisher_tracing_connection_test.cc
@@ -84,7 +84,7 @@ TEST(PublisherTracingConnectionTest, PublishSpanOnSuccess) {
           SpanNamed("projects/test-project/topics/test-topic send"),
           SpanWithStatus(opentelemetry::trace::StatusCode::kOk),
           SpanHasAttributes(
-              OTelAttribute<std::string>(sc::kMessagingSystem, "pubsub"),
+              OTelAttribute<std::string>(sc::kMessagingSystem, "gcp_pubsub"),
               OTelAttribute<std::string>(
                   sc::kMessagingDestinationName,
                   "projects/test-project/topics/test-topic"),
