660cd6b1f975e23eca5a3cc4c079c142e2e833f8
Carlos O'Ryan <coryan@google.com>
2023-11-09 14:52:46 +0000

    Disable Werror when compiling Protobuf

--- ci/cloudbuild/builds/asan.sh
+++ ci/cloudbuild/builds/asan.sh
@@ -22,7 +22,10 @@ source module ci/cloudbuild/builds/lib/integration.sh
 source module ci/lib/io.sh
 
 mapfile -t args < <(bazel::common_args)
-args+=("--config=asan")
+args+=(
+    '--config=asan'
+    '--per_file_copt=com_google_protobuf//@-Wno-error'
+)
 io::run bazel test "${args[@]}" --test_tag_filters=-integration-test ...
 
 mapfile -t integration_args < <(integration::bazel_args)
