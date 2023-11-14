04e310c289131c160ff5d04d81dcdef26a995eba
meilakh <meilakh@google.com>
2023-11-14 15:42:32 +0000

    refactor(bigquery): clang-format

--- google/cloud/bigquery/v2/minimal/internal/job_request.cc
+++ google/cloud/bigquery/v2/minimal/internal/job_request.cc
@@ -139,7 +139,7 @@ StatusOr<rest_internal::RestRequest> BuildRestRequest(GetJobRequest const& r) {
   return request;
 }
 
-auto CastTimeToMilliseconds(const std::chrono::system_clock::time_point time) {
+auto CastTimeToMilliseconds(std::chrono::system_clock::time_point const time) {
   return std::chrono::time_point_cast<std::chrono::milliseconds>(time)
       .time_since_epoch()
       .count();
