ca9e4c5d66081512a230fd765f9cdc8c8505b1a3
Carlos O'Ryan <coryan@google.com>
2023-11-14 15:10:58 +0000

    Update golden files

--- generator/integration_tests/golden/v1/internal/golden_kitchen_sink_logging_decorator.h
+++ generator/integration_tests/golden/v1/internal/golden_kitchen_sink_logging_decorator.h
@@ -74,8 +74,10 @@ class GoldenKitchenSinkLogging : public GoldenKitchenSinkStub {
 
   std::unique_ptr<::google::cloud::internal::StreamingWriteRpc<
       google::test::admin::database::v1::Request,
-      google::test::admin::database::v1::Response>> StreamingWrite(
-      std::shared_ptr<grpc::ClientContext> context) override;
+      google::test::admin::database::v1::Response>>
+  StreamingWrite(
+      std::shared_ptr<grpc::ClientContext> context,
+      Options const& options) override;
 
   std::unique_ptr<::google::cloud::AsyncStreamingReadWriteRpc<
       google::test::admin::database::v1::Request,
