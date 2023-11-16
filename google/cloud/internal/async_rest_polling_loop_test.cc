41cb514d0e1531c046f365fd0e3d2e4c7d7de9e9
dbolduc <dbolduc@google.com>
2023-11-16 13:30:46 -0600

    test(otel): only test the AsyncPollingLoop interface

--- google/cloud/internal/async_rest_polling_loop_test.cc
+++ google/cloud/internal/async_rest_polling_loop_test.cc
@@ -811,7 +811,7 @@ TEST(AsyncRestPollingLoopTest, TracedAsyncBackoff) {
   EXPECT_THAT(spans, AllOf(SizeIs(4), Each(SpanNamed("Async Backoff"))));
 }
 
-TEST(AsyncRestPollingLoopTest, SpanActiveThroughout) {
+TEST(AsyncRestPollingLoopTest, SpanActiveDuringCancel) {
   auto span_catcher = testing_util::InstallSpanCatcher();
 
   auto span = internal::MakeSpan("span");
@@ -820,23 +820,16 @@ TEST(AsyncRestPollingLoopTest, SpanActiveThroughout) {
 
   AsyncSequencer<TimerType> timer_sequencer;
   auto mock_cq = std::make_shared<MockCompletionQueueImpl>();
-  EXPECT_CALL(*mock_cq, MakeRelativeTimer)
-      .Times(AtLeast(1))
-      .WillRepeatedly([&](std::chrono::nanoseconds) {
-        EXPECT_THAT(span, IsActive());
-        return timer_sequencer.PushBack();
-      });
+  EXPECT_CALL(*mock_cq, MakeRelativeTimer).Times(2).WillRepeatedly([&] {
+    return timer_sequencer.PushBack();
+  });
   CompletionQueue cq(mock_cq);
 
   AsyncSequencer<StatusOr<google::longrunning::Operation>> get_sequencer;
   auto mock = std::make_shared<MockStub>();
-  EXPECT_CALL(*mock, AsyncGetOperation)
-      .Times(AtLeast(1))
-      .WillRepeatedly([&](CompletionQueue&, auto, Options const&,
-                          google::longrunning::GetOperationRequest const&) {
-        EXPECT_THAT(span, IsActive());
-        return get_sequencer.PushBack();
-      });
+  EXPECT_CALL(*mock, AsyncGetOperation).Times(2).WillRepeatedly([&] {
+    return get_sequencer.PushBack();
+  });
   EXPECT_CALL(*mock, AsyncCancelOperation)
       .WillOnce([&](CompletionQueue&, auto, Options const&,
                     google::longrunning::CancelOperationRequest const&) {
