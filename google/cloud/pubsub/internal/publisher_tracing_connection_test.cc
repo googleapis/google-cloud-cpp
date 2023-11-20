5c68512350a9dd26befc192240b4b6fa9c461d5c
Anna Levenberg <alevenb@google.com>
2023-11-20 16:33:41 -0500

    use sc direectly

--- google/cloud/pubsub/internal/publisher_tracing_connection_test.cc
+++ google/cloud/pubsub/internal/publisher_tracing_connection_test.cc
@@ -96,8 +96,7 @@ TEST(PublisherTracingConnectionTest, PublishSpanOnSuccess) {
               OTelAttribute<std::int64_t>(/*sc::kMessagingMessageEnvelopeSize=*/
                                           "messaging.message.envelope.size",
                                           45),
-              OTelAttribute<std::string>(/*sc::kMessagingOperation=*/
-                                         "messaging.operation", "create"),
+              OTelAttribute<std::string>(sc::kMessagingOperation, "create"),
               OTelAttribute<std::string>("messaging.message_id", "test-id-0"),
               OTelAttribute<std::string>(
                   sc::kCodeFunction,
@@ -140,8 +139,7 @@ TEST(PublisherTracingConnectionTest, PublishSpanOnError) {
                                          "topic"),
               OTelAttribute<std::string>("messaging.pubsub.ordering_key",
                                          "ordering-key-0"),
-              OTelAttribute<std::string>(/*sc::kMessagingOperation=*/
-                                         "messaging.operation", "create"),
+              OTelAttribute<std::string>(sc::kMessagingOperation, "create"),
               OTelAttribute<int>("gl-cpp.status_code", kErrorCode),
               OTelAttribute<std::int64_t>(/*sc::kMessagingMessageEnvelopeSize=*/
                                           "messaging.message.envelope.size",
