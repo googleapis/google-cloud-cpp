7b9da32d9f3152d762970b584a9e391fb50a5ff8
dbolduc <dbolduc@google.com>
2023-12-04 16:25:24 -0600

    address review comments

--- google/cloud/internal/url_encode.cc
+++ google/cloud/internal/url_encode.cc
@@ -14,9 +14,7 @@
 
 #include "google/cloud/internal/url_encode.h"
 #include "google/cloud/internal/absl_str_cat_quiet.h"
-#include <iomanip>
-#include <sstream>
-#include <unordered_map>
+#include <set>
 
 namespace google {
 namespace cloud {
@@ -25,33 +23,12 @@ namespace internal {
 
 namespace {
 
-std::string ToHex(int i) {
-  std::stringstream stream;
-  stream << "%" << std::setfill('0') << std::setw(2) << std::uppercase
-         << std::hex << i;
-  return stream.str();
-}
-
-std::string Escape(char c) {
-  static auto const* const kDict = []() {
-    static std::unordered_map<char, std::string> m;
-    for (auto i = 0; i < 32; ++i) {
-      m[static_cast<char>(i)] = ToHex(i);
-    }
-    for (auto c : {
-             ' ', '\"', '#', '$', '%', '&',  '+', ',', '/', ':', ';', '<',
-             '=', '>',  '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}',
-         }) {
-      m[c] = ToHex(static_cast<int>(c));
-    }
-    for (auto i = 127; i < 256; ++i) {
-      m[static_cast<char>(i)] = ToHex(i);
-    }
-    return &m;
-  }();
-  auto it = kDict->find(c);
-  if (it != kDict->end()) return it->second;
-  return std::string{c};
+bool WeNeedToEscape(unsigned char c) {
+  static auto const* const kSet = new std::set<char>{
+      ' ', '\"', '#', '$', '%', '&',  '+', ',', '/', ':', ';', '<',
+      '=', '>',  '?', '@', '[', '\\', ']', '^', '`', '{', '|', '}',
+  };
+  return std::isprint(c) == 0 || kSet->find(c) != kSet->end();
 }
 
 // Returns 0-15 if c is in [0-9A-Fa-f], and -1 otherwise.
@@ -66,8 +43,15 @@ int ParseDigitHex(char c) {
 
 std::string UrlEncode(absl::string_view value) {
   std::string s;
-  for (auto c : value) {
-    absl::StrAppend(&s, Escape(c));
+  auto constexpr kDigits = "0123456789ABCDEF";
+  for (unsigned char c : value) {
+    if (WeNeedToEscape(c)) {
+      s.push_back('%');
+      s.push_back(kDigits[(c >> 4) & 0xf]);
+      s.push_back(kDigits[c & 0xf]);
+    } else {
+      s.push_back(c);
+    }
   }
   return s;
 }
@@ -75,7 +59,7 @@ std::string UrlEncode(absl::string_view value) {
 std::string UrlDecode(absl::string_view value) {
   std::string s;
   for (std::size_t i = 0; i < value.size(); ++i) {
-    if (value[i] == '%' && i < value.size() - 2) {
+    if (value[i] == '%' && value.size() - i > 2) {
       auto b1 = ParseDigitHex(value[i + 1]);
       auto b0 = ParseDigitHex(value[i + 2]);
       if (b1 != -1 && b0 != -1) {
