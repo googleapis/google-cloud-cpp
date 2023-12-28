e417e232a3044f820f1d6b7abbc401dea835f811
Anna Levenberg <alevenb@google.com>
2023-12-28 10:57:14 -0500

    impl(pubsub): add pull lease manager factory

---
+++ google/cloud/pubsub/internal/pull_lease_manager_factory.cc
@@ -0,0 +1,45 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/pubsub/internal/pull_lease_manager_factory.h"
+#include "google/cloud/pubsub/internal/default_pull_lease_manager.h"
+#include "google/cloud/pubsub/internal/tracing_pull_lease_manager.h"
+#include "google/cloud/internal/opentelemetry.h"
+#include "google/cloud/options.h"
+#include <memory>
+
+namespace google {
+namespace cloud {
+namespace pubsub_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+std::shared_ptr<PullLeaseManager> CreatePullLeaseManager(
+    CompletionQueue cq, std::weak_ptr<SubscriberStub> stub,
+    pubsub::Subscription subscription, std::string ack_id,
+    Options const& options,
+    std::function<std::chrono::system_clock::time_point()> clock) {
+  std::shared_ptr<PullLeaseManager> manager =
+      std::make_shared<pubsub_internal::DefaultPullLeaseManager>(
+          std::move(cq), std::move(stub), options, std::move(subscription),
+          std::move(ack_id), clock);
+  if (internal::TracingEnabled(options)) {
+    manager = MakeTracingPullLeaseManager(std::move(manager));
+  }
+  return manager;
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace pubsub_internal
+}  // namespace cloud
+}  // namespace google
