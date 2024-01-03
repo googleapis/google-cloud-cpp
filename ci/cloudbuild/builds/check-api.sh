cfba657b08af131b3a406c3ffb6d5b5198f256d0
dbolduc <dbolduc@google.com>
2024-01-03 12:29:39 -0500

    ci: check-api skips grpc namespace

--- ci/cloudbuild/builds/check-api.sh
+++ ci/cloudbuild/builds/check-api.sh
@@ -123,7 +123,7 @@ function check_abi() {
       # characters in the string "internal", and it should again be followed
       # by some other number indicating the length of the symbol within the
       # "internal" namespace. See: https://en.wikipedia.org/wiki/Name_mangling
-      -skip-internal-symbols "(8internal|_internal)\d"
+      -skip-internal-symbols "(8internal|_internal|4grpc)\d"
       # We also ignore the raw gRPC Stub class. The generated gRPC headers that
       # contain these classes are installed alongside our headers. When a new
       # RPC is added to a service, these classes gain a pure virtual method. Our
