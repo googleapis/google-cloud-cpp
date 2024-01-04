60e3653dc26cd33cabd6f9d2c58b0f07359a8ae0
dbolduc <dbolduc@google.com>
2024-01-04 22:27:55 +0000

    regenerate

--- google/cloud/cloudquotas/v1/internal/cloud_quotas_option_defaults.cc
+++ google/cloud/cloudquotas/v1/internal/cloud_quotas_option_defaults.cc
@@ -33,11 +33,10 @@ auto constexpr kBackoffScaling = 2.0;
 }  // namespace
 
 Options CloudQuotasDefaultOptions(Options options) {
-  options = google::cloud::internal::PopulateCommonOptions(
+  options = internal::PopulateCommonOptions(
       std::move(options), "GOOGLE_CLOUD_CPP_CLOUD_QUOTAS_ENDPOINT", "",
       "GOOGLE_CLOUD_CPP_CLOUD_QUOTAS_AUTHORITY", "cloudquotas.googleapis.com");
-  options =
-      google::cloud::internal::PopulateGrpcOptions(std::move(options), "");
+  options = internal::PopulateGrpcOptions(std::move(options));
   if (!options.has<cloudquotas_v1::CloudQuotasRetryPolicyOption>()) {
     options.set<cloudquotas_v1::CloudQuotasRetryPolicyOption>(
         cloudquotas_v1::CloudQuotasLimitedTimeRetryPolicy(
