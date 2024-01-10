5466cd8c71b6b6c59015366899af5351c1e3f6f5
Scott Hart <sdhart@google.com>
2024-01-10 12:17:53 -0500

    address review comments

--- google/cloud/internal/oauth2_minimal_iam_credentials_rest.cc
+++ google/cloud/internal/oauth2_minimal_iam_credentials_rest.cc
@@ -21,7 +21,6 @@
 #include "google/cloud/internal/json_parsing.h"
 #include "google/cloud/internal/make_status.h"
 #include "google/cloud/internal/oauth2_credentials.h"
-#include "google/cloud/internal/oauth2_universe_domain.h"
 #include "google/cloud/internal/parse_rfc3339.h"
 #include "google/cloud/internal/rest_client.h"
 #include "google/cloud/internal/rest_response.h"
