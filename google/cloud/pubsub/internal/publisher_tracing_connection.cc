d7d36bd241d64b687d2dd4af3b8a12500c8a79b1
Anna Levenberg <alevenb@google.com>
2024-01-25 16:09:23 -0500

    refactor(pubsub): split attributes into topic and project id

--- google/cloud/pubsub/internal/publisher_tracing_connection.cc
+++ google/cloud/pubsub/internal/publisher_tracing_connection.cc
@@ -50,7 +50,8 @@ opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> StartPublishSpan(
   auto span = internal::MakeSpan(
       topic.topic_id() + " create",
       {{sc::kMessagingSystem, "gcp_pubsub"},
-       {sc::kMessagingDestinationTemplate, topic.FullName()},
+       {sc::kMessagingDestinationName, topic.topic_id()},
+       {"gcp.project_id", topic.project_id()},
        {sc::kMessagingOperation, "create"},
        {/*sc::kMessagingMessageEnvelopeSize=*/"messaging.message.envelope.size",
         static_cast<std::int64_t>(MessageSize(m))},
