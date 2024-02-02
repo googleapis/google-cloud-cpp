1f05b3d7f9e835a79c60a26c701929a3fc8bfda8
dbolduc <dbolduc@google.com>
2024-02-02 00:43:16 -0500

    impl: add EnableServerRetriesOption

--- google/cloud/grpc_options.h
+++ google/cloud/grpc_options.h
@@ -229,6 +229,24 @@ struct GrpcSetupPollOption {
   using Type = std::function<void(grpc::ClientContext&)>;
 };
 
+/**
+ * Let the server make retry decisions, when applicable.
+ *
+ * In some cases the server knows how to handle retry behavior better than the
+ * client. For example, if a server-side resource is exhausted and the server
+ * knows when it will come back online, it can tell the client exactly when to
+ * retry.
+ *
+ * If this option is enabled, any supplied retry, backoff, or idempotency
+ * policies may be overridden by a recommendation from the server.
+ *
+ * For example, the server may know it is safe to retry a non-idempotent
+ * request, or safe to retry a status code that is typically a permanent error.
+ */
+struct EnableServerRetriesOption {
+  using Type = bool;
+};
+
 /// Configure the ClientContext using options.
 void ConfigureContext(grpc::ClientContext& context, Options const& opts);
 
