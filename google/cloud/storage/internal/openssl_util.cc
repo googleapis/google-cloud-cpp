0af9bb6543c07c284a0d712c92f043954046d194
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-03-16 12:43:12 +0200

    forward `google::cloud::storage::internal::SignStringWithPem` to `google::cloud::internal::SignUsingSha256`
    These functions are doing the same thing.

--- google/cloud/storage/internal/openssl_util.cc
+++ google/cloud/storage/internal/openssl_util.cc
@@ -14,11 +14,8 @@
 
 #include "google/cloud/storage/internal/openssl_util.h"
 #include "google/cloud/internal/base64_transforms.h"
-#include <openssl/bio.h>
-#include <openssl/buffer.h>
+#include "google/cloud/internal/openssl_util.h"
 #include <openssl/evp.h>
-#include <openssl/opensslv.h>
-#include <openssl/pem.h>
 #include <memory>
 
 namespace google {
@@ -26,22 +23,6 @@ namespace cloud {
 namespace storage {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 namespace internal {
-
-namespace {
-// The name of the function to free an EVP_MD_CTX changed in OpenSSL 1.1.0.
-#if (OPENSSL_VERSION_NUMBER < 0x10100000L)  // Older than version 1.1.0.
-inline std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_destroy)>
-GetDigestCtx() {
-  return std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_destroy)>(
-      EVP_MD_CTX_create(), &EVP_MD_CTX_destroy);
-};
-#else
-inline std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_free)> GetDigestCtx() {
-  return std::unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_free)>(
-      EVP_MD_CTX_new(), &EVP_MD_CTX_free);
-};
-#endif
-}  // namespace
 
 StatusOr<std::vector<std::uint8_t>> Base64Decode(std::string const& str) {
   return google::cloud::internal::Base64DecodeToBytes(str);
@@ -61,89 +42,7 @@ std::string Base64Encode(absl::Span<std::uint8_t const> bytes) {
 
 StatusOr<std::vector<std::uint8_t>> SignStringWithPem(
     std::string const& str, std::string const& pem_contents) {
-  auto digest_ctx = GetDigestCtx();
-  if (!digest_ctx) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not create context for OpenSSL digest. ");
-  }
-
-  EVP_MD const* digest_type = EVP_sha256();
-  if (digest_type == nullptr) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not find specified digest in OpenSSL. ");
-  }
-
-  auto pem_buffer = std::unique_ptr<BIO, decltype(&BIO_free)>(
-      BIO_new_mem_buf(pem_contents.data(),
-                      static_cast<int>(pem_contents.length())),
-      &BIO_free);
-  if (!pem_buffer) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not create PEM buffer. ");
-  }
-
-  auto private_key = std::unique_ptr<EVP_PKEY, decltype(&EVP_PKEY_free)>(
-      PEM_read_bio_PrivateKey(
-          pem_buffer.get(),
-          nullptr,  // EVP_PKEY **x
-          nullptr,  // pem_password_cb *cb -- a custom callback.
-          // void *u -- this represents the password for the PEM (only
-          // applicable for formats such as PKCS12 (.p12 files) that use
-          // a password, which we don't currently support.
-          nullptr),
-      &EVP_PKEY_free);
-  if (!private_key) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not parse PEM to get private key ");
-  }
-
-  int const digest_sign_success_code = 1;
-  if (digest_sign_success_code !=
-      EVP_DigestSignInit(digest_ctx.get(),
-                         nullptr,  // `EVP_PKEY_CTX **pctx`
-                         digest_type,
-                         nullptr,  // `ENGINE *e`
-                         private_key.get())) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not initialize PEM digest. ");
-  }
-
-  if (digest_sign_success_code !=
-      EVP_DigestSignUpdate(digest_ctx.get(), str.data(), str.length())) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not update PEM digest. ");
-  }
-
-  std::size_t signed_str_size = 0;
-  // Calling this method with a nullptr buffer will populate our size var
-  // with the resulting buffer's size. This allows us to then call it again,
-  // with the correct buffer and size, which actually populates the buffer.
-  if (digest_sign_success_code !=
-      EVP_DigestSignFinal(digest_ctx.get(),
-                          nullptr,  // unsigned char *sig
-                          &signed_str_size)) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not finalize PEM digest (1/2). ");
-  }
-
-  std::vector<unsigned char> signed_str(signed_str_size);
-  if (digest_sign_success_code != EVP_DigestSignFinal(digest_ctx.get(),
-                                                      signed_str.data(),
-                                                      &signed_str_size)) {
-    return Status(StatusCode::kInvalidArgument,
-                  "Invalid ServiceAccountCredentials: "
-                  "could not finalize PEM digest (2/2). ");
-  }
-
-  return StatusOr<std::vector<unsigned char>>(
-      {signed_str.begin(), signed_str.end()});
+  return google::cloud::internal::SignUsingSha256(str, pem_contents);
 }
 
 StatusOr<std::vector<std::uint8_t>> UrlsafeBase64Decode(
