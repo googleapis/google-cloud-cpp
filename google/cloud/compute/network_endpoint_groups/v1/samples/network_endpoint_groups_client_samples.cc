da093c6ba63e851cbfafc5013f5a1cc5c8be2bf5
Anna Levenberg <alevenb@google.com>
2024-03-21 14:42:05 -0400

    feat(generator): add polling policy sample (#13797)
    * feat(generator): add polling policy sample
    
    * generate golden
    
    * generated codeg
    
    * Update generator/internal/sample_generator.cc
    
    Co-authored-by: Carlos O'Ryan <coryan@google.com>
    
    * generate-libraries-pr
    
    * change to generic
    
    change generator'
    
    change generator'
    
    * generator changes
    
    * generated changes
    
    ---------
    
    Co-authored-by: Carlos O'Ryan <coryan@google.com>

--- google/cloud/compute/network_endpoint_groups/v1/samples/network_endpoint_groups_client_samples.cc
+++ google/cloud/compute/network_endpoint_groups/v1/samples/network_endpoint_groups_client_samples.cc
@@ -23,6 +23,7 @@
 #include "google/cloud/common_options.h"
 #include "google/cloud/credentials.h"
 #include "google/cloud/internal/getenv.h"
+#include "google/cloud/polling_policy.h"
 #include "google/cloud/testing_util/example_driver.h"
 #include <fstream>
 #include <iostream>
@@ -116,6 +117,49 @@ void SetRetryPolicy(std::vector<std::string> const& argv) {
   //! [set-retry-policy]
 }
 
+void SetPollingPolicy(std::vector<std::string> const& argv) {
+  if (!argv.empty()) {
+    throw google::cloud::testing_util::Usage{"set-client-policy-policy"};
+  }
+  //! [set-polling-policy]
+
+  // The polling policy controls how the client waits for long-running
+  // operations. `GenericPollingPolicy<>` combines existing policies.
+  // In this case, keep polling until the operation completes (with success
+  // or error) or 45 minutes, whichever happens first. Initially pause for
+  // 10 seconds between polling requests, increasing the pause by a factor
+  // of 4 until it becomes 2 minutes.
+  auto options =
+      google::cloud::Options{}
+          .set<google::cloud::compute_network_endpoint_groups_v1::
+                   NetworkEndpointGroupsPollingPolicyOption>(
+              google::cloud::GenericPollingPolicy<
+                  google::cloud::compute_network_endpoint_groups_v1::
+                      NetworkEndpointGroupsRetryPolicyOption::Type,
+                  google::cloud::compute_network_endpoint_groups_v1::
+                      NetworkEndpointGroupsBackoffPolicyOption::Type>(
+                  google::cloud::compute_network_endpoint_groups_v1::
+                      NetworkEndpointGroupsLimitedTimeRetryPolicy(
+                          /*maximum_duration=*/std::chrono::minutes(45))
+                          .clone(),
+                  google::cloud::ExponentialBackoffPolicy(
+                      /*initial_delay=*/std::chrono::seconds(10),
+                      /*maximum_delay=*/std::chrono::minutes(2),
+                      /*scaling=*/4.0)
+                      .clone())
+                  .clone());
+
+  auto connection = google::cloud::compute_network_endpoint_groups_v1::
+      MakeNetworkEndpointGroupsConnectionRest(options);
+
+  // c1 and c2 share the same polling policies.
+  auto c1 = google::cloud::compute_network_endpoint_groups_v1::
+      NetworkEndpointGroupsClient(connection);
+  auto c2 = google::cloud::compute_network_endpoint_groups_v1::
+      NetworkEndpointGroupsClient(connection);
+  //! [set-polling-policy]
+}
+
 void WithServiceAccount(std::vector<std::string> const& argv) {
   if (argv.size() != 1 || argv[0] == "--help") {
     throw google::cloud::testing_util::Usage{"with-service-account <keyfile>"};
@@ -152,6 +196,9 @@ void AutoRun(std::vector<std::string> const& argv) {
   std::cout << "\nRunning SetRetryPolicy() example" << std::endl;
   SetRetryPolicy({});
 
+  std::cout << "\nRunning SetPollingPolicy() example" << std::endl;
+  SetPollingPolicy({});
+
   std::cout << "\nRunning WithServiceAccount() example" << std::endl;
   WithServiceAccount({keyfile});
 }
@@ -162,6 +209,7 @@ int main(int argc, char* argv[]) {  // NOLINT(bugprone-exception-escape)
   google::cloud::testing_util::Example example({
       {"set-client-endpoint", SetClientEndpoint},
       {"set-retry-policy", SetRetryPolicy},
+      {"set-polling-policy", SetPollingPolicy},
       {"with-service-account", WithServiceAccount},
       {"auto", AutoRun},
   });
