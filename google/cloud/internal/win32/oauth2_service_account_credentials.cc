adb4b20cfbb2cd999583b635b10abc9885d17c10
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-03-24 04:46:17 +0200

    refactor `ParseServiceAccountP12File` to use multiple functions and dedicated unique pointer types for each handle

--- google/cloud/internal/win32/oauth2_service_account_credentials.cc
+++ google/cloud/internal/win32/oauth2_service_account_credentials.cc
@@ -16,6 +16,7 @@
 #include "google/cloud/internal/oauth2_service_account_credentials.h"
 #include "google/cloud/internal/make_status.h"
 #include "google/cloud/internal/win32/win32_helpers.h"
+#include "absl/types/span.h"
 #include <fstream>
 #include <type_traits>
 #include <vector>
@@ -30,8 +31,56 @@ GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 using ::google::cloud::internal::FormatWin32Errors;
 using ::google::cloud::internal::InvalidArgumentError;
 
-StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
-    std::string const& source) {
+namespace {
+
+struct HCertStoreDeleter {
+  void operator()(HCERTSTORE store) const { CertCloseStore(store, 0); }
+};
+
+using UniqueCertStore =
+    std::unique_ptr<std::remove_pointer_t<HCERTSTORE>, HCertStoreDeleter>;
+
+struct CertContextDeleter {
+  void operator()(PCCERT_CONTEXT cert) const {
+    CertFreeCertificateContext(cert);
+  }
+};
+
+using UniqueCertContext =
+    std::unique_ptr<std::remove_pointer_t<PCCERT_CONTEXT>, CertContextDeleter>;
+
+template <typename T, typename Deleter>
+class UniquePtrReinterpretProxy : private std::unique_ptr<void, Deleter> {
+  static_assert(sizeof(T) == sizeof(void*),
+                "T must be the same size as a pointer");
+
+ public:
+  explicit UniquePtrReinterpretProxy(T ptr)
+      : std::unique_ptr<void, Deleter>(reinterpret_cast<void*>(ptr), {}) {}
+
+  T get() const {
+    return reinterpret_cast<T>(this->std::unique_ptr<void, Deleter>::get());
+  }
+};
+
+struct HCryptProvDeleter {
+  void operator()(void* key) const {
+    CryptReleaseContext(reinterpret_cast<HCRYPTPROV>(key), 0);
+  }
+};
+
+struct HCryptKeyDeleter {
+  void operator()(void* key) const {
+    CryptDestroyKey(reinterpret_cast<HCRYPTKEY>(key));
+  }
+};
+
+using UniqueCryptProv =
+    UniquePtrReinterpretProxy<HCRYPTPROV, HCryptProvDeleter>;
+
+using UniqueCryptKey = UniquePtrReinterpretProxy<HCRYPTKEY, HCryptKeyDeleter>;
+
+StatusOr<UniqueCertStore> OpenP12File(std::string const& source) {
   // Read the PKCS#12 file into memory.
   std::vector<char> data;
   {
@@ -43,63 +92,50 @@ StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
     }
     data.assign(std::istreambuf_iterator<char>{file}, {});
   }
-
+  CRYPT_DATA_BLOB dataBlob = {static_cast<DWORD>(data.size()),
+                              reinterpret_cast<BYTE*>(data.data())};
   // Import the PKCS#12 file into a certificate store.
-  HCERTSTORE certstore_raw = [data = std::move(data)]() mutable {
-    CRYPT_DATA_BLOB dataBlob = {static_cast<DWORD>(data.size()),
-                                reinterpret_cast<BYTE*>(data.data())};
-    return PFXImportCertStore(&dataBlob, L"notasecret", CRYPT_EXPORTABLE);
-  }();
+  HCERTSTORE certstore_raw =
+      PFXImportCertStore(&dataBlob, L"notasecret", CRYPT_EXPORTABLE);
   if (certstore_raw == nullptr) {
     return InvalidArgumentError(
         FormatWin32Errors("Cannot parse PKCS#12 file (", source, "): "),
         GCP_ERROR_INFO());
   }
-  auto close_certstore = [](HCERTSTORE certstore) {
-    CertCloseStore(certstore, 0);
-  };
-  std::unique_ptr<void, decltype(close_certstore)> certstore(certstore_raw,
-                                                             close_certstore);
+  return UniqueCertStore(certstore_raw);
+}
 
+StatusOr<UniqueCertContext> GetCertificate(HCERTSTORE certstore,
+                                           std::string const& source) {
   // Get the certificate from the store.
   PCCERT_CONTEXT cert_raw = CertFindCertificateInStore(
-      certstore.get(), X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, 0,
-      CERT_FIND_ANY, nullptr, nullptr);
+      certstore, X509_ASN_ENCODING | PKCS_7_ASN_ENCODING, 0, CERT_FIND_ANY,
+      nullptr, nullptr);
   if (cert_raw == nullptr) {
     return InvalidArgumentError(
         absl::StrCat("No certificate found in PKCS#12 file (", source, ")"),
         GCP_ERROR_INFO());
   }
-  std::unique_ptr<std::remove_pointer_t<decltype(cert_raw)>,
-                  decltype(&CertFreeCertificateContext)>
-      cert(cert_raw, &CertFreeCertificateContext);
+  return UniqueCertContext(cert_raw);
+}
 
-  // Get the service account ID from the certificate's common name.
-  std::string service_account_id = [](PCCERT_CONTEXT cert) {
-    DWORD size = CertGetNameStringA(cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0,
-                                    nullptr, nullptr, 0);
-    std::string result(size, '\0');
-    CertGetNameStringA(cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr,
-                       &result[0], size);
-    result.pop_back();  // remove the null terminator
-    return result;
-  }(cert.get());
+std::string GetCertificateCommonName(PCCERT_CONTEXT cert) {
+  DWORD size = CertGetNameStringA(cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0,
+                                  nullptr, nullptr, 0);
+  std::string result(size, '\0');
+  CertGetNameStringA(cert, CERT_NAME_SIMPLE_DISPLAY_TYPE, 0, nullptr,
+                     &result[0], size);
+  result.pop_back();  // remove the null terminator
+  return result;
+}
 
-  // Validate the service account ID.
-  if (service_account_id.find_first_not_of("0123456789") != std::string::npos ||
-      service_account_id.empty()) {
-    return InvalidArgumentError(
-        absl::StrCat(
-            "Invalid PKCS#12 file (", source,
-            "): service account id missing or not not formatted correctly"),
-        GCP_ERROR_INFO());
-  }
-
+StatusOr<UniqueCryptKey> GetCertificatePrivateKey(PCCERT_CONTEXT cert,
+                                                  std::string const& source) {
   // Get a provider that has the private key of the certificate.
   HCRYPTPROV prov_raw;
   DWORD pdwKeySpec;
   BOOL pfCallerFreeProvOrNCryptKey;
-  if (!CryptAcquireCertificatePrivateKey(cert.get(), CRYPT_ACQUIRE_SILENT_FLAG,
+  if (!CryptAcquireCertificatePrivateKey(cert, CRYPT_ACQUIRE_SILENT_FLAG,
                                          nullptr, &prov_raw, &pdwKeySpec,
                                          &pfCallerFreeProvOrNCryptKey)) {
     return InvalidArgumentError(
@@ -107,43 +143,44 @@ StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
                           "): "),
         GCP_ERROR_INFO());
   }
-  auto close_prov = [](void* prov) {
-    // According to documentation of CryptAcquireCertificatePrivateKey,
-    // pfCallerFreeProvOrNCryptKey will always be true in our case so
-    // we don't need to check it.
-    CryptReleaseContext(reinterpret_cast<HCRYPTPROV>(prov), 0);
-  };
-  std::unique_ptr<void, decltype(close_prov)> prov(
-      reinterpret_cast<void*>(prov_raw), close_prov);
+  // According to documentation of CryptAcquireCertificatePrivateKey,
+  // pfCallerFreeProvOrNCryptKey will always be true in our case so
+  // we don't need to check it.
+  UniqueCryptProv prov(prov_raw);
 
   // Get the private key from the provider.
   HCRYPTKEY pkey_raw;
-  if (!CryptGetUserKey(prov_raw, pdwKeySpec, &pkey_raw)) {
+  if (!CryptGetUserKey(prov.get(), pdwKeySpec, &pkey_raw)) {
     return InvalidArgumentError(
         FormatWin32Errors("No private key found in PKCS#12 file (", source,
                           "): "),
         GCP_ERROR_INFO());
   }
-  auto close_pkey = [](void* pkey) {
-    CryptDestroyKey(reinterpret_cast<HCRYPTKEY>(pkey));
-  };
-  std::unique_ptr<void, decltype(close_pkey)> pkey(
-      reinterpret_cast<void*>(pkey_raw), close_pkey);
+  return UniqueCryptKey(pkey_raw);
+}
 
-  // Export the private key to a blob.
+StatusOr<std::vector<char>> ExportKey(HCRYPTKEY pkey,
+                                      std::string const& source) {
   DWORD exported_key_length;
-  if (!CryptExportKey(reinterpret_cast<HCRYPTKEY>(pkey.get()), 0,
-                      PRIVATEKEYBLOB, 0, nullptr, &exported_key_length)) {
+  if (!CryptExportKey(pkey, 0, PRIVATEKEYBLOB, 0, nullptr,
+                      &exported_key_length)) {
     return InvalidArgumentError(
         FormatWin32Errors("Could not export private key from PKCS#12 file (",
                           source, "): "),
         GCP_ERROR_INFO());
   }
   std::vector<char> exported_key(exported_key_length);
-  CryptExportKey(reinterpret_cast<HCRYPTKEY>(pkey.get()), 0, PRIVATEKEYBLOB, 0,
+  // We don't have to check again for errors; we already did in the previous
+  // call to get the buffer size. Same with calls to CryptEncodeObjectEx and
+  // CryptBinaryToStringA below.
+  CryptExportKey(pkey, 0, PRIVATEKEYBLOB, 0,
                  reinterpret_cast<BYTE*>(exported_key.data()),
                  &exported_key_length);
+  return exported_key;
+}
 
+StatusOr<std::vector<BYTE>> EncodeRsaPrivateKey(
+    absl::Span<char const> exported_key, std::string const& source) {
   // Encode the blob to PKCS#1 format.
   DWORD pkcs1_encoded_length;
   if (!CryptEncodeObjectEx(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
@@ -158,15 +195,18 @@ StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
   CryptEncodeObjectEx(X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
                       PKCS_RSA_PRIVATE_KEY, exported_key.data(), 0, nullptr,
                       pkcs1_encoded.data(), &pkcs1_encoded_length);
+  return pkcs1_encoded;
+}
 
-  // Wrap the PKCS#1 encoded private key in a PKCS#8 structure.
+StatusOr<std::vector<BYTE>> EncodeRsaPkcs8PrivateKey(
+    absl::Span<BYTE const> pkcs1_encoded, std::string const& source) {
   CRYPT_PRIVATE_KEY_INFO private_key_info;
   private_key_info.Version = 0;
   private_key_info.Algorithm.pszObjId = szOID_RSA_RSA;
   private_key_info.Algorithm.Parameters.cbData = 0;
   private_key_info.Algorithm.Parameters.pbData = nullptr;
-  private_key_info.PrivateKey.cbData = pkcs1_encoded_length;
-  private_key_info.PrivateKey.pbData = pkcs1_encoded.data();
+  private_key_info.PrivateKey.cbData = static_cast<DWORD>(pkcs1_encoded.size());
+  private_key_info.PrivateKey.pbData = const_cast<BYTE*>(pkcs1_encoded.data());
   private_key_info.pAttributes = nullptr;
   DWORD pkcs8_encoded_length;
   if (!CryptEncodeObjectEx(X509_ASN_ENCODING, PKCS_PRIVATE_KEY_INFO,
@@ -181,10 +221,13 @@ StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
   CryptEncodeObjectEx(X509_ASN_ENCODING, PKCS_PRIVATE_KEY_INFO,
                       &private_key_info, 0, nullptr, pkcs8_encoded.data(),
                       &pkcs8_encoded_length);
+  return pkcs8_encoded;
+}
 
-  // Convert to base64 and add the PEM markers.
+StatusOr<std::vector<char>> Base64Encode(absl::Span<BYTE const> blob,
+                                         std::string const& source) {
   DWORD base64_length;
-  if (!CryptBinaryToStringA(pkcs8_encoded.data(), pkcs8_encoded_length,
+  if (!CryptBinaryToStringA(blob.data(), static_cast<DWORD>(blob.size()),
                             CRYPT_STRING_BASE64 | CRYPT_STRING_NOCR, nullptr,
                             &base64_length)) {
     return InvalidArgumentError(
@@ -194,16 +237,62 @@ StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
         GCP_ERROR_INFO());
   }
   std::vector<char> private_key(base64_length);
-  CryptBinaryToStringA(pkcs8_encoded.data(), pkcs8_encoded_length,
+  CryptBinaryToStringA(blob.data(), static_cast<DWORD>(blob.size()),
                        CRYPT_STRING_BASE64 | CRYPT_STRING_NOCR,
                        private_key.data(), &base64_length);
   private_key.pop_back();  // remove the null terminator
+  return private_key;
+}
+}  // namespace
+
+StatusOr<ServiceAccountCredentialsInfo> ParseServiceAccountP12File(
+    std::string const& source) {
+  // Open the PKCS#12 file.
+  auto certstore = OpenP12File(source);
+  if (!certstore) return std::move(certstore).status();
+
+  // Get the certificate from the store.
+  auto cert = GetCertificate(certstore->get(), source);
+  if (!cert) return std::move(cert).status();
+
+  // Get the service account ID from the certificate's common name.
+  std::string service_account_id = GetCertificateCommonName(cert->get());
+
+  // Validate the service account ID.
+  if (service_account_id.find_first_not_of("0123456789") != std::string::npos ||
+      service_account_id.empty()) {
+    return InvalidArgumentError(
+        absl::StrCat(
+            "Invalid PKCS#12 file (", source,
+            "): service account id missing or not not formatted correctly"),
+        GCP_ERROR_INFO());
+  }
+
+  // Get the private key from the certificate.
+  auto pkey = GetCertificatePrivateKey(cert->get(), source);
+  if (!pkey) return std::move(pkey).status();
+
+  // Export the private key to a blob.
+  auto exported_key = ExportKey(pkey->get(), source);
+  if (!exported_key) return std::move(exported_key).status();
+
+  // Encode the blob to PKCS#1 format.
+  auto pkcs1_encoded = EncodeRsaPrivateKey(*exported_key, source);
+  if (!pkcs1_encoded) return std::move(pkcs1_encoded).status();
+
+  // Wrap the PKCS#1 encoded private key in a PKCS#8 structure.
+  auto pkcs8_encoded = EncodeRsaPkcs8PrivateKey(*pkcs1_encoded, source);
+  if (!pkcs8_encoded) return std::move(pkcs8_encoded).status();
+
+  // Convert to base64.
+  auto private_key = Base64Encode(*pkcs8_encoded, source);
+  if (!private_key) return std::move(private_key).status();
 
   return ServiceAccountCredentialsInfo{
       std::move(service_account_id),
       P12PrivateKeyIdMarker(),
       absl::StrCat("-----BEGIN PRIVATE KEY-----\n",
-                   absl::string_view(private_key.data(), private_key.size()),
+                   absl::string_view(private_key->data(), private_key->size()),
                    "-----END PRIVATE KEY-----\n"),
       GoogleOAuthRefreshEndpoint(),
       /*scopes=*/{},
