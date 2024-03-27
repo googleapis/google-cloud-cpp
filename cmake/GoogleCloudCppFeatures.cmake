9474f37606c9155f0cc339151c95d6d248fe7824
Carlos O'Ryan <coryan@google.com>
2024-03-27 13:19:58 +0000

    feat(storage): promote gRPC plugin to GA
    Move the gRPC plugin functions out of the
    `google::cloud::storage_experimental` namespace and remove the
    `experimental-` prefix from the CMake and Bazel targets. I left shims in
    place for backwards compatibility.
    
    The `AsyncClient` remains in the `storage_experimental` namespace, it is
    fully functional, but we may change some APIs.

--- cmake/GoogleCloudCppFeatures.cmake
+++ cmake/GoogleCloudCppFeatures.cmake
@@ -38,7 +38,8 @@ set(GOOGLE_CLOUD_CPP_EXPERIMENTAL_LIBRARIES
 )
 
 set(GOOGLE_CLOUD_CPP_TRANSITION_LIBRARIES # cmake-format: sort
-)
+                                          "storage_grpc"
+                                          "storagecontrol")
 
 set(GOOGLE_CLOUD_CPP_GA_LIBRARIES
     # cmake-format: sort
