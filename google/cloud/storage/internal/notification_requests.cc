06d4cc262a552ff285d850f0f6a57f9ca3a0c0e1
Carlos O'Ryan <coryan@google.com>
2024-05-07 13:02:45 +0000

    Fix formatting

--- google/cloud/storage/internal/notification_requests.cc
+++ google/cloud/storage/internal/notification_requests.cc
@@ -13,8 +13,8 @@
 // limitations under the License.
 
 #include "google/cloud/storage/internal/notification_requests.h"
-#include "google/cloud/storage/internal/notification_metadata_parser.h"
 #include "google/cloud/storage/internal/metadata_parser.h"
+#include "google/cloud/storage/internal/notification_metadata_parser.h"
 #include "google/cloud/internal/absl_str_join_quiet.h"
 #include "google/cloud/internal/make_status.h"
 #include <iostream>
