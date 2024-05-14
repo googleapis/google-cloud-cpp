47b7a851fcd01b3c8d0b4ff2457aa4a86b303118
Carlos O'Ryan <coryan@google.com>
2024-05-14 19:20:47 +0000

    ci: update more clang build scripts (#14239)

--- ci/cloudbuild/builds/development.sh
+++ ci/cloudbuild/builds/development.sh
@@ -38,6 +38,7 @@ fi
 mapfile -t cmake_args < <(cmake::common_args)
 io::run cmake "${cmake_args[@]}" \
   -DCMAKE_CXX_CLANG_TIDY=/usr/local/bin/clang-tidy-wrapper \
+  -DGOOGLE_CLOUD_CPP_ENABLE_CLANG_ABI_COMPAT_17=ON \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_CXX_STANDARD=14 \
   -DGOOGLE_CLOUD_CPP_ENABLE="${ENABLED_FEATURES}"
