46d79fc2920174fd0806eb6cb76a2d794840192a
Scott Hart <sdhart@google.com>
2024-05-31 12:21:23 -0400

    ci: shard bazel gha builds

--- ci/gha/builds/macos-bazel.sh
+++ ci/gha/builds/macos-bazel.sh
@@ -22,16 +22,32 @@ source module ci/gha/builds/lib/macos.sh
 source module ci/gha/builds/lib/bazel.sh
 source module ci/lib/io.sh
 
-# Usage: macos-bazel.sh
+# Usage: macos-bazel.sh [bazel query expression]
+#
+# The build compiles the targets found via `bazel query`. Recall that:
+#    bazel query //a/...
+# returns the targets matching the pattern `//a/...`.` Furthermore, the
+# expressions can be combined using `+` and `-`, so:
+#    bazel query //a/... +//b/... -//a/c/...
+# Returns the targets that match `//a/...` or `//b/...`, but not `//a/c/...`.
+#
 
 mapfile -t args < <(bazel::common_args)
 mapfile -t test_args < <(bazel::test_args)
 mapfile -t integration_test_args < <(bazel::integration_test_args)
 
 TIMEFORMAT="==> 🕑 bazel test done in %R seconds"
+
+io::log_h1 "Get target list for: " "$@"
+mapfile -t targets < <(bazelisk "${args[@]}" query -- "$@")
+
 io::log_h1 "Starting Build"
 time {
-  io::run bazelisk "${args[@]}" test "${test_args[@]}" --test_tag_filters=-integration-test //...
+  # Always run //google/cloud:status_test in case the list of targets has
+  # no unit tests.
+  io::run bazelisk "${args[@]}" test "${test_args[@]}" \
+    --test_tag_filters=-integration-test \
+    //google/cloud:status_test "${targets[@]}"
 }
 
 TIMEFORMAT="==> 🕑 Storage integration tests done in %R seconds"
