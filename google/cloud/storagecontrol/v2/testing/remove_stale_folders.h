28aaec7e90104601e45eef3c3cd0d3aaab792526
Denis DelGrosso <ddelgrosso@google.com>
2024-06-13 21:19:35 +0000

    formatting

--- google/cloud/storagecontrol/v2/testing/remove_stale_folders.h
+++ google/cloud/storagecontrol/v2/testing/remove_stale_folders.h
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_CONTROL_TESTING_REMOVE_STALE_FOLDERS_H
-#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_CONTROL_TESTING_REMOVE_STALE_FOLDERS_H
+#ifndef GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGECONTROL_V2_TESTING_REMOVE_STALE_FOLDERS_H
+#define GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGECONTROL_V2_TESTING_REMOVE_STALE_FOLDERS_H
 
-#include "google/cloud/internal/time_utils.h"
 #include "google/cloud/storagecontrol/v2/storage_control_client.h"
+#include "google/cloud/internal/time_utils.h"
 #include "google/cloud/status.h"
 #include <chrono>
 #include <string>
@@ -35,21 +35,20 @@ namespace testing {
  * folders can accumulate and cause future tests to fail. To prevent
  * these problems we delete any folder that match the pattern of these randomly
  * created folders, as long as the folder was created more than 48 hours ago.
- * 
- * @param client 
- * @param prefix 
- * @param created_time_limit 
- * @return Status 
+ *
+ * @param client
+ * @param prefix
+ * @param created_time_limit
+ * @return Status
  */
 Status RemoveStaleFolders(
     google::cloud::storagecontrol_v2::StorageControlClient client,
-    std::string const& bucket_name,
-    std::string const& prefix,
+    std::string const& bucket_name, std::string const& prefix,
     std::chrono::system_clock::time_point created_time_limit);
 
-} // namespace testing
-} // namespace storagecontrol_v2
-} // namsapce cloud
-} // namespace google
+}  // namespace testing
+}  // namespace storagecontrol_v2
+}  // namespace cloud
+}  // namespace google
 
-#endif // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_CONTROL_TESTING_REMOVE_STALE_FOLDERS_H
+#endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGECONTROL_V2_TESTING_REMOVE_STALE_FOLDERS_H
