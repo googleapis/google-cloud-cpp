594c2087e0ca12cc44f2d8fc22038aa8f8d78d02
Denis DelGrosso <ddelgrosso@google.com>
2024-06-13 21:15:34 +0000

    address feedback, add removal of stale folders, stub test

--- google/cloud/storagecontrol/v2/samples/storage_control_folder_samples.cc
+++ google/cloud/storagecontrol/v2/samples/storage_control_folder_samples.cc
@@ -12,39 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cloud/internal/getenv.h"
 #include "google/cloud/storagecontrol/v2/storage_control_client.h"
+#include "google/cloud/storagecontrol/v2/testing/remove_stale_folders.h"
+#include "google/cloud/internal/getenv.h"
 #include "google/cloud/testing_util/example_driver.h"
 #include <iostream>
+#include <string>
 #include <vector>
 
 namespace {
 
-void CreateFolder(::google::cloud::storagecontrol_v2::StorageControlClient client,
+void CreateFolder(google::cloud::storagecontrol_v2::StorageControlClient client,
                   std::vector<std::string> const& argv) {
   // [START storage_control_create_folder]
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
   [](storagecontrol::StorageControlClient client,
-      std::string const& bucket_name,
-      std::string const& folder_id) {
-     auto const parent = std::string{"projects/_/buckets/"} + bucket_name;
-     auto status = client.CreateFolder(parent, google::storage::control::v2::Folder{}, folder_id);
-     if (!status) throw std::move(status).status();
-     
-     std::cout << "Created folder: " << status->name() << "\n";
+     std::string const& bucket_name, std::string const& folder_id) {
+    auto const parent = std::string{"projects/_/buckets/"} + bucket_name;
+    auto folder = client.CreateFolder(
+        parent, google::storage::control::v2::Folder{}, folder_id);
+    if (!folder) throw std::move(folder).status();
+
+    std::cout << "Created folder: " << folder->name() << "\n";
   }
   // [END storage_control_create_folder]
   (std::move(client), argv.at(0), argv.at(1));
 }
 
-void DeleteFolder(::google::cloud::storagecontrol_v2::StorageControlClient client,
+void DeleteFolder(google::cloud::storagecontrol_v2::StorageControlClient client,
                   std::vector<std::string> const& argv) {
   // [START storage_control_delete_folder]
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
   [](storagecontrol::StorageControlClient client,
-      std::string const& bucket_name,
-      std::string const& folder_id) {
-    auto const name = std::string{"projects/_/buckets/"} + bucket_name + std::string{"/folders/"} + folder_id;
+     std::string const& bucket_name, std::string const& folder_id) {
+    auto const name = std::string{"projects/_/buckets/"} + bucket_name +
+                      std::string{"/folders/"} + folder_id;
     auto status = client.DeleteFolder(name);
     if (!status.ok()) throw std::move(status);
 
@@ -54,34 +56,33 @@ void DeleteFolder(::google::cloud::storagecontrol_v2::StorageControlClient clien
   (std::move(client), argv.at(0), argv.at(1));
 }
 
-void GetFolder(::google::cloud::storagecontrol_v2::StorageControlClient client,
+void GetFolder(google::cloud::storagecontrol_v2::StorageControlClient client,
                std::vector<std::string> const& argv) {
   // [START storage_control_get_folder]
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
   [](storagecontrol::StorageControlClient client,
-      std::string const& bucket_name,
-      std::string const& folder_id) {
-    auto const name = std::string{"projects/_/buckets/"} + bucket_name + std::string{"/folders/"} + folder_id;
-    auto status = client.GetFolder(name);
-    if (!status) throw std::move(status).status();
+     std::string const& bucket_name, std::string const& folder_id) {
+    auto const name = std::string{"projects/_/buckets/"} + bucket_name +
+                      std::string{"/folders/"} + folder_id;
+    auto folder = client.GetFolder(name);
+    if (!folder) throw std::move(folder).status();
 
-    std::cout << "Got folder: " << status->name() << "\n";
+    std::cout << "Got folder: " << folder->name() << "\n";
   }
   // [END storage_control_get_folder]
   (std::move(client), argv.at(0), argv.at(1));
 }
 
-void ListFolders(::google::cloud::storagecontrol_v2::StorageControlClient client,
-                  std::vector<std::string> const& argv) {
+void ListFolders(google::cloud::storagecontrol_v2::StorageControlClient client,
+                 std::vector<std::string> const& argv) {
   // [START storage_control_list_folders]
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
   [](storagecontrol::StorageControlClient client,
-      std::string const& bucket_name) {
+     std::string const& bucket_name) {
     auto const parent = std::string{"projects/_/buckets/"} + bucket_name;
-    auto stream_range = client.ListFolders(parent);
-    for(auto status : stream_range) {
-      if (!status) throw std::move(status).status();
-      std::cout << status->name() << "\n";
+    for (auto folder : client.ListFolders(parent)) {
+      if (!folder) throw std::move(folder).status();
+      std::cout << folder->name() << "\n";
     }
 
     std::cout << bucket_name << std::endl;
@@ -90,68 +91,83 @@ void ListFolders(::google::cloud::storagecontrol_v2::StorageControlClient client
   (std::move(client), argv.at(0));
 }
 
-void RenameFolder(::google::cloud::storagecontrol_v2::StorageControlClient client,
+void RenameFolder(google::cloud::storagecontrol_v2::StorageControlClient client,
                   std::vector<std::string> const& argv) {
   // [START storage_control_rename_folder]
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
   [](storagecontrol::StorageControlClient client,
-      std::string const& bucket_name,
-      std::string const& source_folder_id,
-      std::string const& dest_folder_id) {
+     std::string const& bucket_name, std::string const& source_folder_id,
+     std::string const& dest_folder_id) {
+    auto name = std::string{"projects/_/buckets/"} + bucket_name +
+                std::string{"/folders/"} + source_folder_id;
+    // Start a rename operation and block until it completes. Real applications
+    // may want to setup a callback, wait on a coroutine, or poll until it
+    // completes.
+    auto renamed = client.RenameFolder(name, dest_folder_id).get();
+    if (!renamed) throw std::move(renamed).status();
 
-    auto name = std::string{"projects/_/buckets/"} + bucket_name + std::string{"/folders/"} + source_folder_id;
-    auto status_future = client.RenameFolder(name, dest_folder_id);
-    auto status = status_future.get();
-    if (!status) throw std::move(status).status();
-
-    std::cout << "Renamed: " << source_folder_id << " to: " << dest_folder_id << "\n";
+    std::cout << "Renamed: " << source_folder_id << " to: " << dest_folder_id
+              << "\n";
   }
   // [END storage_control_rename_folder]
   (std::move(client), argv.at(0), argv.at(1), argv.at(2));
 }
 
 void AutoRun(std::vector<std::string> const& argv) {
-    namespace examples = ::google::cloud::testing_util;
-    namespace storagecontrol = ::google::cloud::storagecontrol_v2;
-    if (!argv.empty()) throw examples::Usage{"auto"};
-    examples::CheckEnvironmentVariablesAreSet({
-      "GOOGLE_CLOUD_CPP_STORAGE_TEST_FOLDER_BUCKET_NAME"
-    });
-    auto const bucket_name =
-      google::cloud::internal::GetEnv("GOOGLE_CLOUD_CPP_STORAGE_TEST_FOLDER_BUCKET_NAME").value();
-    
-    auto client =
-      storagecontrol::StorageControlClient(storagecontrol::MakeStorageControlConnection());
-    auto generator = google::cloud::internal::DefaultPRNG(std::random_device{}());
-    auto const prefix = "storage-control-samples";
-    auto const folder_id =
-      prefix + std::string{"-"} + google::cloud::internal::Sample(generator, 32, "abcdefghijklmnopqrstuvwxyz");
-    auto const dest_folder_id = 
-      prefix + std::string{"-"} + google::cloud::internal::Sample(generator, 32, "abcdefghijklmnopqrstuvwxyz");
+  namespace examples = google::cloud::testing_util;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
+  if (!argv.empty()) throw examples::Usage{"auto"};
+  examples::CheckEnvironmentVariablesAreSet(
+      {"GOOGLE_CLOUD_CPP_STORAGE_TEST_FOLDER_BUCKET_NAME"});
+  auto const bucket_name =
+      google::cloud::internal::GetEnv(
+          "GOOGLE_CLOUD_CPP_STORAGE_TEST_FOLDER_BUCKET_NAME")
+          .value();
 
-    std::cout << "\nRunning CreateFolder() example" << std::endl;
-    CreateFolder(client, {bucket_name, folder_id});
+  auto client = storagecontrol::StorageControlClient(
+      storagecontrol::MakeStorageControlConnection());
+  auto generator = google::cloud::internal::DefaultPRNG(std::random_device{}());
+  auto const prefix = "storage-control-samples";
+  auto const folder_id = prefix + std::string{"-"} +
+                         google::cloud::internal::Sample(
+                             generator, 32, "abcdefghijklmnopqrstuvwxyz");
+  auto const dest_folder_id = prefix + std::string{"-"} +
+                              google::cloud::internal::Sample(
+                                  generator, 32, "abcdefghijklmnopqrstuvwxyz");
 
-    std::cout << "\nRunning GetFolder() example" << std::endl;
-    GetFolder(client, {bucket_name, folder_id});
+  // This is the only example that cleans up stale folders. The examples run in
+  // parallel (within a build and across the builds), having multiple examples
+  // doing the same cleanup is probably more trouble than it is worth.
+  auto const create_time_limit =
+      std::chrono::system_clock::now() - std::chrono::hours(48);
+  std::cout << "\nRemoving stale folders for examples" << std::endl;
+  google::cloud::storagecontrol_v2::testing::RemoveStaleFolders(
+      client, bucket_name, prefix, create_time_limit);
 
-    std::cout << "\nRunning ListFolders() example" << std::endl;
-    ListFolders(client, {bucket_name});
+  std::cout << "\nRunning CreateFolder() example" << std::endl;
+  CreateFolder(client, {bucket_name, folder_id});
 
-    std::cout << "\nRunning RenameFolder() example" << std::endl;
-    RenameFolder(client, {bucket_name, folder_id, dest_folder_id});
+  std::cout << "\nRunning GetFolder() example" << std::endl;
+  GetFolder(client, {bucket_name, folder_id});
 
-    std::cout << "\nRunning DeleteFolder() example" << std::endl;
-    DeleteFolder(client, {bucket_name, dest_folder_id});
+  std::cout << "\nRunning ListFolders() example" << std::endl;
+  ListFolders(client, {bucket_name});
+
+  std::cout << "\nRunning RenameFolder() example" << std::endl;
+  RenameFolder(client, {bucket_name, folder_id, dest_folder_id});
+
+  std::cout << "\nRunning DeleteFolder() example" << std::endl;
+  DeleteFolder(client, {bucket_name, dest_folder_id});
 }
 
-} // namespace
+}  // namespace
 
 int main(int argc, char* argv[]) {
-  using ::google::cloud::testing_util::Example;
-  namespace storagecontrol = ::google::cloud::storagecontrol_v2;
-  using ClientCommand = std::function<void(::google::cloud::storagecontrol_v2::StorageControlClient,
-                                           std::vector<std::string> argv)>;
+  using google::cloud::testing_util::Example;
+  namespace storagecontrol = google::cloud::storagecontrol_v2;
+  using ClientCommand =
+      std::function<void(google::cloud::storagecontrol_v2::StorageControlClient,
+                         std::vector<std::string> argv)>;
 
   auto make_entry = [](std::string name,
                        std::vector<std::string> const& arg_names,
@@ -163,21 +179,24 @@ int main(int argc, char* argv[]) {
         for (auto const& a : arg_names) usage += " <" + a + ">";
         throw google::cloud::testing_util::Usage{std::move(usage)};
       }
-      auto client =
-          storagecontrol::StorageControlClient(storagecontrol::MakeStorageControlConnection());;
+      auto client = storagecontrol::StorageControlClient(
+          storagecontrol::MakeStorageControlConnection());
+      ;
       command(client, std::move(argv));
     };
     return google::cloud::testing_util::Commands::value_type(std::move(name),
                                                              adapter);
   };
 
-    Example example({
+  Example example({
       make_entry("create-folder", {"bucket-name", "folder-id"}, CreateFolder),
       make_entry("delete-folder", {"bucket-name", "folder-id"}, DeleteFolder),
       make_entry("get-folder", {"bucket-name", "folder-id"}, GetFolder),
       make_entry("list-folders", {"bucket-name"}, ListFolders),
-      make_entry("rename-folder", {"bucket-name", "source-folder-id", "dest-folder-id"}, RenameFolder),
+      make_entry("rename-folder",
+                 {"bucket-name", "source-folder-id", "dest-folder-id"},
+                 RenameFolder),
       {"auto", AutoRun},
-    });
-    return example.Run(argc, argv);
+  });
+  return example.Run(argc, argv);
 }
