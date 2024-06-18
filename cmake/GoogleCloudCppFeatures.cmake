b5142436f22c805f069226a266f32c86fb64cc81
Darren Bolduc <dbolduc@google.com>
2024-06-18 15:43:34 -0400

    cleanup(storagecontrol): bake period is over (#14347)

--- cmake/GoogleCloudCppFeatures.cmake
+++ cmake/GoogleCloudCppFeatures.cmake
@@ -37,7 +37,7 @@ set(GOOGLE_CLOUD_CPP_EXPERIMENTAL_LIBRARIES
 )
 
 set(GOOGLE_CLOUD_CPP_TRANSITION_LIBRARIES # cmake-format: sort
-                                          "storagecontrol")
+)
 
 set(GOOGLE_CLOUD_CPP_GA_LIBRARIES
     # cmake-format: sort
@@ -153,6 +153,7 @@ set(GOOGLE_CLOUD_CPP_GA_LIBRARIES
     "speech"
     "sql"
     "storage"
+    "storagecontrol"
     "storageinsights"
     "storagetransfer"
     "support"
