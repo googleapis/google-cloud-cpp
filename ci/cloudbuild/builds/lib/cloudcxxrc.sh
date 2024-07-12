89e69e5c8a902d9cb3c926ccb3cfd97f5eec262e
Carlos O'Ryan <coryan@google.com>
2024-07-12 14:26:02 +0000

    ci: workaround bazel startup error
    Bazel can return an error if `stdout` is closed, which can be a problem
    in pipelines.

--- ci/cloudbuild/builds/lib/cloudcxxrc.sh
+++ ci/cloudbuild/builds/lib/cloudcxxrc.sh
@@ -36,9 +36,14 @@ fi
 function bazel::has_no_tests() {
   local target=$1
   shift
+  if [[ "${#BAZEL_TARGETS[@]}" -eq 0 ]]; then
+    return 0
+  fi
   query_expr="$(printf '+ %s' "${BAZEL_TARGETS[@]}")"
   query_expr="tests(${query_expr:2} intersect ${target})"
-  if ! bazel query --noshow_progress "${query_expr}" | grep -q "${target}"; then
+  # Tolerate failures in `bazel` as a workaround for:
+  #   https://github.com/bazelbuild/bazel/issues/14787
+  if ! (bazel query --noshow_progress "${query_expr}" || true) | grep -q "${target}"; then
     return 0
   fi
   return 1
