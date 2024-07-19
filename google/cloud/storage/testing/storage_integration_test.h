952e89e3fa8623f9b489d975c69ba13c46ba83b3
Carlos O'Ryan <coryan@google.com>
2024-07-19 14:32:54 +0000

    Fix formatting

--- google/cloud/storage/testing/storage_integration_test.h
+++ google/cloud/storage/testing/storage_integration_test.h
@@ -41,10 +41,11 @@ class StorageIntegrationTest
 
   /**
    * Returns the recommended options to run integration tests.
-   * 
+   *
    * Most tests should use these options or call `MakeIntegrationTestClient()`.
    */
-  static google::cloud::Options MakeTestOptions(google::cloud::Options opts = {});
+  static google::cloud::Options MakeTestOptions(
+      google::cloud::Options opts = {});
 
   /**
    * Return a client suitable for most integration tests.
