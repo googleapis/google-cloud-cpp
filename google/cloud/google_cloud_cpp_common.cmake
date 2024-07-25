328f6b81db6e70aae058b3e79129b1e3d69223f1
Denis DelGrosso <ddelgrosso@google.com>
2024-07-25 15:22:43 +0000

    feat: add utilities to detect gcp
    spacing
    
    address feedback
    
    wip
    
    add windows header
    
    fix header
    
    do not initialize variable from getenv
    
    do not initialize variable from getenv
    
    use DWORD
    
    typings
    
    cast
    
    fix win32 test
    
    split logic across separate files
    
    use public / private methods
    
    fixes
    
    add additional windows tests

--- google/cloud/google_cloud_cpp_common.cmake
+++ google/cloud/google_cloud_cpp_common.cmake
@@ -69,6 +69,10 @@ add_library(
     internal/debug_future_status.h
     internal/debug_string.cc
     internal/debug_string.h
+    internal/detect_gcp.cc
+    internal/detect_gcp.h
+    internal/detect_gcp_win32.cc
+    internal/detect_gcp_win32.h
     internal/diagnostics_pop.inc
     internal/diagnostics_push.inc
     internal/disable_deprecation_warnings.inc
@@ -366,6 +370,8 @@ if (BUILD_TESTING)
         internal/credentials_impl_test.cc
         internal/debug_future_status_test.cc
         internal/debug_string_test.cc
+        internal/detect_gcp_test.cc
+        internal/detect_gcp_win32_test.cc
         internal/error_context_test.cc
         internal/filesystem_test.cc
         internal/format_time_point_test.cc
