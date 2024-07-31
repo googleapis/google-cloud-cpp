6493789b57a4403f1e0cdb28f4b0743117079f57
Yao Cui <cuiyao@google.com>
2024-07-31 17:32:24 +0000

    format

--- generator/internal/http_option_utils.cc
+++ generator/internal/http_option_utils.cc
@@ -399,9 +399,9 @@ std::string FormatApiVersionFromPackageName(
 std::string FormatApiVersionFromUrlPattern(std::string const& url_pattern,
                                            std::string const& file_name) {
   std::vector<std::string> parts = absl::StrSplit(url_pattern, "/");
-  std::regex regexObj{R"(v\d+)"};
+  std::regex re{R"(v\d+)"};
   for (auto const& part : parts) {
-    if (std::regex_match(part, regexObj)) {
+    if (std::regex_match(part, re)) {
       return part;
     }
   }
