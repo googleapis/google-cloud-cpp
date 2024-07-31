183cd3d8862887d1c193981ec636931f48c1d3af
Yao Cui <cuiyao@google.com>
2024-07-31 18:08:24 +0000

    add tests

--- generator/internal/http_option_utils_test.cc
+++ generator/internal/http_option_utils_test.cc
@@ -772,6 +772,25 @@ TEST_F(HttpOptionUtilsTest, FormatApiVersionFromPackageNameError) {
       "google/foo/v1/service_without_version.proto, package: my.service");
 }
 
+TEST_F(HttpOptionUtilsTest, FormatApiVersionFromUrlPattern) {
+  std::string file_name = "google/foo/v1/service.proto";
+  std::string url_pattern_v1 = "/v1/foo/bar";
+  EXPECT_THAT(FormatApiVersionFromUrlPattern(url_pattern_v1, file_name),
+              Eq("v1"));
+  std::string url_pattern_v2 = "/foo/v2/bar";
+  EXPECT_THAT(FormatApiVersionFromUrlPattern(url_pattern_v2, file_name),
+              Eq("v2"));
+}
+
+TEST_F(HttpOptionUtilsTest, FormatApiVersionFromUrlPatternError) {
+  std::string file_name = "google/foo/v1/service.proto";
+  std::string url_pattern = "/foo/bar";
+  EXPECT_DEATH_IF_SUPPORTED(
+      FormatApiVersionFromUrlPattern(url_pattern, file_name),
+      "Unrecognized API version in file: "
+      "google/foo/v1/service.proto, url pattern: /foo/bar");
+}
+
 }  // namespace
 }  // namespace generator_internal
 }  // namespace cloud
