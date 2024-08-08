76fcfd6058fb2fd26e4f90fb82770caf4e964cdd
Scott Hart <sdhart@google.com>
2024-08-08 14:30:46 -0400

    chore(compute): update_discovery_doc.sh now edits generator_config

--- generator/discovery/update_discovery_doc.sh
+++ generator/discovery/update_discovery_doc.sh
@@ -23,7 +23,10 @@ function print_service_textproto() {
   service_proto_path="${1#protos/}"
   product_path="${service_proto_path%/*.proto}"
   initial_copyright_year=$(date +"%Y")
-  cat <<_EOF_
+
+  (
+    sed '/# Begin update_discovery_doc.sh additions/q' "${PROJECT_ROOT}/${GENERATOR_CONFIG_RELATIVE_PATH}"
+    cat <<_EOF_
   rest_services {
     service_proto_path: "${service_proto_path}"
     product_path: "${product_path}"
@@ -32,14 +35,17 @@ function print_service_textproto() {
     generate_rest_transport: true
     generate_grpc_transport: false
   }
+
 _EOF_
+    sed -n '/# End update_discovery_doc.sh additions/,$p' "${PROJECT_ROOT}/${GENERATOR_CONFIG_RELATIVE_PATH}"
+  ) | sponge "${PROJECT_ROOT}/${GENERATOR_CONFIG_RELATIVE_PATH}"
 }
 
 function add_service_directory() {
   compute_proto_path="${1#protos/google/cloud/compute/}"
   service_dir="${compute_proto_path%/*.proto}/"
   echo "    \"${service_dir}\""
-  sed -i -f - "${PROJECT_ROOT}/google/cloud/compute/service_dirs.cmake" <<EOT
+  sed -i -f - "${PROJECT_ROOT}/${COMPUTE_SERVICE_DIRS_CMAKE_RELATIVE_PATH}" <<EOT
   /^set(service_dirs$/ {
     n  # skip "cmake-format: sort" line
     a\    "${service_dir}"
@@ -59,6 +65,9 @@ fi
 
 readonly COMPUTE_DISCOVERY_DOCUMENT_URL="https://www.googleapis.com/discovery/v1/apis/compute/v1/rest"
 readonly COMPUTE_DISCOVERY_JSON_RELATIVE_PATH="generator/discovery/compute_public_google_rest_v1.json"
+readonly GENERATOR_CONFIG_RELATIVE_PATH="generator/generator_config.textproto"
+readonly COMPUTE_SERVICE_DIRS_CMAKE_RELATIVE_PATH="google/cloud/compute/service_dirs.cmake"
+readonly COMPUTE_SERVICE_DIRS_BZL_RELATIVE_PATH="google/cloud/compute/service_dirs.bzl"
 
 io::log_h2 "Fetching discovery document from ${COMPUTE_DISCOVERY_DOCUMENT_URL}"
 curl "${COMPUTE_DISCOVERY_DOCUMENT_URL}" >"${PROJECT_ROOT}/${COMPUTE_DISCOVERY_JSON_RELATIVE_PATH}"
@@ -69,7 +78,8 @@ readonly REVISION
 io::run git checkout -B update_compute_discovery_circa_"${REVISION}"
 
 io::log_h2 "Adding updated Discovery JSON ${COMPUTE_DISCOVERY_JSON_RELATIVE_PATH}"
-git add "${PROJECT_ROOT}/${COMPUTE_DISCOVERY_JSON_RELATIVE_PATH}"
+git commit -m"chore(compute): update discovery doc circa ${REVISION}" \
+  "${PROJECT_ROOT}/${COMPUTE_DISCOVERY_JSON_RELATIVE_PATH}"
 
 io::log_h2 "Running generate-libraries with UPDATED_DISCOVERY_DOCUMENT=compute"
 UPDATED_DISCOVERY_DOCUMENT=compute ci/cloudbuild/build.sh -t generate-libraries-pr
@@ -79,14 +89,27 @@ if [[ -n "${NEW_FILES}" ]]; then
   io::log_red "New resources defined in Discovery Document created new protos:"
   echo "${NEW_FILES}"
   mapfile -t proto_array < <(echo "${NEW_FILES}")
-  io::log_red "Add rest_services definitions to the generator_config.textproto and re-run this script."
+  io::log_red "Adding rest_services definitions to the generator_config.textproto."
   for i in "${proto_array[@]}"; do
     print_service_textproto "${i}"
   done
   git add protos
-  io::log_yellow "Adding new directories to google/cloud/compute/service_dirs.cmake"
+
+  io::log_h2 "Running generate-libraries again with updated generator_config.textproto"
+  UPDATED_DISCOVERY_DOCUMENT=compute ci/cloudbuild/build.sh -t generate-libraries-pr
+
+  io::log_yellow "Adding new directories to ${COMPUTE_SERVICE_DIRS_CMAKE_RELATIVE_PATH}"
   for i in "${proto_array[@]}"; do
     add_service_directory "${i}"
   done
-  exit 1
+
+  io::log_yellow "Adding new directories to ${COMPUTE_SERVICE_DIRS_BZL_RELATIVE_PATH}"
+  cmake -DGOOGLE_CLOUD_CPP_ENABLE=compute -S . -B ./cmake-build-debug
+
+  git commit -m"Update generator_config.textproto and service_dirs files" \
+    "${PROJECT_ROOT}/${GENERATOR_CONFIG_RELATIVE_PATH}" \
+    "${PROJECT_ROOT}/${COMPUTE_SERVICE_DIRS_CMAKE_RELATIVE_PATH}" \
+    "${PROJECT_ROOT}/${COMPUTE_SERVICE_DIRS_BZL_RELATIVE_PATH}"
 fi
+
+git commit -m"Update generated code" .
