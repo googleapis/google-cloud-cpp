c3a0cc24b290f4d0afa49a10a2a2f90f93e6c33a
dbolduc <dbolduc@google.com>
2024-08-12 11:38:47 -0400

    refactor(bigtable): no need for inline

--- google/cloud/bigtable/mocks/mock_row_reader.cc
+++ google/cloud/bigtable/mocks/mock_row_reader.cc
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,41 +23,36 @@ namespace cloud {
 namespace bigtable_mocks {
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 
-/**
- * Returns a `RowReader` with a fixed output stream.
- *
- * This factory function is offered for customers to mock the output of
- * `Table::ReadRows(...)` in their tests.
- *
- * @note If `Cancel()` is called on the `RowReader`, the stream will terminate
- *     and return `final_status`.
- *
- * @param rows a vector containing the `Row`s returned by iterating over the
- *     `RowReader`.
- *
- * @param final_status the final Status of the stream. Defaults to OK.
- *
- * @code
- * TEST(ReadRowsTest, Success) {
- *   using ::google::cloud::StatusOr;
- *   using cbt = ::google::cloud::bigtable;
- *   using cbtm = ::google::cloud::bigtable_mocks;
- *
- *   std::vector<cbt::Row> rows = {cbt::Row("r1", {}), cbt::Row("r2", {})};
- *
- *   auto mock = std::shared_ptr<cbtm::MockDataConnection>();
- *   EXPECT_CALL(*mock, ReadRowsFull)
- *       .WillOnce(Return(cbtm::MakeRowReader(rows)));
- *
- *   auto table = cbt::Table(mock);
- *   auto reader = table.ReadRows(...);
- *
- *   // Verify your code works when reading rows: {"r1", "r2"}
- * }
- * @endcode
- */
 bigtable::RowReader MakeRowReader(std::vector<bigtable::Row> rows,
-                                  Status final_status = {});
+                                  Status final_status) {
+  class ConvenientRowReader : public bigtable_internal::RowReaderImpl {
+   public:
+    explicit ConvenientRowReader(std::vector<bigtable::Row> rows,
+                                 Status final_status)
+        : final_status_(std::move(final_status)),
+          rows_(std::move(rows)),
+          iter_(rows_.cbegin()) {}
+
+    ~ConvenientRowReader() override = default;
+
+    /// Skips remaining rows and invalidates current iterator.
+    void Cancel() override { iter_ = rows_.cend(); };
+
+    absl::variant<Status, bigtable::Row> Advance() override {
+      if (iter_ == rows_.cend()) return final_status_;
+      return *iter_++;
+    }
+
+   private:
+    Status final_status_;
+    std::vector<bigtable::Row> const rows_;
+    std::vector<bigtable::Row>::const_iterator iter_;
+  };
+
+  auto impl = std::make_shared<ConvenientRowReader>(std::move(rows),
+                                                    std::move(final_status));
+  return bigtable_internal::MakeRowReader(std::move(impl));
+}
 
 GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
 }  // namespace bigtable_mocks
