a8486fd729089156ba692dc3767081c47df09306
Bharadwaj V.R. <bharadwajvr@google.com>
2024-08-22 16:14:48 -0700

    Applied formatting fixes

--- google/cloud/spanner/samples/graph_samples.cc
+++ google/cloud/spanner/samples/graph_samples.cc
@@ -41,9 +41,9 @@ std::string Basename(absl::string_view name) {
 //! [START spanner_create_database_with_property_graph]
 void CreateDatabaseWithPropertyGraph(
 		google::cloud::spanner_admin::DatabaseAdminClient client,
-                std::string const& project_id,
-                std::string const& instance_id,
-                std::string const& database_id) {
+    std::string const& project_id,
+    std::string const& instance_id,
+    std::string const& database_id) {
   google::cloud::spanner::Database database(project_id, instance_id,
                                             database_id);
   google::spanner::admin::database::v1::CreateDatabaseRequest request;
@@ -108,37 +108,75 @@ void InsertData(google::cloud::spanner::Client client) {
 
   auto insert_accounts = spanner::InsertMutationBuilder(
     "Account", {"id", "create_time", "is_blocked", "nick_name"})
-    .EmplaceRow(7, google::cloud::spanner::Value("2020-01-10T06:22:20.12Z"), false, "Vacation Fund")
-    .EmplaceRow(16, google::cloud::spanner::Value("2020-01-27T17:55:09.12Z"), true, "Vacation Fund")
-    .EmplaceRow(20, google::cloud::spanner::Value("2020-02-18T05:44:20.12Z"), false, "Rainy Day Fund")
+    .EmplaceRow(7,
+                spanner::Value("2020-01-10T06:22:20.12Z"),
+                false,
+                "Vacation Fund")
+    .EmplaceRow(16,
+                spanner::Value("2020-01-27T17:55:09.12Z"),
+                true,
+                "Vacation Fund")
+    .EmplaceRow(20,
+                spanner::Value("2020-02-18T05:44:20.12Z"),
+                false,
+                "Rainy Day Fund")
     .Build();
 
   auto insert_persons = spanner::InsertMutationBuilder(
     "Person", {"id", "name", "birthday", "country", "city"})
-    .EmplaceRow(1, "Alex", google::cloud::spanner::Value("1991-12-21T00:00:00.12Z"), "Australia"," Adelaide")
-    .EmplaceRow(2, "Dana", google::cloud::spanner::Value("1980-10-31T00:00:00.12Z"),"Czech_Republic", "Moravia")
-    .EmplaceRow(3, "Lee", google::cloud::spanner::Value("1986-12-07T00:00:00.12Z"), "India", "Kollam")
+    .EmplaceRow(1,
+                "Alex",
+                spanner::Value("1991-12-21T00:00:00.12Z"),
+                "Australia",
+                "Adelaide")
+    .EmplaceRow(2,
+                "Dana",
+                spanner::Value("1980-10-31T00:00:00.12Z"),
+                "Czech_Republic",
+                "Moravia")
+    .EmplaceRow(3,
+                "Lee",
+                spanner::Value("1986-12-07T00:00:00.12Z"),
+                "India",
+                "Kollam")
     .Build();
 
 
   auto insert_transfers = spanner::InsertMutationBuilder(
-    "AccountTransferAccount", {"id", "to_id", "amount", "create_time", "order_number"})
-    .EmplaceRow(7, 16, 300.0, google::cloud::spanner::Value("2020-08-29T15:28:58.12Z"), "304330008004315")
-    .EmplaceRow(7, 16, 100.0, google::cloud::spanner::Value("2020-10-04T16:55:05.12Z"), "304120005529714")
-    .EmplaceRow(16, 20, 300.0, google::cloud::spanner::Value("2020-09-25T02:36:14.12Z"), "103650009791820")
-    .EmplaceRow(20, 7, 500.0, google::cloud::spanner::Value("2020-10-04T16:55:05.12Z"), "304120005529714")
-    .EmplaceRow(20, 16, 200.0, google::cloud::spanner::Value("2020-10-17T03:59:40.12Z"), "302290001255747") 
+    "AccountTransferAccount",
+      {"id", "to_id", "amount", "create_time", "order_number"})
+    .EmplaceRow(7, 16, 300.0,
+                spanner::Value("2020-08-29T15:28:58.12Z"),
+                "304330008004315")
+    .EmplaceRow(7, 16, 100.0,
+                spanner::Value("2020-10-04T16:55:05.12Z"),
+                "304120005529714")
+    .EmplaceRow(16, 20, 300.0,
+                spanner::Value("2020-09-25T02:36:14.12Z"),
+                "103650009791820")
+    .EmplaceRow(20, 7, 500.0,
+                spanner::Value("2020-10-04T16:55:05.12Z"),
+                "304120005529714")
+    .EmplaceRow(20, 16, 200.0,
+                spanner::Value("2020-10-17T03:59:40.12Z"),
+                "302290001255747") 
     .Build();
 
   auto insert_ownerships = spanner::InsertMutationBuilder(
     "PersonOwnAccount", {"id", "account_id", "create_time"})
-    .EmplaceRow(1, 7, google::cloud::spanner::Value("2020-01-10T06:22:20.12Z"))
-    .EmplaceRow(2, 20, google::cloud::spanner::Value("2020-01-27T17:55:09.12Z"))
-    .EmplaceRow(3, 16, google::cloud::spanner::Value("2020-02-18T05:44:20.12Z"))
+    .EmplaceRow(1, 7, spanner::Value("2020-01-10T06:22:20.12Z"))
+    .EmplaceRow(2, 20, spanner::Value("2020-01-27T17:55:09.12Z"))
+    .EmplaceRow(3, 16, spanner::Value("2020-02-18T05:44:20.12Z"))
     .Build();
 
   auto commit_result =
-      client.Commit(spanner::Mutations{insert_accounts, insert_persons, insert_transfers, insert_ownerships});
+      client.Commit(
+        spanner::Mutations{
+          insert_accounts,
+          insert_persons,
+          insert_transfers,
+          insert_ownerships
+        });
   if (!commit_result) throw std::move(commit_result).status();
   std::cout << "Insert was successful [spanner_insert_data]\n";
 }
@@ -148,17 +186,19 @@ void InsertData(google::cloud::spanner::Client client) {
 void InsertDataWithDml(google::cloud::spanner::Client client) {
   using ::google::cloud::StatusOr;
   namespace spanner = ::google::cloud::spanner;
+
   std::int64_t rows_inserted;
   auto commit_result = client.Commit(
-      [&client, &rows_inserted](
+    [&client, &rows_inserted](
           spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
-        auto insert = client.ExecuteDml(
-            std::move(txn),
-            spanner::SqlStatement(
-                "INSERT INTO Account (id, create_time, is_blocked) "
-                "  VALUES"
-                "    (1, CAST('2000-08-10 08:18:48.463959-07:52' AS TIMESTAMP), false),"
-                "    (2, CAST('2000-08-12 07:13:16.463959-03:41' AS TIMESTAMP), true)"));
+      auto insert = client.ExecuteDml(
+        std::move(txn),
+        spanner::SqlStatement(R"""(
+          INSERT INTO Account (id, create_time, is_blocked)
+          VALUES
+          (1, CAST('2000-08-10 08:18:48.463959-07:52' AS TIMESTAMP), false),
+          (2, CAST('2000-08-12 07:13:16.463959-03:41' AS TIMESTAMP), true)
+        )"""));
         if (!insert) return std::move(insert).status();
         rows_inserted = insert->RowsModified();
         return spanner::Mutations{};
@@ -167,15 +207,16 @@ void InsertDataWithDml(google::cloud::spanner::Client client) {
   std::cout << "Rows inserted into Account: " << rows_inserted;
 
   commit_result = client.Commit(
-      [&client, &rows_inserted](
+    [&client, &rows_inserted](
           spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
-        auto insert = client.ExecuteDml(
-            std::move(txn),
-            spanner::SqlStatement(
-                "INSERT INTO AccountTransferAccount (id, to_id, create_time, amount) "
-                "  VALUES"
-                "    (1, 2, CAST('2000-09-11 03:11:18.463959-06:36' AS TIMESTAMP), 100),"
-                "    (1, 1, CAST('2000-09-12 04:09:34.463959-05:12' AS TIMESTAMP), 200) "));
+      auto insert = client.ExecuteDml(
+        std::move(txn),
+        spanner::SqlStatement(R"""(
+          INSERT INTO AccountTransferAccount (id, to_id, create_time, amount)
+          VALUES
+          (1, 2, CAST('2000-09-11 03:11:18.463959-06:36' AS TIMESTAMP), 100),
+          (1, 1, CAST('2000-09-12 04:09:34.463959-05:12' AS TIMESTAMP), 200)
+        )"""));
         if (!insert) return std::move(insert).status();
         rows_inserted = insert->RowsModified();
         return spanner::Mutations{};
@@ -191,23 +232,25 @@ void InsertDataWithDml(google::cloud::spanner::Client client) {
 void UpdateDataWithDml(google::cloud::spanner::Client client) {
   using ::google::cloud::StatusOr;
   namespace spanner = ::google::cloud::spanner;
+
   auto commit_result = client.Commit(
-      [&client](spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
-        auto update = client.ExecuteDml(
-            std::move(txn),
-            spanner::SqlStatement(
-                "UPDATE Account SET is_blocked = false WHERE id = 2"));
+    [&client](spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
+      auto update = client.ExecuteDml(
+        std::move(txn),
+        spanner::SqlStatement(
+          "UPDATE Account SET is_blocked = false WHERE id = 2"));
         if (!update) return std::move(update).status();
         return spanner::Mutations{};
       });
   if (!commit_result) throw std::move(commit_result).status();
 
   commit_result = client.Commit(
-      [&client](spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
-        auto update = client.ExecuteDml(
-            std::move(txn),
-            spanner::SqlStatement(
-                "UPDATE AccountTransferAccount SET amount = 300 WHERE id = 1 AND to_id = 2"));
+    [&client](spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
+      auto update = client.ExecuteDml(
+        std::move(txn),
+        spanner::SqlStatement(R"""(
+          UPDATE AccountTransferAccount
+            SET amount = 300 WHERE id = 1 AND to_id = 2)"""));
         if (!update) return std::move(update).status();
         return spanner::Mutations{};
       });
@@ -225,17 +268,18 @@ void UpdateDataWithGraphQueryInDml(google::cloud::spanner::Client client) {
       [&client](spanner::Transaction txn) -> StatusOr<spanner::Mutations> {
         auto update = client.ExecuteDml(
             std::move(txn),
-            spanner::SqlStatement(
-                "UPDATE Account SET is_blocked = true "
-                "WHERE id IN {"
-                "  GRAPH FinGraph"
-                "  MATCH (a:Account WHERE a.id = 1)-[:TRANSFERS]->{1,2}(b:Account)"
-                "  RETURN b.id}"));
+            spanner::SqlStatement(R"""(
+              UPDATE Account SET is_blocked = true
+              WHERE id IN {
+                GRAPH FinGraph
+                MATCH (a:Account WHERE a.id = 1)-[:TRANSFERS]->{1,2}(b:Account)
+                RETURN b.id})"""));
         if (!update) return std::move(update).status();
         return spanner::Mutations{};
       });
   if (!commit_result) throw std::move(commit_result).status();
-  std::cout << "Update was successful [spanner_update_graph_data_with_graph_query_in_dml]\n";
+  std::cout << "Update was successful " << 
+               "[spanner_update_graph_data_with_graph_query_in_dml]\n";
 }
 //! [END spanner_update_graph_data_with_graph_query_in_dml]
 
@@ -243,11 +287,16 @@ void UpdateDataWithGraphQueryInDml(google::cloud::spanner::Client client) {
 void QueryData(google::cloud::spanner::Client client) {
   namespace spanner = ::google::cloud::spanner;
 
-  spanner::SqlStatement select(
-    "Graph FinGraph "
-    "MATCH (a:Person)-[o:Owns]->()-[t:Transfers]->()<-[p:Owns]-(b:Person)"
-    "RETURN a.name AS sender, b.name AS receiver, t.amount, t.create_time AS transfer_at");
-  using RowType = std::tuple<std::string, std::string, double, spanner::Timestamp>;
+  spanner::SqlStatement select(R"""(
+    Graph FinGraph
+    MATCH (a:Person)-[o:Owns]->()-[t:Transfers]->()<-[p:Owns]-(b:Person)
+    RETURN a.name AS sender,
+           b.name AS receiver,
+           t.amount,
+          t.create_time AS transfer_at
+  )""");
+  using RowType =
+    std::tuple<std::string, std::string, double, spanner::Timestamp>;
   auto rows = client.ExecuteQuery(std::move(select));
   for (auto& row : spanner::StreamOf<RowType>(rows)) {
     if (!row) throw std::move(row).status();
@@ -266,13 +315,17 @@ void QueryData(google::cloud::spanner::Client client) {
 void QueryWithParameter(google::cloud::spanner::Client client) {
   namespace spanner = ::google::cloud::spanner;
 
-  spanner::SqlStatement select(
-      "Graph FinGraph "
-      "MATCH (a:Person)-[o:Owns]->()-[t:Transfers]->()<-[p:Owns]-(b:Person) "
-      "WHERE t.amount >= @min " 
-      "RETURN a.name AS sender, b.name AS receiver, t.amount, t.create_time AS transfer_at",
+  spanner::SqlStatement select(R"""(
+      Graph FinGraph
+      MATCH (a:Person)-[o:Owns]->()-[t:Transfers]->()<-[p:Owns]-(b:Person)
+      WHERE t.amount >= @min 
+      RETURN a.name AS sender,
+             b.name AS receiver,
+             t.amount,
+             t.create_time AS transfer_at)""",
       {{"min", spanner::Value(500)}});
-  using RowType = std::tuple<std::string, std::string, double, spanner::Timestamp>;
+  using RowType =
+    std::tuple<std::string, std::string, double, spanner::Timestamp>;
   auto rows = client.ExecuteQuery(std::move(select));
   for (auto& row : spanner::StreamOf<RowType>(rows)) {
     if (!row) throw std::move(row).status();
@@ -290,23 +343,24 @@ void QueryWithParameter(google::cloud::spanner::Client client) {
 void DeleteDataWithDml(google::cloud::spanner::Client client) {
   using ::google::cloud::StatusOr;
   namespace spanner = ::google::cloud::spanner;
+
   auto commit_result = client.Commit([&client](spanner::Transaction txn)
                                          -> StatusOr<spanner::Mutations> {
-    auto dele = client.ExecuteDml(
+    auto deleted = client.ExecuteDml(
         std::move(txn),
         spanner::SqlStatement(
             "DELETE FROM AccountTransferAccount WHERE id = 1 AND to_id = 2"));
-    if (!dele) return std::move(dele).status();
+    if (!deleted) return std::move(deleted).status();
     return spanner::Mutations{};
   });
   if (!commit_result) throw std::move(commit_result).status();
 
   commit_result = client.Commit([&client](spanner::Transaction txn)
                                          -> StatusOr<spanner::Mutations> {
-    auto dele = client.ExecuteDml(
+    auto deleted = client.ExecuteDml(
         std::move(txn),
         spanner::SqlStatement("DELETE FROM Account WHERE id = 2"));
-    if (!dele) return std::move(dele).status();
+    if (!deleted) return std::move(deleted).status();
     return spanner::Mutations{};
   });
   if (!commit_result) throw std::move(commit_result).status();
@@ -321,9 +375,10 @@ void DeleteData(google::cloud::spanner::Client client) {
 
   // Delete the 'Owns' relationships with key (1,7) and (2,20).
   auto delete_ownerships = spanner::DeleteMutationBuilder(
-                           "PersonOwnAccount", spanner::KeySet()
-                                         .AddKey(spanner::MakeKey(1, 7))
-                                         .AddKey(spanner::MakeKey(2, 20)))
+                           "PersonOwnAccount",
+                           spanner::KeySet()
+                              .AddKey(spanner::MakeKey(1, 7))
+                              .AddKey(spanner::MakeKey(2, 20)))
                            .Build();
 
   // Delete transfers using the keys in the range [1, 8]
@@ -335,18 +390,22 @@ void DeleteData(google::cloud::spanner::Client client) {
                 spanner::MakeKeyBoundOpen(8)))
           .Build();
 
-  // Deletes rows from the Account table and the AccountTransferAccount table,
-  // because the AccountTransferAccount table is defined with ON DELETE CASCADE.
+  // Deletes rows from the Account table and the AccountTransferAccount
+  // table, because the AccountTransferAccount table is defined with
+  // ON DELETE CASCADE.
   auto delete_accounts_all =
       spanner::MakeDeleteMutation("Account", spanner::KeySet::All());
 
-  // Deletes rows from the Person table and the PersonOwnAccount table, because
-  // the PersonOwnAccount table is defined with ON DELETE CASCADE.
+  // Deletes rows from the Person table and the PersonOwnAccount table,
+  // because the PersonOwnAccount table is defined with ON DELETE CASCADE.
   auto delete_persons_all =
       spanner::MakeDeleteMutation("Person", spanner::KeySet::All());
 
   auto commit_result = client.Commit(spanner::Mutations{
-      delete_ownerships, delete_transfer_range, delete_accounts_all, delete_persons_all});
+      delete_ownerships,
+      delete_transfer_range,
+      delete_accounts_all,
+      delete_persons_all});
   if (!commit_result) throw std::move(commit_result).status();
   std::cout << "Delete was successful [spanner_delete_graph_data]\n";
 }
@@ -363,8 +422,8 @@ int RunOneCommand(std::vector<std::string> argv) {
                            SampleFunction sample) {
       return [sample_name, sample](std::vector<std::string> const& argv) {
         if (argv.size() != 3) {
-          throw std::runtime_error(sample_name +
-                                   " <project-id> <instance-id> <database-id>");
+          throw std::runtime_error(
+            sample_name + " <project-id> <instance-id> <database-id>");
         }
         sample(MakeSampleClient(argv[0], argv[1], argv[2]));
       };
@@ -382,8 +441,8 @@ int RunOneCommand(std::vector<std::string> argv) {
                            DatabaseAdminSampleFunction sample) {
       return [sample_name, sample](std::vector<std::string> const& argv) {
         if (argv.size() != 3) {
-          throw std::runtime_error(sample_name +
-                                   " <project-id> <instance-id> <database-id>");
+          throw std::runtime_error(
+            sample_name + " <project-id> <instance-id> <database-id>");
         }
         google::cloud::spanner_admin::DatabaseAdminClient client(
             google::cloud::spanner_admin::MakeDatabaseAdminConnection());
@@ -400,7 +459,8 @@ int RunOneCommand(std::vector<std::string> argv) {
       make_command_entry("insert-data", InsertData),
       make_command_entry("insert-data-with-dml", InsertDataWithDml),
       make_command_entry("update-data-with-dml", UpdateDataWithDml),
-      make_command_entry("update-data-with-graph-query-in-dml", UpdateDataWithGraphQueryInDml),
+      make_command_entry("update-data-with-graph-query-in-dml",
+                         UpdateDataWithGraphQueryInDml),
       make_command_entry("query-data", QueryData),
       make_command_entry("query-data-with-parameter", QueryWithParameter),
       make_command_entry("delete-data-with-dml", DeleteDataWithDml),
