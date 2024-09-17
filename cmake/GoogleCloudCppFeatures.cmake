2fd308d5268eb547accd20cc16848e09151d0b89
Denis DelGrosso <ddelgrosso@google.com>
2024-09-17 19:52:28 +0000

    continue from Carlos previous work

--- cmake/GoogleCloudCppFeatures.cmake
+++ cmake/GoogleCloudCppFeatures.cmake
@@ -38,8 +38,7 @@ set(GOOGLE_CLOUD_CPP_EXPERIMENTAL_LIBRARIES
 )
 
 set(GOOGLE_CLOUD_CPP_TRANSITION_LIBRARIES # cmake-format: sort
-                                          "storage_grpc"
-                                          "storagecontrol")
+                                          "storage_grpc" "storagecontrol")
 
 set(GOOGLE_CLOUD_CPP_GA_LIBRARIES
     # cmake-format: sort
