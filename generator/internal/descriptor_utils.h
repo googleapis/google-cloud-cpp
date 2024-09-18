e99845f8eb8bbc30c97d8452b66299904a25abda
Yao Cui <cuiyao@google.com>
2024-09-18 17:10:59 +0000

    feat(mixin): add mixin support in descriptor utils

--- generator/internal/descriptor_utils.h
+++ generator/internal/descriptor_utils.h
@@ -16,6 +16,7 @@
 #define GOOGLE_CLOUD_CPP_GENERATOR_INTERNAL_DESCRIPTOR_UTILS_H
 
 #include "generator/internal/generator_interface.h"
+#include "generator/internal/mixin_utils.h"
 #include "generator/internal/predicate_utils.h"
 #include "generator/internal/printer.h"
 #include "absl/types/variant.h"
@@ -38,14 +39,16 @@ namespace generator_internal {
  */
 VarsDictionary CreateServiceVars(
     google::protobuf::ServiceDescriptor const& descriptor,
-    std::vector<std::pair<std::string, std::string>> const& initial_values);
+    std::vector<std::pair<std::string, std::string>> const& initial_values,
+    std::vector<MixinMethod> const& mixin_methods = std::vector<MixinMethod>());
 
 /**
  * Extracts method specific substitution data for each method in the service.
  */
 std::map<std::string, VarsDictionary> CreateMethodVars(
     google::protobuf::ServiceDescriptor const& service,
-    YAML::Node const& service_config, VarsDictionary const& service_vars);
+    YAML::Node const& service_config, VarsDictionary const& service_vars,
+    std::vector<MixinMethod> const& mixin_methods = std::vector<MixinMethod>());
 
 /**
  * Determines which `MethodPattern` to use from patterns for the given method
