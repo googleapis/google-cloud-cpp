b8b56693211f485e13df15b8a1cb3c46e3ec18c7
Yao Cui <cuiyao@google.com>
2024-09-27 18:49:44 +0000

    remove code generated for datamigration

--- google/cloud/datamigration/v1/data_migration_connection.cc
+++ google/cloud/datamigration/v1/data_migration_connection.cc
@@ -704,60 +704,6 @@ StreamRange<std::string> DataMigrationServiceConnection::FetchStaticIps(
       StreamRange<std::string>>();
 }
 
-StreamRange<google::cloud::location::Location>
-DataMigrationServiceConnection::ListLocations(
-    google::cloud::location::
-        ListLocationsRequest) {  // NOLINT(performance-unnecessary-value-param)
-  return google::cloud::internal::MakeUnimplementedPaginationRange<
-      StreamRange<google::cloud::location::Location>>();
-}
-
-StatusOr<google::cloud::location::Location>
-DataMigrationServiceConnection::GetLocation(
-    google::cloud::location::GetLocationRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-StatusOr<google::iam::v1::Policy> DataMigrationServiceConnection::SetIamPolicy(
-    google::iam::v1::SetIamPolicyRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-StatusOr<google::iam::v1::Policy> DataMigrationServiceConnection::GetIamPolicy(
-    google::iam::v1::GetIamPolicyRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-StatusOr<google::iam::v1::TestIamPermissionsResponse>
-DataMigrationServiceConnection::TestIamPermissions(
-    google::iam::v1::TestIamPermissionsRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-StreamRange<google::longrunning::Operation>
-DataMigrationServiceConnection::ListOperations(
-    google::longrunning::
-        ListOperationsRequest) {  // NOLINT(performance-unnecessary-value-param)
-  return google::cloud::internal::MakeUnimplementedPaginationRange<
-      StreamRange<google::longrunning::Operation>>();
-}
-
-StatusOr<google::longrunning::Operation>
-DataMigrationServiceConnection::GetOperation(
-    google::longrunning::GetOperationRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-Status DataMigrationServiceConnection::DeleteOperation(
-    google::longrunning::DeleteOperationRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
-Status DataMigrationServiceConnection::CancelOperation(
-    google::longrunning::CancelOperationRequest const&) {
-  return Status(StatusCode::kUnimplemented, "not implemented");
-}
-
 std::shared_ptr<DataMigrationServiceConnection>
 MakeDataMigrationServiceConnection(Options options) {
   internal::CheckExpectedOptions<CommonOptionList, GrpcOptionList,
--- google/cloud/datamigration/v1/internal/data_migration_stub_factory.cc
+++ google/cloud/datamigration/v1/internal/data_migration_stub_factory.cc
@@ -29,9 +29,6 @@
 #include "google/cloud/log.h"
 #include "google/cloud/options.h"
 #include <google/cloud/clouddms/v1/clouddms.grpc.pb.h>
-#include <google/cloud/location/locations.grpc.pb.h>
-#include <google/iam/v1/iam_policy.grpc.pb.h>
-#include <google/longrunning/operations.grpc.pb.h>
 #include <memory>
 #include <utility>
 
@@ -47,15 +44,9 @@ std::shared_ptr<DataMigrationServiceStub> CreateDefaultDataMigrationServiceStub(
                                      internal::MakeChannelArguments(options));
   auto service_grpc_stub =
       google::cloud::clouddms::v1::DataMigrationService::NewStub(channel);
-  auto service_operations_stub =
-      google::longrunning::Operations::NewStub(channel);
-  auto service_iampolicy_stub = google::iam::v1::IAMPolicy::NewStub(channel);
-  auto service_locations_stub =
-      google::cloud::location::Locations::NewStub(channel);
   std::shared_ptr<DataMigrationServiceStub> stub =
       std::make_shared<DefaultDataMigrationServiceStub>(
-          std::move(service_grpc_stub), std::move(service_operations_stub),
-          std::move(service_iampolicy_stub), std::move(service_locations_stub),
+          std::move(service_grpc_stub),
           google::longrunning::Operations::NewStub(channel));
 
   if (auth->RequiresConfigureContext()) {
