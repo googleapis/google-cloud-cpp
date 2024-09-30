e0813408610869d02c3fedb69c180d126890296d
dbolduc <dbolduc@google.com>
2024-09-30 14:42:55 -0400

    feat: API key support over REST transport

--- google/cloud/internal/rest_set_metadata_test.cc
+++ google/cloud/internal/rest_set_metadata_test.cc
@@ -52,6 +52,7 @@ TEST(RestContextTest, SetMetadataFull) {
               Options{}
                   .set<UserProjectOption>("user-project")
                   .set<QuotaUserOption>("quota-user")
+                  .set<ApiKeyOption>("api-key")
                   .set<FieldMaskOption>("items.name,token")
                   .set<ServerTimeoutOption>(std::chrono::milliseconds(1050))
                   .set<CustomHeadersOption>(
@@ -63,6 +64,7 @@ TEST(RestContextTest, SetMetadataFull) {
                   Pair("x-goog-request-params", ElementsAre("p1=v1&p2=v2")),
                   Pair("x-goog-user-project", ElementsAre("user-project")),
                   Pair("x-goog-quota-user", ElementsAre("quota-user")),
+                  Pair("x-goog-api-key", ElementsAre("api-key")),
                   Pair("x-goog-fieldmask", ElementsAre("items.name,token")),
                   Pair("x-server-timeout", ElementsAre("1.050")),
                   Pair("custom-header-1", ElementsAre("v1")),
