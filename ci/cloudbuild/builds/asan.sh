5f5408a4ec177f28fd4182b173bf24e595436f8f
Scott Hart <sdhart@google.com>
2024-12-04 16:16:51 -0500

    disable bzlmod for asan

--- ci/cloudbuild/builds/asan.sh
+++ ci/cloudbuild/builds/asan.sh
@@ -23,7 +23,10 @@ source module ci/cloudbuild/builds/lib/integration.sh
 source module ci/lib/io.sh
 
 mapfile -t args < <(bazel::common_args)
-args+=(--config=asan)
+args+=(--config=asan
+  # TODO(#14874): re-enable bzlmod once BCR grpc module is fixed.
+  --noenable_bzlmod
+)
 io::run bazel test "${args[@]}" --test_tag_filters=-integration-test "${BAZEL_TARGETS[@]}"
 
 mapfile -t integration_args < <(integration::bazel_args)
