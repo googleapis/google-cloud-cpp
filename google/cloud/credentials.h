5fd9ab0614f65c2542c35f5ac8e3dc853a79afbd
Yao Cui <cuiyao@google.com>
2025-01-16 20:44:58 +0000

    add more comments

--- google/cloud/credentials.h
+++ google/cloud/credentials.h
@@ -273,6 +273,14 @@ std::shared_ptr<Credentials> MakeServiceAccountCredentials(
  * identity provider that supports OpenID Connect (OIDC), such as Microsoft
  * Azure, or SAML 2.0.
  *
+ * Important: If you accept a credential configuration (credential
+ * JSON/File/Stream) from an external source for authentication to Google Cloud
+ * Platform, you must validate it before providing it to any Google API or
+ * client library. Providing an unvalidated credential configuration to Google
+ * APIs can compromise the security of your systems and data. For more
+ * information, refer to
+ * https://cloud.google.com/docs/authentication/external/externally-sourced-credentials.
+ *
  * @see https://cloud.google.com/docs/authentication for more information on
  *     authentication in GCP.
  *
