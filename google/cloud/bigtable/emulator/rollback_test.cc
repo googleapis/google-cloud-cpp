7f95ac2f3db6e87ab1484d4169045d14e680ab65
Brian Gitonga Marete <bgmarete@gmail.com>
2025-02-24 20:50:27 +0300

    emulator: testing: rollback_test: Fix crash when setting up SetCell mutation.
    There is a crash later on, in the code that sets up the undo log, that
    will be fixed in an upcoming commit.

--- google/cloud/bigtable/emulator/rollback_test.cc
+++ google/cloud/bigtable/emulator/rollback_test.cc
@@ -14,9 +14,13 @@
 
 #include "google/cloud/bigtable/emulator/table.h"
 #include "google/cloud/bigtable/table.h"
+#include "google/cloud/testing_util/status_matchers.h"
 #include <google/bigtable/admin/v2/bigtable_table_admin.grpc.pb.h>
 #include <google/bigtable/admin/v2/bigtable_table_admin.pb.h>
 #include <google/bigtable/admin/v2/table.pb.h>
+#include <google/bigtable/v2/bigtable.grpc.pb.h>
+#include <google/bigtable/v2/bigtable.pb.h>
+#include <google/bigtable/v2/data.pb.h>
 #include <gtest/gtest.h>
 #include <memory>
 
@@ -31,10 +35,29 @@ TEST(TransactonRollback, SetCellBasicFunction) {
   ::google::bigtable::admin::v2::Table schema;
   ::google::bigtable::admin::v2::ColumnFamily column_family;
 
-  schema.set_name("projects/test/instances/test/tables/test");
+  const auto *const table_name = "projects/test/instances/test/tables/test";
+  const auto *const row_key = "0";
+
+  schema.set_name(table_name);
   (*schema.mutable_column_families())["test"] = column_family;
 
   auto table = Table::Create(schema);
+  ASSERT_STATUS_OK(table);
+
+  ::google::bigtable::v2::Mutation_SetCell set_cell_mutation;
+  set_cell_mutation.set_family_name("test");
+  set_cell_mutation.set_column_qualifier("test");
+  set_cell_mutation.set_timestamp_micros(1234);
+  set_cell_mutation.set_value("test");
+
+  ::google::bigtable::v2::MutateRowRequest mutation_request;
+  mutation_request.set_table_name(table_name);
+  mutation_request.set_row_key(row_key);
+  auto *mutation_request_mutation = mutation_request.add_mutations();
+  mutation_request_mutation->set_allocated_set_cell(&set_cell_mutation);
+
+  auto status = table.value()->MutateRow(mutation_request);
+  ASSERT_STATUS_OK(status);
 }
 
 }  // namespace emulator
