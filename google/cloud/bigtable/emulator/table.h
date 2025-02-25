77adf301cf475713fc87ba6e6253297f76183d82
Brian Gitonga Marete <bgmarete@gmail.com>
2025-02-25 13:11:23 +0300

    emulator: table: Add a method to return an iterator to the column families map.

--- google/cloud/bigtable/emulator/table.h
+++ google/cloud/bigtable/emulator/table.h
@@ -56,6 +56,9 @@ class Table {
 
   Status ReadRows(google::bigtable::v2::ReadRowsRequest const& request,
                   RowStreamer& row_streamer) const;
+  std::map<std::string, std::shared_ptr<ColumnFamily>>::iterator find(std::string const &column_family) {
+    return column_families_.find(column_family);
+  }
 
  private:
   Table() = default;
