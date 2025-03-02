21051c1bf04ff449470d8867607caa2e1b34ecda
Brian Gitonga Marete <bgmarete@gmail.com>
2025-03-02 19:24:23 +0300

    emulator: add helper to test > 1 SetCell mutations at once.

--- google/cloud/bigtable/emulator/rollback_test.cc
+++ google/cloud/bigtable/emulator/rollback_test.cc
@@ -36,6 +36,13 @@ namespace cloud {
 namespace bigtable {
 namespace emulator {
 
+struct SetCellParams {
+  std::string column_family_name;
+  std::string column_qualifier;
+  int64_t timestamp_micros;
+  std::string data;
+};
+
 StatusOr<std::shared_ptr<Table>> create_table(
     std::string const& table_name, std::vector<std::string>& column_families) {
   ::google::bigtable::admin::v2::Table schema;
@@ -46,6 +53,27 @@ StatusOr<std::shared_ptr<Table>> create_table(
   }
 
   return Table::Create(schema);
+}
+
+Status set_cells(
+    std::shared_ptr<google::cloud::bigtable::emulator::Table>& table,
+    std::string const& table_name, std::string const& row_key,
+    std::vector<SetCellParams>& set_cell_params) {
+
+  ::google::bigtable::v2::MutateRowRequest mutation_request;
+  mutation_request.set_table_name(table_name);
+  mutation_request.set_row_key(row_key);
+
+  for (auto m : set_cell_params) {
+    auto* mutation_request_mutation = mutation_request.add_mutations();
+    auto* set_cell_mutation = mutation_request_mutation->mutable_set_cell();
+    set_cell_mutation->set_family_name(m.column_family_name);
+    set_cell_mutation->set_column_qualifier(m.column_qualifier);
+    set_cell_mutation->set_timestamp_micros(m.timestamp_micros);
+    set_cell_mutation->set_value(m.data);
+  }
+
+  return table->MutateRow(mutation_request);
 }
 
 Status set_cell(
