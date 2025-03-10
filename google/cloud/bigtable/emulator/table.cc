a518e8d925da9c36c2145b6a0287c5f2c71d2abc
Brian Gitonga Marete <bgm@unoperate.com>
2025-03-10 17:03:33 +0300

    emulator: Rename several iterator variables more appropriately.

--- google/cloud/bigtable/emulator/table.cc
+++ google/cloud/bigtable/emulator/table.cc
@@ -420,33 +420,33 @@ Status RowTransaction::DeleteFromFamily(
     return status.status();
   }
 
-  auto table_it = table_->find(delete_from_family.family_name());
-  if (table_it == table_->end()) {
+  auto column_family_it = table_->find(delete_from_family.family_name());
+  if (column_family_it == table_->end()) {
     return Status(StatusCode::kNotFound,
                   absl::StrFormat("column family %s not found in table",
                                   delete_from_family.family_name()),
                   ErrorInfo());
   }
 
-  if (auto column_family_it = table_it->second->find(request_.row_key());
-      column_family_it != table_it->second->end()) {
+  if (auto column_family_row_it = column_family_it->second->find(request_.row_key());
+      column_family_row_it != column_family_it->second->end()) {
     RestoreColumnFamilyRow restore_row;
 
-    restore_row.table_it = table_it;
+    restore_row.table_it = column_family_it;
     restore_row.row_key = request_.row_key();
     std::vector<RestoreColumnFamilyRow::Cell> cells;
-    for (auto const& column : column_family_it->second) {
-      for (auto const& column_row : column.second) {
+    for (auto const& column : column_family_row_it->second) {
+      for (auto const& column_row_it : column.second) {
         RestoreColumnFamilyRow::Cell cell;
 
         cell.column_qualifer = std::move(column.first);
-        cell.timestamp = column_row.first;  // Wait, is this correct?
-        cell.value = std::move(column_row.second);
+        cell.timestamp = column_row_it.first;
+        cell.value = std::move(column_row_it.second);
         cells.push_back(cell);
       }
     }
     restore_row.cells = cells;
-    table_it->second->DeleteRow(request_.row_key());  // Is certain
+    column_family_it->second->DeleteRow(request_.row_key());  // Is certain
                                                       // to succeed
                                                       // unless we
                                                       // run out of
@@ -456,7 +456,7 @@ Status RowTransaction::DeleteFromFamily(
     // The row does not exist
     return Status(StatusCode::kNotFound,
                   absl::StrFormat("row key %s not found in column family %s",
-                                  request_.row_key(), table_it->first),
+                                  request_.row_key(), column_family_it->first),
                   ErrorInfo());
   }
 
