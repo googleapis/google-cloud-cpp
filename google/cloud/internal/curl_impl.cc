a17334299384824ad7b7cfa4410d8a3812bdd56b
Scott Hart <sdhart@google.com>
2025-03-28 16:36:24 -0400

    assert libcurl minimum version for mtls

--- google/cloud/internal/curl_impl.cc
+++ google/cloud/internal/curl_impl.cc
@@ -326,6 +326,7 @@ Status CurlImpl::MakeRequest(HttpMethod method, RestContext& context,
   }
 
   if (mtls_config_.has_value()) {
+    static_assert(CURL_AT_LEAST_VERSION(7, 9, 3));
     status = handle_.SetOption(CURLOPT_SSL_VERIFYPEER, 1);
     if (!status.ok()) return OnTransferError(context, std::move(status));
     status = handle_.SetOption(CURLOPT_SSL_VERIFYHOST, 2);
