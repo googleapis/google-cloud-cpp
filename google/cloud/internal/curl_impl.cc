b3156ecee8c982b4576dfce426b65cbd52e9927e
Scott Hart <sdhart@google.com>
2025-04-03 18:10:19 -0400

    add libcurl min version

--- google/cloud/internal/curl_impl.cc
+++ google/cloud/internal/curl_impl.cc
@@ -333,6 +333,7 @@ Status CurlImpl::MakeRequest(HttpMethod method, RestContext& context,
     if (!status.ok()) return OnTransferError(context, std::move(status));
   }
 
+#if CURL_AT_LEAST_VERSION(7, 71, 0)
   if (client_ssl_cert_.has_value()) {
     status = handle_.SetOption(CURLOPT_SSL_VERIFYPEER, 1L);
     if (!status.ok()) return OnTransferError(context, std::move(status));
@@ -361,6 +362,7 @@ Status CurlImpl::MakeRequest(HttpMethod method, RestContext& context,
     status = handle_.SetOption(CURLOPT_SSLKEY_BLOB, &ssl_key_blob);
     if (!status.ok()) return OnTransferError(context, std::move(status));
   }
+#endif
 
   if (method == HttpMethod::kGet) {
     status = handle_.SetOption(CURLOPT_NOPROGRESS, 1L);
