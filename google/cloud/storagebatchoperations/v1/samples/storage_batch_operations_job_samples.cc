b857ca891b668d4ddce5e19a2846edbe86647681
Eli Kozah <kozah@google.com>
2025-05-02 00:18:44 +0000

    docs: add batch operations samples

--- google/cloud/storagebatchoperations/v1/samples/storage_batch_operations_job_samples.cc
+++ google/cloud/storagebatchoperations/v1/samples/storage_batch_operations_job_samples.cc
@@ -30,17 +30,25 @@ void CreateJob(
   //! [storage_batch_create_job]
   [](google::cloud::storagebatchoperations_v1::StorageBatchOperationsClient
          client,
-     std::string const& project_id, std::string const& job_id) {
+     std::string const& project_id, std::string const& job_id,
+     std::string const& target_bucket_name, std::string const& object_prefix) {
     auto const parent =
         std::string{"projects/"} + project_id + "/locations/global";
     namespace sbo = google::cloud::storagebatchoperations::v1;
     sbo::Job job;
+    sbo::BucketList* bucket_list = job.mutable_bucket_list();
+    sbo::BucketList::Bucket* bucket_config = bucket_list->add_buckets();
+    bucket_config->set_bucket(target_bucket_name);
+    sbo::PrefixList* prefix_list_config = bucket_config->mutable_prefix_list();
+    prefix_list_config->add_included_object_prefixes(object_prefix);
+    sbo::DeleteObject* delete_object_config = job.mutable_delete_object();
+    delete_object_config->set_permanent_object_deletion_enabled(false);
     auto result = client.CreateJob(parent, job, job_id).get();
     if (!result) throw result.status();
     std::cout << "Created job: " << result->name() << "\n";
   }
   //! [storage_batch_create_job]
-  (std::move(client), argv.at(0), argv.at(1));
+  (std::move(client), argv.at(0), argv.at(1), argv.at(2), argv.at(3));
 }
 
 void ListJobs(
@@ -122,15 +130,20 @@ void DeleteJob(
 void AutoRun(std::vector<std::string> const& argv) {
   if (!argv.empty()) throw google::cloud::testing_util::Usage{"auto"};
   google::cloud::testing_util::CheckEnvironmentVariablesAreSet(
-      {"GOOGLE_CLOUD_PROJECT"});
+      {"GOOGLE_CLOUD_PROJECT", "GOOGLE_CLOUD_CPP_TEST_BUCKET_NAME"});
   auto const project_id =
       google::cloud::internal::GetEnv("GOOGLE_CLOUD_PROJECT").value();
+  auto const target_bucket_name =
+      google::cloud::internal::GetEnv("GOOGLE_CLOUD_CPP_TEST_BUCKET_NAME").value();
 
   auto gen = google::cloud::internal::DefaultPRNG(std::random_device{}());
   auto const prefix = std::string{"storage-batch-operations-samples"};
   auto const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
   auto const job_id =
-      prefix + "-" + google::cloud::internal::Sample(gen, 32, alphanumeric);
+      prefix + "-" +
+      google::cloud::internal::Sample(gen, 32, alphanumeric);
+
+  std::string object_prefix = "sbo-test-objects/";
 
   auto client =
       google::cloud::storagebatchoperations_v1::StorageBatchOperationsClient(
@@ -138,7 +151,7 @@ void AutoRun(std::vector<std::string> const& argv) {
               MakeStorageBatchOperationsConnection());
 
   std::cout << "\nRunning CreateJob() example\n";
-  CreateJob(client, {project_id, job_id});
+  CreateJob(client, {project_id, job_id, target_bucket_name, object_prefix});
 
   std::cout << "\nRunning GetJob() example\n";
   GetJob(client, {project_id, job_id});
@@ -178,7 +191,7 @@ int main(int argc, char* argv[]) {
         std::move(name), std::move(adapter));
   };
   Example example({
-      make_entry("create-job", {"project-id", "job-id"}, CreateJob),
+      make_entry("create-job", {"project-id", "job-id", "target-bucket-name", "object-prefix"}, CreateJob),
       make_entry("get-job", {"project-id", "job-id"}, GetJob),
       make_entry("list-jobs", {"project-id"}, ListJobs),
       make_entry("cancel-job", {"project-id", "job-id"}, CancelJob),
