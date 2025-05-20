57d5c6eec0336bd01633d6256ee3a99a43d37742
bajajnehaa <bajajnehaa@google.com>
2025-05-20 07:00:53 +0000

    feat(ACv2): support to handle large ranges

--- google/cloud/storage/internal/async/object_descriptor_connection_tracing.cc
+++ google/cloud/storage/internal/async/object_descriptor_connection_tracing.cc
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 #include "google/cloud/storage/internal/async/object_descriptor_connection_tracing.h"
+#include "google/cloud/storage/async/object_descriptor.h"
+#include "google/cloud/storage/async/reader.h"
 #include "google/cloud/storage/async/reader_connection.h"
 #include "google/cloud/internal/opentelemetry.h"
 #include "google/cloud/version.h"
@@ -60,6 +62,10 @@ class AsyncObjectDescriptorConnectionTracing
                      {"read-length", p.length}});
     return result;
   }
+
+  void MakeSubsequentStream() override {
+    return impl_->MakeSubsequentStream();
+  };
 
  private:
   opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> span_;
