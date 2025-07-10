38eba9dd651dabda02be7004c82ec0b9ca9a496d
diegomarquezp <diegomarquezp@google.com>
2025-07-10 02:31:05 +0000

    chore: POC of publishing googleapis to BCR via `git tag`
    WIP

--- external/googleapis/renovate.sh
+++ external/googleapis/renovate.sh
@@ -24,8 +24,25 @@ function banner() {
   SEPARATOR=Y
 }
 
+# Uses a shallow clone of "${REPO}" and tags at the specified "${COMMIT}"
+# The format of the commit is 0.0.0-YYYYMMDD-[8-char-SHA] by default, but can be overriden.
+# You must specify the "TOKEN" environment variable with your GH token in order to create the tag
+function tag_googleapis() {
+  set -x
+  GOOGLEAPIS="$(mktemp -d)"
+  if [[ -z "${TOKEN}" ]]; then
+    echo "Env var TOKEN must set to a valid GH token"
+    exit 1
+  fi
+  git clone --depth 1 "https://${TOKEN}@github.com/${REPO}" ${GOOGLEAPIS}
+  pushd "${GOOGLEAPIS}"
+  git tag "${TAG}"
+  git push --tags && MESSAGE="Successfully tagged ${REPO} with ${TAG}"
+  set +x
+}
+
 banner "Determining googleapis HEAD commit and tarball checksum"
-REPO="googleapis/googleapis"
+REPO="${REPO:-googleapis/googleapis}"
 BRANCH="master"
 if [[ -z "${COMMIT}" ]]; then
   COMMIT=$(curl -fsSL -H "Accept: application/vnd.github.VERSION.sha" \
@@ -87,25 +104,49 @@ if git diff --quiet bazel/workspace0.bzl \
   exit 0
 fi
 
-banner "Regenerating libraries"
-# generate-libraries fails if it creates a diff, so ignore its status.
-TRIGGER_TYPE='pr' ci/cloudbuild/build.sh \
-  --docker --trigger=generate-libraries-pr || true
+#banner "Regenerating libraries"
+## generate-libraries fails if it creates a diff, so ignore its status.
+#TRIGGER_TYPE='pr' ci/cloudbuild/build.sh \
+  #--docker --trigger=generate-libraries-pr || true
+#
+#banner "Creating commits"
+#git commit -m"chore: update googleapis SHA circa ${COMMIT_DATE}" \
+  #${PIPERORIGIN_REVID:+-m "${PIPERORIGIN_REVID}"} \
+  #bazel/workspace0.bzl cmake/GoogleapisConfig.cmake MODULE.bazel
+#if ! git diff --quiet external/googleapis/protodeps \
+  #external/googleapis/protolists; then
+  #git commit -m"Update the protodeps/protolists" \
+    #external/googleapis/protodeps external/googleapis/protolists
+#fi
+#if ! git diff --quiet .; then
+  #git commit -m"Regenerate libraries" .
+#fi
+#
+#banner "Showing git state"
+#git status --untracked-files=no
+#echo ""
+#git show-branch
+#
+banner "Creating tag in ${REPO}"
+TAG="${TAG:-0.0.0-$(date +"%Y%m%d")-${COMMIT:0:8}}"
+#tag_googleapis
 
-banner "Creating commits"
-git commit -m"chore: update googleapis SHA circa ${COMMIT_DATE}" \
-  ${PIPERORIGIN_REVID:+-m "${PIPERORIGIN_REVID}"} \
-  bazel/workspace0.bzl cmake/GoogleapisConfig.cmake MODULE.bazel
-if ! git diff --quiet external/googleapis/protodeps \
-  external/googleapis/protolists; then
-  git commit -m"Update the protodeps/protolists" \
-    external/googleapis/protodeps external/googleapis/protolists
-fi
-if ! git diff --quiet .; then
-  git commit -m"Regenerate libraries" .
-fi
+banner "Confirming presence of generated googleapis module in bazel-central-registry"
+JSON=$(gh search prs --repo=bazelbuild/bazel-central-registry --state=open --match=title "Publish googleapis@${TAG}" --json=url,title --order=desc)
 
-banner "Showing git state"
-git status --untracked-files=no
-echo ""
-git show-branch
+function filter_json() {
+   set -x
+   jq -r --arg tag "${TAG}" '.[] | select(.title | test('\"'$tag'\"'))' <<< $"{json}"
+}
+
+while true; do
+  FILTERED_JSON=$(filter_json)
+  if -n "${FILTERED_JSON}"; then
+    URL=$(jq '.url' <<< "${FILTERED_JSON}")
+    echo
+    banner "Found PR with new googleapis module in bazel-centra-registry: ${URL}"
+    exit 0
+  fi
+  sleep 5
+  printf '.'
+done
