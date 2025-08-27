c8dd3b0f775e37ee135637f26822d8f0590bf0ba
Jin Seop Kim <jinseop@google.com>
2025-08-27 12:23:04 -0400

    fix: brew install for cmake URL

--- ci/kokoro/macos/builds/cmake-vcpkg.sh
+++ ci/kokoro/macos/builds/cmake-vcpkg.sh
@@ -39,8 +39,10 @@ io::log_h2 "DEBUG: Before brew install bash ninja"
 brew install bash ninja
 io::log_h2 "DEBUG: After brew install bash ninja"
 
-# Install a specific version of CMake to match our GHA builds
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/fd21fcf239bcd0231c9fed5719403ec128151af4/Formula/c/cmake.rb
+# Install v3.28.1 CMake
+io::log_h2 "DEBUG: Before brew install for cmake"
+brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/4c210b9063c89a89abd9a6db5bae1b5f4e8fdb7d/Formula/c/cmake.rb
+io::log_h2 "DEBUG: After brew install for cmake"
 
 io::log_h2 "Using CMake version"
 cmake --version
