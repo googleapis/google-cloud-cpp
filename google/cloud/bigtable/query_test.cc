d0bc36a0813522a31458d55f0ac193fee25fa1b0
diegomarquezp <diegomarquezp@google.com>
2025-10-22 21:25:27 +0000

    chore: decompose BasicInputs for readability

--- google/cloud/bigtable/query_test.cc
+++ google/cloud/bigtable/query_test.cc
@@ -2,8 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may
-// obtain a copy of the License at
+// You may obtain a copy of the License at
 //
 // https://www.apache.org/licenses/LICENSE-2.0
 //
@@ -25,30 +24,29 @@ GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
 namespace {
 using ::google::bigtable::v2::PrepareQueryResponse;
 
-class BasicInputs {
- public:
-  CompletionQueue cq;
-  Project p = Project("dummy-project");
-  InstanceResource instance = InstanceResource(p, "dummy-instance");
-  std::string statement_contents =
-      "SELECT * FROM my_table WHERE col1 = @val1 and col2 = @val2;";
-  SqlStatement sql_statement = SqlStatement(statement_contents);
-  PrepareQueryResponse response;
-  std::unordered_map<std::string, Value> parameters = {{"val1", Value(true)},
-                                                       {"val2", Value(2.0)}};
-};
-
 TEST(PreparedQuery, DefaultConstructor) {
-  auto inputs = BasicInputs();
-  PreparedQuery q(inputs.cq, inputs.instance, inputs.sql_statement,
-                  inputs.response);
-  EXPECT_EQ(inputs.instance.FullName(), q.instance().FullName());
-  EXPECT_EQ(inputs.statement_contents, q.sql_statement().sql());
+  CompletionQueue cq;
+  Project p("dummy-project");
+  InstanceResource instance(p, "dummy-instance");
+  std::string statement_contents(
+      "SELECT * FROM my_table WHERE col1 = @val1 and col2 = @val2;");
+  SqlStatement sql_statement(statement_contents);
+  PrepareQueryResponse response;
+  PreparedQuery q(cq, instance, sql_statement, response);
+  EXPECT_EQ(instance.FullName(), q.instance().FullName());
+  EXPECT_EQ(statement_contents, q.sql_statement().sql());
 }
 
 TEST(BoundQuery, FromPreparedQuery) {
-  auto inputs = BasicInputs();
-  auto response = PrepareQueryResponse();
+  CompletionQueue cq;
+  Project p("dummy-project");
+  InstanceResource instance(p, "dummy-instance");
+  std::string statement_contents(
+      "SELECT * FROM my_table WHERE col1 = @val1 and col2 = @val2;");
+  SqlStatement sql_statement(statement_contents);
+  PrepareQueryResponse response;
+  std::unordered_map<std::string, Value> parameters = {{"val1", Value(true)},
+                                                       {"val2", Value(2.0)}};
 
   // The following variables are only meant to confirm the metadata is correctly
   // passed down to the BoundQuery.
@@ -60,27 +58,35 @@ TEST(BoundQuery, FromPreparedQuery) {
   metadata->set_allocated_proto_schema(schema.release());
   response.set_allocated_metadata(metadata.release());
 
-  PreparedQuery pq(inputs.cq, inputs.instance, inputs.sql_statement, response);
-  auto bq = pq.BindParameters(inputs.parameters);
-  EXPECT_EQ(inputs.instance.FullName(), bq.instance().FullName());
-  EXPECT_EQ(inputs.statement_contents, bq.prepared_query());
-  EXPECT_EQ(inputs.parameters, bq.parameters());
+  PreparedQuery pq(cq, instance, sql_statement, response);
+  auto bq = pq.BindParameters(parameters);
+  EXPECT_EQ(instance.FullName(), bq.instance().FullName());
+  EXPECT_EQ(statement_contents, bq.prepared_query());
+  EXPECT_EQ(parameters, bq.parameters());
   EXPECT_TRUE(bq.metadata().has_proto_schema());
   EXPECT_EQ(1, bq.metadata().proto_schema().columns_size());
   EXPECT_EQ("col1", bq.metadata().proto_schema().columns()[0].name());
 }
 
 TEST(BoundQuery, ToRequestProto) {
-  auto inputs = BasicInputs();
-  PreparedQuery pq(inputs.cq, inputs.instance, inputs.sql_statement,
-                   inputs.response);
-  auto bq = pq.BindParameters(inputs.parameters);
+  CompletionQueue cq;
+  Project p("dummy-project");
+  InstanceResource instance(p, "dummy-instance");
+  std::string statement_contents(
+      "SELECT * FROM my_table WHERE col1 = @val1 and col2 = @val2;");
+  SqlStatement sql_statement(statement_contents);
+  PrepareQueryResponse response;
+  std::unordered_map<std::string, Value> parameters = {{"val1", Value(true)},
+                                                       {"val2", Value(2.0)}};
+
+  PreparedQuery pq(cq, instance, sql_statement, response);
+  auto bq = pq.BindParameters(parameters);
   google::bigtable::v2::ExecuteQueryRequest proto = bq.ToRequestProto();
-  EXPECT_EQ(inputs.instance.FullName(), proto.instance_name());
-  EXPECT_EQ(inputs.statement_contents, proto.prepared_query());
+  EXPECT_EQ(instance.FullName(), proto.instance_name());
+  EXPECT_EQ(statement_contents, proto.prepared_query());
 
   // Test param contents.
-  EXPECT_EQ(inputs.parameters.size(), proto.mutable_params()->size());
+  EXPECT_EQ(parameters.size(), proto.mutable_params()->size());
 
   // The first parameter is a boolean.
   EXPECT_TRUE(proto.params().contains("val1"));
