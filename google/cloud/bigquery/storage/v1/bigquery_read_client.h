b628565ba6cf0bcc567d9181684456b0356ef6ca
Scott Hart <sdhart@google.com>
2025-11-11 09:44:44 -0500

    Regenerate libraries

--- google/cloud/bigquery/storage/v1/bigquery_read_client.h
+++ google/cloud/bigquery/storage/v1/bigquery_read_client.h
@@ -197,9 +197,9 @@ class BigQueryReadClient {
   // clang-format off
   ///
   /// Reads rows from the stream in the format prescribed by the ReadSession.
-  /// Each response contains one or more table rows, up to a maximum of 100 MiB
+  /// Each response contains one or more table rows, up to a maximum of 128 MB
   /// per response; read requests which attempt to read individual rows larger
-  /// than 100 MiB will fail.
+  /// than 128 MB will fail.
   ///
   /// Each request also returns a set of stream statistics reflecting the current
   /// state of the stream.
@@ -231,9 +231,9 @@ class BigQueryReadClient {
   // clang-format off
   ///
   /// Reads rows from the stream in the format prescribed by the ReadSession.
-  /// Each response contains one or more table rows, up to a maximum of 100 MiB
+  /// Each response contains one or more table rows, up to a maximum of 128 MB
   /// per response; read requests which attempt to read individual rows larger
-  /// than 100 MiB will fail.
+  /// than 128 MB will fail.
   ///
   /// Each request also returns a set of stream statistics reflecting the current
   /// state of the stream.
