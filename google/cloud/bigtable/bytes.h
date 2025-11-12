8081829f3f87bb3562ef03db86c80b92ce68a787
Scott Hart <sdhart@google.com>
2025-11-12 12:11:53 -0500

    update tests

--- google/cloud/bigtable/bytes.h
+++ google/cloud/bigtable/bytes.h
@@ -44,7 +44,7 @@ class Bytes {
   /// An empty sequence.
   Bytes() = default;
 
-  /// Strips the null-terminator character from input bytes.
+  /// Stops copying at the null-terminator character from input bytes.
   explicit Bytes(char const* bytes) : bytes_(bytes) {}
 
   /// @name Construction from a sequence of octets.
