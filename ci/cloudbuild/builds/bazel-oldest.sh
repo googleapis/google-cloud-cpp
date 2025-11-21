039808762392734570cc71a4d7bd076f788aae69
Scott Hart <sdhart@google.com>
2025-11-21 17:04:17 -0500

    chore(v3): update bazel versions and bazel deps

--- ci/cloudbuild/builds/bazel-oldest.sh
+++ ci/cloudbuild/builds/bazel-oldest.sh
@@ -16,7 +16,7 @@
 
 set -euo pipefail
 
-export USE_BAZEL_VERSION=7.5.0
+export USE_BAZEL_VERSION=7.7.1
 
 source "$(dirname "$0")/../../lib/init.sh"
 source module ci/cloudbuild/builds/lib/bazel.sh
