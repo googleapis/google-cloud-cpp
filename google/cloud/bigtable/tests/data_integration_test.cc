3087d97e8ac6b081bb6c6403b64e8b8cd8be6eac
mpeddada1 <mpeddada@google.com>
2025-12-24 15:50:05 +0000

    feat: remove DataClient and it's usages

--- google/cloud/bigtable/tests/data_integration_test.cc
+++ google/cloud/bigtable/tests/data_integration_test.cc
@@ -17,12 +17,14 @@
 #include "google/cloud/bigtable/options.h"
 #include "google/cloud/bigtable/retry_policy.h"
 #include "google/cloud/bigtable/testing/table_integration_test.h"
+#include "google/cloud/grpc_options.h"
 #include "google/cloud/log.h"
 #include "google/cloud/testing_util/chrono_literals.h"
 #include "google/cloud/testing_util/scoped_environment.h"
 #include "google/cloud/testing_util/scoped_log.h"
 #include "google/cloud/testing_util/status_matchers.h"
 #include "absl/strings/str_format.h"
+#include "absl/strings/str_split.h"
 #include <thread>
 
 namespace google {
@@ -43,13 +45,7 @@ using ::testing::HasSubstr;
 using ::testing::UnorderedElementsAre;
 using ms = std::chrono::milliseconds;
 
-class DataIntegrationTest : public TableIntegrationTest,
-                            public ::testing::WithParamInterface<std::string> {
-};
-
-INSTANTIATE_TEST_SUITE_P(, DataIntegrationTest,
-                         ::testing::Values("with-data-connection",
-                                           "with-data-client"));
+class DataIntegrationTest : public TableIntegrationTest {};
 
 /// Use Table::Apply() to insert a single row.
 void Apply(Table& table, std::string const& row_key,
@@ -90,8 +86,8 @@ std::string const kFamily2 = "family2";
 std::string const kFamily3 = "family3";
 std::string const kFamily4 = "family4";
 
-TEST_P(DataIntegrationTest, TableApply) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableApply) {
+  auto table = GetTable();
 
   std::string const row_key = "row-key-1";
   std::vector<Cell> created{{row_key, kFamily4, "c0", 1000, "v1000"},
@@ -104,8 +100,8 @@ TEST_P(DataIntegrationTest, TableApply) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableBulkApply) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableBulkApply) {
+  auto table = GetTable();
 
   std::vector<Cell> created{{"row-key-1", kFamily4, "c0", 1000, "v1000"},
                             {"row-key-1", kFamily4, "c1", 2000, "v2000"},
@@ -129,9 +125,7 @@ TEST_P(DataIntegrationTest, TableBulkApply) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableBulkApplyThrottling) {
-  if (GetParam() == "with-data-client") GTEST_SKIP();
-
+TEST_F(DataIntegrationTest, TableBulkApplyThrottling) {
   // Make a custom table with throttling enabled.
   auto table =
       Table(MakeDataConnection(
@@ -158,9 +152,9 @@ TEST_P(DataIntegrationTest, TableBulkApplyThrottling) {
   CheckEqualUnordered({expected}, actual);
 }
 
-TEST_P(DataIntegrationTest, TableSingleRow) {
+TEST_F(DataIntegrationTest, TableSingleRow) {
   std::string const row_key = "row-key-1";
-  auto table = GetTable(GetParam());
+  auto table = GetTable();
 
   auto mutation =
       SingleRowMutation(row_key, SetCell(kFamily4, "c1", 1_ms, "V1000"),
@@ -175,8 +169,8 @@ TEST_P(DataIntegrationTest, TableSingleRow) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableReadRowTest) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowTest) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
 
@@ -192,8 +186,8 @@ TEST_P(DataIntegrationTest, TableReadRowTest) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableReadRowNotExistTest) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowNotExistTest) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
 
@@ -205,8 +199,8 @@ TEST_P(DataIntegrationTest, TableReadRowNotExistTest) {
   EXPECT_FALSE(row_cell->first);
 }
 
-TEST_P(DataIntegrationTest, TableReadRowsAllRows) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowsAllRows) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
   std::string const row_key3(1024, '3');    // a long key
@@ -237,8 +231,8 @@ TEST_P(DataIntegrationTest, TableReadRowsAllRows) {
   CheckEqualUnordered(created, MoveCellsFromReader(read4));
 }
 
-TEST_P(DataIntegrationTest, TableReadRowsPartialRows) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowsPartialRows) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
   std::string const row_key3 = "row-key-3";
@@ -279,11 +273,10 @@ TEST_P(DataIntegrationTest, TableReadRowsPartialRows) {
   }
 }
 
-TEST_P(DataIntegrationTest, TableReadRowsReverseScan) {
-  if (GetParam() == "with-data-client") GTEST_SKIP();
+TEST_F(DataIntegrationTest, TableReadRowsReverseScan) {
   // The emulator does not yet support reverse scans.
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
-  auto table = GetTable(GetParam());
+  auto table = GetTable();
 
   std::vector<Cell> created{{"row-key-1", kFamily4, "c1", 1000, "a"},
                             {"row-key-1", kFamily4, "c2", 2000, "b"},
@@ -303,8 +296,8 @@ TEST_P(DataIntegrationTest, TableReadRowsReverseScan) {
               ElementsAre("row-key-3", "row-key-2", "row-key-1", "row-key-1"));
 }
 
-TEST_P(DataIntegrationTest, TableReadRowsNoRows) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowsNoRows) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
   std::string const row_key3 = "row-key-3";
@@ -329,8 +322,8 @@ TEST_P(DataIntegrationTest, TableReadRowsNoRows) {
   CheckEqualUnordered(expected, MoveCellsFromReader(read3));
 }
 
-TEST_P(DataIntegrationTest, TableReadRowsWrongTable) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadRowsWrongTable) {
+  auto table = GetTable();
   auto other_table = table.WithNewTarget(table.project_id(),
                                          table.instance_id(), RandomTableId());
 
@@ -343,8 +336,8 @@ TEST_P(DataIntegrationTest, TableReadRowsWrongTable) {
   EXPECT_EQ(read1.end(), it);
 }
 
-TEST_P(DataIntegrationTest, TableCheckAndMutateRowPass) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableCheckAndMutateRowPass) {
+  auto table = GetTable();
   std::string const key = "row-key";
 
   std::vector<Cell> created{{key, kFamily4, "c1", 0, "v1000"}};
@@ -361,8 +354,8 @@ TEST_P(DataIntegrationTest, TableCheckAndMutateRowPass) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableCheckAndMutateRowFail) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableCheckAndMutateRowFail) {
+  auto table = GetTable();
   std::string const key = "row-key";
 
   std::vector<Cell> created{{key, kFamily4, "c1", 0, "v1000"}};
@@ -379,8 +372,8 @@ TEST_P(DataIntegrationTest, TableCheckAndMutateRowFail) {
   CheckEqualUnordered(expected, actual);
 }
 
-TEST_P(DataIntegrationTest, TableReadModifyWriteAppendValueTest) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadModifyWriteAppendValueTest) {
+  auto table = GetTable();
   std::string const row_key1 = "row-key-1";
   std::string const row_key2 = "row-key-2";
   std::string const add_suffix1 = "-suffix";
@@ -415,8 +408,8 @@ TEST_P(DataIntegrationTest, TableReadModifyWriteAppendValueTest) {
                       actual_cells_ignore_timestamp);
 }
 
-TEST_P(DataIntegrationTest, TableReadModifyWriteRowIncrementAmountTest) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadModifyWriteRowIncrementAmountTest) {
+  auto table = GetTable();
   std::string const key = "row-key";
 
   // An initial; big-endian int64 number with value 0.
@@ -442,8 +435,8 @@ TEST_P(DataIntegrationTest, TableReadModifyWriteRowIncrementAmountTest) {
   CheckEqualUnordered(expected_ignore_timestamp, actual_ignore_timestamp);
 }
 
-TEST_P(DataIntegrationTest, TableReadModifyWriteRowMultipleTest) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadModifyWriteRowMultipleTest) {
+  auto table = GetTable();
   std::string const key = "row-key";
 
   std::string v1("\x00\x00\x00\x00\x00\x00\x00\x00", 8);
@@ -486,8 +479,8 @@ TEST_P(DataIntegrationTest, TableReadModifyWriteRowMultipleTest) {
   CheckEqualUnordered(expected_ignore_timestamp, actual_ignore_timestamp);
 }
 
-TEST_P(DataIntegrationTest, TableCellValueInt64Test) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableCellValueInt64Test) {
+  auto table = GetTable();
   std::string const key = "row-key";
 
   std::vector<Cell> created{{key, kFamily1, "c1", 0, 42},
@@ -521,8 +514,8 @@ TEST_P(DataIntegrationTest, TableCellValueInt64Test) {
   CheckEqualUnordered(expected_ignore_timestamp, actual_ignore_timestamp);
 }
 
-TEST_P(DataIntegrationTest, TableReadMultipleCellsBigValue) {
-  auto table = GetTable(GetParam());
+TEST_F(DataIntegrationTest, TableReadMultipleCellsBigValue) {
+  auto table = GetTable();
 
   std::string const row_key = "row-key-1";
   // cell vector contains 4 cells of 32 MiB each, or 128 MiB (without
@@ -573,7 +566,7 @@ TEST_P(DataIntegrationTest, TableReadMultipleCellsBigValue) {
   CheckEqualUnordered(expected_ignore_timestamp, actual_ignore_timestamp);
 }
 
-TEST_P(DataIntegrationTest, TableApplyWithLogging) {
+TEST_F(DataIntegrationTest, TableApplyWithLogging) {
   // In our ci builds, we set GOOGLE_CLOUD_CPP_ENABLE_TRACING to log our tests,
   // by default. We should unset this variable and create a fresh client in
   // order to have a conclusive test.
@@ -585,13 +578,9 @@ TEST_P(DataIntegrationTest, TableApplyWithLogging) {
   // Make a `Table` with an implementation that depends on the test's value
   // parameter.
   auto make_table = [&](Options const& options) {
-    if (GetParam() == "with-data-connection") {
-      auto conn = MakeDataConnection(options);
-      return Table(std::move(conn),
-                   TableResource(project_id(), instance_id(), table_id));
-    }
-    auto data_client = MakeDataClient(project_id(), instance_id(), options);
-    return Table(std::move(data_client), table_id);
+    auto conn = MakeDataConnection(options);
+    return Table(std::move(conn),
+                 TableResource(project_id(), instance_id(), table_id));
   };
 
   std::string const row_key = "row-key-1";
@@ -610,7 +599,7 @@ TEST_P(DataIntegrationTest, TableApplyWithLogging) {
   EXPECT_THAT(log.ExtractLines(), Not(Contains(HasSubstr("MutateRow"))));
 }
 
-TEST_P(DataIntegrationTest, ClientQueryColumnFamily) {
+TEST_F(DataIntegrationTest, ClientQueryColumnFamily) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
@@ -676,7 +665,7 @@ TEST_P(DataIntegrationTest, ClientQueryColumnFamily) {
                                      {Bytes(column2), Bytes(value2)}}));
 }
 
-TEST_P(DataIntegrationTest, ClientQueryColumnFamilyWithHistory) {
+TEST_F(DataIntegrationTest, ClientQueryColumnFamilyWithHistory) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
@@ -820,76 +809,7 @@ TEST_P(DataIntegrationTest, ClientQueryColumnFamilyWithHistory) {
             column_2_value_old);
 }
 
-// TODO(#8800) - remove after deprecation is complete
-#include "google/cloud/internal/disable_deprecation_warnings.inc"
-
-TEST(ConnectionRefresh, Disabled) {
-  auto data_client = bigtable::MakeDataClient(
-      testing::TableTestEnvironment::project_id(),
-      testing::TableTestEnvironment::instance_id(),
-      Options{}.set<MaxConnectionRefreshOption>(std::chrono::seconds(0)));
-  // In general, it is hard to show that something has *not* happened, at best
-  // we can show that its side-effects have not happened. In this case we want
-  // to show that the channels have not been refreshed. A side-effect of
-  // refreshing a channel is that it enters the `READY` state, so we check that
-  // this has not taken place and take that as evidence that no refresh has
-  // taken place.
-  //
-  // After the `CompletionQueue` argument is removed from the `Bigtable` API, we
-  // will have an option to provide a mock `CompletionQueue` to the `DataClient`
-  // for test purposes and verify that no timers are created, which will be a
-  // superior way to write this test.
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-  for (int i = 0; i < internal::DefaultConnectionPoolSize(); ++i) {
-    auto channel = data_client->Channel();
-    EXPECT_EQ(GRPC_CHANNEL_IDLE, channel->GetState(false));
-  }
-  // Make sure things still work.
-  bigtable::Table table(data_client, testing::TableTestEnvironment::table_id());
-  std::string const row_key = "row-key-1";
-  std::vector<Cell> created{{row_key, kFamily4, "c0", 1000, "v1000"},
-                            {row_key, kFamily4, "c1", 2000, "v2000"}};
-  Apply(table, row_key, created);
-  // After performing some operations, some of the channels should be in ready
-  // state.
-  auto check_if_some_channel_is_ready = [&] {
-    for (int i = 0; i < internal::DefaultConnectionPoolSize(); ++i) {
-      if (data_client->Channel()->GetState(false) == GRPC_CHANNEL_READY) {
-        return true;
-      }
-    }
-    return false;
-  };
-  EXPECT_TRUE(check_if_some_channel_is_ready());
-}
-
-TEST(ConnectionRefresh, Frequent) {
-  auto data_client = bigtable::MakeDataClient(
-      testing::TableTestEnvironment::project_id(),
-      testing::TableTestEnvironment::instance_id(),
-      Options{}
-          .set<MaxConnectionRefreshOption>(std::chrono::milliseconds(100))
-          .set<MinConnectionRefreshOption>(std::chrono::milliseconds(100)));
-
-  for (;;) {
-    if (data_client->Channel()->GetState(false) == GRPC_CHANNEL_READY) {
-      // We've found a channel which changed its state from IDLE to READY,
-      // which means that our refreshing mechanism works.
-      break;
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-
-  // Make sure things still work.
-  bigtable::Table table(data_client, testing::TableTestEnvironment::table_id());
-  std::string const row_key = "row-key-1";
-  std::vector<Cell> created{{row_key, kFamily4, "c0", 1000, "v1000"},
-                            {row_key, kFamily4, "c1", 2000, "v2000"}};
-  Apply(table, row_key, created);
-}
-
-TEST_P(DataIntegrationTest, SingleColumnQuery) {
+TEST_F(DataIntegrationTest, SingleColumnQuery) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
@@ -952,7 +872,7 @@ TEST_P(DataIntegrationTest, SingleColumnQuery) {
   EXPECT_EQ(*value, value1);
 }
 
-TEST_P(DataIntegrationTest, SingleColumnQueryWithHistory) {
+TEST_F(DataIntegrationTest, SingleColumnQueryWithHistory) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
@@ -1056,7 +976,7 @@ TEST_P(DataIntegrationTest, SingleColumnQueryWithHistory) {
   EXPECT_EQ(std::get<1>(entry1), value_old);
 }
 
-TEST_P(DataIntegrationTest, MultiColumnQuery) {
+TEST_F(DataIntegrationTest, MultiColumnQuery) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
@@ -1123,7 +1043,7 @@ TEST_P(DataIntegrationTest, MultiColumnQuery) {
                         RowType("multi-column-query-row-2", "v21", "v22")));
 }
 
-TEST_P(DataIntegrationTest, QueryWithNulls) {
+TEST_F(DataIntegrationTest, QueryWithNulls) {
   if (UsingCloudBigtableEmulator()) GTEST_SKIP();
   auto const table_id = testing::TableTestEnvironment::table_id();
   auto retry_policy_option = DataLimitedErrorCountRetryPolicy(0).clone();
