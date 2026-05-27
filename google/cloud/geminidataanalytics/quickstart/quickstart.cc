8eb34d519ea3bd7ccf42dd8c03ed291c89771136
Scott Hart <sdhart@google.com>
2026-05-27 10:23:27 -0400

    Manually update READMEs, quickstart, and top-level stuff

--- google/cloud/geminidataanalytics/quickstart/quickstart.cc
+++ google/cloud/geminidataanalytics/quickstart/quickstart.cc
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 //! [all]
-#include "google/cloud/geminidataanalytics/v1/ EDIT HERE _client.h"
+#include "google/cloud/geminidataanalytics/v1/data_chat_client.h"
 #include "google/cloud/location.h"
 #include <iostream>
 
@@ -26,10 +26,10 @@ int main(int argc, char* argv[]) try {
   auto const location = google::cloud::Location(argv[1], argv[2]);
 
   namespace geminidataanalytics = ::google::cloud::geminidataanalytics_v1;
-  auto client = geminidataanalytics::ServiceClient(
-      geminidataanalytics::MakeServiceConnection());  // EDIT HERE
+  auto client = geminidataanalytics::DataChatServiceClient(
+      geminidataanalytics::MakeDataChatServiceConnection());
 
-  for (auto r : client.List /*EDIT HERE*/ (location.FullName())) {
+  for (auto r : client.ListConversations(location.FullName())) {
     if (!r) throw std::move(r).status();
     std::cout << r->DebugString() << "\n";
   }
