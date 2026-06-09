c10e1cc5fcdadbde5f4e7e514d661f8f798383ae
bajajnehaa <bajajnehaa@google.com>
2026-06-09 10:56:34 +0000

    move BucketMetadataCache to separate file

---
+++ google/cloud/storage/internal/bucket_metadata_cache.cc
@@ -0,0 +1,91 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/storage/internal/bucket_metadata_cache.h"
+#include <mutex>
+#include <utility>
+
+namespace google {
+namespace cloud {
+namespace storage_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+
+absl::optional<BucketCacheEntry> BucketMetadataCache::Get(
+    std::string const& bucket_name) {
+  std::lock_guard<std::mutex> lock(mu_);
+  auto it = map_.find(bucket_name);
+  if (it == map_.end()) return absl::nullopt;
+
+  list_.erase(it->second.second);
+  list_.push_front(bucket_name);
+  it->second.second = list_.begin();
+  return it->second.first;
+}
+
+void BucketMetadataCache::Put(std::string const& bucket_name,
+                              BucketCacheEntry entry) {
+  std::lock_guard<std::mutex> lock(mu_);
+  auto it = map_.find(bucket_name);
+  if (it != map_.end()) {
+    it->second.first = std::move(entry);
+    list_.erase(it->second.second);
+    list_.push_front(bucket_name);
+    it->second.second = list_.begin();
+    return;
+  }
+
+  if (map_.size() >= max_size_) {
+    auto oldest = list_.back();
+    list_.pop_back();
+    map_.erase(oldest);
+  }
+
+  list_.push_front(bucket_name);
+  map_[bucket_name] = {std::move(entry), list_.begin()};
+}
+
+void BucketMetadataCache::Invalidate(std::string const& bucket_name) {
+  std::lock_guard<std::mutex> lock(mu_);
+  auto it = map_.find(bucket_name);
+  if (it != map_.end()) {
+    list_.erase(it->second.second);
+    map_.erase(it);
+  }
+}
+
+void BucketMetadataCache::Clear() {
+  std::lock_guard<std::mutex> lock(mu_);
+  map_.clear();
+  list_.clear();
+  in_flight_fetch_.clear();
+}
+
+bool BucketMetadataCache::StartFetch(std::string const& bucket_name) {
+  std::lock_guard<std::mutex> lock(mu_);
+  if (in_flight_fetch_.find(bucket_name) != in_flight_fetch_.end()) {
+    return false;
+  }
+  in_flight_fetch_.insert(bucket_name);
+  return true;
+}
+
+void BucketMetadataCache::EndFetch(std::string const& bucket_name) {
+  std::lock_guard<std::mutex> lock(mu_);
+  in_flight_fetch_.erase(bucket_name);
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace storage_internal
+}  // namespace cloud
+}  // namespace google
