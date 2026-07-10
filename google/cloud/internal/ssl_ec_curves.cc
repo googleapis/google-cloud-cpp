895d75a585ed9c419cc1fe6d05b5a768aa6b6f0a
Scott Hart <sdhart@google.com>
2026-07-10 13:53:11 -0400

    impl(rest): use PQC algorithm when available

---
+++ google/cloud/internal/ssl_ec_curves.cc
@@ -0,0 +1,78 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/internal/ssl_ec_curves.h"
+#include "google/cloud/internal/getenv.h"
+#include "google/cloud/internal/make_status.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_join.h"
+#include <curl/curl.h>
+#include <algorithm>
+#include <utility>
+
+namespace google {
+namespace cloud {
+namespace rest_internal {
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_BEGIN
+namespace {
+
+// libcurl encodes version Ma.Mi.Pa as (Ma << 16) | (Mi << 8) | Pa.
+// CURLOPT_SSL_EC_CURVES was introduced in libcurl version 7.73.0.
+constexpr unsigned int kMinimumLibcurlVersionForEcCurves =
+    (7U << 16) | (73U << 8) | 0U;
+
+bool SupportsSslEcCurves() {
+  auto* vinfo = curl_version_info(CURLVERSION_NOW);
+  return vinfo != nullptr &&
+         vinfo->version_num >= kMinimumLibcurlVersionForEcCurves;
+}
+
+}  // namespace
+
+StatusOr<std::string> PrependPqcEcCurve(std::vector<std::string> groups,
+                                        bool supports_ssl_ec_curves) {
+  if (internal::GetEnv("GOOGLE_CLOUD_CPP_DISABLE_PQC")) {
+    return absl::StrJoin(groups, ":");
+  }
+  if (!supports_ssl_ec_curves) {
+    return internal::UnavailableError(
+        "libcurl version 7.73.0 or later is required to set SSL EC curves.");
+  }
+
+  std::string const target = "X25519MLKEM768";
+  auto it = std::find_if(groups.begin(), groups.end(),
+                         [&target](std::string const& s) {
+                           return absl::EqualsIgnoreCase(s, target);
+                         });
+
+  if (it == groups.end()) {
+    return internal::UnavailableError(
+        "X25519MLKEM768 is not supported by the underlying crypto library.");
+  }
+
+  std::string exact_name = *it;
+  groups.erase(it);
+  groups.insert(groups.begin(), std::move(exact_name));
+
+  return absl::StrJoin(groups, ":");
+}
+
+StatusOr<std::string> GetPqcEcCurves() {
+  return PrependPqcEcCurve(AvailableCryptoGroups(), SupportsSslEcCurves());
+}
+
+GOOGLE_CLOUD_CPP_INLINE_NAMESPACE_END
+}  // namespace rest_internal
+}  // namespace cloud
+}  // namespace google
