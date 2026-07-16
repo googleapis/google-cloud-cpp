848cb58f9b9bf34ced09e02abf77a4e191c091c3
bajajnehaa <bajajnehaa@google.com>
2026-07-16 03:49:58 +0000

    fix ci failures, rebase with main branch

--- google/cloud/storage/internal/storage_auth_decorator.cc
+++ google/cloud/storage/internal/storage_auth_decorator.cc
@@ -265,25 +265,6 @@ StatusOr<google::storage::v2::Object> StorageAuth::MoveObject(
   return child_->MoveObject(context, options, request);
 }
 
-future<StatusOr<google::storage::v2::Object>> StorageAuth::AsyncComposeObject(
-    google::cloud::CompletionQueue& cq,
-    std::shared_ptr<grpc::ClientContext> context,
-    google::cloud::internal::ImmutableOptions options,
-    google::storage::v2::ComposeObjectRequest const& request) {
-  return auth_->AsyncConfigureContext(std::move(context))
-      .then([cq, child = child_, options = std::move(options),
-             request](future<StatusOr<std::shared_ptr<grpc::ClientContext>>>
-                          f) mutable {
-        auto context = f.get();
-        if (!context) {
-          return make_ready_future(StatusOr<google::storage::v2::Object>(
-              std::move(context).status()));
-        }
-        return child->AsyncComposeObject(cq, *std::move(context),
-                                         std::move(options), request);
-      });
-}
-
 future<StatusOr<google::storage::v2::Bucket>> StorageAuth::AsyncGetBucket(
     google::cloud::CompletionQueue& cq,
     std::shared_ptr<grpc::ClientContext> context,
@@ -300,6 +281,25 @@ future<StatusOr<google::storage::v2::Bucket>> StorageAuth::AsyncGetBucket(
         }
         return child->AsyncGetBucket(cq, *std::move(context),
                                      std::move(options), request);
+      });
+}
+
+future<StatusOr<google::storage::v2::Object>> StorageAuth::AsyncComposeObject(
+    google::cloud::CompletionQueue& cq,
+    std::shared_ptr<grpc::ClientContext> context,
+    google::cloud::internal::ImmutableOptions options,
+    google::storage::v2::ComposeObjectRequest const& request) {
+  return auth_->AsyncConfigureContext(std::move(context))
+      .then([cq, child = child_, options = std::move(options),
+             request](future<StatusOr<std::shared_ptr<grpc::ClientContext>>>
+                          f) mutable {
+        auto context = f.get();
+        if (!context) {
+          return make_ready_future(StatusOr<google::storage::v2::Object>(
+              std::move(context).status()));
+        }
+        return child->AsyncComposeObject(cq, *std::move(context),
+                                         std::move(options), request);
       });
 }
 
