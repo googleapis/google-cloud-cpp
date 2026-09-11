0708fabb9535c3742991b8a52ad28c25f19839e1
Darren Bolduc <dbolduc@google.com>
2026-09-11 20:31:06 +0000

    d'oh; checkers

--- google/cloud/storage/testing/detect_sanitizers.h
+++ google/cloud/storage/testing/detect_sanitizers.h
@@ -24,13 +24,13 @@
 // See: https://github.com/googleapis/google-cloud-cpp/issues/16430
 #if defined(__SANITIZE_ADDRESS__) || defined(__SANITIZE_THREAD__) || \
     defined(__SANITIZE_MEMORY__) || defined(__SANITIZE_UNDEFINED__)
-#  define GCS_TEST_HAVE_SANITIZER 1
+#define GCS_TEST_HAVE_SANITIZER 1
 #elif defined(__has_feature)
-#  if __has_feature(address_sanitizer) || __has_feature(thread_sanitizer) || \
-      __has_feature(memory_sanitizer) || \
-      __has_feature(undefined_behavior_sanitizer)
-#    define GCS_TEST_HAVE_SANITIZER 1
-#  endif
+#if __has_feature(address_sanitizer) || __has_feature(thread_sanitizer) || \
+    __has_feature(memory_sanitizer) ||                                     \
+    __has_feature(undefined_behavior_sanitizer)
+#define GCS_TEST_HAVE_SANITIZER 1
+#endif
 #endif
 
 #endif  // GOOGLE_CLOUD_CPP_GOOGLE_CLOUD_STORAGE_TESTING_DETECT_SANITIZERS_H
